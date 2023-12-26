185cfa91cda4cc1a2ed1e2d27046d14f43ff9750
DavidRiiV <davidrivasmartin11@gmail.com>
2023-12-26 21:13:31 +0100

    Reto #00 python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mouredev.py
@@ -0,0 +1,27 @@
+# https://www.python.org/
+
+"""
+https://www.python.org/
+"""
+# Variable en Python
+miVariable = 10
+
+# Constante en Python
+MI_VARIABLE_CONSTANTE = "CONSTANT"
+
+# Tipos de constante
+miInt = 5
+miFloat = 23.23
+miComplex = 2j
+miStr = "Python"
+miBoolean = True
+miLista = [1,2,3,4]
+miTupla = (1,2,3,4,5)
+miRango = range(0,10)
+miDict = {
+    "campo01": "valor01",
+    "campo02": "valor02"
+}
+miSet = {"valor01", "valor02"}
+
+print("Hola Python!")
