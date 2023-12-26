f803d7b4fd1a53a641db5f23ccead98c8af3fbc7
Ciroo1210 <141521453+Ciroo1210@users.noreply.github.com>
2023-12-26 17:49:49 -0300

    Add files via upload

---
+++ Ciroo1210.py
@@ -0,0 +1,22 @@
+#la pagina oficial de python es: python.org
+# para hacer un comentario de una linea usa # y para varias lineas usa """ o ''' 
+# ejemplo:
+'''
+Este es un comentario
+multilínea con triple comillas.
+Puede ser útil para comentarios extensos.
+'''
+
+"""
+También se pueden utilizar comillas dobles para comentarios multilínea.
+"""
+
+
+#declaracion de variables
+string = 'hola' #los strings almacenan texto y siempre de ve estar entre "" o ''
+numbersInt = 12 #los numeros enteros almacen numeros como 15,100,85,74 
+numbersFloat = 3.14 #los numeros flotantes almacenan numeros decimales ejemplo 2.71828 , 3.14
+boolean = True #los booleanos solo almacenan dos valores True(verdadero) o False(falso)
+
+#muestra en pantalla el texto Hola Python
+print("Hola Python")
