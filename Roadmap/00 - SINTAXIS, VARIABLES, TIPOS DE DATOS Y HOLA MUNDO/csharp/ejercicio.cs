4790adcf786dc4edaf404049504bae895dad1a25
Miguel Gargallo <mgargallo16@gmail.com>
2023-12-26 21:05:03 +0100

    #00 - CS

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/csharp/ejercicio.cs
@@ -0,0 +1,20 @@
+// URL of the official C# website: https://learn.microsoft.com/en-us/dotnet/csharp/
+
+// Single-line comment in C#
+
+/*
+  Multi-line comment in C#
+*/
+
+// Variable and constant declaration
+string languageName = "C#";
+const string GREETING = "¡Hola, ";
+
+// Primitive data types in C#
+int number = 42;
+double fractionalNumber = 3.14;
+bool isProgrammingFun = true;
+char singleCharacter = 'A';
+
+// Printing the message
+Console.WriteLine(GREETING + languageName + "!");
