430b41e95736e698408622d9011cd0a32393768e
avcenal <134477562+avcenal@users.noreply.github.com>
2023-12-28 00:00:50 +0100

    Create avcenal - 00_hello_python.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/avcenal - 00_hello_python.py
@@ -0,0 +1,24 @@
+#AUTOR: avcenal
+#LENGUAJE ELEGIDO: Python
+#URL: https://www.python.org
+
+"""
+Comentario
+en varias
+líneas
+"""
+#Comentario en una línea
+
+my_variable = "Hola Mundo"
+#no existen constantes en Python
+
+
+#Variables básicas
+my_integer = 39
+my_float = 1.82
+my_string = "Hola Alex"
+my_boolean = True
+my_complex_number = 1 + 4j
+
+my_language = "Python"
+print(f"Hola {my_language}!!!")
