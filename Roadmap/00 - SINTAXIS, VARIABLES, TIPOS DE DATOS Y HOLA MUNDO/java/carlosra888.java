91308296528654b0b70b79d96cf766327944410a
carlosra888 <3901@holbertonschool.com>
2023-12-26 19:31:27 -0500

    Reto #00 java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/carlosra888.java
@@ -0,0 +1,22 @@
+// Sitio Web oficial de java: https://www.java.com/en/
+// Para comentar una linea usamos ---> //
+// Para comentar un bloque de lineas usamos --> /* esto seria un bloque de lineas a comentar */
+
+public class carlosra888 {
+
+// Variable tipo entero
+    int num = 12345;
+// Constante tipo string
+    final String Constante = "Hola, ";
+
+// Tipo de datos primitivos
+    int n = 23456;
+    float j = 12345.678f;
+    boolean y = true;
+    char k = 'c';
+
+   public static void main(String[] args) {
+       System.out.println("¡Hola, java!");
+
+    }
+}
