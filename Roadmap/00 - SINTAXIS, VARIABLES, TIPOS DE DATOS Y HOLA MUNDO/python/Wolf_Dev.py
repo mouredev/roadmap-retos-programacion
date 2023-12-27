cbc1eb8d1aa6b34154e420089b4cd2a6a53fa495
wolfJORDAN <emersonandrejordanavila08@gmail.com>
2023-12-26 19:20:24 -0600

    Agregados primer ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Wolf_Dev.py
@@ -0,0 +1,38 @@
+# #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+#https://www.python.org/
+
+#Este es un comentario de solo una línea.
+
+"""
+Este
+es
+un
+comentario
+de
+varias
+líneas.
+"""
+
+#Tipos de datos:
+
+#numero entero y flotante
+numero_entero = 1
+print(numero_entero)
+
+numero_flotante = 1.0
+print(numero_flotante)
+
+#string
+cadena_caracteres = "Hola Maquina :)"
+print(cadena_caracteres)
+
+#valores booleanos, True o False, Verdadero o Falso.
+verdadero = True
+print(verdadero)
+
+falso = False
+print(falso)
+
+lenguaje = "Python"
+print(f"Hola, {lenguaje}")
