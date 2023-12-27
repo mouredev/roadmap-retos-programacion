eecdf7e3ee6a53431d396f2957f011f49cdc38ee
Enrique Cervantes <prueba@gmail.com>
2023-12-26 23:24:09 -0600

    "Reto #00 javascript python"

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/prog_enrique.py
@@ -0,0 +1,30 @@
+
+
+
+# https://www.python.org/
+""" comentario en diferentes 
+lineas de codigo """
+
+#declaracion de variables 
+nombre = "Juan"
+edad = 25
+pi = 3.14159
+
+#no existen constantes en pyton pero se pueden poner en mayusculas para indicar que no deben de modificarse 
+
+NOMBRE="Enrique"
+EDAD=35
+
+#tipos de datos primitivos 
+
+int=5 #numeros enteros 
+float =5.5 #numeros decimales 
+string ="hola mundo" #cadena de texto
+bool = False #boleano
+NoneType = None #ausencia de valor
+complex =3 + 4j #numeros complejos 
+
+
+#imprimir en consola 
+
+print("hola pyton")
