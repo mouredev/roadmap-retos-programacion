4d8a0764065669b316f9e0e2ecb2e33ffca899a2
Ivanpelu <ivanpelufo98@gmail.com>
2023-12-27 17:53:11 +0100

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Ivanpelu7.py
@@ -0,0 +1,21 @@
+# Esta es la página oficial de Python: https://www.python.org/
+
+# COMENTARIO DE UNA LINEA
+
+"""
+Este texto es
+un comentario
+en varias
+lineas
+"""
+
+MI_CONSTANTE = "Constante"
+mi_variable = "Variable"
+
+mi_string = "Python"
+mi_int = 1
+mi_booleano = False
+mi_float = 4.5
+sin_valor = None
+
+print(f"¡Hola, {mi_string}!")
