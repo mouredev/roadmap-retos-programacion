4fa200dc48ef2bb2828f13b6e334128c0acdd348
charly17 <charly.yepez@gmail.com>
2023-12-28 10:13:39 -0600

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/charly17.py
@@ -0,0 +1,15 @@
+# Esto es un comentario
+# https://www.python.org/
+
+""" Comentario
+multilinea
+Se agregan parrafos de este modo """
+
+var = 1
+string = "Hola"
+int = 5
+float = 2.5
+bol = False
+null = None
+
+print("Hola python, https://www.python.org/")
