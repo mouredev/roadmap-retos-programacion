c469345665c0976503c35425515870d08a69d5e2
John Dev <82047029+Geek669@users.noreply.github.com>
2023-12-28 14:31:21 -0500

    Create JohnCode.py
    Welcome Python!!  for me

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JohnCode.py
@@ -0,0 +1,37 @@
+# Sitio web oficial de Python: https://www.python.org/
+#Este lenguaje lo quiero aprender en 2024 con actitud y perserverancia
+
+#  Este es un omentario de una línea
+
+variable_una_linea = 10
+
+"""
+Comentario
+de varias
+líneas y demas este se usa triple comilla
+"""
+#ejemplo de variale
+
+nombre = "JohnP"
+variable_varias_lineas = 3.14
+
+# Var
+Mi_Variable = "Hola, soy una cadena de texto"
+
+# Const
+MI_CONSTANTE = 42
+
+# Tipos de datos primitivos
+#int 
+entero = 7
+#float 
+
+flotante = 3.14
+#bool
+
+booleano = True
+#cadena 
+cadena = "Texto"
+
+# Imprimir por terminal
+print(f"¡Hola, Python!")
