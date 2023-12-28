ce71d8ab90b63d86448e5ca6be30f1f1f1d76cd6
Diego Martinez Nuñez <diego.martinez@frenetic.ai>
2023-12-28 16:15:12 +0100

    Add basic data types in Python

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dimanu-py.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dimanu-py.py
@@ -36,3 +36,16 @@ gravity = constants_dataclass.GRAVITY
 print(f"Gravity value: {gravity}")
 
 # constants_dataclass.GRAVITY = 100 # This will raise an error if uncommented
+
+# Data types
+name = "Diego"
+age = 25
+height = 1.79
+is_developer = True
+languages = ["Python", "C++", "Java"]
+birth_date = (1_998, 12, 6)
+dog = {
+    "name": "Bruno",
+    "age": 2,
+    "breed": "French Poodle"
+}
