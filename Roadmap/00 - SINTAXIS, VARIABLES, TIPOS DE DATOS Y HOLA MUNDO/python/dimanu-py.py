63315e36fa05436539197cc8935b19939b1fb1ae
Diego Martinez Nuñez <diego.martinez@frenetic.ai>
2023-12-28 16:14:37 +0100

    Explain how to create constants in a more strict way.
    Even if Python doesn't support constants as we know them in C++ or Java, we have tools to define variables and don't be able to modify them, as if they were real constants.

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dimanu-py.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dimanu-py.py
@@ -6,3 +6,33 @@ programming_language = "Python" # This is a variable and a single line comment
 In Python we don't have constants, but we can use a variable with uppercase letters to indicate that it is a constant
 '''
 GRAVITY = 9.81
+
+"""
+One problem we can face defining constants this way is that we can change its value.
+
+One alternative way to create constants value is to create enumerations or dataclasses with the argument frozen=True
+"""
+
+# Using enumerations
+from enum import Enum
+
+class ConstantsEnum(Enum):
+    GRAVITY = 9.81
+
+gravity = ConstantsEnum.GRAVITY.value
+print(f"Gravity value: {gravity}")
+
+# ConstantsEnum.GRAVITY.value = 100 # This will raise an error if uncommented
+
+# Using dataclasses
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class ConstantsDataClass:
+    GRAVITY = 9.81
+    
+constants_dataclass = ConstantsDataClass()
+gravity = constants_dataclass.GRAVITY
+print(f"Gravity value: {gravity}")
+
+# constants_dataclass.GRAVITY = 100 # This will raise an error if uncommented
