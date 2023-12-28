7db1f2f6d8f3af186e31cbbc760bf61df31ce884
Cocoterooo <103317717+Cocotterooo@users.noreply.github.com>
2023-12-29 00:38:44 +0100

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/cocoterooo.py
@@ -0,0 +1,51 @@
+'''
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+'''
+
+# URL del sitio web oficial del lenguaje (Python): https://es.python.org/
+
+# Comentarioso en una línea
+
+'''
+    Comentario 
+    de 
+    varias 
+    líneas
+'''
+
+# Variable
+variable = 1
+CONSTANTE = 1
+
+# Tipos de datos primitivos:
+
+# - Cadenas de texto
+cadena:str = "Hola mundo"
+# - Enteros
+entero:int = 1
+# - Booleanos
+booleano:bool = True
+# - Decimales
+decimal:float = 1.1
+
+print('¡Hola Python! Se vienen cositas chulas en 2024')
