d082bd3f845eea9168bbb2757c8c4f1dd95b3d7f
JD <jsdvd.guerrero@gmail.com>
2023-12-28 20:36:34 +0100

    #00 - kotlin

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/jd-gm.kt
@@ -0,0 +1,31 @@
+// https://kotlinlang.org/
+
+// Comentario de una línea
+
+/* Comentario de
+ * varias líneas
+ */
+
+fun main() {
+
+    // Creación de una variable
+    var variable = "¡Hola, mundo!"
+
+    // Creación de una constante
+    val constante = "¡Hola, Kotlin!"
+
+    // Creación de variables con los 10 tipos de datos
+    var byte: Byte = 1
+    var short: Short = 2
+    var int: Int = 3
+    var long: Long = 4L
+    var float: Float = 5.0F
+    var double: Double = 6.0 
+    var character: Char = 'A'
+    var string: String = "Hola"
+    var boolean: Boolean = true
+    var array = arrayOf("Hola", "Kotlin")
+
+    // Imprimir saludo en consola
+    println(constante)
+}
