66ddabce16b575ca0d73410a4eebe0731e9dba84
LUCC4SZ <luccazamora@gmail.com>
2023-12-28 13:03:41 -0300

    #00 - TypeScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/typescript/LUCC4SZ.ts
@@ -0,0 +1,13 @@
+// https://www.typescriptlang.org
+
+/*
+https://www.typescriptlang.org
+*/
+
+let myString= "Mi cadena"
+let myNumber= 7
+let myBoolean= true
+
+const miSaludo= "Hola, TypeScript!"
+
+console.log(miSaludo)
