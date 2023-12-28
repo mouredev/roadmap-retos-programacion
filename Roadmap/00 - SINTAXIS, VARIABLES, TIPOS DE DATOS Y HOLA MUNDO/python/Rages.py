2fe2f5e3c61c571429609269f814028e869b3e21
Rages7 <73047949+Rages7@users.noreply.github.com>
2023-12-28 01:49:14 -0300

    Rages.py
    Mi primera vez en el curso de Dev

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rages.py
@@ -0,0 +1,27 @@
+# Trabajo con Python
+# URL del sitio web oficial de Python: https://www.python.org/
+
+# Comentarios
+"""
+Esto es un comentario en varias líneas
+"""
+'''
+Esto también es un comentario en varias líneas
+'''
+
+# Trabajo con variables
+variables = "mi fecha de nacimiento"
+print(variables)
+
+variables_int = 1993
+print(variables_int)
+
+variables_int = str(variables_int)  # Convertir el entero a cadena
+print("\n", variables_int)
+print(type(variables_int))  # Imprimir el tipo de dato de la variable
+
+bool_prueba = False
+print("\n", bool_prueba)
+
+# Imprimir un saludo
+print("¡Hola, Python!")
