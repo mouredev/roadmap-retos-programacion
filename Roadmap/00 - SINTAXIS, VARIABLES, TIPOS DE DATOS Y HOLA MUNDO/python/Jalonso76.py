13bf9be3d4c3501851d6d01d31a7c429daf80cb2
jalonso76 <jalonsodelacruz@gmail.com>
2023-12-28 12:18:23 +0100

    solución reto semanal 00 de Jalonso76

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jalonso76.py
@@ -0,0 +1,37 @@
+# https://www.python.org/
+
+# Este es un comentario de una línea
+
+"""
+Este es un comentario
+en vairas líneas 
+para probar que funciona
+"""
+
+'''
+esto tambien es
+un comentario en 
+varias líneas
+'''
+
+#Variables
+
+my_str_variable="cadena de texto"
+print (my_str_variable)
+
+my_int_variable=7
+print (my_int_variable)
+
+my_float_variable=0.1093
+print (my_float_variable)
+
+my_complex_number_variable=5+5j
+print (my_complex_number_variable)
+
+my_bool_variable=True
+print (my_bool_variable)
+
+my_list_variable=(2,5,'Hola', 0.34)
+print (my_list_variable)
+
+print ("¡Hola, Python!")
