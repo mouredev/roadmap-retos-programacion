0440631a63b59f564cf92ce3aa3f8588d6e43a31
Adrián Carretero Alcázar <acarreteroal@outlook.com>
2023-12-28 13:45:45 +0100

    ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/adcarret.py
@@ -0,0 +1,24 @@
+#esta es la URL del lenguaje elegido: https://es.python.org/ ESTO ES UN COMENTARIO EN LINEA
+"""
+Esto es un comentario en bloque
+Aqui podemos escribir todas las lineas que queramos.
+Se usan con tres comillas
+"""
+
+#declaramos una variable
+numero = 5
+#declaramos una variable constante. No hace falta ponerle final como en java, simplemente el nombre en mayusculas.
+PI = 3.14
+"""
+Crea variables representando todos los tipos de datos primitivos
+ del lenguaje (cadenas de texto, enteros, booleanos...).
+"""
+
+cadena = "Esto es una cadena de texto"
+numeroEntero = 5 #esto es una variable de numero entero
+numeroNegativo = -7
+numeroDecimal = 3.2 #esto es una variable de numero decimal
+lenguaje = "Python"
+boolean = True #esto es un booleano
+#ahora imprimimos el tipico hola con nuestro lenguaje
+print("¡Hola, " + lenguaje + "!")
