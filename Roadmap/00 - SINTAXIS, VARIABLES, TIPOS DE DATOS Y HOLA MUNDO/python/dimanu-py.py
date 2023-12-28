91bfb7492b9420efd1ab4faea35db73f1135bcf7
Diego Martinez Nuñez <diego.martinez@frenetic.ai>
2023-12-28 16:12:02 +0100

    Create a constant and a multiline comment
    In Python there aren't constants, we use a convention to write the variable name in upper case. A multiline comment can be created with either three ' or three " for both opening and closing the comment

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dimanu-py.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dimanu-py.py
@@ -1,3 +1,8 @@
 # Selected language: https://www.python.org/
 
 programming_language = "Python" # This is a variable and a single line comment
+
+'''
+In Python we don't have constants, but we can use a variable with uppercase letters to indicate that it is a constant
+'''
+GRAVITY = 9.81
