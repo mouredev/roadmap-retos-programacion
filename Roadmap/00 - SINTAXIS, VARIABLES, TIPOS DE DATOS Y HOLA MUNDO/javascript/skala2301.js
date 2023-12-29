e3380de10b1f8212dc6207c5ad46eab95202f045
Carlos Iglesias <krlos_2301@hotmail.com>
2023-12-29 12:17:40 -0600

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/skala2301.js
@@ -0,0 +1,11 @@
+/****************REFERENCIAS**************************************************************
+*Referencia de JavaScript: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference
+*Referencia de JavaScript Estandarizado (ECMAScript): 
+*   https://www.ecma-international.org/
+*   https://ecma-international.org/publications-and-standards/standards/ecma-262/
+*******************************************************************************************/
+
+const UNO = 1;
+
+
+
