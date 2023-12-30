5cf9bfb4a72ccbd88a080ea47bcf500382fa8da3
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-30 13:48:17 -0400

    Punto #2 finalizado

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Pancratzia.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Pancratzia.js
@@ -5,3 +5,15 @@ Sin embargo, este sitio solo se ocupa de la estandarización.
 Las especificaciones del lenguaje para desarrolladores se encuentra en el siguiente url: https://developer.mozilla.org/en/JavaScript
 */
 /*********************************************************/
+
+/************ 2 - TIPOS DE COMENTARIOS ************/
+
+// COMENTARIOS DE UNA SOLA LINEA
+/*
+COMENTARIOS
+DE
+VARIAS
+LINEAS
+*/
+
+/**************************************************/
