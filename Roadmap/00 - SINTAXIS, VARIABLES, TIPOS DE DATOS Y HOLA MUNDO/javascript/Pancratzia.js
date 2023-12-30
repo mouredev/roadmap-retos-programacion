79c098ff1797215d462548020fc4995a79b7dd74
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2023-12-30 13:44:43 -0400

    Primer reto del año. Pregunta #1 resuelta

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Pancratzia.js
@@ -0,0 +1,7 @@
+/************ 1 - SITIO OFICIAL DE JAVASCRIPT ************/
+/*
+Podría decirse que, como standard (ECMAScript) el sitio oficial del lenguaje es: http://www.ecmascript.org/
+Sin embargo, este sitio solo se ocupa de la estandarización.
+Las especificaciones del lenguaje para desarrolladores se encuentra en el siguiente url: https://developer.mozilla.org/en/JavaScript
+*/
+/*********************************************************/
