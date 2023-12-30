944cc44a69ed8e6dc59332d52814db16d94279af
Daniel Olano <daniel.030211@gmail.com>
2023-12-30 02:36:30 -0600

    #00 - Java

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Daeduol.java
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Daeduol.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Daeduol {
+public class Daeduol {
+    //    linea comentada
+    /*multiples lineas comentadas
+    daeduol
+    java
+    */
+//    https://dev.java/
+
+    /* Variables primitivas */
+    byte number8 = 1;
+    short number16 = -32768;
+    int number32 = 12;
+    long number64 = 2147483647;
+    float aFloat32 = 1.13f;
+    double height = 1.70;
+    boolean SkyIsBlue = true;
+    char char16 = 'a';
+    /*Variables de tipo string*/
+    String nombre = "Juan";
+    String apellido = "Pérez";
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola Java!");
+    }
 }
