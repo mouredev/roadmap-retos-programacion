a833ef2f5985dde039f8e7dcf97a7c9fd8efc0f6
Felipe Herrera Perea <felipeherrerapere03@gmail.com>
2023-12-31 18:31:27 -0500

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/felipeherrera.java
@@ -0,0 +1,82 @@
+/*  
+ *  EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ * 
+ */
+ 
+// SITIO WEB DEL LEGUAJE DE PROGRAMACIÓN.
+ 
+/*  
+    https://www.java.com/es/ 
+*/
+    
+// Empecemos con pues....
+
+public class FelipeHerrerPerea {
+
+    // Esta es la forma de como comentar y quedar todo más legible.
+
+// DISCURSO MOTIVACIONAL
+
+/*
+    Muchos jóvenes se sienten atraídos por el mundo del crimen, 
+    creyendo que es una forma fácil y rápida de conseguir dinero y fama. 
+    Sin embargo, lo que no saben es que el crimen tiene un precio muy alto,
+    tanto para ellos como para sus familias y la sociedad. 
+    El crimen no paga, el código sí.
+    El código es el lenguaje de la programación, una habilidad muy demandada y valorada en el mercado laboral actual. 
+    Aprender a programar puede abrir muchas puertas a los jóvenes, 
+    tanto en términos de empleo como de desarrollo personal. 
+    El código les permite crear soluciones innovadoras, 
+    expresar su creatividad y contribuir al bien común.
+    Por eso, es importante que los jóvenes se alejen del crimen y se acerquen al código. 
+    No solo mejorarán su calidad de vida, sino que también tendrán un impacto positivo en el mundo. 
+    El código es una herramienta poderosa para el cambio social, que puede ayudar a resolver problemas reales y mejorar la vida de las personas.
+    Así que ya sabes, si quieres cambiar tu vida para mejor, no te dejes seducir por el crimen. Elige el código, y verás cómo tu futuro se ilumina. 
+*/
+
+// Espero lo disfruten.
+    
+
+    public static void main(String[] args){
+    String saludoFormal = "Holaa, desde Medellin nadie se de....";
+    final String constanteCafe = "Java!!!";
+
+
+    boolean esFeliz = true;
+    boolean esMalo = false;
+
+    int = 1098765432;
+    byte = 123;
+    short = 20032;
+    long = 278376542903987635L;    
+
+    float = 23.00f;
+    double = 13.3;
+
+    char = 'H';
+    String = "Comuna 13";
+
+    System,out.println(saludoFormal+constanteCafe);
+
+    }
+
+}
+
+
+
+
+
+
+ 
+
