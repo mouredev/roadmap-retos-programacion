34b92860baacf3404db167cc62f5e6defa76f2af
Carlos F.O. <tecfer2020@gmail.com>
2024-01-01 22:30:54 +0100

    #00 - python

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tecfer.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tecfer.py
@@ -29,9 +29,20 @@
 '''
 ## Solución
 
-# Url del lenguaje  https://www.python.org/  -> Este es una línea de comentario
+# Url del lenguaje  https://www.python.org/  -> los comenterios simples empiezan por "#" Este es una línea de comentario.
 '''
-Lo que es encuentre en medio de las commilas es un bloque de comenterio 
+Lo que es encuentre en medio de las commilas es un bloque de comenterio, 
+puede ser de múltiples líneas. 
 '''
+variable =''
+CONSTANTE = 10 # el lenguaje no soporta constantes pero por convención se utilizan las mayúsculas.
+
+#el tipado de datos en python es dinámico
+boobleano = True # False    
+entero = 15
+float = 1.2
+complejo = 2 + 5j
+string = "Esto es una cadena"
+
 
 print("¡Hola, python!")
