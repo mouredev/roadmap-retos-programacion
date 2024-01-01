a2815ee2eba6b3a913bd50706d58b87728d2c0bc
Marcel Barrios <gabrielmbarrios@gmail.com>
2024-01-01 13:00:42 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/gmbarrios.py
@@ -0,0 +1,21 @@
+# 1. Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+# https://www.python.org/
+
+# 2. Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una linea, varias...)
+# De esta manera se escribe un comentario de una linea.
+"""
+Tengo entendido que en realidad esto NO es un comentario de multiples lineas, sino una cadena de texto (string) que ocupa varias lineas.
+"""
+
+# 3. Crea una variable (y una constante si el lenguaje lo soporta).
+example = 10
+EXAMPLE_CONST = 500
+
+# 4. Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+integer = 99
+float_number = 6.9
+language = "PYTHON"
+boolean = True
+
+# 5. Imprime por terminal el texto:
+print(f"¡Hola {language}!")
