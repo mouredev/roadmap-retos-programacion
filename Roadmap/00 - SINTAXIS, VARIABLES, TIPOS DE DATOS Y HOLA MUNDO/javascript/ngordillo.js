e806b6eb7cfa8f66bfec7617d520b99796f3d90b
Nelson Gordillo <nelson.071595@gmail.com>
2024-01-01 08:28:27 -0500

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/ngordillo.js
@@ -0,0 +1,18 @@
+//Este es un comentario https://lenguajejs.com/
+
+/* Este es un comentario
+de doble linea */
+
+let primeraVariable;
+
+const PRIMERACONSTANTE = 1;
+
+let variableString = "Hola";
+let variableNumber = 123;
+let variableBoolean = true;
+let variableNull = null;
+let variableUndefined;
+
+console.log(variableString, variableNumber, variableBoolean, variableNull, variableUndefined);
+
+console.log("Hola JavaScript");
