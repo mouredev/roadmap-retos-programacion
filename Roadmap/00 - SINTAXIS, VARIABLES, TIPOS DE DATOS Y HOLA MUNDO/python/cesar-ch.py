62fa3f54c53e03129e9649707ec7f7587cc0d919
Cesar-Ch <chafloque1530@gmail.com>
2024-01-01 03:28:43 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/cesar-ch.py
@@ -0,0 +1,24 @@
+# URL: https://www.python.org/
+
+# Comentarios
+# ------------
+# Este es un comentario de una sola línea 
+"""
+    Este es un comentario
+    de varias líneas
+"""
+
+# Variable y constante
+# ---------------------
+my_variable = "Python"
+MY_CONSTANT = 3.14 # En python no hay constantes, pero por convencion se usan mayúsculas
+
+# Tipos de datos primitivos
+# -------------------------
+my_int = 2024 # Este es un número entero
+my_float = 2024.1 # Este es un número real
+my_string = "Python" # Este es una cadena de texto
+my_boolean = True # Este es un valor lógico
+
+
+print(f"Hola {my_string}")
