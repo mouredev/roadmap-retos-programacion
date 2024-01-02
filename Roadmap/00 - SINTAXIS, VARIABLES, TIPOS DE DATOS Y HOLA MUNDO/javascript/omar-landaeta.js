fdd2e3a9cd5cb564e00394793debe71ef3ad5907
OmarjLand <omar.land2010@gmail.com>
2024-01-02 21:59:42 +0100

    #00 - JavasCript - OmarLandaeta

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/omar-landaeta.js
@@ -0,0 +1,30 @@
+// 1 - Este es un comentario con la URL de documentación Oficial de Javascript
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario con una sola linea usando Javascript
+
+/*
+    2 - Comentario usando varias lineas con Javascript
+    Comentario usando varias lineas con Javascript
+    Comentario usando varias lineas con Javascript
+*/
+
+// 3 - Crea una variable (y una constante si el lenguaje lo soporta).
+let saludo = "HOla Moure soy Omar Venezolano que vive en Coruña neno xD"
+const aprendizaje = true // constante
+
+ 
+/*
+4 - 
+Crea variables representando todos los tipos de datos primitivos
+del lenguaje (cadenas de texto, enteros, booleanos...).
+*/
+
+let myLanguage = "JavaScript";
+let number = 30;
+let learning = true;
+let age = undefined;
+let htmlanguage = null;
+
+// 5 Imprimiendo en consola mi lenguaje de programación
+console.log("Hola usaré: ", myLanguage);
