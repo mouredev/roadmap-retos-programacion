cbc209f9d1df0cd64a3eff3961fdbeb5b15ae415
DavidRm1911 <david_gq_06@hotmail.com>
2024-01-02 15:03:33 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rumizaske.py
@@ -0,0 +1,19 @@
+'''
+Comentario:
+https://www.python.org
+'''
+
+"""
+Hola, este es un comentario
+"""
+
+variable_random=12
+
+numero=1
+numerodecimal=12.1
+nombre= "david"
+booleano= True
+
+
+
+print("Hola, Python!")
