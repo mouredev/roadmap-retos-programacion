60fb9631975815063b72f00da22089ad413ee6a6
Sergio Toral García <103923255+toral24@users.noreply.github.com>
2024-01-02 19:41:36 +0100

    00 - python

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/toral24.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/toral24.py
@@ -1,6 +1,6 @@
 # https://www.python.org/
 
-# Comentario de una linea
+# Comentario de una linea.
 
 """"Comentario
 de
@@ -8,17 +8,17 @@ varias
 líneas
 """
 
-# En Python no existen constantes, por lo que se crea una variable
+# En Python no existen constantes, por lo que se crea una variable.
 
 Variable1 = "Primera variable del roadmap"
 
-# Tipos de datos primitivos
+# Tipos de datos primitivos.
 
 Cadena = "Python"
 Entero = int(3)
 Flotante = float(3.14)
 Booleano = True
 
-# Impresión por pantalla de hola Python
+# Impresión por pantalla de hola Python.
 
 print("¡Hola, "+Cadena+"!")
