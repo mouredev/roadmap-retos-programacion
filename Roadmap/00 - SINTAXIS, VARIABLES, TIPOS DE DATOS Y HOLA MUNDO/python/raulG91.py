1be6a84451ffe1b4a1fa68b4762235caa37e4b75
raulG91 <rgarciapedrosa@gmail.com>
2024-01-02 16:31:01 +0100

    #0 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/raulG91.py
@@ -0,0 +1,15 @@
+#https://docs.python.org/3/
+
+# This is a comment
+'''
+This is also a comment
+'''
+variable = "Hello"
+COSNTANT = 3.14
+integer = 1
+double = 5.5
+boolean = True
+string = "This is a string"
+
+print("¡Hola, Python!")
+
