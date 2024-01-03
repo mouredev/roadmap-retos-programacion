e0fdfb22818b9d174e6aadd7773985df486f9d6f
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-01-03 17:50:38 -0600

    00-angelcruz07

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/angelcruz07.py
@@ -0,0 +1,22 @@
+# https://python.org
+
+# Comment in line  with python
+
+"""
+Comment in multi-line with python 
+"""
+
+my_variable = "My Variable in python"
+
+MY_CONSTANT = "My constant in python"
+
+my_string = "This is a string"
+my_boolean = True
+my_integer = 12
+my_float = 1.56
+
+my_language = 'Python'
+
+print("This is my chosen language " + my_language)
+user_name = "JohnDoe"
+total_count = 100
