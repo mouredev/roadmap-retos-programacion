fb7a46e6d98fd689eee54b6c2f34b7e463743579
Ziellucio01 <ziellucio@outlook.com>
2024-01-02 22:41:40 -0600

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/ziellucio01.py
@@ -0,0 +1,34 @@
+# https://www.python.org/
+
+# Comentario en una linea 
+
+"""
+Esto tambien es 
+un comentario
+en varias lineas 
+"""
+
+''' 
+Esto tambien es 
+un comentario
+en varias lineas 
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" # por convención
+
+my_int: int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
