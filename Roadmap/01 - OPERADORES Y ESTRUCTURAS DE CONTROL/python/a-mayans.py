4282f02bbe9084a4fe4ee8d438232cdc4362bbe4
MayiDev <alejandromayans13@gmail.com>
2024-01-03 19:38:48 +0100

    #00 - Python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/a-mayans.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/a-mayans.py
@@ -9,32 +9,32 @@
 # Aritmeticos ( +, -, *, /, %, **, // )
 operando_1 = 10
 operando_2 = 7
-print(f'Suma: {operando_1 + operando_2}') # devuelve la suma
-print(f'Resta: {operando_1 - operando_2}') # devuelve la resta
-print(f'Multiplicacion: {operando_1 * operando_2}') # devulve la multiplicacion
-print(f'Division: {operando_1 / operando_2}') # devuelve el cociente (inclye decimales)
-print(f'Modulo: {operando_1 % operando_2}') # devuelve el resto/residuo
-print(f'Potencia: {operando_1 ** operando_2}') # devuelve el numero en potencia
-print(f'Division entero: {operando_1 // operando_2}') # devuelve el cociente entero
+print(f'Suma: {operando_1 + operando_2}')               # devuelve la suma
+print(f'Resta: {operando_1 - operando_2}')              # devuelve la resta
+print(f'Multiplicacion: {operando_1 * operando_2}')     # devulve la multiplicacion
+print(f'Division: {operando_1 / operando_2}')           # devuelve el cociente (inclye decimales)
+print(f'Modulo: {operando_1 % operando_2}')             # devuelve el resto/residuo
+print(f'Potencia: {operando_1 ** operando_2}')          # devuelve el numero en potencia
+print(f'Division entero: {operando_1 // operando_2}')   # devuelve el cociente entero
 
 
 # Logicos ( para trabajar con valores de tipo bool ) -> AND, OR, NOT
-print(True and True) # devuelve True
-print(True and False) # devuelve False
-print(True or False) # devuelve True
-print(not True) # devuelve False
-print(not False) # devuelve True
-print(not 0) # cero es igual a False, entonces devuelve True
-print(not 1) # uno es igual a True, entonces devuelve False
+print(True and True)        # devuelve True
+print(True and False)       # devuelve False
+print(True or False)        # devuelve True
+print(not True)             # devuelve False
+print(not False)            # devuelve True
+print(not 0)                # cero es igual a False, entonces devuelve True
+print(not 1)                # uno es igual a True, entonces devuelve False
 
 
 # Comparacion ( >, <, ==, >=, <=, !=, ) -> devuelve un valor booleano
-print(f'Mayor que: {operando_1 > operando_2}') # devuelve True
-print(f'Menor que: {operando_1 < operando_2}') # devuelve False
-print(f'Igual que: {operando_1 == operando_2}') # devuelve False
-print(f'Mayor o igual que: {operando_1 >= operando_2}') # devuelve True
-print(f'Menor o igual que: {operando_1 <= operando_2}') # devuelve False
-print(f'Distinto que: {operando_1 != operando_2}') # devuelve True
+print(f'Mayor que: {operando_1 > operando_2}')              # devuelve True
+print(f'Menor que: {operando_1 < operando_2}')              # devuelve False
+print(f'Igual que: {operando_1 == operando_2}')             # devuelve False
+print(f'Mayor o igual que: {operando_1 >= operando_2}')     # devuelve True
+print(f'Menor o igual que: {operando_1 <= operando_2}')     # devuelve False
+print(f'Distinto que: {operando_1 != operando_2}')          # devuelve True
 
 
 # Asignacion ( =, +=, -=, *=, /=, %=, **=, //= ) -> asignar valores a una variable
@@ -61,22 +61,22 @@ print(f'Division entera 10 a la variable b: {b}')
 a = 3
 b = 3
 c = 4
-print(a is b) # devuelve True
-print(a is not b) # devuelve False
-print( a is c) # devuelve False
-print( a is not c) # devuelve True
+print(a is b)               # devuelve True
+print(a is not b)           # devuelve False
+print( a is c)              # devuelve False
+print( a is not c)          # devuelve True
 
 
 # Pertenencia ( in, not it) -> para indicar pertenencia a alguna secuencia (listas, tuplas, strings)
 lista = [1, 2, 3, 4, 5]
-print(3 in lista) # devuelve True
-print(15 not in lista) # devuelve True
+print(3 in lista)           # devuelve True
+print(15 not in lista)      # devuelve True
 
 string = 'Hola, que tal'
-print('que' in string) # devuelve True
-print('de' in string) # devuelve False
-print('Ho' in string) # devuelve True
-print('ho' in string) # devuelve False, distingue entre mayusculas y minusculas, es case sensitive
+print('que' in string)      # devuelve True
+print('de' in string)       # devuelve False
+print('Ho' in string)       # devuelve True
+print('ho' in string)       # devuelve False, distingue entre mayusculas y minusculas, es case sensitive
 
 
 
@@ -108,5 +108,8 @@ for color in tupla:
     print(color)
 
 
-# Print de los ejemplos expuestos
-
+#---  Reto Extra  ---#
+# Iterando con un bucle For sobre el rango, y utilizando el condicional If con operadores de comparacion
+for num in range(10, 56):
+    if num % 2 == 0 and num != 16 and num % 3 != 0:
+        print(num)
