5c242e0108aac85c3945851116185e6dbdddd5b3
Morochief <84405756+Morochief@users.noreply.github.com>
2024-01-02 23:39:38 -0300

    Create RetoDeProgramacion.java
    Ejercicio 00 del reto de programación

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/RetoDeProgramacion.java
@@ -0,0 +1,34 @@
+package retodeprogramacion;
+
+public class RetoDeProgramacion {
+    public static void main(String[] args) {
+        //https://www.java.com/
+        //Comentario de una línea
+        /*Comentario de varias líneas*/
+        /**
+         * Esto es un comentario para javadoc ejemplo https://retosdeprogramacion.com/
+         */
+        String nombre; // Variable
+        final int edad = 28; // Constante
+        
+        //Tipo Primitivos en Java de Tipo Entero
+        byte variableByte;
+        short variableShort;
+        char variableChar;
+        int variableInt;
+        long variableLong;
+        
+        //Tipo Primitivo en Java de Tipo Boolean
+        boolean variableBoolean;
+        
+        //Tipo Primitivos en Java de Tipo Flotante
+        float variableFloat;
+        double variableDouble;
+        
+        //String no es un Tipo Primitivo sino de tipo Object
+        String variableString;
+
+        //Hola Mundo desde Java
+        System.out.println("Hola, Java [Morochief]");
+    }
+}
