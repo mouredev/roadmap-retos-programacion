a03d6499ed0f8dcdba71260800b228debc98d4bd
Murquisdev <murquis@gmail.com>
2024-01-04 22:45:58 +0100

    #01-javascript-cambiado titulos en comentarios

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/murquisdev.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/murquisdev.js
@@ -322,6 +322,7 @@ do{
     }
 }while(!encontrado) // condición
 
+// EJERCICIO PROPUESTO
 /*
     Crea un programa que imprima por consola todos los números comprendidos
     entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
