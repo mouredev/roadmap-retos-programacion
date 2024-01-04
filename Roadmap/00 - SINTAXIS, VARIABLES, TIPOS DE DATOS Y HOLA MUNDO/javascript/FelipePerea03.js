a8e6400a9d40a77f0d283818a71056faf7dcbfad
FelipePerea03 <felipeherrerapere03@gmail.com>
2024-01-04 12:26:39 -0500

    00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/FelipePerea03.js
@@ -0,0 +1,55 @@
+// CTRL + P (Buscar archivos por nombre)
+
+/* Los comentariso nos sirven para el ¿Por qué? y ¿Para qué? del código.
+    Sitio Web del lenguaje de programación
+    https://developer.mozilla.org/es/docs/Web/JavaScript
+*/
+
+/**
+ * Este es un comentario 
+ * de muchas 
+ * lineas más elegante.
+ * :#
+ */
+
+let name = "Felipe";
+
+// UpperCamelCase: Debe de tener mayúsculas al principio y al final.
+let VariableSuperHerrera;
+// camelCase: Debe de tener la primera letra en minúscula y la la otra en mayúscula.
+let variableSuper;
+// snake_case: Debe de separarse por _ y las letras en minúscula.
+let variable_super;
+
+const nickname = "Mr Overman";
+
+// UpperCamelCase: Debe de tener mayúsculas al principio y al final.
+const HerreraEdadSuper = 21;
+// camelCase: Debe de tener la primera letra en minúscula y la la otra en mayúscula.
+const herreraCiudad = "Medellin";
+// snake_case: Debe de separarse por _ y las letras en minúscula.
+const herrera_estudia = "CódigoC13 Academy";
+
+// Tipos de datos
+
+/**
+ * Primitivos: Estático o stack.
+ * Number
+ * String
+ * Boolean
+ * Undefined
+ * Null
+ */
+
+const number = 3;
+const text = "Green Day - Una gran canción";
+const isOpen = true;
+const isClosed = false;
+
+let noDefinido;
+let nulo = null;
+
+const saludoInformatico = "Hola, JavaScript."
+console.log(saludoInformatico); // Salida por consola
+
+alert(saludoInformatico); // Salida por interfaz
