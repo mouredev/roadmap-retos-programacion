cc3088ff9ba319d4b51e03b2026dbb615cf9eb43
lorenamesa <lorena.mesa.glez@gmail.com>
2024-01-04 15:08:58 +0000

    added new file

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/php/lorenamesa.php
@@ -0,0 +1,26 @@
+<?php
+
+/****************** OPERADORES ****************/
+
+// Aritméticos
+echo 5 + 3; // Suma
+echo 5 - 3; // Resta
+echo 5 * 3; // Multiplicación
+echo 5 / 3; // División
+echo 5 % 3; // Modular
+echo 5 ** 3; // Exponenciación
+
+// Asignación
+$a = 3;
+echo $a; // Devuelve 3
+$a += 5;
+echo $a; // Devuelve 8
+$a -= 5;
+echo $a; // Devuelve 7
+
+$b = 'Hola';
+echo $b; // Devuelve "Hola"
+$b = ' PHP';
+echo $b; // Devuelve "Hola PHP"
+
+// Bits
