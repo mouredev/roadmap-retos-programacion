ddb0a30f4d3163f924efa290a166b4b65883323b
Francisco Karriere <francisco.karriere@gmail.com>
2024-01-05 12:53:38 -0500

    #00 - scala

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/scala/franciscokarriere.scala
@@ -0,0 +1,29 @@
+// link oficial de lenguaje SCALA
+// https://www.scala-lang.org/
+
+//sintaxis de comentarios
+ //comentario en Scala de una sola linea
+ val a = 10 // puede ir continuando una declaracion de variable. 
+/* comentario en Scala 
+de
+multiples lineas
+*/
+
+/**
+  * Este es comentario de tipo JavaDoc
+  * se puede tags como @param, @return, @throws, etc. entremezclado
+  * con el comentario. 
+
+  */
+
+// variable ejemplo
+val b: Int = 25 //tipo de dato : Int
+var c: Double = 34.67 //tipo de dato : Double (
+    //se pueden asignar y cambiar su valor)
+
+//constante
+val pi = scala.math.Pi //la constante Pi es un valor numerico que representa el valor representado en matematica por la letra griega π,
+val scala = "scala"
+//imprimo en terminal saludo
+println("Hola, "+scala+"!")
+
