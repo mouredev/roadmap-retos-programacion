2dc6d74ffa4ceab05bd85a5e53fb797b9ba0360c
gusgonza <gusgonza@student.42barcelona.fr>
2024-01-06 01:05:43 +0100

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/yowcloud.java
@@ -0,0 +1,46 @@
+// URL oficial de Java: https://www.java.com/es/
+// una linea comentada
+/*
+Comentario
+De
+Varias
+Lineas
+*/
+public static yowcloud {
+    private static final int variableGlobalConstante = 3.1333; //una variable global entera constante
+
+    public static void main (String[] args)
+    {
+        int variable = 10; // una variable
+        variable = 12; // nuevo valor de la variable
+        final int variableConstante = 21; // variable constante
+
+        // tipos de datos primitivos
+        String str = "¡Hola, Java!";
+        int variablEntero = 42;
+        variableEntero = -42;
+        long variableLargo = 9898989L;
+        float variableFlotante = 3.14f;
+        double variableDoble = 4.223123213;
+        byte variableByte = 123;
+        short variableShort = 12345;
+        char variableCharacter = 'A';
+        boolean variableBooleano = true;
+        variableBooleano = false;
+
+        System.out.println(str);//imprimir texto
+    }
+}
+/*
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios  en el lenguaje (en una línea, varias...).
+ - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+* ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+  */
