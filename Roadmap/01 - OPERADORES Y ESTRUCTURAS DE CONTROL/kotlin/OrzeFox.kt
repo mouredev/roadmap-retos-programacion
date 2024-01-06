1035dbe6825bc2fe96002ad0a1b266fa5bfe5178
OrzeFox <dacm1997@yahoo.com>
2024-01-06 13:09:31 +0100

    mouredev#1 - Kotlin

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/OrzeFox.kt
@@ -0,0 +1,40 @@
+fun main() {
+    var a = 2
+    var b = 3
+
+    // Operadores Aritmeticos
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+
+    // Operadores Logicos
+    println(a < b && a > b)
+    pritnln(a < b || a > b)
+    printls(!true)
+
+    //Operadores de comparacion
+    println(a == b)
+    println(a != b)
+    println(a < b)
+    println(a > b)
+    println(a >= b)
+    println(a <= b)
+
+    // Operadores de Asignacion
+    var c = 1
+    println(c)
+    c += 1
+    println(c)
+    c -= 2
+    println(c)
+    c *= 3
+    println(c)
+    c /= 4
+    println(c)
+    c %= 5
+    println(c)
+    c++
+    println(c)
+}
