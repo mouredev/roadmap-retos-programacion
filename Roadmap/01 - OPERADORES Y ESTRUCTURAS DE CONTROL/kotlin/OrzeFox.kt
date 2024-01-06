4c4cf2f4e8d57c3ffc070a7457f94deecff8a728
OrzeFox <dacm1997@yahoo.com>
2024-01-06 14:00:37 +0100

    mouredev#1 - Kotlin

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/OrzeFox.kt
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/OrzeFox.kt
@@ -1,3 +1,5 @@
+import java.lang.ArithmeticException
+
 fun main() {
     var a = 2
     var b = 3
@@ -35,6 +37,81 @@ fun main() {
     println(c)
     c %= 5
     println(c)
-    c++
-    println(c)
+
+    // Operadores de pertenencia
+    println(15 in 1..35)
+    println(25 !in 1..10)
+
+    // Operadores de Bits
+
+    println(a and b)
+    println(a or b)
+    println(a xor b)
+    println(a shl 2)
+    println(a shr 1)
+    println(a ushr b)
+
+    // Condicionales
+
+    // if
+    if(a == b)
+        println("$a es igual a $b")
+    else
+        println("$a es diferente a $b")
+
+    // when
+
+    when (a)
+    {
+        2 -> println("Es dos")
+        3 -> println("Es tres")
+        else -> println("No es ni dos ni tres")
+    }
+
+    // while
+
+    while (a < 5)
+    {
+        println("La variable a es $a")
+        contador++
+    }
+
+    // do while
+
+    do
+    {
+        println("La variable b es $b")
+        b--
+    }
+    while (b > 0)
+
+    // for
+
+    for (i in 0 until 10)
+        println("valor del indice es $i")
+
+    // try catch
+
+    try
+    {
+        val d = b / 0
+        println("el resultado es : $d")
+    }
+    catch (e: ArithmeticException)
+    {
+        println("ERROR ARITMETICO")
+    }
+
+    retroExtra()
+}
+
+fun retroExtra()
+{
+    var ind = 10
+    while (ind != 55)
+    {
+        if (ind % 2 == 0 && ind != 16 && !(ind % 3 == 0)
+            println(ind)
+        ind++
+    }
 }
