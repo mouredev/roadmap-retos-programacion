a7ac65683ec0e64053be99d6d2bbae6235dbee31
Sara <71656774+SaraCorraless@users.noreply.github.com>
2024-01-07 22:20:25 +0100

    #01 - Kotlin

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/saracorraless.kt
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/kotlin/saracorraless.kt
@@ -4,7 +4,6 @@
 //http://www.androidcurso.com/index.php/99-kotlin/912-estructuras-de-control-en-kotlin
 //https://kotlin.desarrollador-android.com/basico/control-de-flujo/
 
-
 fun main(): Unit {
     //OPERADORES ARITMÉTICOS
     var ope1 = 5
@@ -47,22 +46,89 @@ fun main(): Unit {
     //Decremento: puede ir como prefijo o sufijo
     println("Decremento "+ (ope1--))
 
-    //Operadores relacionales   https://www.develou.com/operadores-en-kotlin/
+    //Operadores relacionales
     //igualdad
+    println("los númenos 2 y 2 son iguales: "+ (2==2))
 
     //diferente de...
+    println("los númenos 2 y 1 son diferentes: "+ (2!=1))
 
     //menor que
+    println("2 es menor que 3: "+ (2<3))
 
     //mayor que
+    println("4 es menor que 3: "+ (4>3))
 
     //menos o igual que
+    println("2 es menor o igual que 3: "+ (2<=3))
 
     //mayor o igual que
+    println("3 es mayor o igual que 3: "+ (2>=3))
 
 
     //Operadores lógicos
+    var num1 = 1
+    var num2 = 4
+    //AND
+    println("num1 and num2: ${num1 and num2}")
+    //OR
+    println("num1 or num2: ${num1 or num2}")
+    //XOR
+    println("num1 xor num2: ${num1 xor num2}")
+
+    //NOT
+    println("num1.inv(): ${num1.inv()}")
+
+    //DESPLAZAMIENTO DE BITS A LA IZQUIERDA
+    println("num1 shl num2: ${num1 shl num2}")
+    //DESPLAZAMIENTO DE BITS A LA DERECHA
+    println("num1 shr num2: ${num1 shr num2}")
+    //DESPLAZAMIENTO DE BITS A LA DERECHA SIN SIGNO
+    println("num1 ushr num2: ${num1 ushr num2}")
 
 
+    //ESTRUCTURAS DE CONTROL
+    //IF
+    if (2<3){
+        println("2 es menor a 3")
+    }
+
+    //IF ELSE
+    if ("agua" != "cafe"){
+        println("El café es mejor que el agua")
+    } else{
+        println("Hay que beber agua para estar sano")
+    }
+
+    //WHEN
+    when("Agua"){
+        "Agua" -> print("sanito")
+        "zumo" -> print("Mejor come fruta natural")
+        else -> { println("mejora tu dieta")
+        }
+    }
+
+
+    //BUCLE FOR
+    for (i in 0..5){
+        println("contador for: "+ i)
+    }
+
+    //BUCLE WHILE
+    while (num2 > 0){
+        println("contador while: "+ num2)
+        num2--
+    }
+    num2 = 6
+    //BUCLE DO WHILE
+    do {
+        println("contador do while: "+ num2)
+        num2--
+    }while(num2 > 0)
+
+    //se pueden usar controladores para salir de la estructura
+    //return : retornamos de la función actual (o función anónima).
+    //break : terminamos el bucle más cercano.
+    //continue : continuamos con el siguiente paso en el bucle más cercano.
 
 }
