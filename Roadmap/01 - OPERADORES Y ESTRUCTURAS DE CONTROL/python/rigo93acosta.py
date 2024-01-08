a74aadd428682c3e6d0110079e15e26b35c8b19c
riacosta <rigo93acosta@gmail.com>
2024-01-08 16:48:04 -0300

    Include Extra

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/rigo93acosta.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/rigo93acosta.py
@@ -131,6 +131,5 @@ except:
 Crea un programa que imprima por consola todos los números comprendidos
 entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
 """
-
 numbers = [i for i in range(10, 56) if i % 2 == 0 and i != 16 and i % 3 != 0]
 print(numbers)
