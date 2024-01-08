0acb6457d6dbf8f64c7fb13e466ed7dbb251c2e4
EmilioRDev <joseemilio0501@gmail.com>
2024-01-08 18:18:17 +0100

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/EmilioRDev.js
@@ -0,0 +1,69 @@
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+// Sitio web oficial de javascript: https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// Comentario de línea
+
+/* Comentario
+de
+varias
+líneas */
+
+/**
+ * Comentario
+ * de
+ * varias
+ * lineas
+ * esta forma mantiene una sangría común
+ */
+
+let variable = '';
+const constante = '';
+
+//Datos primitivos
+//String
+let cadenaTexto = 'Hola mundo!!';
+
+//Number
+let numeroEntero = 3;
+let numeroDecimal = 3.14;
+
+//BigInt
+let numeroGrande = 9007199254740991n;
+
+//Boolean
+let esVerdad = true;
+let esFalso = false;
+
+//Symbol
+let simbolo = Symbol();
+
+//Undefined y null
+let variableIndefinida;
+let variableNula = null;
+
+//Impresión por consola
+console.log('¡Hola, javascript!');
+
