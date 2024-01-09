8f515321f868c771e35115bf1b9fc03068738692
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-01-09 16:39:01 -0400

    Primera parte solucionada

--- Roadmap/02 - FUNCIONES Y ALCANCE/javascript/Pancratzia.js
+++ Roadmap/02 - FUNCIONES Y ALCANCE/javascript/Pancratzia.js
@@ -34,5 +34,45 @@ function returnFunction(programmingLang){
     return mensaje;
 }
 
+function functionInsideAntoherFunction(){
+    console.log("¿Es posible crear funciones dentro de otras?");
+
+    function isPossible(){
+        return "Si, es posible! Pero las mismas solo pueden ser llamadas dentro de la función en la que fueron creadas.";
+    }
+
+    console.log(isPossible());
+
+}
+
+function javaScriptOriginalFunction(){
+
+    console.log('Existen funciones nativas de JavaScript. Por ejemplo, el método Math.max() que retorna el número mayor entre dos números o el console.log que muestra por consola el argumento que le pasemos');
+}
+
+const arrowFunction = () =>{
+    console.log("Existen las funciones flecha, que se definen de la siguiente manera: let arrowFunction = () => { }");
+}
+
+//Una funcion declarada como function puede ser llamada antes o después de ser declarada. Pero una arrow function no puede ser llamada antes de ser declarada.
+
+let globalVariable = "Variable Global";
+
+function globalAndLocal(){
+    let localVariable = "Variable Local";
+
+    console.log(globalVariable);
+    console.log(localVariable);
+
+    globalVariable = "Variable Global Modificada";
+}
+
+
 intro();
-paramsFunction(1,2,3,4,5,6,7,8,9,10)
+paramsFunction(1,2,3,4,5,6,7,8,9,10);
+console.log(returnFunction("Javascript"));
+functionInsideAntoherFunction();
+// isPossible(); - Aquí esa función no está definida
+javaScriptOriginalFunction();
+globalAndLocal();
+console.log(`En la función globalAndLocal() la variable global fue modificada a: ${globalVariable}. Además, la variable local solo existe dentro de la función.`);
