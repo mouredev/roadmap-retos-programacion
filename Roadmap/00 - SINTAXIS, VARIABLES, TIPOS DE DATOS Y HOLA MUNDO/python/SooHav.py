0753a743219a5c79ef6fa446f2b0de0d6c672ea7
SooHav <havrylenko.sb@gmail.com>
2024-01-10 17:24:39 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/SooHav.py
@@ -0,0 +1,31 @@
+#00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+#Este es un comentario que indica la URL del sitio web oficial de python: https://www.python.org/
+
+""" 
+Esto es un bloque de código comentado en Python,
+generalmente usado para documentar el codigo para 
+los potenciales usuarios.
+
+"""
+
+#Variable
+mi_variable = "variable"
+
+#Constante
+"""
+Las contantes no existen en python como tal, sin embargo,
+por convención se asigna a una variable como contante y
+se nombran con el nombre todo en mayúsculas.
+"""
+MI_CONTANTE = "constante" 
+
+#Tipos de datos primitivos: enteros, flotantes, complejos, booleanos y cadenas.
+a = 1       #entero
+b = 2.3     #flotantes
+c = 5j-87   #complejo
+x = True    #booleano
+s = "sofia" #string
+
+#Impresión por consola
+print("¡Hola, Python!")
