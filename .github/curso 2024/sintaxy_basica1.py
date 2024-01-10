73be0283a3b4e2054057ff8629f984eea9b23205
AristiDevs <fmorenoxtv@hotmail.com>
2024-01-10 18:35:14 +0100

    added line of code

---
+++ .github/curso 2024/sintaxy_basica1.py
@@ -0,0 +1,29 @@
+"""
+Este es curso de programación para este año....
+
+Empezamos el curso de python 2024!
+
+"""
+# Simbolo almohadilla al principio para añadir comentarios o deshabilitar líneas de codigo.
+
+# Sintaxis de programación
+
+print("Hola Mundo")
+print("Hola Fran")
+# añadir ; entre las instrucciones es lo mismo que un salto de línea. (no recomendable)
+print("Hasta luego Fran")
+
+variable = "Mi nombre es : \
+Fran"  # añadir simbolo \ puedes saltar la línea,
+# los espacios en blanco cuentan en el espacio en el codigo
+print(variable)
+
+
+# los espacios en blanco cuentan en el espacio en el codigo
+
+# Identación
+
+a = 0
+for i in range(10):
+    a += 1
+    print(a)
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/pakomor.py
@@ -0,0 +1,51 @@
+# https://www.python.org/
+
+"Aquí un comentario en una línea"
+
+"""
+Insertando 3 comillas tenemos 
+opción de comentar en 
+varías líneas.
+
+"""
+
+#  Una variable, una constante
+
+nombre = "fran"
+
+constante = 3.1416  # esta es una constante
+
+print(nombre)
+print(constante)
+
+# Variables con datos primitivos
+
+# String
+saludar = "Hola Mundo!"
+despedirse = "Adios Mundo!"
+
+
+# Int
+edad = 10
+mes = 8
+
+# Float
+pi = 3.1416
+peso = 75.600
+
+# Boolean
+tengo_frio = True
+tengo_calor = False
+
+print(type(saludar))
+print(type(despedirse))
+print(type(edad))
+print(type(mes))
+print(type(pi))
+print(type(peso))
+print(type(tengo_frio))
+print(type(tengo_calor))
+
+# Imprimir por terminal un texto
+
+print("Hola, Python....")
