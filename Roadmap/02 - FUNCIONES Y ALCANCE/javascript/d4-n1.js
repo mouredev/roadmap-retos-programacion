f58891353b62db657b16da3690bc5cecca21d602
d4-n1 <dscasares@gmail.com>
2024-01-10 19:05:26 +0100

    #02-Javascript

--- Roadmap/02 - FUNCIONES Y ALCANCE/javascript/d4-n1.js
+++ Roadmap/02 - FUNCIONES Y ALCANCE/javascript/d4-n1.js
@@ -5,22 +5,52 @@ function helloWorld() {
 
 // Función con argumento
 function helloWord(word) {
-  console.log(`hello ${word}!`)
+  console.log(`Hello ${word}!`)
 }
 
 // Función con argumentos
-const sumAll = function(...args) {
-  for (let i = 0; i < args.length; i++ ) {
-    let x = 0
-    x += args.value
-    return x
+function sumAll(...args) {
+  let sum = 0
+  for (let element of args) {
+    sum += element
   }
+  console.log(sum)
 }
 
 // Función recursiva
+function power(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
 
 // Arrow functions
+const arrow = () => {
+  console.log("Hello arrow function!")
+}
 
 helloWorld()
 helloWord("there")
-sum(2, 3)
+sumAll(2, 6)
+power(2, 3)
+arrow()
+
+// Dificultad extra
+
+function exercise(threeMultiplier, fiveMultiplier) {
+  let counter = 0
+  
+  for (i = 1; i <= 100; i++) {
+    let output = ""
+    if(i % 3 == 0) {output += threeMultiplier}
+    if(i % 5 == 0) {output += fiveMultiplier}
+    if (!output) {counter++}
+    console.log(output || i)
+  }
+  
+  return counter
+}
+
+exercise("three", "five")
