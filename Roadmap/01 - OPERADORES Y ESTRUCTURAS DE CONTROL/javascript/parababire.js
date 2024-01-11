d9e8b17ce23728971d3b00d6e3c6866c25a72b22
Parababire <angelenarvaezm@gmail.com>
2024-01-11 10:12:32 -0400

    ultimo cambio

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/parababire.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/parababire.js
@@ -222,7 +222,7 @@ manejarErrors();
 entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3*/
 
 for (let i = 10; i <= 55; i++) {
-  if (i % 2 === 0) {
+  if (i % 2 === 0 && i % 3 !== 0) {
     console.log(i);
   }
 }
