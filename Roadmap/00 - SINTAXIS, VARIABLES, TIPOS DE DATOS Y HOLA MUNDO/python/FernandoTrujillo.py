09ce0ac4615cf45a7752ba9467b1a64dbc1aaf88
Fernando <87361660+jtrujilloalcocer@users.noreply.github.com>
2024-01-12 14:07:34 +0100

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/FernandoTrujillo.py
@@ -0,0 +1,34 @@
+"""  * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!" """
+ 
+#http://www.python.org/
+
+#Esto es un comentario con almoadilla
+
+'''
+Este es un ejemplo de comentario en donde se pueden escribir varias lines, tantas como necesites.
+'''
+
+"""
+Otro ejemplo de comentario para mas de una linea.
+"""
+
+#Declaracion de Variables
+DatoVacio=None
+DatoEntero=2024
+DatoFlotante=3.14
+
+DatoCadena="Este es mi primer reto"
+DatoCaracter="A"
+
+DatoBool=True
+
+print("¡Hola Python!") 
+ 
