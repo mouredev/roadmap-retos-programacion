7f5fcb034dc15ba18f627d4bf814980545f8c5c7
DobleA74 <andresandriani@gmail.com>
2024-01-12 12:45:21 -0300

    #00 - Javascrip

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/DobleA74.js
@@ -0,0 +1,15 @@
+
+/* https://www.javascript.com/*/
+
+//1° forma
+
+/* 2°
+forma*/
+
+let name = "Andres";
+const surname = "Andriani";
+let age = 31;
+let single = true;
+const lenguaje = "JavaScript"
+
+console.log("¡Hola, " + lenguaje + "!");
