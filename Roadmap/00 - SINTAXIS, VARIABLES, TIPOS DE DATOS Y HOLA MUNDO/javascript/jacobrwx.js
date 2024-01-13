57b857648a0275e4c037f26b9015a78734684524
jacobrwx <danieljacobo9706@gmail.com>
2024-01-12 18:32:04 -0600

    #00 - Javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/jacobrwx.js
@@ -0,0 +1,27 @@
+/* EJERCICIO:
+- Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+- Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+- Crea una variable (y una constante si el lenguaje lo soporta).
+- Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+*/
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//This is a Single-Line Comment.
+/*This is a Multi-Line Comment.
+*/
+
+var varible1;
+let variable2;
+
+const VARIABLE_CONST = "";
+
+let num = 11;
+let str = "Hello, World";
+let bool = true;
+let undef;
+let n = null;
+const SYM = Symbol("foo");
+
+console.log("Hello, Javascript");
