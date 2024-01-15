3c6f96f2a508c74da2a1e411b5ac7b492f58eab3
Gustavo Gomez <gagomezq1992@gmail.com>
2024-01-15 08:22:09 -0500

    00 - Java

---
+++ GustavoGomez19.java
@@ -0,0 +1,37 @@
+public class GustavoGomez19 {
+    public static void main(String[] args) {
+        //https://www.java.com/es/
+        // Comentario de una línea
+
+        /*
+        Comentario
+        de varias líneas
+        */
+
+        //Declaración de una variable
+        String nombre = "Gustavo";
+
+        //Declaración de variable constante
+        final String apellido = "Gomez";
+
+        //Declaración de los tipos de datos primitivos en Java
+        //Numéricos
+        byte numByte = 1;
+        short numShort = 2;
+        int numEntero = 17;
+        long numLong = 306546132;
+        float numFloat = 19.5f;
+        double numDouble = 9.5;
+
+        //Carácter
+        char letra = 'G';
+
+        //Booleano
+        boolean datoBooleano = false;
+
+        //Cadena de texto
+        String cadena = "Seré un próximo Java dev";
+
+        System.out.println("¡Hola, Java!");
+    }
+}
