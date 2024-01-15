a66a23057e87e7f1582a092c0cdd8dca7fb81f91
Pablo Guilló <pabloguilloj@gmail.com>
2024-01-16 00:16:29 +0100

    Agenda

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/pguillo02.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/pguillo02.py
@@ -56,4 +56,68 @@ diccionario[5]="fsdfaf"
 print(diccionario)
 
 #OPCIONAL
+agenda = {}
 
+def interaccion_agenda():
+    print("Qué desea hacer")
+    operacion =int(input("Añadir(1), Eliminar(2), Modificar(3), Buscar(4), Parar(5): "))
+
+    if operacion == 1:
+        nombre = input("Nombre del contacto: ")
+        try:
+            numero = int(input("Número: "))
+
+            if len(str(numero))>9:
+                print("El número no puede tener más de 9 dígitos")
+                interaccion_agenda()
+
+        except ValueError:
+            print("Tiene que ser un número")
+            interaccion_agenda()
+
+        agenda[nombre] = numero
+
+        interaccion_agenda()
+    
+    elif operacion == 2:
+        nombre = input("Nombre que desea eliminar: ")
+        try:
+            agenda.pop(nombre)
+        except:
+            print("El contacto no está en la agenda")
+            interaccion_agenda()
+            
+        interaccion_agenda()
+
+    elif operacion == 3:
+        nombre = input("Nombre del contacto a modificar: ")
+        try:
+            nuevo_numero = int(input("Nuevo número: "))
+
+            if len(str(nuevo_numero))>9:
+                print("El número no puede tener más de 9 dígitos")
+                interaccion_agenda()
+
+        except ValueError:
+            print("Tiene que ser un número")
+            interaccion_agenda()
+
+        agenda[nombre] = nuevo_numero
+
+        interaccion_agenda()
+    
+    elif operacion == 4:
+        nombre = input("Nombre del contacto a buscar: ")
+
+        try:
+            print(agenda[nombre])
+        except KeyError:
+            print("Contacto no existente")
+            interaccion_agenda()
+
+        interaccion_agenda()
+
+    elif operacion == 5:
+        return 0 
+
+interaccion_agenda()
