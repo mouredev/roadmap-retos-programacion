869ecdf4a71a91e2022d3ecf09b6995bc968a1c1
Warclimb <84227345+Warclimb@users.noreply.github.com>
2024-01-16 14:57:08 +0100

    Update warclimb.py

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/warclimb.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/warclimb.py
@@ -37,6 +37,8 @@ herramientas.sort()
 ## Igual que las tablas pero inmutables, se usan para datos que no van a cambiar
 power_rangers = ("Jason", "Zack", "Billy", "Trini", "Kimberly")
 
+# Las tuplas no se pueden editar, asi que: ¯\_(ツ)_/¯
+
 # Diccionarios
 ## conjunto de datos ordenados por claves. en este caso la clave es la herramienta y el valor representa la resistencia.
 herramientas_dict = {"martillo": 40, "destornillador": 6, "sierra": 20, "taladro": 80}
