51eb70630ce8cc2882ff869436d2844765eca231
Warclimb <84227345+Warclimb@users.noreply.github.com>
2024-01-16 16:53:58 +0100

    Update warclimb.py

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/warclimb.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/warclimb.py
@@ -43,9 +43,86 @@ power_rangers = ("Jason", "Zack", "Billy", "Trini", "Kimberly")
     - conjunto de datos ordenados por claves. en este caso la clave es la herramienta y el valor representa la resistencia."""
 herramientas_dict = {"martillo": 40, "destornillador": 6, "sierra": 20, "taladro": 80}
 
-# Conjuntos / sets
-## conjunto de datos no ordenados, no se pueden repetir
+# insercion
+herramientas_dict["alicates"] = 10
+# borrado
+del herramientas_dict["sierra"]
+# actualizacion
+herramientas_dict["taladro"] = 90
+
+"""CONJUNTOS / SETS
+- conjunto de datos no ordenados, no se pueden repetir"""
 herramientas_set = {"martillo", "destornillador", "sierra", "taladro"}
 
+# insercion
+herramientas_set.add("alicates")
+# borrado
+herramientas_set.remove("sierra")
+# actualizacion
+herramientas_set.add("serrucho")
+
+""" DIFICULTAD EXTRA """
+""" * Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
+ *   los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos.
+ *   (o el número de dígitos que quieras)
+ * - También se debe proponer una operación de finalización del programa.
+"""
 
 
+def agregar_contacto(contactos):
+    nombre = input("Introduce el nombre del contacto: ")
+    telefono = input("Introduce el teléfono del contacto: ")
+    contactos[nombre] = telefono
+
+def buscar_contacto(contactos):
+    nombre = input("Introduce el nombre del contacto: ")
+    if nombre in contactos:
+        print(nombre, ":", contactos[nombre])
+    else:
+        print("No existe el contacto")
+
+def actualizar_contacto(contactos):
+    nombre = input("Introduce el nombre del contacto: ")
+    if nombre in contactos:
+        telefono = input("Introduce el teléfono del contacto: ")
+        contactos[nombre] = telefono
+    else:
+        print("No existe el contacto")
+
+def borrar_contacto(contactos):
+    nombre = input("Introduce el nombre del contacto: ")
+    if nombre in contactos:
+        del contactos[nombre]
+    else:
+        print("No existe el contacto")
+
+def agenda():
+    contactos = {}
+    while True:
+        print("¿Qué quieres hacer?")
+        print("1. Añadir contacto")
+        print("2. Buscar contacto")
+        print("3. Actualizar contacto")
+        print("4. Borrar contacto")
+        print("5. Salir")
+        opcion = int(input("Introduce una opción: "))
+        if opcion == 1:
+            agregar_contacto(contactos)
+        elif opcion == 2:
+            buscar_contacto(contactos)
+        elif opcion == 3:
+            actualizar_contacto(contactos)
+        elif opcion == 4:
+            borrar_contacto(contactos)
+        elif opcion == 5:
+            break
+        else:
+            print("Opción incorrecta")
+    print("Fin")
+
+if __name__ == "__main__":
+    agenda()
