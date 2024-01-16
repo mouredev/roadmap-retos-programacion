e563695dcd1390b8ab3e46d2ac50502d21521803
Warclimb <84227345+Warclimb@users.noreply.github.com>
2024-01-16 13:15:32 +0100

    Update warclimb.py

---
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/warclimb.py
@@ -0,0 +1,29 @@
+# #03 ESTRUCTURAS DE DATOS
+#### Dificultad: Media | Publicación: 15/01/24 | Corrección: 22/01/24
+
+## Ejercicio
+"""
+ * EJERCICIO:
+ * - Muestra ejemplos de creación de todas las estructuras soportadas por defecto en tu lenguaje.
+ * - Utiliza operaciones de inserción, borrado, actualización y ordenación.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
+ *   los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos.
+ *   (o el número de dígitos que quieras)
+ * - También se debe proponer una operación de finalización del programa.
+ """
+
+# Los tipos de estrucutras de datos en Python son: Tablas, tuplas, listas, diccionarios y conjuntos
+
+# Tablas
+## conjunto de datos ordenados por índices
+herramientas = ["martillo", "destornillador", "sierra", "taladro"]
+
+# Tuplas
+## Igual que las tablas pero inmutables, se usan para datos que no van a cambiar
+
