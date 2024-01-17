2338e9ce5f6811d9a6697876c5c2554616ff8ede
Parababire <angelenarvaezm@gmail.com>
2024-01-17 12:30:59 -0400

    #03 - Javascript

--- Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/parababire.js
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/parababire.js
@@ -1,6 +1,4 @@
-//Built in data structure (primitive)
-
-/**Arrays**/
+/*Arrays, colección de datos ordenados por indices*/
 
 let names = ["maria", "angel", "pedro", "luisa"];
 names.push("rogelio");//Insertar dato
@@ -12,7 +10,7 @@ console.log(names);
 names.sort();//Reordenar datos
 console.log(names);
 
-/**Objects**/
+/*Objectos literales, colección de datos ordenados por pares (clave:valor)*/
 
 const joy = {
   raza: "poodle",
@@ -31,5 +29,23 @@ joy.vive = false;
 console.log(joy);
 delete joy.vive;//Eliminar propiedad
 console.log(joy);
+//Objetos literales no poseen un método que reordene sus pares.
 
-//Not built in data structure (non-primitive)
+/*Date*/
+
+//Esta estructura de datos posee muchos métodos que permiten su manipulación. Esto permite la representación de los datos en diferentes formatos, acceder a datos por separado (año, día, mes...).
+
+let diaActual = new Date().toString();
+console.log(diaActual);
+
+/*Map, colección de datos ordenados por pares*/
+
+const map1 = new Map();
+map1.set("a", 1);//Crear dato
+map1.set("b", true);
+console.log(map1);
+map1.set("a", 44);//Modificar dato
+console.log(map1.get("a"));
+map1.delete("b");//Borrar dato
+console.log(map1);
+//El objeto Map() no posee un método que reordene sus pares.
