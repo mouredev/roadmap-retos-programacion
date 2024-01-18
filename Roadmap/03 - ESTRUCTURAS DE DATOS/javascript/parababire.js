d55d812958c721623b4b22c7f360cb1bae6dd093
Parababire <angelenarvaezm@gmail.com>
2024-01-18 12:27:18 -0400

    #03 - Javascript

--- Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/parababire.js
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/parababire.js
@@ -2,13 +2,9 @@
 
 let names = ["maria", "angel", "pedro", "luisa"];
 names.push("rogelio");//Insertar dato
-console.log(names);
 names.pop();//Eliminar dato
-console.log(names);
 names[0] = "lupita";//Actualizar dato
-console.log(names);
 names.sort();//Reordenar datos
-console.log(names);
 
 /*Objectos literales, colección de datos ordenados por pares (clave:valor)*/
 
@@ -20,15 +16,10 @@ const joy = {
     console.log("woof, woof, woof");
   }
 }
-console.log(joy);
 joy.raza = "mixed";//Actualizar propiedad
-console.log(joy);
 joy.cola = true;//Crear propiedad
-console.log(joy);
 joy.vive = false;
-console.log(joy);
 delete joy.vive;//Eliminar propiedad
-console.log(joy);
 //Objetos literales no poseen un método que reordene sus pares.
 
 /*Date*/
@@ -36,16 +27,29 @@ console.log(joy);
 //Esta estructura de datos posee muchos métodos que permiten su manipulación. Esto permite la representación de los datos en diferentes formatos, acceder a datos por separado (año, día, mes...).
 
 let diaActual = new Date().toString();
-console.log(diaActual);
 
 /*Map, colección de datos ordenados por pares*/
 
 const map1 = new Map();
 map1.set("a", 1);//Crear dato
 map1.set("b", true);
-console.log(map1);
 map1.set("a", 44);//Modificar dato
-console.log(map1.get("a"));
 map1.delete("b");//Borrar dato
-console.log(map1);
 //El objeto Map() no posee un método que reordene sus pares.
+
+/*Set, colección de datos únicos, primitivos u objetos*/
+
+const mySet1 = new Set();
+
+mySet1.add(1); //Crear dato
+mySet1.add(5); // Set(2) { 1, 5 }
+mySet1.add(5); // Set(2) { 1, 5 }, los datos son únicos. No se repíten.
+mySet1.add("some text");
+const o = { a: 1, b: 2 };
+mySet1.add(o);
+//Los datos en el objeto creado no pueden ser actualizados o reordenarse su posición.
+mySet1.add({ a: 1, b: 2 }); // o hace referencia a un objeto diferente.
+mySet1.delete(5);//Borrar dato
+
+/*Weakset, colección de datos tipo objeto y Symbol*/
+
