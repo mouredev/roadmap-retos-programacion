f2c26c0c3d720be59e303851a8afbbc82559612e
luisv <luisvizcayno@gmail.com>
2024-01-19 21:51:19 +0100

    Adding Solution #00_Kotlin_Luis-VB

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/Luis-VB.kt
@@ -0,0 +1,28 @@
+// https://kotlinlang.org/
+
+//  Comentario en una linea
+
+/*
+*   Comentario en varias lineas
+*
+*/
+
+// Variables y constantes
+var = "Variable"
+val = "Constante"
+
+//Tipos de variables
+fun main() {
+    val a: Int = 1000
+    val b: String = "log message"
+    val c: Double = 3.14
+    val d: Long  = 100_000_000_000_000
+    val e: Boolean = false
+    val f: Char = '\n'
+}
+
+// Imprimir por consola hola kotlin
+fun main() {
+    val kotlin = "!Hola, Kotlin :)¡"
+    println(kotlin)
+}
