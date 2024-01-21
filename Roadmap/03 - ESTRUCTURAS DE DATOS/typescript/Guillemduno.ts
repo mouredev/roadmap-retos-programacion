a7a9c513d5e96cf5f36ea2e74efc12432789c48a
gduno <guillem.duno@ithinkupc.com>
2024-01-21 06:31:50 +0100

    #03-typescript

--- Roadmap/03 - ESTRUCTURAS DE DATOS/typescript/Guillemduno.ts
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/typescript/Guillemduno.ts
@@ -2,7 +2,6 @@
  * EJERCICIO:
  * - Muestra ejemplos de creación de todas las estructuras soportadas por defecto en tu lenguaje.
  * - Utiliza operaciones de inserción, borrado, actualización y ordenación.
- 
  */
 
 /**
