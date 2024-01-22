55034e137b0987285dc75fa143420f05a912fdf9
CaveroBrandon <brandon@pushpress.com>
2024-01-22 13:29:54 -0400

    #03 - Python

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/CaveroBrandon.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/CaveroBrandon.py
@@ -1,9 +1,9 @@
+import sys
 """
 * EJERCICIO:
 Muestra ejemplos de creación de todas las estructuras soportadas por defecto en tu lenguaje.
 Utiliza operaciones de inserción, borrado, actualización y ordenación.
 """
-import sys
 
 
 # **** LIST ****
