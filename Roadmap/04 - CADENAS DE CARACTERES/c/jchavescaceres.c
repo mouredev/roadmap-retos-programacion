2883aba1fbde48d3551fbe1fb08fad0c2b555a6b
Julian <no@yahoo.es>
2024-01-23 22:08:52 +0100

    #04 - primer commit

---
+++ Roadmap/04 - CADENAS DE CARACTERES/c/jchavescaceres.c
@@ -0,0 +1,71 @@
+/*
+ * EJERCICIO:
+ * Muestra ejemplos de todas las operaciones que puedes realizar con cadenas de caracteres
+ * en tu lenguaje. Algunas de esas operaciones podrían ser (busca todas las que puedas):
+ * - Acceso a caracteres específicos, subcadenas, longitud, concatenación, repetición, recorrido,
+ *   conversión a mayúsculas y minúsculas, reemplazo, división, unión, interpolación, verificación...
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que analice dos palabras diferentes y realice comprobaciones
+ * para descubrir si son:
+ * - Palíndromos
+ * - Anagramas
+ * - Isogramas
+ */
+
+/*
+Funciones definidas en string.h:
+
+memcpy 
+memccpy 
+strcpy 
+strncpy 
+strcat 
+strncat 
+strcmp 
+strncmp 
+strcoll 
+strxfrm 
+strcoll_l 
+strxfrm_l 
+strdup 
+strndup 
+strchr 
+strchr 
+strchr 
+strrchr 
+strrchr 
+strrchr 
+strchrnul 
+strcspn 
+strspn 
+strpbrk 
+strpbrk 
+strpbrk 
+strstr 
+strstr 
+strstr 
+strtok 
+strtok_r 
+strcasestr 
+strlen 
+strnlen 
+strerror 
+strerror_r 
+strerrordesc_np 
+strerrorname_np 
+strerror_l 
+strsep 
+strsignal 
+stpcpy 
+stpncpy 
+strverscmp 
+strfry 
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+void main () {
+};
+
