fb6381cbb180ed19b95c3a41e95f974c5d96180a
hozlucas28 <hozlucas28@gmail.com>
2024-01-25 19:56:17 -0300

    Improve references

--- .github/actions/pull-requests/check-committed-files/main.js
+++ .github/actions/pull-requests/check-committed-files/main.js
@@ -8,20 +8,21 @@ const committedFiles = getInput('committed-files').split('\\n')
 debug(`'author' (input): ${author}`)
 debug(`'committed-files' (input): ${committedFiles}`)
 
+// Required data
+const authorLowerCased = author.toLowerCase()
+
 // Check each commited file
 for (const committedFile of committedFiles) {
 	const parsedFile = path.parse(committedFile)
 	debug(`Committed file properties: ${parsedFile}`)
 
 	// Check file name
-	const isValidFileName = parsedFile.name.toLowerCase() === author.toLowerCase()
+	const isValidFileName = parsedFile.name.toLowerCase() === authorLowerCased
 	debug(`Is valid file name? ${isValidFileName}`)
 
 	if (!isValidFileName) {
 		setFailed(
-			`The file name: '${parsedFile.name + parsedFile.ext}', inside '${
-				parsedFile.dir
-			}' directory doesn't match with the author name of the pull request. ` +
+			`The file name: '${parsedFile.base}', inside '${parsedFile.dir}' directory doesn't match with the author name of the pull request. ` +
 				'Please rename the file name. ' +
 				`It should be: '${author}', case insensitive. ` +
 				'If you think this is an error, please contact an administrator.'
@@ -33,7 +34,7 @@ for (const committedFile of committedFiles) {
 	debug(`Last folder of the commited file: ${lastFolder}`)
 
 	if (!lastFolder) {
-		throw new Error(`The last folder of '${parsedFile.name + parsedFile.ext}' (committed file) is undefined!`)
+		throw new Error(`The last folder of '${parsedFile.base}' (committed file) is undefined!`)
 	}
 
 	const expectedFileExtBasedOnLastFolder = getProgrammingLanguageExtension(lastFolder)
@@ -46,9 +47,7 @@ for (const committedFile of committedFiles) {
 		debug(`Expected last folder based on the extension of the commited file: ${expectedLastFolderBasedOnCommittedFileExt}`)
 
 		setFailed(
-			`The file extension of '${parsedFile.name + parsedFile.ext}' file, inside '${
-				parsedFile.dir
-			}' directory doesn't match with the programming language folder. ` +
+			`The file extension of '${parsedFile.base}' file, inside '${parsedFile.dir}' directory doesn't match with the programming language folder. ` +
 				'Check the file extension or the programming language folder where it is located. ' +
 				`The file extension should be: '${expectedFileExtBasedOnLastFolder}, ` +
 				`or the programming language folder where it is located should be: '${expectedLastFolderBasedOnCommittedFileExt}'. ` +
