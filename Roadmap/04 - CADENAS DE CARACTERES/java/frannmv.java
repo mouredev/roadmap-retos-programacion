120c1e7257aac3ec2dfb1e421b406840053e7fe8
frannmv <morenoverafranco@gmail.com>
2024-01-25 13:14:53 -0300

    Operaciones con Cadena TERMINADO

--- Roadmap/04 - CADENAS DE CARACTERES/java/frannmv.java
+++ Roadmap/04 - CADENAS DE CARACTERES/java/frannmv.java
@@ -1,4 +1,7 @@
 import java.lang.String;
+
+import static java.lang.StringTemplate.STR;
+
 public class frannmv {
 
     public static void main(String[] args) {
@@ -19,11 +22,7 @@ public class frannmv {
 
         System.out.println(cadena.length()); // Longitud de la Cadena
 
-        String nuevaCadena = " Con Java";
-        String cadenaConcatenada = cadena + nuevaCadena;
-        System.out.println(cadenaConcatenada); // Concatenacion
-
-        System.out.println(cadena.concat(nuevaCadena).concat(" - OOP")); // Concatenacion
+        System.out.println(cadena.concat(" Con Java").concat(" - OOP")); // Concatenacion
 
         System.out.println(cadena.repeat(2)); // Repetición - El String "cadena" se repite 2 veces
 
@@ -36,6 +35,24 @@ public class frannmv {
 
         String reemplazarCadena = cadena.replace("RoadMap", "MapRoad");
         System.out.println(reemplazarCadena); // Reemplazar Cadena
+
+        String nuevaCadena = "Java-Object-Oriented-Programming";
+        String[] palabras = nuevaCadena.split("-"); // División
+        for(String palabra : palabras){
+            System.out.println(palabra);
+        }
+
+        String cadenaConcatenada = cadena + nuevaCadena;
+        System.out.println(cadenaConcatenada); // Union - Concatenacion
+
+        nuevaCadena = "Java";
+        String inter = STR."Estoy programando en \{nuevaCadena}";
+        System.out.println(inter); // Interpolación
+
+        inter = String.format("Estoy programando en %s", nuevaCadena);
+        System.out.println(inter); // Interpolación
+
+        System.out.println(nuevaCadena.equals(cadena)); // Verificacion
 
     }
 }
