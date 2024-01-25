d1c3605847aeaf2dd435244c6017ce76a5bd38a2
hozlucas28 <hozlucas28@gmail.com>
2024-01-25 16:26:15 -0300

    Update "check-committed-files" job in order to compare the file name with the author name case insensitive

--- .github/actions/pull-requests/check-committed-files/main.js
+++ .github/actions/pull-requests/check-committed-files/main.js
@@ -1,19 +1,29 @@
 import { debug, getInput, setFailed } from '@actions/core'
+import path from 'node:path'
 
 // Inputs
+const author = getInput('author')
 const committedFiles = getInput('committed-files').split('\\n')
+debug(`'author' (input): ${author}`)
 debug(`'committed-files' (input): ${committedFiles}`)
 
-// Check if changed and modified files are valid
-const isValidCommittedFiles = committedFiles.length === 1
-debug(`Is valid committed files? ${isValidCommittedFiles}`)
+// Check each commited file
+for (const committedFile of committedFiles) {
+	const parsedFile = path.parse(committedFile)
+	debug(`Committed file properties: ${parsedFile}`)
 
-if (isValidCommittedFiles) {
-} else {
-	// On invalid files, set the action as failed
-	setFailed(
-		'Pull request must commit just one file. ' +
-			'Please, check the pull request. ' +
-			'If you think this is an error, please contact an administrator.'
-	)
+	// Check file name
+	const isValidFileName = parsedFile.name.toLowerCase() === author.toLowerCase()
+	debug(`Is valid file name? ${isValidFileName}`)
+
+	if (!isValidFileName) {
+		setFailed(
+			`The file name: '${parsedFile.name + parsedFile.ext}', inside '${
+				parsedFile.dir
+			}' directory doesn't match with the author name of the pull request. ` +
+				'Please rename the file name. ' +
+				`It should be: '${author}', case insensitive. ` +
+				'If you think this is an error, please contact an administrator.'
+		)
+	}
 }
