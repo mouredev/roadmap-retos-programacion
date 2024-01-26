e06e16c6e674edbedcc83e771f8b5428b1b362e3
Warclimb <javisempere@gmail.com>
2024-01-26 20:45:49 +0100

    Update warclimb.py

--- Roadmap/04 - CADENAS DE CARACTERES/python/warclimb.py
+++ Roadmap/04 - CADENAS DE CARACTERES/python/warclimb.py
@@ -94,3 +94,26 @@ print(text == "") # comprobamos si esta vacio
  * - Isogramas
 '''
 
+# Palindromo si es un palindromo, que se lea igual de izquierda a derecha o al revés
+def palindromo(palabra1, palabra2):
+    palabra1 = palabra1.lower()
+    if palabra1 == palabra1[::-1]:
+        print(f"{palabra1} es un palíndromo")
+    else:
+        print(f"{palabra1} no es un palíndromo")
+
+# Anagrama, comprobamos si cambiando el orden puede formar otra palabra
+def anagrama(palabra1, palabra2):
+    pass
+
+# Isograma, cada letra aparece el mismo número de veces
+
+# Heterograma, que no tiene ninguna letra repetida
+
+# comprobamos las palabras
+palabra1 = "abba"
+palabra2 = "calabaza"
+
+palindromo(palabra1, palabra2)
+
+
