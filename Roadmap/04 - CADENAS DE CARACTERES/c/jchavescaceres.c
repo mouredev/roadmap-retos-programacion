8fb84e3140e6bcb79c209ec3106cbb8409053557
Julian <no@yahoo.es>
2024-01-28 20:08:12 +0100

    #04 - commit basico

--- Roadmap/04 - CADENAS DE CARACTERES/c/jchavescaceres.c
+++ Roadmap/04 - CADENAS DE CARACTERES/c/jchavescaceres.c
@@ -29,25 +29,22 @@ strpbrk
 strstr 
 strtok 
 strtok_r 
-strcasestr 
 strlen 
 strerror 
 strerror_r 
-strerrordesc_np 
-strerrorname_np 
+strerrordesc_np (NOT COVERED) 
+strerrorname_np  (NOT COVERED)
 strerror_l 
 strsep 
 strsignal 
 stpcpy 
-stpncpy 
-strverscmp 
-strfry 
 
 strncpy 
 strncat 
 strncmp 
 strndup 
 strnlen 
+stpncpy 
 */
 
 #include <stdio.h>
@@ -66,17 +63,36 @@ void main () {
 	setlocale(LC_COLLATE, "es_ES.UTF-8");
 
 	/* Copy */
+	memset (buffer, 0, sizeof (buffer));
 	strcpy (buffer, string1);
 	printf ("String1: %s, strcpy (buffer, string1), buffer: %s\n", string1, buffer);
 
+	/* Copy but n characters*/
+	memset (buffer, 0, sizeof (buffer));
+	strncpy (buffer, string1, 4);
+	printf ("String1: %s, strncpy (buffer, string1, %d), buffer: %s\n", string1, 4, buffer);
+
 	/* Concat */
+	memset (buffer, 0, sizeof (buffer));
+	strcpy (buffer, string1);
 	printf ("buffer: %s, string2: %s, strcat (buffer, string2), ", buffer, string2);
 	strcat (buffer, string2);
 	printf ("buffer: %s\n", buffer);
 
+	/* Concat n characters */
+	memset (buffer, 0, sizeof (buffer));
+	strcpy (buffer, string1);
+	printf ("buffer: %s, string2: %s, strcat (buffer, string2, %d), ", buffer, string2, 4);
+	strncat (buffer, string2, 4);
+	printf ("buffer: %s\n", buffer);
+
+
 	/* Compare */
 	strcpy (buffer, string1);
 	printf( "String1: %s, buffer: %s, strcmp (buffer, string1) = %d\n", string1, buffer, strcmp (buffer, string1));
+
+	/* Compare n characters*/
+	printf( "String1: %s, String2: %s, strncmp (string1, string2, %d) = %d\n", string1, string2, 4, strncmp (string1, string2, 4));
 
 	/* Compare using locale LC_COLLATE */
 	strcpy (buffer, string1);
@@ -85,6 +101,12 @@ void main () {
 	/* Duplicate string, memory allocated must be freed */
 	ptr = strdup (string1);
 	printf( "String1: %s, strdup: %s\n", string1, ptr);
+	free (ptr);
+	ptr = NULL;
+	
+	/* Duplicate string but n characters, memory allocated must be freed */
+	ptr = strndup (string1, 4);
+	printf( "String1: %s, strndup(%d): %s\n", string1, 4, ptr);
 	free (ptr);
 	ptr = NULL;
 
@@ -111,7 +133,39 @@ void main () {
 	printf( "strtok (%s, \",\"): ", testStrtok);
 	printf( "%s\n", strtok (testStrtok, "-,"));
 	printf( "strtok (NULL, \",\"): %s\n", strtok (NULL, "-,"));
-	printf( "strtok (NULL, \",\"): %s\n", strtok (NULL, "-,"));
+
+	/* Same as strtok but savePtr keep status in sucesive calls */
+	char * savePtr = NULL;
+	char testStrtok_r [] = "1, 2, 3 y 4";
+	printf( "strtok_r (%s, \",\", &savePtr): ", testStrtok_r);
+	printf( "%s\n", strtok_r (testStrtok_r, "-,", &savePtr));
+	printf( "strtok_r (NULL, \",\", &savePtr): %s\n", strtok_r (NULL, "-,", &savePtr));
+	printf( "strtok_r (NULL, \",\", &savePtr): %s\n", strtok_r (NULL, "-,", &savePtr));
+
+	/* Another implementation of tokens separator, testStrsep is updated with rest of string after token */
+	char *testStrsep = malloc (200); 
+	char *copiaTestStrsep = testStrsep;
+	strcpy (testStrsep , "1, 2, 3 y 4");
+	printf( "strsep (%s, \",\"): ", testStrsep);
+	printf( "%s\n", strsep (&testStrsep, "-,"));
+	printf( "%s\n", testStrsep);
+	free (copiaTestStrsep);
+
+	/* String length without counting \0 at the end */
+	printf( "String1: %s, strlen (string1): %lu\n", string1, strlen (string1));
+
+	/* String length with a maximum characters without counting \0 at the end */
+	printf( "String1: %s, strnlen (string1, %d): %lu\n", string1, 4, strnlen (string1, 4));
+	printf( "String1: %s, strnlen (string1, %d): %lu\n", string1, 400, strnlen (string1, 400));
+	
+	/* System error message, several implementations */
+	printf( "strerror 4 %s\n", strerror (4));
+	strerror_r (4, buffer, sizeof (buffer) / sizeof (char));
+	printf( "strerror 4 %s\n", buffer);
+	printf( "strerror_l 4 %s\n", strerror_l (4, 0));
+
+	/* Signal name as string */
+	printf( "Signal %d, name %s \n", 15, strsignal (15));
 
 
 };
