cc21fc796e98ad6a984c274bff6c89516328d36a
adriantaf <atafoya863@gmail.com>
2024-01-28 21:23:55 -0800

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/adrianTM.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Adrian TM
+/* Adrian
+	T
+	M
+*/
+
+const nombre = 'Adrian'; // Es una constante
+let edad = 19; // Es variable
+
+let numeroEntero = 10;
+let numeroFlotante = 10.99;
+let cadenaDeTexto = "TM";
+let booleano = true;
+let simbolo = Symbol("Descripcion");
+
+console.log("¡Hola, JavaScript!")
