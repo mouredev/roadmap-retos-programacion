460a2f551dde62a824404a08c570dff7d9075051
Pablo Guilló <pabloguilloj@gmail.com>
2024-02-02 15:27:10 +0100

    Declaración

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/pguillo02.js
@@ -0,0 +1,6 @@
+//Se declaran variables con let y constantes con const
+//Se pueden incializar variables sin valor y asignarselo después, también se pueden cambiar después de inicializarlas
+let variable = 'Hola Mundo';
+variable = 5
+let mundo;
+mundo = 'Hola Mundo';
