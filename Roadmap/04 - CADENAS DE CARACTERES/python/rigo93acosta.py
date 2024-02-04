f38a0cb8f6ceca7b5306903bc2550d50c5676a72
rigo93acosta <rigo93acosta@gmail.com>
2024-02-04 09:54:39 -0300

    Created File

---
+++ Roadmap/04 - CADENAS DE CARACTERES/python/rigo93acosta.py
@@ -0,0 +1,18 @@
+#04 CADENAS DE CARACTERES
+'''
+EJERCICIO:
+Muestra ejemplos de todas las operaciones que puedes realizar con cadenas de caracteres
+en tu lenguaje. Algunas de esas operaciones podrían ser (busca todas las que puedas):
+- Acceso a caracteres específicos, subcadenas, longitud, concatenación, repetición, recorrido,
+  conversión a mayúsculas y minúsculas, reemplazo, división, unión, interpolación, verificación...
+
+DIFICULTAD EXTRA (opcional):
+Crea un programa que analice dos palabras diferentes y realice comprobaciones
+para descubrir si son:
+- Palíndromos
+- Anagramas
+- Isogramas
+'''
+
+if __name__ == '__main__':
+    pass
