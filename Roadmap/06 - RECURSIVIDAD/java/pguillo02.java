e9a2b2a5aed70d8c62e6adca53cc41c90c6c03ff
Pablo Guilló <pabloguilloj@gmail.com>
2024-02-06 11:03:52 +0100

    Definición de una función recursiva básica

---
+++ Roadmap/06 - RECURSIVIDAD/java/pguillo02.java
@@ -0,0 +1,21 @@
+//Definición de una función recursiva
+
+/**
+ * pguillo02
+ */
+public class pguillo02 {
+    //Stop recomending
+
+    public static void main(String[] args) {
+        recursiva(100);
+    }
+
+    public static void recursiva(n) {
+        if (item == 0){
+            System.out.println(0,"Fin de la función recursiva");
+        } else{
+            System.out.println(n);
+            recursiva(n-1);
+        }
+    }
+}
