965b89c0b9713ceab8bf02d618596b213bdddda8
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-06 11:50:42 -0400

    Haciendo el ejercicio recursivo de la semana #7 - Ejercicio 6

---
+++ Roadmap/06 - RECURSIVIDAD/javascript/Pancratzia.js
@@ -0,0 +1,20 @@
+/*
+ * EJERCICIO:
+ * Entiende el concepto de recursividad creando una función recursiva que imprima
+ * números del 100 al 0.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Utiliza el concepto de recursividad para:
+ * - Calcular el factorial de un número concreto (la función recibe ese número).
+ * - Calcular el valor de un elemento concreto (según su posición) en la
+ *   sucesión de Fibonacci (la función recibe la posición).
+ */
+
+function countTo0(num) {
+  console.log(num);
+
+  if (num === 0) return;
+  else countTo0(num - 1);
+}
+
+countTo0(100);
