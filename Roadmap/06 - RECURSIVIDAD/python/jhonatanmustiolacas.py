28d70974281c1f2f17f60238529aa2a69449f2e3
JhonatanMustiolaCas <jhonmustiomaths@protonmail.com>
2024-02-08 15:50:46 -0400

    #06 - python

--- Roadmap/06 - RECURSIVIDAD/python/jhonatanmustiolacas.py
+++ Roadmap/06 - RECURSIVIDAD/python/jhonatanmustiolacas.py
@@ -12,6 +12,12 @@
 
 # TODO: función recursiva que imprime números del 100 al 0
 def imprime_del_cien_al_cero_recursivo() -> None:
+    """
+    Imprime los números del 100 al cero recursivamente
+
+    >>> imprime_del_cien_al_cero_recursivo()
+
+    """
     if hasattr(imprime_del_cien_al_cero_recursivo, "count"):
         if imprime_del_cien_al_cero_recursivo.count < 0:
             return
@@ -25,11 +31,40 @@ def imprime_del_cien_al_cero_recursivo() -> None:
 
 # TODO: funcion recursiva para calcular factorial de un número dado
 def calculo_factorial_recursivo(numero: int) -> int:
+    """
+    Retorna el cálculo factorial del argumento numero
+
+    >>> calculo_factorial_recursivo(5)
+    120
+    """
     if numero == 0:
         return 1
     return numero * calculo_factorial_recursivo(numero - 1)
 
 
 # TODO: funcion recursivo para calcular valor de un número fibonacci en una posición dada
-def fibonacci_en_recursivo(n: int) -> int:
-    return 0 if (n == 0) else 1 if (n == 1) else  (fibonacci_en_recursivo(n-1) + fibonacci_en_recursivo(n-2))
+def fibonacci_en_recursivo(posicion: int) -> int:
+    """
+    Retorna el valor del número fibonacci en la posición dada con el argumento posicion
+
+    >>> fibonacci_en_recursivo(10)
+    55
+    """
+    return 0 if (posicion == 0) else 1 if (posicion == 1) else  (fibonacci_en_recursivo(posicion-1) + fibonacci_en_recursivo(posicion-2))
+
+
+
+# TEST
+
+resultado = imprime_del_cien_al_cero_recursivo()
+print(resultado)
+
+resultado = calculo_factorial_recursivo(5)
+print(resultado)
+
+resultado = fibonacci_en_recursivo(10)
+print(resultado)
+
+import doctest
+
+doctest.testmod()
