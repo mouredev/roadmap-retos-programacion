a7664fe7cdc415be30c79c09bd93f4dffe854d87
Alberto Estella Local <esteya92@gmail.com>
2024-02-08 05:05:51 +0100

    añadido encabezado opcional

--- Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/Jalivur.js
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/Jalivur.js
@@ -228,8 +228,17 @@ mySet.delete("foo");
 mySet.size; // 2
 for (let item of mySet) console.log(item);
 console.log(`===============`);
-
-
+/*
+* DIFICULTAD EXTRA (opcional):
+ * Crea una agenda de contactos por terminal.
+ * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
+ * - Cada contacto debe tener un nombre y un número de teléfono.
+ * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
+ *   los datos necesarios para llevarla a cabo.
+ * - El programa no puede dejar introducir números de teléfono no númericos y con más de 11 dígitos.
+ *   (o el número de dígitos que quieras)
+ * - También se debe proponer una operación de finalización del programa.
+ */
 
 
 
