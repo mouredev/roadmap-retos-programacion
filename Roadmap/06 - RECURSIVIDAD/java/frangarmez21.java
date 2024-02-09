97fef0d4616f84bd2d8db0de00b644c5f997886b
frangarmez21 <littletimetolearn@gmail.com>
2024-02-09 17:55:34 +0100

    #06 - java -Added file with exercise structure

---
+++ Roadmap/06 - RECURSIVIDAD/java/frangarmez21.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class frangarmez21 {
+
+    public static void main(String[] args) {
+
+        System.out.println("##Ejercicio de recursividad##");
+        /*
+         * EJERCICIO:
+         * Entiende el concepto de recursividad creando una función recursiva que imprima
+         * números del 100 al 0.
+         */
+
+        System.out.println();
+        System.out.println("##Creando función recursiva que imprime números del 100 al 0.##");
+        System.out.println();
+
+
+
+        System.out.println();
+        System.out.println("##Dificultad Extra##");
+        System.out.println();
+
+        /* * DIFICULTAD EXTRA (opcional):
+         * Utiliza el concepto de recursividad para:
+         * - Calcular el factorial de un número concreto (la función recibe ese número).
+         * - Calcular el valor de un elemento concreto (según su posición) en la
+         *   sucesión de Fibonacci (la función recibe la posición).
+         */
+
+        System.out.println("Calcular el factorial de un número concreto");
+        System.out.println();
+
+
+
+        System.out.println();
+        System.out.println("Calcular el valor de un elemento concreto en la sucesión de Fibonacci");
+        System.out.println();
+
+
+    }
+}
