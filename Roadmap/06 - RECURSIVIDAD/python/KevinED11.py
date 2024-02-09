d00ab8ae67e0e5dd0cc38c299c8b59776c0d224d
KevinED11 <mendez_kael@hotmail.com>
2024-02-08 22:29:25 -0700

    reto #06 - python

---
+++ Roadmap/06 - RECURSIVIDAD/python/KevinED11.py
@@ -0,0 +1,33 @@
+import functools
+
+
+def decrement_counter(start_number: int = 100) -> None:
+    if start_number >= 0:
+        print(start_number)
+        decrement_counter(start_number - 1)
+
+
+@functools.lru_cache()
+def factorial(number: int = 5) -> int:
+    return 1 if number == 1 else number * factorial(number - 1)
+
+
+@functools.lru_cache()
+def fibonacci(number: int) -> int:
+    if number == 0:
+        return 0
+
+    if number == 1:
+        return 1
+
+    return fibonacci(number - 2) + fibonacci(number - 1)
+
+
+def main() -> None:
+    decrement_counter(start_number=100)
+    print(factorial(number=5))
+    print(fibonacci(number=10))
+
+
+if __name__ == "__main__":
+    main()
