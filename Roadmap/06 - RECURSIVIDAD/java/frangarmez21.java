b82a55d07192b292f8ef69e606c2098c2b2bedf6
frangarmez21 <littletimetolearn@gmail.com>
2024-02-12 10:20:58 +0100

    #06 - java -solution to the factorial exercise

--- Roadmap/06 - RECURSIVIDAD/java/frangarmez21.java
+++ Roadmap/06 - RECURSIVIDAD/java/frangarmez21.java
@@ -1,4 +1,3 @@
-
 public class frangarmez21 {
 
     public static void main(String[] args) {
@@ -9,8 +8,6 @@ public class frangarmez21 {
          * Entiende el concepto de recursividad creando una función recursiva que imprima
          * números del 100 al 0.
          */
-
-        System.out.println();
         System.out.println("##Creando función recursiva que imprime números del 100 al 0.##");
         System.out.println();
 
@@ -22,18 +19,27 @@ public class frangarmez21 {
         System.out.println();
         System.out.println("##Dificultad Extra##");
         System.out.println();
-//
-//        /* * DIFICULTAD EXTRA (opcional):
-//         * Utiliza el concepto de recursividad para:
-//         * - Calcular el factorial de un número concreto (la función recibe ese número).
-//         * - Calcular el valor de un elemento concreto (según su posición) en la
-//         *   sucesión de Fibonacci (la función recibe la posición).
-//         */
-//
-//        System.out.println("Calcular el factorial de un número concreto");
-//        System.out.println();
-//
-//
+
+        /* * DIFICULTAD EXTRA (opcional):
+         * Utiliza el concepto de recursividad para:
+         * - Calcular el factorial de un número concreto (la función recibe ese número).
+         * - Calcular el valor de un elemento concreto (según su posición) en la
+         *   sucesión de Fibonacci (la función recibe la posición).
+         */
+
+        System.out.println("Calcular el factorial de un número concreto");
+        System.out.println();
+
+        int number = 4;
+        System.out.println("Llamamos al método calcular factorial con el número: " + number);
+        System.out.println();
+        int factorial = factorialCalculation(number, number);
+        if ((factorial >= 0)) {
+            System.out.println("El resultado es: " + factorial);
+        } else {
+            System.out.println("No se ha podido calcular, debe ser un numero natural");
+        }
+
 //        System.out.println();
 //        System.out.println("Calcular el valor de un elemento concreto en la sucesión de Fibonacci");
 //        System.out.println();
@@ -45,6 +51,23 @@ public class frangarmez21 {
         System.out.println(initialNumber);
         if (initialNumber > 0) {
             recursiveCounting(initialNumber - 1);
+        }
+    }
+
+    private static int factorialCalculation(int number, int initMultiplier) {
+        int multiplier = initMultiplier - 1;
+        int result = number * multiplier;
+
+        if (multiplier == 0) {
+            result = 1;
+            return result;
+        } else if (multiplier == 1) {
+            return result;
+        } else if (multiplier > 1) {
+            return factorialCalculation(result, multiplier);
+        } else {
+            result = -1;
+            return result;
         }
     }
 }
