ab2ffc7d6107fa8a546f60d1a704865a962025e3
Warclimb <84227345+Warclimb@users.noreply.github.com>
2024-02-12 14:48:46 +0100

    Primer ejercicio recursividad

---
+++ Roadmap/06 - RECURSIVIDAD/python/warclimb.py
@@ -0,0 +1,42 @@
+# #06 RECURSIVIDAD
+#### Dificultad: Difícil | Publicación: 05/02/24 | Corrección: 12/02/24
+
+## Ejercicio
+
+
+"""
+* EJERCICIO:
+ * Entiende el concepto de recursividad creando una función recursiva que imprima
+ * números del 100 al 0.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Utiliza el concepto de recursividad para:
+ * - Calcular el factorial de un número concreto (la función recibe ese número).
+ * - Calcular el valor de un elemento concreto (según su posición) en la 
+ *   sucesión de Fibonacci (la función recibe la posición).
+"""
+# contar de 100 a 0
+def recursiva():
+    for num in range(100, -1, -1):
+        print(num)
+
+
+# Ejercicio extra
+# Calcular el factorial de un número concreto (la función recibe ese número)
+def factorial(num_factorial):
+    pass
+
+# Calcular el valor de un elemento concreto (según su posición) en la sucesión de Fibonacci.
+def fibonacci(num_fibonacci):
+    pass
+
+if __name__ == "__main__":
+    # Llamamos a la funcion del ejercicio 1
+    recursiva()
+    factorial(58)
+
+    
+    
+
+    
+
