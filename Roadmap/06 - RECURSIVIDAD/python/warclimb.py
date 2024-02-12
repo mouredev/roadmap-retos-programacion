a1ccaaf296965cacc6f3cd89b3204081fee76afc
Warclimb <84227345+Warclimb@users.noreply.github.com>
2024-02-12 18:02:38 +0100

    Update warclimb.py

--- Roadmap/06 - RECURSIVIDAD/python/warclimb.py
+++ Roadmap/06 - RECURSIVIDAD/python/warclimb.py
@@ -24,19 +24,22 @@ def recursiva():
 # Ejercicio extra
 # Calcular el factorial de un número concreto (la función recibe ese número)
 def factorial(num_factorial):
+    """Los factoriales son funciones matemáticas.
+    Multiplican un número por todos los números que lo preceden.
+    La fórmula del factorial puede expresarse como: = n ( n − 1 )
+    """
     pass
 
 # Calcular el valor de un elemento concreto (según su posición) en la sucesión de Fibonacci.
 def fibonacci(num_fibonacci):
+    """La secuencia de sucesión de Fibonacci tiene la fórmula
+    Fn = Fn-1 + Fn-2 .
+    En otras palabras, el siguiente número es una suma de los dos anteriores.
+    Los dos primeros números son 1 , luego 2(1+1) , luego 3(1+2) , 5(2+3) y así sucesivamente: 1, 1, 2, 3, 5, 8, 13, 21...
+    """
     pass
 
 if __name__ == "__main__":
     # Llamamos a la funcion del ejercicio 1
     recursiva()
     factorial(58)
-
-    
-    
-
-    
-
