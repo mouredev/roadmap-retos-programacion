dd56eefbc80834033de14967b9fd0dd10585b3e8
alberba <albertsalomv@gmail.com>
2024-02-12 20:14:40 +0100

    Reto #3 - Python

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/alberba.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/alberba.py
@@ -1,4 +1,7 @@
 
+import time
+
+
 lista: list = ["Jose", "Juan", "Miguel"]
 print(lista)
 
@@ -21,18 +24,57 @@ print(lista)
 # EXTRA
 
 def agenda():
-    lista = list
+
+    def insertar_contacto():
+        print("Introduce el teléfono del contacto")
+        telefono = input()
+        if telefono.isdigit() and len(telefono) <= 11:
+            lista[nombre] = telefono
+        else:
+            print("Introduce un número de teléfono de cómo máximo 11 dígitos")
+
+    lista = dict()
 
     while True:
         print("¿Que operación quieres hacer?")
-        print("1. Nuevo Contacto\t2. Actualizar contacto\n3. Buscar contacto\t4. Eliminar contacto")
+        print("1. Nuevo Contacto\t2. Actualizar contacto\n3. Buscar contacto\t4. Eliminar contacto\n5. Salir de la agenda")
 
         opcion = int(input())
 
-        if opcion == 1:
+        match opcion:
 
+            case 1:
+                print("Introduce el nombre del contacto")
+                nombre = input()
+                insertar_contacto()
+                
+            case 2:
+                print("Introduce el nombre del contacto que quieres actualizar")
+                nombre = input()
+                if nombre in lista:
+                    insertar_contacto()
+                else:
+                    print("El nombre del contacto no figura en la lista")
+                
+            case 3:
+                print("Introduce el nombre del contacto que desee buscar: ")
+                nombre = input()
+                print(lista[nombre]) if nombre in lista else print("No se encuentra el contacto")
+            case 4:
+                print("Introduce el nombre del contacto que quieres eliminar")
+                nombre = input()
+                if nombre in lista:
+                    lista.pop(nombre)
+                else:
+                    print("El nombre del contacto no figura en la lista")
+            case 5:
+                print("Saliento de la agenda")
+                break
+            case _:
+                print("Opción no válida")
 
-        
+        # Tiempo de espera para poder leer el mensaje
+        time.sleep(1.5)
 
 
 agenda()
