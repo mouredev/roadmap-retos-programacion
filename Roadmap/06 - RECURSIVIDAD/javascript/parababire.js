b9f2cce142aa95bbd5babef46cff9a667e7b7773
Parababire <angelenarvaezm@gmail.com>
2024-02-13 08:24:00 -0400

    #06 - Javascript

--- Roadmap/06 - RECURSIVIDAD/javascript/parababire.js
+++ Roadmap/06 - RECURSIVIDAD/javascript/parababire.js
@@ -1,4 +1,4 @@
-
+//Vuenta regresiva por recursión
 
 let cuentaRegresiva = numero => {
   //Base case: donde finaliza la función de llamarse así misma.
@@ -8,21 +8,11 @@ let cuentaRegresiva = numero => {
   console.log(numero);
   return cuentaRegresiva(numero - 1);
 }
-//console.log(cuentaRegresiva(100));
-
-let parImpar = numero => {
-  if (numero === 0) {
-    return "Par";
-  } else if (numero === 1) {
-    return "Impar";
-  } else {
-    return parImpar(numero - 2)
-  }
-}
-//console.log(parImpar(7));
+console.log(cuentaRegresiva(100));
 
 //Extra
 
+/*Número factorial*/
 let factorial = n => {
   if (n < 0) {
     return -1;
@@ -34,3 +24,13 @@ let factorial = n => {
 }
 
 console.log(factorial(5));
+
+/*Fibonachi*/
+let fibNum = n => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fibNum(n - 1) + fibNum(n - 2);
+  }
+}
+console.log(fibNum(8));
