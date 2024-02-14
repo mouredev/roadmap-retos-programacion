114ca41df1f2c0f9c75162caed06b9d83c52d21a
mauricioobgo <mauricioobgo@gmail.com>
2024-02-14 14:53:08 -0500

    #[06] - [PYTHON]

--- Roadmap/06 - RECURSIVIDAD/python/mauricioobgo.py
+++ Roadmap/06 - RECURSIVIDAD/python/mauricioobgo.py
@@ -22,21 +22,12 @@ def factorial(number: int, fact=1) -> int:
 #print(factorial(3))
 
 """
-
+Nth Fibonacci number
 """
-""" def fibonacci(n_fib :  int, fib1=0, fib2=1):
+def fibonacci(n_fib :  int, fib1=0, fib2=1):
     if n_fib == 0:
         return fib1
     elif n_fib == 1:
         return fib2
     else:
-        return n_fibonacci(n_fib - 1, fib2, fib1 + fib2) """
-
-#print(n_fibonacci(4))
-def n_fibonacci(n_fib :  int, fib1=0):
-    if n_fib == 0:
-        return fib1
-    elif n_fib == 1:
-        return fib1 + 1
-    else:
-        return fib1 + n_fibonacci(n_fib - 1,  fib1 + fib1 - 1)
+        return n_fibonacci(n_fib - 1, fib2, fib1 + fib2)
