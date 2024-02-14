33acaa28d13c8241413424aaddaca19a6cfa30fc
oriaj3 <oriaj3@gmail.com>
2024-02-14 15:48:16 +0100

    Solución #07 - Python

--- Roadmap/07 - PILAS Y COLAS/python/oriaj3.py
+++ Roadmap/07 - PILAS Y COLAS/python/oriaj3.py
@@ -13,26 +13,12 @@ el primer elemento que se introduce es el primero en salir. Las colas se utiliza
 como la gestión de tareas, la gestión de recursos compartidos, la impresión de documentos, etc.
 """	
 
-"""	
- * EJERCICIO:	
- * Entiende el concepto de recursividad creando una función recursiva que imprima	
- * números del 100 al 0.	
-"""	
+# EJERCICIO BÁSICO:
 """
 EJERCICIO:
 Implementa los mecanismos de introducción y recuperación de elementos propios de las
 pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
 o lista (dependiendo de las posibilidades de tu lenguaje).
-DIFICULTAD EXTRA (opcional):
-- Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
-  de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
-  que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
-  Las palabras "adelante", "atras" desencadenan esta acción, el resto se interpreta como
-  el nombre de una nueva web.
-- Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
-  impresora compartida que recibe documentos y los imprime cuando así se le indica.
-  La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
-  interpretan como nombres de documentos.
 """
 
 # Implementación de pilas sin usar clases con listas 
@@ -71,7 +57,7 @@ for i in range(5):
 
 l_mostrar_pila()
 
-#Implementación de colas sin clases con array
+#Implementación de colas sin clases con listas
 cola = []
 
 def l_encolar(numero: int):
@@ -105,3 +91,100 @@ for i in range(5):
 
 mostrar_cola()
 
+
+# EJERCICIO EXTRA NAVEGADOR WEB
+"""
+- Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+Las palabras "adelante", "atras" desencadenan esta acción, el resto se interpreta como
+el nombre de una nueva web.
+"""
+
+#Al ser una pila, cuando se añade un elemento se añade al final, y cuando se saca se saca del final
+def w_new(elemento: str)-> str:
+    global lista_web
+    lista_web.append(elemento)
+    return str(elemento)
+    
+def w_atras()-> str:
+    global lista_web
+    aux = lista_web.pop()
+    if len(lista_web) > 0:
+        lista_web.insert(0, aux)
+        #Devuelve el último elemento de la lista
+        return lista_web[-1]
+    else:
+        return "No hay más webs"
+
+def w_siguiente()-> str:
+    global lista_web
+    if len(lista_web) > 0:
+        #copia el primer elemento de la lista
+        aux = lista_web.pop(0)
+        lista_web.append(aux)
+        actual = lista_web[-1]
+        return actual
+    else:
+        return "No hay más webs" 
+
+lista_web = ["www.google.com", "www.youtube.com", "www.facebook.com"]
+#Instrucciones
+print("Introduce una web o 'salir' para terminar, 'atras' para retroceder y 'adelante' para avanzar en el historial de navegación.")
+
+while True:
+    print(f"Historial de navegación:")
+    for url in lista_web:
+        print(url, end=" - ")
+    
+    print()
+    print()
+
+    web = input("Introduce una web: ")
+    if (web == "atras"):
+        print(f"Has navegado a {w_atras()}")
+    elif (web == "adelante"):
+        print(f"Has navegado a {w_siguiente()}")
+    elif (web.startswith("www.")):
+        print(f"Has navegado a {w_new(web)}")
+    elif (web == "salir"):
+        break
+    else:
+        print("No es una web válida")
+
+
+# EJERCICIO EXTRA IMPRESORA
+"""
+- Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+impresora compartida que recibe documentos y los imprime cuando así se le indica.
+La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+interpretan como nombres de documentos.
+"""
+
+#Al ser una cola, cuando se añade un elemento se añade al final, y cuando se saca se saca del principio
+lista_impresion = ["Documento1", "Documento2", "Documento3", "Documento4", "Documento5"]
+
+def imprimir()-> str:
+    print(f"Imprimiendo {lista_impresion.pop(0)}")
+
+def nuevo_documento(nombre: str)-> str:
+    lista_impresion.append(nombre)
+    return nombre
+
+print("Introduce un documento o 'salir' para terminar, 'imprimir' para imprimir un documento.")
+
+while True:
+    print(f"Documentos en la cola de impresión:")
+    for doc in lista_impresion:
+        print(doc, end=" - ")
+    
+    print()
+    print()
+
+    doc = input("Introduce un documento: ")
+    if (doc == "imprimir"):
+        imprimir()
+    elif (doc == "salir"):
+        break
+    else:
+        print(f"Has añadido el documento {nuevo_documento(doc)}")
