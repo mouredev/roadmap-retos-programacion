efeebe0eba685f824faa480316e6091196b41159
CesarCarmona <clcg852@gmail.com>
2024-02-16 00:06:32 -0600

    #07 - Python

--- Roadmap/07 - PILAS Y COLAS/python/CesarCarmona30.py
+++ Roadmap/07 - PILAS Y COLAS/python/CesarCarmona30.py
@@ -29,7 +29,7 @@ class Queue:
     try:
       element = self.data.pop(0)
     except IndexError as e:
-      print(f'Error: {type(e).__name__}, empty stack')
+      print(f'Error: {type(e).__name__}, empty queue')
       return
     print(f'Queue element out: {element}')
 
