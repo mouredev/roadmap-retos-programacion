bbf0c9436dfc7f80215b63af982f9e1b3074f49e
frangarmez21 <littletimetolearn@gmail.com>
2024-02-17 20:10:54 +0100

    #07 - Java -added file

---
+++ Roadmap/07 - PILAS Y COLAS/java/frangarmez21.java
@@ -0,0 +1,54 @@
+public class frangarmez21 {
+
+    public static void main(String[] args) {
+
+        System.out.println("##Ejercicios de pilas y colas##");
+        /*
+         * EJERCICIO:
+         * Implementa los mecanismos de introducción y recuperación de elementos propios de las
+         * pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
+         * o lista (dependiendo de las posibilidades de tu lenguaje).
+         */
+        System.out.println("##Implementando los mecanismos de introducción y recuperación de elementos.##");
+        System.out.println();
+        System.out.println("##Las pilas (stacks - LIFO)##");
+        System.out.println();
+
+
+
+        System.out.println("##Las colas (queue - FIFO)##");
+        System.out.println();
+
+
+
+        System.out.println();
+        System.out.println("##Dificultad Extra##");
+        System.out.println();
+
+        /*
+         * DIFICULTAD EXTRA (opcional):
+         * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+         *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+         *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+         *   Las palabras "adelante", "atras" desencadenan esta acción, el resto se interpreta como
+         *   el nombre de una nueva web.
+         *
+         * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+         *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+         *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+         *   interpretan como nombres de documentos.
+         */
+
+        System.out.println("Simula el mecanismo adelante/atrás de un navegador web:");
+        System.out.println();
+
+
+
+        System.out.println();
+        System.out.println("Simula el mecanismo de una impresora:");
+        System.out.println();
+
+
+
+    }
+}
