3183201859a397b9820be47808ed301dedcdc921
CesarCarmona <clcg852@gmail.com>
2024-02-17 14:17:38 -0600

    #07 - Python

--- Roadmap/07 - PILAS Y COLAS/python/CesarCarmona30.py
+++ Roadmap/07 - PILAS Y COLAS/python/CesarCarmona30.py
@@ -57,3 +57,88 @@ fruits.showData()
 fruits.dequeue()
 fruits.dequeue()
 fruits.showData()
+
+class Browser(Stack):
+  
+  def __init__(self):
+    super().__init__()
+    self.currentPage = None
+    self.index = None
+
+  def addPage(self, page):
+    self.push(page)
+    self.index = len(self.data) - 1
+    self.currentPage = self.data[self.index]
+
+  def goBack(self):
+    try:
+      self.index -= 1
+      self.currentPage = self.data[self.index]
+      return self.currentPage
+    except Exception:
+      print('Sin páginas en el historial')
+      return '-'
+
+  def goForward(self):
+    try:
+      self.index += 1
+      self.currentPage = self.data[self.index]
+      return self.currentPage
+    except Exception:
+      print('Sin referencia de página adelante')
+      return '-'
+
+def history(browser):
+  while True:
+    option = input('>_')
+    page = ''
+    match option.lower():
+      case 'adelante':
+        page = browser.goForward()
+        print(f'Página actual: {page}')
+      case 'atras':
+        page = browser.goBack()
+        print(f'Página actual: {page}')
+      case 'salir':
+        break
+      case _:
+        new_page = option
+        browser.addPage(new_page)
+        print(f'Nueva página: {new_page}')
+
+myBrowser = Browser()
+history(myBrowser)
+
+class Printer(Queue):
+  def __init__(self):
+    super().__init__()
+  
+  def printDoc(self):
+    printout = self.dequeue()
+    return printout
+
+  def addDoc(self, doc):
+    self.enqueue(doc)
+
+  def dequeue(self):
+    try:
+      element = self.data.pop(0)
+      return element
+    except Exception:
+      return 'No hay elemento'
+    
+def printQueue(printer):
+  while True:
+    option = input('doc name: ')
+    match option.lower():
+      case 'imprimir':
+        docPrinted = printer.printDoc()
+        print(f'Documento impreso: {docPrinted}')
+      case 'salir':
+        break
+      case _:
+        new_doc = option
+        printer.addDoc(new_doc)
+
+myPrinter = Printer()
+printQueue(myPrinter)
