4f3f08a247d5cea06af858600647181225d1cf1a
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-21 00:13:38 -0400

    Iniciando el ejercicio de la semana 9 (Ejercicio 08)

---
+++ Roadmap/08 - CLASES/javascript/Pancratzia.js
@@ -0,0 +1,15 @@
+/*
+ * EJERCICIO:
+ * Explora el concepto de clase y crea un ejemplo que implemente un inicializador,
+ * atributos y una función que los imprima (teniendo en cuenta las posibilidades
+ * de tu lenguaje).
+ * Una vez implementada, créala, establece sus parámetros, modifícalos e imprímelos
+ * utilizando su función.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas
+ * en el ejercicio número 7 de la ruta de estudio)
+ * - Deben poder inicializarse y disponer de operaciones para añadir, eliminar,
+ *   retornar el número de elementos e imprimir todo su contenido.
+ * 
+ */
