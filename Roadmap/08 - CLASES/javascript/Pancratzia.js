b1fe2f14fce6b0e68bbcce3b608050207ac95b1c
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-02-21 00:22:52 -0400

    Creando la primera parte del ejercicio

--- Roadmap/08 - CLASES/javascript/Pancratzia.js
+++ Roadmap/08 - CLASES/javascript/Pancratzia.js
@@ -11,5 +11,50 @@
  * en el ejercicio número 7 de la ruta de estudio)
  * - Deben poder inicializarse y disponer de operaciones para añadir, eliminar,
  *   retornar el número de elementos e imprimir todo su contenido.
- * 
+ *
  */
+
+class Perro {
+  constructor(nombre = "Tiffany", raza = "Poodle", edad = 8) {
+    this._nombre = nombre;
+    this._raza = raza;
+    this._edad = edad;
+  }
+
+  get nombre() {
+    return this._nombre;
+  }
+
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+
+  get raza() {
+    return this._raza;
+  }
+
+  set raza(raza) {
+    this._raza = raza;
+  }
+
+  get edad() {
+    return this._edad;
+  }
+
+  set edad(edad) {
+    this._edad = edad;
+  }
+
+  toString() {
+    return `Nombre: ${this._nombre}
+    Raza: ${this._raza}
+    Edad: ${this._edad}`;
+  }
+}
+
+const perro = new Perro();
+console.log(perro.toString());
+
+console.log("Pero espera... ¡ES EL CUMPLEAÑOS DE TIFFANY!")
+perro.edad = 9;
+console.log(perro.toString());
