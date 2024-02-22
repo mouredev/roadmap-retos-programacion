9f2f34f80d9c90bea467f45e9d53b2027ff1c343
Dealber <160842351+Dealber@users.noreply.github.com>
2024-02-22 12:18:08 +0100

    Update dealber.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dealber.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dealber.py
@@ -1,1 +1,16 @@
+#Esto es un comentario
 
+"""
+ Esto tambien 
+ es un comentario 
+"""
+
+my_variable = 5
+
+var_int = 5
+var_float = 5.3
+var_bool = True
+var_bool = False
+var_string = "cadena de texto"
+
+print ("¡Hola, Python!")
