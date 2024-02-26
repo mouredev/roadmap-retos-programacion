30aaecade44e8abfd61d0ef31703035ac0194175
Marta Tirador <martatiradorgutierrez@gmail.com>
2024-02-26 16:44:19 +0100

    # 05 -Java

--- Roadmap/05 - VALOR Y REFERENCIA/java/mtirador.java
+++ Roadmap/05 - VALOR Y REFERENCIA/java/mtirador.java
@@ -3,17 +3,6 @@ import java.util.Arrays;
 public class mtirador {
     public static void main(String[] args) {
         
-        /*
- 
- * DIFICULTAD EXTRA (opcional):
- * Crea dos programas que reciban dos parámetros (cada uno) definidos como variables anteriormente.
- * - Cada programa recibe, en un caso, dos parámetros por valor, y en otro caso, por referencia.
- *   Estos parámetros los intercambia entre ellos en su interior, los retorna, y su retorno
- *   se asigna a dos variables diferentes a las originales. A continuación, imprime el valor de las
- *   variables originales y las nuevas, comprobando que se ha invertido su valor en las segundas.
- *   Comprueba también que se ha conservado el valor original en las primeras.
- */
-
 
  /*ASIGNACIÓN POR VALOR */
 
