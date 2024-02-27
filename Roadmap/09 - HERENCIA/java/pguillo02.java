ef439eaa341d49170d1c1b0bb2a67c1ecb1092e9
Pablo Guilló <pabloguilloj@gmail.com>
2024-02-27 11:10:51 +0100

    Definición de la clase padre

---
+++ Roadmap/09 - HERENCIA/java/pguillo02.java
@@ -0,0 +1,15 @@
+class Animal{
+    private final String name;
+    private final int peso;
+    private final int tamaño;
+
+    public Animal(String name, int peso, int tamaño){
+        this.name = name;
+        this.tamaño = tamaño;
+        this.peso = peso;
+    }
+
+    public void sound(){}
+
+}
+
