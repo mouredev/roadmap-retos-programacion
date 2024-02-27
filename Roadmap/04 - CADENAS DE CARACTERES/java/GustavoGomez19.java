202442bbd91371e54585b238069d01b28169e088
GustavoGomez19 <gagomezq1992@gmail.com>
2024-02-26 20:13:03 -0500

    04 - Java Avance, reto aún no concluido

---
+++ Roadmap/04 - CADENAS DE CARACTERES/java/GustavoGomez19.java
@@ -0,0 +1,25 @@
+/**
+ * GustavoGomez19
+ */
+public class GustavoGomez19 {
+
+    // Ejemplos de las operaciones con cadenas de carácteres en Java
+
+    // Creación de una cadena de forma literal
+    String name = "Gustavo";
+    // Creacupin de una cadena por medio de instancia
+    String lastName = new String("Gomez");
+    // Concatenacion de Strings
+    String fullName = name + lastName;
+    // Comparción si son el mismo objeto
+    boolean mismoObj = name == lastName;
+
+    // Métodos de la clase String
+    int largo = name.length(); // Devuelve el número de caráteres que posee el String
+    boolean esIgaul = name.equals(lastName); // Compara el valor del String
+    boolean esIgual2 = name.equalsIgnoreCase(lastName); // Compara el valor del String ignorando mayúsculas y minúsculas
+    int valor = name.compareTo(lastName); //Devuelve un 0 si las cadenas son iguales, un # (-) si 'name' tiene menos carácteres y un # (+) si 'name' tiene más carácteres
+    String nuevoValor = 
+
+    
+}
