0c8f974563f387c9e4653816743d03353fa3df0e
Parababire <angelenarvaezm@gmail.com>
2024-02-29 12:34:15 -0400

    #09 - Javascript

--- Roadmap/09 - HERENCIA/javascript/parababire.js
+++ Roadmap/09 - HERENCIA/javascript/parababire.js
@@ -1,29 +1,71 @@
 class Animal {
-  constructor(color, nombre) {
+  constructor(raza, color, nombre) {
+    this.raza = raza;
     this.color = color;
     this.nombre = nombre;
   }
+  sonido() {
+    return `${this.nombre} es un ${this.raza} de color ${this.color}`;
+  }
 }
 class Perro extends Animal {
-  constructor(color, nombre, sonido) {
-    super(color, nombre);
-    this.sonido = sonido;
+  constructor(raza, color, nombre) {
+    super(raza, color, nombre);
   }
   oir() {
-    console.log(this.sonido);
+    return `${this.sonido()} hace Woof Woof.`;
   }
 }
 class Gato extends Animal {
-  constructor(color, nombre, sonido) {
-    super(color, nombre);
-    this.sonido = sonido;
+  constructor(raza, color, nombre) {
+    super(raza, color, nombre);
   }
   oir() {
-    console.log(this.sonido);
+    return `${this.sonido()} hace Meau.`;
   }
 }
 
-let perro = new Perro("blanco", "FeFe", "Wof Wof");
-let gato = new Gato("negro", "pesadilla", "Meau");
-perro.oir();
-gato.oir();
+let perro = new Perro("Poodle", "blanco", "FeFe");
+let gato = new Gato("Abisinio", "negro", "Pesadilla");
+console.log(perro.oir());
+console.log(gato.oir());
+
+/*Extra*/
+
+class Empleado {
+  constructor(id, nombre) {
+    this.id = id;
+    this.nombre = nombre;
+  }
+  informacion() {
+    return `ID: ${this.id}, Nombre: ${this.nombre}`;
+  }
+}
+class Gerente extends Empleado {
+  constructor(id, nombre) {
+    super(id, nombre);
+    this.nomina = [];
+  }
+  contratar(empleado) {
+    this.nomina.push(empleado);
+  }
+  nomina() {
+    return `El cargo de ${this.nombre} maneja una nómina de ${this.nomina.length} empleados`
+  }
+}
+class GerenteProyecto extends Empleado {
+  constructor(id, nombre, cargo, area) {
+    super(id, nombre, cargo);
+    this.area = area;
+  }
+  mostrar() {
+    return `${this.identificarse()} de ${this.area}.`;
+  }
+  nomina() {
+    return `El cargo de ${this.cargo} de ${this.area} maneja una nómina de 5 empleados`
+  }
+}
+const gerenteVentas = new Gerente(12075, "Ángel");
+console.log(gerenteVentas.informacion());
+gerenteVentas.contratar("empleado1");
+gerenteVentas.nomina();
