f482bd95ae2de7488ac34c9d643e93bf445aca4c
Parababire <angelenarvaezm@gmail.com>
2024-03-02 09:19:35 -0400

    #09 - Javascript

--- Roadmap/09 - HERENCIA/javascript/parababire.js
+++ Roadmap/09 - HERENCIA/javascript/parababire.js
@@ -38,34 +38,89 @@ class Empleado {
     this.nombre = nombre;
   }
   informacion() {
-    return `ID: ${this.id}, Nombre: ${this.nombre}`;
+    return `Mi nombre es: ${this.nombre}, número de identificación ${this.id}.`;
   }
 }
 class Gerente extends Empleado {
-  constructor(id, nombre) {
+  constructor(id, nombre, cargo) {
     super(id, nombre);
+    this.cargo = cargo;
     this.nomina = [];
+  }
+  presentacion() {
+    return `${this.informacion()} Manejo el área de ${this.cargo}.`
   }
   contratar(empleado) {
     this.nomina.push(empleado);
   }
-  nomina() {
-    return `El cargo de ${this.nombre} maneja una nómina de ${this.nomina.length} empleados`
+  empleados() {
+    if (this.nomina.length === 0) {
+      return `${this.nombre} aún no ha contratado empleados.`;
+    } else if (this.nomina.length === 1) {
+      return `${this.nombre} maneja una nómina de ${this.nomina.length} empleado.`;
+    } else {
+      return `${this.nombre} maneja una nómina de ${this.nomina.length} empleados.`;
+    }
   }
 }
 class GerenteProyecto extends Empleado {
-  constructor(id, nombre, cargo, area) {
-    super(id, nombre, cargo);
-    this.area = area;
+  constructor(id, nombre, cargo) {
+    super(id, nombre);
+    this.cargo = cargo;
+    this.nomina = [];
+    this.proyecto = [];
   }
-  mostrar() {
-    return `${this.identificarse()} de ${this.area}.`;
+  presentacion() {
+    return `${this.informacion()} Manejo el área de ${this.cargo}.`
   }
-  nomina() {
-    return `El cargo de ${this.cargo} de ${this.area} maneja una nómina de 5 empleados`
+  contratar(empleado) {
+    this.nomina.push(empleado);
+  }
+  empleados() {
+    if (this.nomina.length === 0) {
+      return `La ${this.cargo} a cargo de ${this.nombre} aún no ha contratado empleados.`;
+    } else if (this.nomina.length === 1) {
+      return `La ${this.cargo} a cargo de ${this.nombre} maneja una nómina de ${this.nomina.length} empleado.`;
+    } else {
+      return `La ${this.cargo} a cargo de ${this.nombre} maneja una nómina de ${this.nomina.length} empleados.`;
+    }
+  }
+  desarrollos(proyecto) {
+    return this.proyecto.push(proyecto);
+  }
+  planificacion() {
+    if (this.proyecto.length === 0) {
+      return `La ${this.cargo} no está desaarrollando proyectos actualmente.`;
+    } else if (this.proyecto.length === 1) {
+      return `La ${this.cargo} desarrolla actualmente ${this.proyecto.length} proyecto.`;
+    } else {
+      return `La ${this.cargo} desarrolla ${this.proyecto.length} proyectos simultaneamente.`;
+    }
   }
 }
-const gerenteVentas = new Gerente(12075, "Ángel");
-console.log(gerenteVentas.informacion());
+class Programador extends Empleado {
+  constructor(id, nombre, cargo, lenguaje) {
+    super(id, nombre);
+    this.cargo = cargo;
+    this.lenguaje = lenguaje;
+  }
+}
+const gerenteVentas = new Gerente(12075, "Ángel", "Gerencia");
+console.log(gerenteVentas.presentacion());
 gerenteVentas.contratar("empleado1");
-gerenteVentas.nomina();
+console.log(gerenteVentas.empleados());
+gerenteVentas.contratar("empleado2");
+console.log(gerenteVentas.empleados());
+
+const gerenteProyecto = new GerenteProyecto(31023, "Luis", "Gerencia de Proyecto");
+console.log(gerenteProyecto.presentacion());
+console.log(gerenteProyecto.empleados());
+gerenteProyecto.contratar("Empleado1");
+console.log(gerenteProyecto.empleados());
+gerenteProyecto.contratar("Empleado2");
+console.log(gerenteProyecto.empleados());
+console.log(gerenteProyecto.planificacion());
+gerenteProyecto.desarrollos("Proyecto1");
+console.log(gerenteProyecto.planificacion());
+gerenteProyecto.desarrollos("Proyecto2");
+console.log(gerenteProyecto.planificacion());
