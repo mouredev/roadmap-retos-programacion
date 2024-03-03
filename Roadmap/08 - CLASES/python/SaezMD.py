984d399e5107e0781bb27e883c7664d1ee0d820a
SaezMD <saulsaezrodriguez@gmail.com>
2024-03-03 20:12:46 +0100

    #08 - Python

---
+++ Roadmap/08 - CLASES/python/SaezMD.py
@@ -0,0 +1,107 @@
+#08 CLASES
+"""
+ * EJERCICIO:
+ * Explora el concepto de clase y crea un ejemplo que implemente un inicializador, atributos y una función que los imprima (teniendo en cuenta las posibilidades de tu lenguaje).
+ * Una vez implementada, créala, establece sus parámetros, modifícalos e imprímelos utilizando su función.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas en el ejercicio número 7 de la ruta de estudio)
+ * - Deben poder inicializarse y disponer de operaciones para añadir, eliminar, retornar el número de elementos e imprimir todo su contenido.
+"""
+
+#Attribute references
+class MyClass:
+    """A simple example class"""
+    i = 12345
+
+    def f(self):
+        return 'hello world'
+
+print(MyClass.i)
+
+#Class instantiation 
+class Complex:
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+x = Complex(3.0, -4.5)
+print(x.r)
+print(x.i)
+
+#another example:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("Saez", 36)
+
+print(p1.name)
+print(p1.age)
+p1.name = "MD"
+print(p1.name)
+
+#EXTRA
+
+class Queue:
+    def __init__(self):
+        self.listOfObjects = []
+
+    def addItem(self,toAdd):
+        self.listOfObjects.append(toAdd)
+    
+    def removeItem(self):
+        try:
+            return self.listOfObjects.pop(0)
+        except IndexError:
+            print("Empty queue!")
+    
+    def lenght(self):
+        return len(self.listOfObjects)
+    
+    def printAll(self):
+        return print(self.listOfObjects)
+
+d = Queue()
+d.addItem("cap01")
+print(d.listOfObjects)
+d.addItem("cap02")
+print(d.listOfObjects)
+d.removeItem()
+print(d.listOfObjects)
+print(d.lenght())
+d.printAll()
+
+class Stack:
+    def __init__(self):
+        self.listOfObjects = []
+
+    def addItem(self,toAdd):
+        self.listOfObjects.append(toAdd)
+    
+    def removeItem(self):
+        try:
+            self.listOfObjects.pop()
+        except IndexError:
+            print("Stack is empty!")
+    
+    def lenght(self):
+        return len(self.listOfObjects)
+    
+    def printAll(self):
+        return print(self.listOfObjects)
+
+s = Stack()
+s.addItem("show01")
+print(s.listOfObjects)
+s.addItem("show02")
+print(s.listOfObjects)
+s.removeItem()
+print(s.listOfObjects)
+print(s.lenght())
+s.printAll()
+
+        
+
+
