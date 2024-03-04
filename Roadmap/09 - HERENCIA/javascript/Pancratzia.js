be95aa341edf503ecf4d24ffd6d07193b484c861
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-04 15:42:09 -0400

    Comenzando el reto de Herencia

---
+++ Roadmap/09 - HERENCIA/javascript/Pancratzia.js
@@ -0,0 +1,13 @@
+/*
+ * EJERCICIO:
+ * Explora el concepto de herencia según tu lenguaje. Crea un ejemplo que
+ * implemente una superclase Animal y un par de subclases Perro y Gato,
+ * junto con una función que sirva para imprimir el sonido que emite cada Animal.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Implementa la jerarquía de una empresa de desarrollo formada por Empleados que
+ * pueden ser Gerentes, Gerentes de Proyectos o Programadores.
+ * Cada empleado tiene un identificador y un nombre.
+ * Dependiendo de su labor, tienen propiedades y funciones exclusivas de su
+ * actividad, y almacenan los empleados a su cargo.
+ */
