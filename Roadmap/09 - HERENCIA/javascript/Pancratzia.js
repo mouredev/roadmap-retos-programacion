ae09186b0bd7999ed5e0c3d8b37a960286b6ce4f
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-04 16:02:33 -0400

    Terminada la parte 1

--- Roadmap/09 - HERENCIA/javascript/Pancratzia.js
+++ Roadmap/09 - HERENCIA/javascript/Pancratzia.js
@@ -11,3 +11,50 @@
  * Dependiendo de su labor, tienen propiedades y funciones exclusivas de su
  * actividad, y almacenan los empleados a su cargo.
  */
+
+/*******************PARTE 1*******************/
+
+/* Definición de la superclase Animal */
+
+class Animal {
+  constructor(sound) {
+    this._sound = sound;
+  }
+
+  makeSound() {
+    console.log(this._sound);
+  }
+}
+
+/* Definición de las subclases Perro*/
+class Dog extends Animal {
+  constructor(breed) {
+    super("Guaugau");
+    this._breed = breed;
+  }
+
+  makeSound() {
+    console.log(`Hi! I'm a dog. My breed is ${this._breed}. And my sound is: `);
+    super.makeSound();
+  }
+}
+
+/* Definición de las subclases Gato */
+class Cat extends Animal {
+  constructor(favFood) {
+    super("Miau");
+    this._favFood = favFood;
+  }
+
+  makeSound() {
+    console.log(`Hi! I'm a cat. My favorite food is ${this._favFood}. And my sound is: `);
+    super.makeSound();
+  }
+}
+
+// Objetos de perro y gato
+const dog = new Dog("Boxer");
+dog.makeSound();
+
+const cat = new Cat("Tuna");
+cat.makeSound();
