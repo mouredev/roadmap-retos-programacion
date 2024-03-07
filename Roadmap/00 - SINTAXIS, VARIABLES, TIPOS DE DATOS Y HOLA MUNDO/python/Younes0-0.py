36707c52ec9ba815061c1efc58fd786db30db385
Younes0-0 <younesdemraoui@gmail.com>
2024-03-07 14:53:53 +0100

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Younes0-0.py
@@ -0,0 +1,43 @@
+# Documentación: https://docs.python.org/es/3/
+
+# Comentario de una línea
+
+"""
+Comentarios 
+de varias
+líneas
+"""
+
+'''
+Comentarios 
+de varias
+líneas
+'''
+import math 
+
+# Variables: no existe una palabra reservada para declarar una variable 
+string = "hola"
+
+
+# Constantes
+# No hay constantes en Python, pero sí hay forma de declarar una constante y esperar a que los demás respeten.
+CONSTANTE = math.pi
+
+# Integer ( int )
+entero = 1
+
+# Float ( float )
+flotante = 1.5
+
+# Boolean ( bool ) 
+verdadero = True
+falso = False
+
+# String ( str )
+cadena_doble = "Cadena de caracteres "
+cadena_simple = 'Cadena de caracteres'
+
+# None
+nada = None
+
+
