9fbd5a9babbaad0bb1fd90e3d23da0df7b040aa6
Parababire <angelenarvaezm@gmail.com>
2024-03-08 11:36:24 -0400

    #10 - Javascript

--- Roadmap/10 - EXCEPCIONES/javascript/parababire.js
+++ Roadmap/10 - EXCEPCIONES/javascript/parababire.js
@@ -1,14 +1,6 @@
-/*function UserException(message) {
-  this.message = message;
-  this.name = "UserException";
-}
+//Provocar error
 
-UserException.prototype.toString = function () {
-  return `${this.name}: "${this.message}"`
-}
-
-throw new UserException("Valor muy Alto");*/
-
+/*La declaración try...catch detecta el error(forzado) lo registra e imprime el mensage creado*/
 try {
   console.lg("Hello World");
 } catch (error) {
@@ -18,7 +10,23 @@ try {
 class Error_Personalizado extends Error {
   constructor(message) {
     super(message);
-    this.name = "Este es un error personalizado";
+    this.name = "NumberError";
   }
 }
 
+function evaluarNumero(num) {
+  try {
+    if (typeof num === "string") {
+      throw new Error_Personalizado("No es un número");
+    } else  if (typeof num === "number") {
+      console.log(num + num);
+    }
+  } catch (err) {
+    if (err instanceof Error_Personalizado) {
+      console.log("Dato ingresado no es un número: " + err.message);
+    } else {
+      throw err;
+    }
+  }
+}
+evaluarNumero("3");
