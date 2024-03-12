367f914f4ff8159675216739d3bab58c4e70f8d1
SaezMD <saulsaezrodriguez@gmail.com>
2024-03-12 15:44:38 +0100

    #11 - Python. Newfile with first part done

---
+++ Roadmap/11 - MANEJO DE FICHEROS/python/SaezMD.py
@@ -0,0 +1,40 @@
+#11 Manejo de ficheros
+"""
+ * IMPORTANTE: Sólo debes subir el fichero de código como parte del ejercicio.
+ *
+ * EJERCICIO:
+ * Desarrolla un programa capaz de crear un archivo que se llame como tu usuario de GitHub y tenga la extensión .txt.
+ * Añade varias líneas en ese fichero:
+ * - Tu nombre.
+ * - Edad.
+ * - Lenguaje de programación favorito.
+ * Imprime el contenido.
+ * Borra el fichero.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Desarrolla un programa de gestión de ventas que almacena sus datos en un 
+ * archivo .txt.
+ * - Cada producto se guarda en una línea del arhivo de la siguiente manera:
+ *   [nombre_producto], [cantidad_vendida], [precio].
+ * - Siguiendo ese formato, y mediante terminal, debe permitir añadir, consultar,
+ *   actualizar, eliminar productos y salir.
+ * - También debe poseer opciones para calcular la venta total y por producto.
+ * - La opción salir borra el .txt.
+ */
+"""
+import os
+abspath = os.path.abspath(__file__) #get the file directory
+dname = os.path.dirname(abspath)
+os.chdir(dname) #change to file directory
+
+nameOfFile = "SaezMD"
+extension = "." + "txt"
+myName = "Name: Write your name here: **SMM**"
+myAge = "Age: 12"
+myFavouriteLanguage = "Language: Java" 
+
+with open(nameOfFile + extension, 'w') as file:
+    file.write(myName + '\n' + myAge + '\n' + myFavouriteLanguage)
+
+
+
