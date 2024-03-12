30d585e981d98bc0430a27091b62be8fcb1035a2
Ahinar <ahinar@gmail.com>
2024-03-11 21:03:36 -0500

    solucion reto 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/ahinar.js
@@ -0,0 +1,39 @@
+/*
+ * -
+ *
+ *
+ * -
+ *
+ * 
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+//solución
+
+//-Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//- Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+// Este es comentario de una linea
+/*este
+es un comentario
+multilinea*/
+
+//-Crea una variable (y una constante si el lenguaje lo soporta).
+
+let var1 = 20; //variable let
+const var2 = "Constante"; //constante
+
+/*- Crea variables representando todos los tipos de datos primitivos
+del lenguaje (cadenas de texto, enteros, booleanos...).*/
+
+let cadena = "Cadena o string"; //cadena
+let numeroEntero = 4565; // entero
+const pi = 3.1416; // float
+const verdadero = true; //boleano
+
+//- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+console.log ("¡Hola, Javascript!");
