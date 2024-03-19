6f0666df22ddf6b570d5aaee33c741aac97fe69e
SaezMD <saulsaezrodriguez@gmail.com>
2024-03-19 10:44:42 +0100

    #11 - Python

--- Roadmap/11 - MANEJO DE FICHEROS/python/SaezMD.py
+++ Roadmap/11 - MANEJO DE FICHEROS/python/SaezMD.py
@@ -41,11 +41,98 @@ with open(nameOfFile + extension, 'r') as file:
     print(myFile)
 
 #delete file
-input("press any key to delete the file...")
+#input("press enter key to delete the file...")
 os.remove(nameOfFile + extension)
 
 
 #EXTRA
+
+file = open("shopFileRecords.txt",'w')
+file.write("nails" + ", " + "12.33" + ", " + "6\n")
+file.write("polish" + ", " + "22.55" + ", " + "10\n")
+file.close()
+
+file = open("shopFileRecords.txt",'r')
+fileData = file.read().split('\n')
+file.close()
+print(fileData)
+
+
+while True:
+    inputUser = input("Select an option [add, search, update, delete, exit]: ").lower()
+    if inputUser == "exit":
+        file.close()
+        os.remove("shopFileRecords.txt")
+        break
+    elif inputUser == "add":
+        inputAdd = input("New item: ").lower()
+
+        file = open("shopFileRecords.txt",'r')
+        fileData = file.read()
+
+        if inputAdd in fileData:
+            print("Item already exist!")
+
+        else: 
+            inputAddPrice = input("New price: ")
+            inputAddQty = input("New quantity: ")
+            file = open("shopFileRecords.txt",'a')
+            file.write( inputAdd + ", " + inputAddPrice + ", " + inputAddQty + "\n")
+   
+        file.close()
+
+    elif inputUser == "search":
+        with open("shopFileRecords.txt") as file:
+            for line in file:
+                print(line.rstrip())
+
+    elif inputUser == "delete":
+        inputDelete = input("Delete item: ").lower()
+
+        try: 
+            file = open("shopFileRecords.txt",'r')
+            lines = file.readlines()
+            file.close()
+            
+            with open("shopFileRecords.txt",'w') as file:
+                for line in lines:
+                    if line.find(inputDelete) != -1:  # find() returns -1 if no match is found
+                        pass
+                    else:
+                        file.write(line)
+        except:
+            print("Item not found!")
     
+    elif inputUser == "update":
+        inputUpdate = input("Update item: ").lower()
+
+        #Update1: delete line
+        try: 
+            file = open("shopFileRecords.txt",'r')
+            lines = file.readlines()
+            file.close()
+            
+            with open("shopFileRecords.txt",'w') as file:
+                for line in lines:
+                    if line.find(inputUpdate) != -1:  # find() returns -1 if no match is found
+                        pass
+                    else:
+                        file.write(line)
+        
+            #Update2: add line
+            inputAddPrice = input("New price: ")
+            inputAddQty = input("New quantity: ")
+            file = open("shopFileRecords.txt",'a')
+            file.write( inputUpdate + ", " + inputAddPrice + ", " + inputAddQty + "\n")
+    
+            file.close()
+        
+        except:
+            print("Item not found!")        
+
+
+
+
+
 
 
