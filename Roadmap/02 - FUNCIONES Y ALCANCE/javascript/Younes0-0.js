e2b78b4a3e75dbc2b1eb5903c0a2f6f6d9547e39
Younes0-0 <younesdemraoui@gmail.com>
2024-03-20 16:33:14 +0100

    #02 - JavaScript

--- Roadmap/02 - FUNCIONES Y ALCANCE/javascript/Younes0-0.js
+++ Roadmap/02 - FUNCIONES Y ALCANCE/javascript/Younes0-0.js
@@ -19,21 +19,82 @@
  *
  * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
  * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
-*/
+ */
 
-function extra(texto1,texto){
-    let contador = 0
-    for(let i = 1; i <= 100; i++){
-        if(i % 3 == 0 && i % 5 == 0){
-            console.log(texto1 + texto)
-        }else if(i % 3 == 0){
-            console.log(texto1)
-        }else if(i % 5 == 0){
-            console.log(texto)
-        }else{
-            console.log(i)
-            contador++
-        }
+// Funcion básica
+
+function saludar() {
+  console.log("Hola Mundo!!")
+}
+saludar()
+
+// Funcion con parámetros
+
+function saludar2(texto) {
+  console.log(`Bienvenido ${texto}`)
+}
+saludar2("Younes")
+
+// Funcion con retorno
+
+function saludar3(texto) {
+  return `Hola ${texto}`
+}
+console.log(saludar3("Younes"))
+
+// Funcion dentro de funcion
+
+function saludar4() {
+  function saludar5() {
+    console.log("Hola")
+  }
+  saludar5()
+}
+saludar4()
+
+// Funcion dentro de funcion con retorno
+
+function saludar6() {
+  function saludar7() {
+    return "Hola"
+  }
+  return saludar7()
+}
+console.log(saludar6())
+
+// Variable global
+
+let variableGlobal = "Variable global"
+function saludar8() {
+  console.log(variableGlobal)
+}
+saludar8()
+console.log(variableGlobal)
+
+// Variable local
+
+function saludar9() {
+  let variableLocal = "Variable local"
+  console.log(variableLocal)
+}
+saludar9()
+// console.log(variableLocal) -> Error de ejecución
+
+//DIFICULTAD EXTRA
+
+function extra(texto1, texto) {
+  let contador = 0
+  for (let i = 1 ; i <= 100 ; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      console.log(texto1 + texto)
+    } else if (i % 3 == 0) {
+      console.log(texto1)
+    } else if (i % 5 == 0) {
+      console.log(texto)
+    } else {
+      console.log(i)
+      contador++
     }
-    return `Los numeros impresos, que NO son texto, son: ${contador}`
+  }
+  return `Los numeros impresos, que NO son texto, son: ${contador}`
 }
