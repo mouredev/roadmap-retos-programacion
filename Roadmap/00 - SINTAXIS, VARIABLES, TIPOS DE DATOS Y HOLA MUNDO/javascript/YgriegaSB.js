32c4a170e527c393161fb7d89f006a7225e518d2
YgriegaSB <nicolas.apf@outlook.com>
2024-03-20 04:28:22 -0300

    primeros 5 ejercicios

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/YgriegaSB.js
@@ -0,0 +1,38 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+// https://262.ecma-international.org/#sec-intro
+
+/*
+    Comentario multiples lineas
+    1
+    2
+    3
+*/
+
+const myLenguage = "JavaScript";
+
+// Constante y variable
+const cantChange = 10;
+let canChange2 = 10;
+
+// Datos primitivos en JavaScript
+
+// Números (Number)
+let numero = 42;
+let decimal = 3.14;
+
+// Cadenas de texto (String)
+let cadena = "Hola, mundo!";
+let otraCadena = 'Esto es una cadena';
+
+// Booleanos (Boolean)
+let verdadero = true;
+let falso = false;
+
+// Valores nulos (Null) y sin definir (Undefined)
+let nulo = null;
+let indefinido = undefined;
+
+// Símbolos (Symbol) (nuevo en ECMAScript 2015)
+let simbolo = Symbol("descripción");
+
+console.log(`Hola ${myLenguage}`);
