d43d411159b17b3fc563023e102a350c91682637
Sergio.GI99 <Sergio.jks99@gmail.com>
2024-03-25 16:36:52 +0100

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/SergioGI99.js
@@ -0,0 +1,43 @@
+/*
+------------------------------------------------
+SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+------------------------------------------------
+¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+- Recuerda que todas las instrucciones de participación están en el
+  repositorio de GitHub.
+
+Lo primero... ¿Ya has elegido un lenguaje?
+- No todos son iguales, pero sus fundamentos suelen ser comunes.
+- Este primer reto te servirá para familiarizarte con la forma de participar
+  enviando tus propias soluciones.
+
+EJERCICIO:
+- Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado.
+- Representa las diferentes sintaxis que existen de crear comentarios
+  en el lenguaje (en una línea, varias...).
+- Crea una variable (y una constante si el lenguaje lo soporta).
+- Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+- Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+debemos comenzar por el principio.
+*/
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+var myString = "Esto es un string"
+
+const myConst = "Esto es una constante"
+
+var myInt = 8
+var myFloat = 3.14
+var myBool = true
+var myNull = null
+var myUndefined = undefined 
+var myBigInt = 123124235234123123124n
+var mySymbol = Symbol()
+
+console.log("¡Hola, JavaScript")
+
