3e73b9953a5a547dee123a28ed2f9986346ca3fa
Elliotwith1T <elliotscott1999@Gmail.com>
2024-03-27 18:13:25 +0000

    #00 C#

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/elliotwith1T.cs
@@ -0,0 +1,74 @@
+using System;
+/*EJERCICIO:
+*-Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ *-Crea una variable(y una constante si el lenguaje lo soporta).
+ *-Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ *-Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+*/
+
+//https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/
+
+#region Ejercicio 1
+
+//comentario en una linea
+
+#endregion
+
+#region ejercicio 2
+
+/* esto tambien es 
+ * un tipo de comentario
+ * usando varias lineas
+ */
+
+#endregion
+
+#region Ejercicio 3
+
+int numeroDeEjercicio = 3;
+const int cantidadDeEjercicios = 5;
+
+#endregion
+
+
+#region Ejercicio 4
+
+int numero = 7;
+long numeroLong = 2,200,000,000;
+float pi = 3.1415f;
+double masPi = 3.14159265359;
+char letra = "a";
+string palabra = "Esto es un String";
+bool booleano = false;
+
+
+#endregion
+
+#region Ejercicio 5
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Hola, C#");
+	
+	}
+	
+}
+
+
+
+#endregion
+
+
+
+
+
+
