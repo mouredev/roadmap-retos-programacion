c0850d4a653d685c6072296d8b4ccce6459a80ae
Laura Ortega <54899954+Pancratzia@users.noreply.github.com>
2024-03-26 20:26:18 -0400

    Primera parte del ejercicio de archivos finalizada

---
+++ Roadmap/11 - MANEJO DE FICHEROS/javascript/Pancratzia.js
@@ -0,0 +1,39 @@
+/*
+ * IMPORTANTE: Sólo debes subir el fichero de código como parte del ejercicio.
+ * 
+ * EJERCICIO:
+ * Desarrolla un programa capaz de crear un archivo que se llame como
+ * tu usuario de GitHub y tenga la extensión .txt.
+ * Añade varias líneas en ese fichero:
+ * - Tu nombre.
+ * - Edad.
+ * - Lenguaje de programación favorito.
+ * Imprime el contenido.
+ * Borra el fichero.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Desarrolla un programa de gestión de ventas que almacena sus datos en un 
+ * archivo .txt.
+ * - Cada producto se guarda en una línea del arhivo de la siguiente manera:
+ *   [nombre_producto], [cantidad_vendida], [precio].
+ * - Siguiendo ese formato, y mediante terminal, debe permitir añadir, consultar,
+ *   actualizar, eliminar productos y salir.
+ * - También debe poseer opciones para calcular la venta total y por producto.
+ * - La opción salir borra el .txt.
+ */
+
+/* USANDO ES MODULES DE NODE */
+
+import { writeFileSync, readFileSync, unlinkSync } from 'fs';
+
+const fileName = 'Pancratzia.txt'
+const content = ['Pancratzia', '24', 'JavaScript'];
+
+writeFileSync(fileName, content.join('\n'));
+
+const datos = readFileSync(fileName, 'utf-8');
+
+console.log(`Datos del archivo ${fileName}:`);
+console.log(datos);
+
+unlinkSync(fileName);
