9835cc0db384f76830d58f16ab2b611f8ab2cd6d
hozlucas28 <hozlucas28@gmail.com>
2024-03-28 16:57:51 -0300

    Solution 13 - Go

---
+++ Roadmap/13 - PRUEBAS UNITARIAS/go/hozlucas28.go
@@ -0,0 +1,20 @@
+package main
+
+/*
+	Tests...
+*/
+
+func add(a int, b int) int {
+	return a + b
+}
+
+/*
+	Additional challenge...
+*/
+
+var author map[string]interface{} = map[string]interface{}{
+	"name":                 "Lucas",
+	"age":                  "22",
+	"born_date":             "2002-02-20",
+	"programming_languages": []string{"TypeScript", "Python", "Go (Golang)"},
+}
