97a8b7f93e605d6f8dc9913f752f9b9c1f0db34b
Parababire <angelenarvaezm@gmail.com>
2024-04-01 12:41:50 -0400

    #11 - Javascript

--- Roadmap/11 - MANEJO DE FICHEROS/parababire.js
+++ Roadmap/11 - MANEJO DE FICHEROS/parababire.js
@@ -1,54 +1,107 @@
 //const prompt = require('prompt-sync')();
-//const fsPromises = require('fs').promises;
+const fsPromises = require('fs').promises;
+const { File } = require('buffer');
+const { log } = require('console');
 const fs = require('fs');
 const readline = require('readline');
 const userName = "parababire";
 const fileName = `${userName}.txt`;
-const fileContent = "Nombre: Ángel Narváez.\n Edad: 44 años.\n Lenguaje de programación: Javascript."
+const fileContent = "Nombre: Ángel Narváez.\nEdad: 44 años.\nLenguaje de programación: Javascript."
 
-/*const fileOps = async () => {
-  try {
-    await fsPromises.writeFile(fileName, fileContent);//Crear archivo.
-    const data = await fsPromises.readFile(fileName, 'utf8');//Leer Archivo.
-    console.log(data);
-    await fsPromises.appendFile(fileName, '\nHello World');//Agregar contenido.
-    const newData = await fsPromises.readFile(fileName, 'utf8');
-    console.log(newData);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-const finalArchivo = async () => {
-  try {
-    await fsPromises.unlink(fileName);//Borrar archivo.
-  } catch (error) {
-    console.error(error);
-  }
-}
-setTimeout(finalArchivo, 5000);*/
+fs.open(fileName, "w", (err, fd) => {
+  if (err) throw err;
+  fs.appendFile(fileName, fileContent, (err) => {
+    if (err) throw err;
+    fs.readFile(fileName, "utf8", (err, data) => {
+      if (err) throw err;
+      console.log(data);
+      const borrado = () => {
+        fs.unlink(fileName, err => {
+          if (err) throw err;
+          console.log("Archivo borrado");
+        });
+      } 
+      setTimeout(borrado, 5000);
+    })
+    console.log("Datos agregados");
+  });
+  console.log("archivo abierto");
+});
 
 //Extra
-let rl = readline.createInterface(process.stdin, process.stdout);
+/* let rl = readline.createInterface(process.stdin, process.stdout);
 
-const writeFile = async (name, quantity, price) => {
-  try {
-    await fsPromises.writeFile(fileName, `${name + ", " + quantity + ", " + price + "\n"}`);//Crear archivo.
-    /*const data = await fsPromises.readFile(fileName, 'utf8');//Leer Archivo.
-    console.log(data);*/
-  } catch (error) {
-    console.error(error);
-  }
+const leerArchivo = (archivo) => {
+  fs.readFile(archivo, 'utf8', (err, data) => {
+    if (err) throw err;
+    return data.split("\n");
+  });
 }
-const readFile = async () => {
-  try {
-    /*await fsPromises.writeFile(fileName, `${name + quantity + price}`);//Crear archivo.*/
-    const data = await fsPromises.readFile(fileName, 'utf8');//Leer Archivo.
+
+const crearProducto = () => {
+  rl.question("Nombre del producto: ", nombre => {
+    rl.question("Cantidad del producto: ", cantidad => {
+      rl.question("Precio del producto: ", precio => {
+        const producto = `${nombre}, ${cantidad}, ${precio}\n`;
+        fs.appendFile("producto.txt", `${producto}`, err => {
+          if (err) throw err;
+          console.log("Producto creado");
+          menu();
+          selecionarOperacion();
+        })
+      });
+    });
+  });
+}
+
+const consultarProducto = () => {
+  rl.question("Producto solicitado: ", (nombre) => {
+    const data = fs.readFileSync("producto.txt", "utf8");
+      const productos = data.split("\n");
+      const productoBuscado = productos.find((producto) => producto.split(",")[0] === nombre);
+      if (productoBuscado) {
+        console.log(productoBuscado);
+        menu();
+        selecionarOperacion();
+      } else {
+        console.log("Producto no encontrado");
+        menu();
+        selecionarOperacion();
+      }
+  });
+}
+
+const actualizarProducto = () => {
+  rl.question("Nombre del producto: ", nombre => {
+    rl.question("Cantidad del producto: ", cantidad => {
+      rl.question("Precio del producto: ", precio => {
+        //const productoActualizado = `${nombre}, ${cantidad}, ${precio}`;
+        fs.open("producto.txt", "r", (err, data) => {
+          if (err) throw err;
+          console.log(data.toLocaleString());
+          menu();
+          selecionarOperacion();
+        })
+      });
+    });
+  });
+}
+
+const mostrarProductos = () => {
+  fs.readFile("producto.txt", "utf8", (err, data) => {
+    if (err) throw err;
     console.log(data);
-  } catch (error) {
-    console.error(error);
-  }
+    menu();
+    selecionarOperacion();
+  });
+}
+
+const cerrarPrograma = () => {
+  fs.unlink("producto.txt", err => {
+    if (err) console.log(err = "Archivo no creado");
+    console.log("Saliste del programa");
+  })
+  rl.close();
 }
 
 const menu = () => {
@@ -57,9 +110,10 @@ const menu = () => {
   console.log("2.- Consultar producto");
   console.log("3.- Actualizar producto");
   console.log("4.- Borrar producto");
-  console.log("5.- Calcular venta total");
-  console.log("6.- Calcular venta por producto");
-  console.log("7.- Salir");
+  console.log("5.- Mostrar productos");
+  console.log("6.- Calcular venta total");
+  console.log("7.- Calcular venta por producto");
+  console.log("8.- Salir");
   selecionarOperacion();
 }
 
@@ -67,52 +121,37 @@ const selecionarOperacion = () => {
   rl.question("Elige una opción ", opcion => {
     switch (opcion) {
       case "1":
-        rl.question("Nombre del producto ", nombre => {
-          rl.question("Cantidad del producto ", cantidad => {
-            rl.question("Precio del producto ", precio => {
-              const producto = `${nombre + ", " + cantidad + ", " + precio}`;
-              fs.writeFile("producto.txt", producto, err => {
-                if (err) throw err;
-                console.log("Producto creado");
-                menu();
-                selecionarOperacion();
-              })
-            });
-          });
-        });
+        crearProducto();
         break;
       case "2":
-        fs.readFile("producto.txt", "utf8", (err, data) => {
-          if (err) throw err;
-          console.log(data);
-          menu();
-          selecionarOperacion();
-        });
+        consultarProducto();
         break;
       case "3":
-        
+        actualizarProducto();
         break;
       case "4":
         
         break;
       case "5":
-        
+        mostrarProductos();
         break;
       case "6":
         
         break;
       case "7":
-        fs.unlink("producto.txt", err => {
-          if (err) throw err;
-          console.log("Saliste del programa");
-        })
-        rl.close(); 
+        
+        break;
+      case "8":
+        cerrarPrograma();
         break;
     
       default:
+        console.log("Ingresa una opción valida");
+        menu();
+        selecionarOperacion();
         break;
     }
   });
 }
-menu();
+menu(); */
 
