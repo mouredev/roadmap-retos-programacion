6ed12778e2a7125508c31264524093258731b927
SaezMD <saulsaezrodriguez@gmail.com>
2024-04-02 11:54:45 +0200

    #12 - Python

--- Roadmap/12 - JSON Y XML/python/SaezMD.py
+++ Roadmap/12 - JSON Y XML/python/SaezMD.py
@@ -17,7 +17,51 @@
  * Borra los archivos.
 """
 
+import xml.etree.ElementTree as ET
+import os
+
+abspath = os.path.abspath(__file__) #get the file directory
+dname = os.path.dirname(abspath)
+os.chdir(dname) #change to file directory
+
+#Create XML file
+root = ET.Element("ContactsXML")
+contact = ET.SubElement(root, "Contact")
+name = ET.SubElement(contact, "Name")
+name.text = "Saul"
+age = ET.SubElement(contact, "Age")
+age.text = "25"
+birthdate = ET.SubElement(contact, "Birthdate")
+birthdate.text = "02/12/2010"
+coddingList = ET.SubElement(contact, "Coddinglist")
+coddingList.text = "Pyhton, VBA, GOlang, JavaScript"
+
+#Write to XML file
+tree = ET.ElementTree(root)
+tree.write("contacts.xml")
+
+#Print XML as string
+xml_string = ET.tostring(root)
+print(xml_string)
+
+#Remove file XML
+os.remove("contacts.xml")
 
 
+import json
 
+filename = 'contacts.json'
+isFile = os.path.isfile(filename)
+ 
+emps = {"contactsJSON" :[{"Name": "Saul Saez", "Age": "22", "BirthDate":"02/12/2010", "Coddinglist":"Pyhton, VBA, GOlang, JavaScript"}, 
+                     ]}
 
+#Save JSON as file
+with open(filename, 'w') as fp:
+    json.dump(emps, fp, indent=4)
+
+#Print JSON as string
+print(emps)
+
+#Delete JSON
+os.remove("contacts.json")
