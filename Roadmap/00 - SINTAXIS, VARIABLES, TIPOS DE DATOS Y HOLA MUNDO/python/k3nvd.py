3ee1d2770d27dbc3afb6bfeb1e1d13b4b1ab617b
Kevin Pedro <sancheskevin09@gmail.com>
2024-04-05 16:29:51 -0600

    #00Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/k3nvd.py
@@ -0,0 +1,25 @@
+#https://www.python.org/
+
+#There are two ways to comment in Python
+#                                                    "#Hello World"
+#                                                ''' Hello World'''
+
+var1 = 'Python'                                              #simple var
+PI = 3.1426                                                  #const var   
+
+#Primitive data types 
+
+age = 25  
+score = -7  
+number_of_lives = 3 
+stringg = 'Hello' 
+dictt = {
+    'name':'Kevin',
+}
+litt = [1,2,4,5]
+sett = (1,'Hello',2,35,3,100)
+
+
+print('Hello', var1)                       
+
+
