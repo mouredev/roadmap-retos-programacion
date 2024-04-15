721e26e32aed460132b2dd67ac17d9f60a5d49ad
MullerJuan <mullerjuanignacio1@gmail.com>
2024-04-15 12:32:52 -0300

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Juani-Müller.js
@@ -0,0 +1,13 @@
+// https://www.javascript.com/ //
+/* https://www.javascript.com/ */
+
+var suma = a + b;
+var a = Number;
+var b = Number;
+const pi = 3.14;
+
+var palabraArgentina = "Boludo";
+var neutroSuma = 0;
+var verdadero = true;
+
+console.log("¡Hola, JavaScript!")
