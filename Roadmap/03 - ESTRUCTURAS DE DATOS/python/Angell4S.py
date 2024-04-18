38d6255721bed2b5d07bc89a13cdb1ab8bab57ff
4ngell4S <4senc1os@gmail.com>
2024-04-18 18:04:04 -0500

    #03 - python

---
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/Angell4S.py
@@ -0,0 +1,16 @@
+# Listas
+my_list = ["Brais", "Bl4ck", "Wolfy", "Visions"]
+print(my_list)
+
+my_list.append("Angel") # Inserción
+print(my_list)
+
+my_list.remove('Angel') # Eliminación
+print(my_list)
+
+print(my_list[1]) # Acceso
+my_list[1] = 'Cuervillo' # Modificación
+print(my_list)
+
+my_list.sort() # Ordenación
+print(my_list)
