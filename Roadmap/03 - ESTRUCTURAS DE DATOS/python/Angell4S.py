a0f45b32dc16f2cb9529a1e581b89e781b75ccfe
4ngell4S <4senc1os@gmail.com>
2024-04-19 11:42:54 -0500

    #03 - python - diccionario

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/Angell4S.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/Angell4S.py
@@ -27,7 +27,7 @@ print(my_tuple)
 # Sets
 my_set = {'Moure', 'Dev', '@mouredev', '36'}
 print(my_set)
-my_set.add('angel@gmail.com')
+my_set.add('angel@gmail.com') # Inserción
 my_set.add('angel@gmail.com')
 my_set.remove('Moure') # Eliminación
 print(my_set)
@@ -35,3 +35,21 @@ print(my_set)
 my_set = set(sorted(my_set)) # No se puede ordenar, set no es una estructura ordenada
 print(my_set)
 print(type(my_set))
+
+# Diccionarios
+my_dict: dict = {
+   'name':'Moure',
+   'surname':'Dev',
+   'alias':'@mouredev',
+   'edad':'36'
+   }
+my_dict['email'] = 'mouredev@gmail.com' # inserción
+print(my_dict)
+del my_dict['surname'] # Eliminación
+print(my_dict)
+print(my_dict['name']) # Acceso
+my_dict['edad'] = '37' # Modificación
+print(my_dict)
+my_dict = dict(sorted(my_dict.items())) # Ordenación
+print(my_dict)
+print(type(my_dict))
