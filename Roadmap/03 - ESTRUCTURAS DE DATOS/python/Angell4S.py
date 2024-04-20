ab22b7ea6b4262fbe6eb366a13a2e956199d1ba3
Angell4S <4senc1os@gmail.com>
2024-04-20 15:20:53 -0500

    #03 - Python

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/Angell4S.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/Angell4S.py
@@ -53,3 +53,60 @@ print(my_dict)
 my_dict = dict(sorted(my_dict.items())) # Ordenación 
 print(my_dict)
 print(type(my_dict))
+
+"""
+EXTRA
+"""
+
+def my_agenda():
+
+   agenda = {}
+
+   def insert_contact():
+      phone = input("Introduce el teléfono del contacto: ")
+      if phone.isdigit() and len(phone) > 0 and len(phone) <= 11:
+         agenda[name] = phone
+      else:
+         print("Debes introducir un número de teléfono con menos de 11 dígitos.")
+
+   while True:
+
+      print("")
+      print("1. Buscar contacto")
+      print("2. Insertar contacto")
+      print("3. Actualizar contacto")
+      print("4. Eliminar contacto")
+      print("5. Salir")
+
+      option = input("\nSelecciona una opción: ")
+
+      match option:
+         case "1":
+            name = input("Introduce el nombre del contacto a buscar: ")
+            if name in agenda:
+               print(f"El número de teléfono de {name} es {agenda[name]}")
+            else:
+               print(f"El contacto {name} no existe")
+         case "2":
+            name = input("Introduce el nombre del contacto a insertar: ")
+            insert_contact()
+         case "3":
+            name = input("Introduce el nombre del contacto a actualizar: ")
+            if name in agenda:
+               insert_contact()
+            else:
+               print(f"El contacto {name} no existe")
+         case "4":
+            name = input("Introduce el nombre del contacto a eliminar: ")
+            if name in agenda:
+               del agenda[name]
+            else:
+               print(f"El contacto {name} no existe")
+         case "5":
+            print("Saliendo de la agenda.")
+            break
+         case _:
+            print("Opción no válida, Elige una opción del 1 al 5.")
+
+
+my_agenda()
