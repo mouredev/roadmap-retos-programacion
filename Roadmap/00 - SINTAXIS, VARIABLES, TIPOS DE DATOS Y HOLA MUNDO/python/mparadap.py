0d500a6c6bf0af1ab5f5f9f646533e4f4890d554
mparadap <m.paradap19@gmail.com>
2024-04-19 22:27:10 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mparadap.py
@@ -0,0 +1,48 @@
+# Enlace de documentación del lenguaje de programación Pyhton
+# https://www.python.org/
+
+
+# Comentario en una línea: utilizando la almohadilla (#)
+# 2.1.3. Comentarios
+
+
+# Comentario en varias líneas: utilizando triple comillas dobles ("""")
+"""
+Un comentario comienza con un carácter de almohadilla (#) que no es parte de un literal de cadena, y termina
+al final de la línea física. Un comentario implica el final de la línea lógica, a menos que se invoque la regla implícita
+de unión de líneas. Los comentarios son ignorados por la sintaxis.
+"""
+
+
+# Comentario en varias líneas: utilizando triple comillas simples (''')
+'''
+Un comentario comienza con un carácter de almohadilla (#) que no es parte de un literal de cadena, y termina
+al final de la línea física. Un comentario implica el final de la línea lógica, a menos que se invoque la regla implícita
+de unión de líneas. Los comentarios son ignorados por la sintaxis.
+'''
+
+# Crear una variable
+my_variable = "Mi variable"
+
+
+# Crear una constante
+MY_CONSTANT = "Mi constante" # Por convención se crea en letras mayúsculas para distinguirla de las variables
+
+
+# Tipos de Datos primitivos: NUMÉRICOS
+my_int = 1 # Entero
+my_float = 1.5 # Decimal
+
+
+# Tipos de Datos primitivos: CADENAS DE TEXTO
+my_str = "Mi cadena de texto" # Utilizando comillas dobles
+my_other_str = 'Mi otra cadena de texto' # Utilizando comillas simples
+
+
+# Tipos de Datos primitivos: BOOLEANOS
+my_bool = True
+my_bool = False
+
+
+# Imprimir texto por Terminal
+print("¡Hola, Python!")
