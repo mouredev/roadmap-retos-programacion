b9dc33d7218e12bfa1042f83e3416c4e15f565b9
Dota43ver <facundoagueroc@gmail.com>
2024-04-20 11:33:41 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Dota43ver.py
@@ -0,0 +1,29 @@
+# https://python.org
+
+# esto es un comentario
+
+"""
+esto es
+un comentario en
+varias lineas
+
+"""
+
+'''
+esto es
+un comentario en
+varias lineas
+tambien
+'''
+
+mi_variable = "esto es una variable"
+
+MY_CONSTANT = "esto simula una constante"
+
+mi_int = 1
+mi_float = 1.5
+mi_bool = True
+mi_bool2 = False
+mi_str = "esto es un str"
+
+print("¡Hola, Python!")
