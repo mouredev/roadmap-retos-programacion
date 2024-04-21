83a919bd7e590429471ad0d9492daed792d0a3b1
CurtoBrull <curto.brull.javier@gmail.com>
2024-04-21 11:56:59 +0200

    #00 - go

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/go/curtobrull.go
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/go/curtobrull.go
@@ -16,19 +16,21 @@ var variable = "Curto"
 
 func main() {
 
-	// Primitivos
-	var integer uint32 = 10
-	var integerNeg int32 = -10
-	var numFloat32 float32 = 10.5
-	var numFloat64 float64 = 10.5
-	var boolTrue bool = true
-	var boolFalse bool = false
-	var text string = "Javier"
-	var charRune rune = 'a'
-
-	var language string = "Go"
+	var (
+		// Primitivos
+		integerPos   uint32    = 10
+		integerNeg   int32     = -10
+		numFloat32   float32   = 10.5
+		numFloat64   float64   = 10.5
+		numComplex64 complex64 = 3 + 4i
+		boolTrue     bool      = true
+		boolFalse    bool
+		text         string = "Javier"
+		charRune     rune   = 'a'
+		language     string = "Go"
+	)
 
 	fmt.Println("¡Hola, " + language + "!")
 
-	fmt.Println("Primitivos: "+Constante, variable, integer, integerNeg, numFloat32, numFloat64, boolTrue, boolFalse, text, charRune)
+	fmt.Println("Primitivos: "+Constante, variable, integerPos, integerNeg, numFloat32, numFloat64, numComplex64, boolTrue, boolFalse, text, charRune)
 }
