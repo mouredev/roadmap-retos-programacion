eba305910b4e49407bca6e7c4f2aa04d7fe42335
Pablo Montalvillo <pablo@MacBook-Pro-de-Pablo.local>
2024-04-21 12:32:45 +0200

    Añadir solución para el ejercicio 0 en Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/00_Pablink18dos2.py
@@ -0,0 +1,28 @@
+# https://www.python.org
+
+# '''Otra forma de escribir comentarios'''
+
+"""
+Esto es un comentario
+"""
+
+'''esto es un comentario'''
+
+my_variable = "my variable"
+my_variable= "my variable V2"
+
+MY_CONSTANT = "my constant" # Cuando se escibre en mayusculas se considera una constante por convencion
+
+# Tipos de datos
+my_int = 1
+my_float = 1.0 
+my_bool = True
+my_str = "Hola Mundo"
+
+
+print("Hola estoy utilizando Python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
