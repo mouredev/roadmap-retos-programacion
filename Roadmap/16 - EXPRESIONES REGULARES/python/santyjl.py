6f05725ab041f81ec2b537338e2595f68dfbd27f
santyjL <jlopezsanty1@gmail.com>
2024-04-20 21:58:53 -0600

    #16 - python

--- Roadmap/16 - EXPRESIONES REGULARES/python/santyjl.py
+++ Roadmap/16 - EXPRESIONES REGULARES/python/santyjl.py
@@ -14,11 +14,17 @@
  * - Validar una url.
  */
 """
-import re
+import re  # Importamos el módulo re para trabajar con expresiones regulares
 
 
 def exprecion_regular_numeros(texto: str):
-    numeros = re.findall(r'\d+', texto)
+    """
+    Función para encontrar y mostrar los números en un texto utilizando expresiones regulares.
+
+    Args:
+    texto (str): El texto en el que se buscarán los números.
+    """
+    numeros = re.findall(r'\d+', texto)  # Buscamos todos los números en el texto
 
     print("Números encontrados:")
     for numero in numeros:
@@ -27,23 +33,35 @@ def exprecion_regular_numeros(texto: str):
 texto = """hola mundo esto es una prueba del reto de programacion numero 16 , esto significa que
 que han vido otros retos como el 10 , 5 o un reto numero 0"""
 
-exprecion_regular_numeros(texto)
+exprecion_regular_numeros(texto)  # Llamamos a la función para buscar y mostrar los números en el texto
 
 ### Extra
-email_exprecion = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-numero_exprecion = r'^\d{4}\s\d{4}$'
+email_exprecion = r'^[\w\.-]+@[\w\.-]+\.\w+$'  # Patrón de expresión regular para validar correos electrónicos
+numero_exprecion = r'^\d{4}\s\d{4}$'  # Patrón de expresión regular para validar números de teléfono
 
-url_exprecion = r'^(https?://)?([a-z0-9]*\.)?[a-z0-9-]+\.[a-z]{2,3}$'  # Patrón de URL modificado
+# Patrón de expresión regular para validar URLs
+url_exprecion = r'^(https?://)?([a-z0-9]*\.)?[a-z0-9-]+\.[a-z]{2,3}$'
+
 def email_numero_url(elemento: str):
+    """
+    Función para verificar si un elemento es un correo electrónico, número de teléfono o URL válidos.
+
+    Args:
+    elemento (str): El elemento a verificar.
+    """
     if re.match(email_exprecion, elemento):
         print(f"El texto '{elemento}' es un correo electrónico válido.")
+
     elif re.match(numero_exprecion, elemento):
         print(f"El texto '{elemento}' es un número de teléfono válido.")
+
     elif re.match(url_exprecion, elemento):
         print(f"El texto '{elemento}' es una URL válida.")
+
     else:
         print(f"El texto '{elemento}' no coincide con ningún patrón de correo electrónico, número de teléfono o URL.")
 
+# Definimos algunos ejemplos válidos e inválidos de números de teléfono, correos electrónicos y URLs
 numero_valido = "8888 9999"
 numero_invalido = "8888999"
 
@@ -53,6 +71,7 @@ email_invalido = "13lopez@com"
 url_valida = "https://www.holamundo.day"
 url_invalida = "www.hola_mundo.dev"
 
+# Probamos la función con los ejemplos definidos
 email_numero_url(numero_valido)
 email_numero_url(email_valido)
 email_numero_url(url_valida)
