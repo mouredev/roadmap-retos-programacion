b4097f465f0f8fa8e45b3458ac1ffb52c5db4b5b
Pedro Omar <126366180+pedroomar23@users.noreply.github.com>
2024-04-23 00:06:32 -0400

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/swift/#00 - Swift.swift
@@ -0,0 +1,17 @@
+// El sitio web oficial de Swift es https://wwww.swift.org
+
+// Esto es un comentario en una sola linea 
+
+/* Esto es un comentario 
+    en varias lineas 
+    */
+
+var text 
+let texto 
+
+var text = "Esto es un texto de tipo String o cadena de texto" // Se usa para escribir textos entre otros 
+let double = 100 // Esto es para poner numeros 
+var bool = true or false // Se usa a la hora de crear propiedades Ej: @State var text = false 
+
+var lenguaje = "!Hola, Estoy aprediendo Swift!"
+print("\(lenguaje)")
