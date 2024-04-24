612ce8307f29f3c199ab4191cef94c292eaf4852
Anyelo Gomez <anyelo290681@gmail.com>
2024-04-23 23:06:53 -0500

    Este es la solucion al reto #00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/aegpgrafologo.py
@@ -0,0 +1,38 @@
+#RETO 00
+# https://python.org
+# Comentario en una sola linea
+"""
+Comentario  lineas
+en varias
+lineas de codigo
+"""
+'''
+Otra forma de 
+comentar en 
+mas de una linea
+'''
+# Variable tipo entero o integer.
+my_codigo = 88 
+
+# El lenguaje Phyton no tiene constantes definidas, todo puede cambiar.
+# Por convencion se usa el nombre de una variable en mayuscula sostenida
+CONSTANTE = "Faro"
+
+# Tipos de datos primitivos (Hay cuatro tipos)
+
+entero_integer = 8
+float_decimal = 1.5
+booleanos = True
+booleanos1 = False
+string_cadenatexto = "FFF"
+
+# Impresión texto por consola
+print("!hola, Phyton¡")
+
+# Por ser lenguaje debilmente tipado, es posible omprobar el tipo de dato, asi:
+
+print(type(entero_integer))
+print(type(float_decimal))
+print(type(booleanos))
+print(type(booleanos1))
+print(type(string_cadenatexto))
