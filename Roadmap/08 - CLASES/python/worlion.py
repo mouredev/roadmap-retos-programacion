ce91f62c64ef7db6d6ed56a55cd00b891e23a233
Imanol <imanol.conde@gmail.com>
2024-04-24 23:42:29 +0200

    stack

--- Roadmap/08 - CLASES/python/worlion.py
+++ Roadmap/08 - CLASES/python/worlion.py
@@ -24,3 +24,58 @@ myObject.print()
 myObject.atributo_int = 55
 myObject.atributo_str = "otro texto"
 myObject.print()
+
+
+"""
+DIFICULTAD EXTRA (opcional):
+Implementa dos clases que representen las estructuras de Pila y Cola (estudiadas
+en el ejercicio número 7 de la ruta de estudio)
+- Deben poder inicializarse y disponer de operaciones para añadir, eliminar,
+    retornar el número de elementos e imprimir todo su contenido.
+"""
+
+print("\n ---- 🌩 DIFICULTAD EXTRA 🌩 ----\n")
+
+# Pila / Stack
+
+class Stack:
+    
+    data = []
+    
+    def __init__(self) -> None:
+        data = []
+    
+    def push(self, item) -> None:
+        self.data.append(item)
+    
+    def pop(self):
+        if(len(self.data) == 0):
+            print("WARN: empty stack!")
+        else:
+            return self.data.pop()
+    
+    def size(self):
+        return len(self.data)
+    
+    def print(self):
+        print("heap")
+        print("  ↓")
+        print(self.data[::-1])
+
+def test_stack():
+    pila = Stack()
+    pila.push("A")
+    pila.push("B")
+    pila.push("C")
+    pila.print()
+
+    print(f"pop: {pila.pop()}")
+    pila.print()
+    print(f"pop: {pila.pop()}")
+    pila.print()
+    print(f"pop: {pila.pop()}")
+    pila.print()
+    print(f"pop: {pila.pop()}")
+    pila.print()
+
+test_stack()
