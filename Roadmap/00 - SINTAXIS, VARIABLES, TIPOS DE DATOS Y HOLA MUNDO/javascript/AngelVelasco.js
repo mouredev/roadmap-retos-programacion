2c6bf6211ab6d5e1c5474e2fc1c253858da1d89a
Angel Velasco <82744167+AngelVelasco1@users.noreply.github.com>
2024-04-24 10:29:29 -0500

    Create AngelVelasco.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/AngelVelasco.js
@@ -0,0 +1,24 @@
+/* https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+//? 1. Representa las diferentes formas de comentar 
+// -> En una linea
+/* 
+    --> Multiples 
+    Lineas
+*/
+
+//? 2. Variables y constantes 
+let number = 1;
+const athmosphere = 3.4745;
+
+//? 3. Tipos de datos primitivos
+let string = "Hi";
+let int = 1;
+let boolean = true;
+let float = 1.5;
+let notDefined = undefined;
+let symbol = Symbol
+
+//? 4. Mensaje por consola
+const languageName = "JavaScript"
+console.log(`¡Hola ${languageName}!`);
