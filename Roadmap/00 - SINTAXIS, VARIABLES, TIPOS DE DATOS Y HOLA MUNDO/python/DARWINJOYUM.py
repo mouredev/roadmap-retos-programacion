34a47f484e4f6995443cf508d9d09bece472560f
Darwin Moreno <bludarwintsu@gmail.com>
2024-04-25 07:31:22 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DARWINJOYUM.py
@@ -0,0 +1,30 @@
+# https://www.python.org, Url del la pagina oficial de Python
+
+# Diferentes sintaxis para crar comentarios
+
+# Este es un comentario de una sola linea
+
+"""
+Utilizamos las tres comillas para hacer 
+comentarios de varias lineas. las colocamos al
+inicio y al final del bloque del cometario.
+"""
+
+print("Hola Python")
+
+#Variables por asignacion, tipos de Datos y datos primitivos
+
+variable_str = "hola Darwin"
+print(f'Variable tipo cadena de texto: {variable_str}, {type(variable_str)}')
+
+variable_entero = 3456
+print(f'Variable tipo entero:  {variable_entero}, {type(variable_entero)}')
+
+variable_float = 34.56
+print(f'Variable tipo flotante: {variable_float}, {type(variable_float)}')
+
+variable_bool_1 = True
+print(f'Variable tipo booleano: {variable_bool_1}, {type(variable_bool_1)}')
+
+variable_bool_0 = False
+print(f'Variable tipo booleano: {variable_bool_0}, {type(variable_bool_0)}')
