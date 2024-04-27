4765ac39931d6cfd94c92753ccb61e53bc4a47a2
miguel2rar <167903711+miguel2rar@users.noreply.github.com>
2024-04-27 20:00:53 +0200

    Miguel2rar.py
    hola esta es la primera vez que utilizo github para aportar una solución espero que no la haya liado mucho

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Miguel2rar.py
@@ -0,0 +1,34 @@
+"""EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio."""
+
+""" Estos son mis primeros pasos en python y voy a comenzar de la incluyendo la url del sitio python
+https://www.python.org/"""
+
+# Este es otro modo de crear un comentario en python
+
+variable = "esta es una variable en python"
+
+CONSTANTE = "Esta es una constante en python"
+
+entero = 4
+float = 3.4
+bool = True
+numeros_complejos = 2+ 3j
+string = "texto"
+lista = [34, 56, 32]
+tupla = (34, 22, 56)
+set = { 34, 33, 34}
+dic = { "region": "polo norte",
+       "localidad": "farenheit"}
+
+print("hola PYTHON")
