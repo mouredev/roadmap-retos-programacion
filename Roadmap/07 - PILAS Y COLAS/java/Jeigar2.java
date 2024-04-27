e5cc690b567110ec178afe003808a9550e0c287d
Jesus García <jeigar2@gmail.com>
2024-04-27 03:30:01 +0200

    #07 - java

---
+++ Roadmap/07 - PILAS Y COLAS/java/Jeigar2.java
@@ -0,0 +1,22 @@
+public class Jeigar2 {
+    /*
+     * EJERCICIO:
+     * Implementa los mecanismos de introducción y recuperación de elementos propios de las
+     * pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
+     * o lista (dependiendo de las posibilidades de tu lenguaje).
+     *
+     * DIFICULTAD EXTRA (opcional):
+     * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+     *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+     *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+     *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+     *   el nombre de una nueva web.
+     * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+     *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+     *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+     *   interpretan como nombres de documentos.
+     */
+    public static void main(String[] args) {
+        System.out.println("OK");
+    }
+}
