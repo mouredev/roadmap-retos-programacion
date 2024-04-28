538f466690f6251fa23526bd1d14eb5caba2246e
s9code <shotarocode@gmail.com>
2024-04-28 18:56:03 +0200

    Añadir archivo

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/javikoi.js
@@ -0,0 +1,29 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una linea
+
+/* Comentario 
+de varias
+lineas
+*/
+
+/* 
+1. Variable y constante
+2. Crear viarbles representando los tipos de datos primitivos
+*/
+
+let variable;
+const noDoyError = 'Si no declaro, doy error'
+
+const string = 'Hola'
+const number = 1
+const boolean = true
+const nulo = null
+let undefined;
+let symbol = Symbol("simbolo")
+const bigInt = 222n
+
+// Imprimir por terminal
+
+console.log('Hola, Javascript')
+
