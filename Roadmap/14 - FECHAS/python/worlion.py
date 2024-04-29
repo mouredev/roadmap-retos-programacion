d42f937c5d8037f293bb0e70c3ce5bc81aa2a023
Imanol <imanol.conde@gmail.com>
2024-04-29 16:49:18 +0200

    start

---
+++ Roadmap/14 - FECHAS/python/worlion.py
@@ -0,0 +1,20 @@
+"""
+* EJERCICIO:
+* Crea dos variables utilizando los objetos fecha (date, o semejante) de tu lenguaje:
+* - Una primera que represente la fecha (día, mes, año, hora, minuto, segundo) actual.
+* - Una segunda que represente tu fecha de nacimiento (te puedes inventar la hora).
+* Calcula cuántos años han transcurrido entre ambas fechas.
+"""
+
+
+"""
+* DIFICULTAD EXTRA (opcional):
+* Utilizando la fecha de tu cumpleaños, formatéala y muestra su resultado de
+* 10 maneras diferentes. Por ejemplo:
+* - Día, mes y año.
+* - Hora, minuto y segundo.
+* - Día de año.
+* - Día de la semana.
+* - Nombre del mes.
+* (lo que se te ocurra...)
+"""
