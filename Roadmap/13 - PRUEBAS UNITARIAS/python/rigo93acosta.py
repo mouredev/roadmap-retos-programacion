0cdd96b988da2ac5f0248c1d4f6203034de115e1
riacosta <rigo93acosta@gmail.com>
2024-04-30 09:40:07 -0300

    Create File

---
+++ Roadmap/13 - PRUEBAS UNITARIAS/python/rigo93acosta.py
@@ -0,0 +1,18 @@
+'''
+ * EJERCICIO:
+ * Crea una función que se encargue de sumar dos números y retornar
+ * su resultado.
+ * Crea un test, utilizando las herramientas de tu lenguaje, que sea
+ * capaz de determinar si esa función se ejecuta correctamente.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un diccionario con las siguientes claves y valores:
+ * "name": "Tu nombre"
+ * "age": "Tu edad"
+ * "birth_date": "Tu fecha de nacimiento"
+ * "programming_languages": ["Listado de lenguajes de programación"]
+ * Crea dos test:
+ * - Un primero que determine que existen todos los campos.
+ * - Un segundo que determine que los datos introducidos son correctos.
+'''
+
