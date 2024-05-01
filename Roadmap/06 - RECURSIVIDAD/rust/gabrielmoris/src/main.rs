b49ccfa417ab31a1cb78946393888ecbb9bbdec7
gabriel <gabrielcmoris@gmail.com>
2024-05-01 04:45:51 -0400

    start challenge

---
+++ Roadmap/06 - RECURSIVIDAD/rust/gabrielmoris/src/main.rs
@@ -0,0 +1,16 @@
+/*
+ * EXERCISE:
+ * Understand the concept of recursion by creating a recursive function that
+ * prints numbers from 100 to 0.
+ *
+ * EXTRA DIFFICULTY (optional):
+ * Use the concept of recursion to:
+ * - Calculate the factorial of a specific number (the function receives that number).
+ * - Calculate the value of a specific element (according to its position) in the
+ *   Fibonacci sequence (the function receives the position).
+ */
+
+
+fn main() {
+    println!("Hello, world!");
+}
