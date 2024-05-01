8c7960ffeeb6577615c3c409ab7df680e1176d08
riacosta <rigo93acosta@gmail.com>
2024-05-01 09:35:05 -0300

    Finished

--- Roadmap/14 - FECHAS/python/rigo93acosta.py
+++ Roadmap/14 - FECHAS/python/rigo93acosta.py
@@ -16,3 +16,47 @@
  * (lo que se te ocurra...)
 '''
 
+'''
+Ejercicio
+'''
+
+from datetime import datetime
+
+now = datetime.now()
+birthday = datetime(1993, 5, 25, 0, 0, 0)
+
+if now.month < birthday.month or (now.month == birthday.month and now.day < birthday.day):
+    years = now.year - birthday.year - 1
+else:
+    years = now.year - birthday.year
+
+print(f"Han pasado {years} años desde mi nacimiento")
+
+'''
+Extra
+'''
+
+# Día, mes y año
+print(f'{birthday.strftime("%d/%m/%y")}')
+print(f'{birthday.strftime("%d/%m/%Y")}')
+
+# Hora, minuto y segundo
+print(f'{birthday.strftime("%H:%M:%S")}')
+
+# Día de año
+print(f'{birthday.strftime("%j")}')
+
+# Día de la semana
+print(f'{birthday.strftime("%A")}')
+
+# Nombre del mes
+print(f'{birthday.strftime("%B")}')
+print(f'{birthday.strftime("%h")}')
+
+# Representación por defecto del locale
+print(f'{birthday.strftime("%c")}')
+print(f'{birthday.strftime("%x")}')
+print(f'{birthday.strftime("%X")}')
+
+# AM/PM
+print(f'{birthday.strftime("%I:%M:%S %p")}')
