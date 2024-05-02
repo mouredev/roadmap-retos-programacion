c24fe744c270f7d721ab0dfd0359544e92c6d8ec
César Díaz <113660331+CD1974@users.noreply.github.com>
2024-05-02 20:14:56 +0200

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/hola_mundo.py
@@ -0,0 +1,18 @@
+# El lenguaje que he escogido es Python y esta es su página oficial https://www.python.org/doc/
+
+"""Esta es otra manera de realizar
+comentarios de varias líneas..."""
+
+my_var = 'Variable inicial'
+
+# Datos primitivos
+
+var_texto = 'Variable que representa una cadena de texto'
+
+var_enteros = int(numero)
+
+var_flotantes = float(numero)
+
+var_booleanos = True and False
+
+print("¡Hola, Python!")
