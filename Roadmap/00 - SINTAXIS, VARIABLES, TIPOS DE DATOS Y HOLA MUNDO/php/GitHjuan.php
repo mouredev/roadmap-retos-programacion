040525c3f0f069eabd57cdb0753b7d1974b7297b
gitjuan <gitjuan7@gmail.com>
2024-05-02 13:11:07 -0400

    fetch

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/GitHjuan.php
@@ -0,0 +1,23 @@
+<?php
+
+// https://www.php.net/
+
+//comentario de una linea
+
+/*
+comentario
+de varias
+lineas
+*/
+
+//CREA UNA VARIABLE Y UNA CONSTANTE
+
+$variable = "Valor de mi variable"; // Esto es una variable
+
+define(CONSTANTE, "Esto es una constante" ); //Esto es una constante
+
+$string = "Cadena de texto";
+$int = 12 ; //numero entero
+$false = false;
+$true = true;
+$decimal = 1.2;
