6a15400ed4bc9ec32c0788aaa5a5c5c0782fbd63
AngelGutierrezVar <asking.01.angelhotmail.com>
2024-05-02 14:27:17 -0600

    Reto 00 completado

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AngelGutierrezVar.py
@@ -0,0 +1,29 @@
+# https://python.org
+
+# Comentario en una lineas
+
+"""
+Esto tambien es
+un comentario
+en varias lineas
+"""
+
+'''
+Esto tambien es
+un comentario
+en varias lineas
+'''
+
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante"  # por convencion
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_str = "Mi cadena de texto"
+my_str = 'Mi otra cadena de texto'
+
+print("¡Hola, Python!")
