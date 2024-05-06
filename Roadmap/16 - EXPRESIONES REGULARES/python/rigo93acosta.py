7509085115277a3888a172e09fa739b8f6a71451
riacosta <rigo93acosta@gmail.com>
2024-05-06 19:21:41 -0300

    Created File

---
+++ Roadmap/16 - EXPRESIONES REGULARES/python/rigo93acosta.py
@@ -0,0 +1,14 @@
+'''
+ * EJERCICIO:
+ * Utilizando tu lenguaje, explora el concepto de expresiones regulares,
+ * creando una que sea capaz de encontrar y extraer todos los números
+ * de un texto.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea 3 expresiones regulares (a tu criterio) capaces de:
+ * - Validar un email.
+ * - Validar un número de teléfono.
+ * - Validar una url.
+ */
+'''
+
