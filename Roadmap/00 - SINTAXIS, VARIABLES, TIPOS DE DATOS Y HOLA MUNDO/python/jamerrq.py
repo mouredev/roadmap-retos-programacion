f9fa2eda9110a4802a4e3a8d795be8831966f73b
jamerrq <jamerrq@gmail.com>
2024-05-07 14:58:37 -0500

    roadmap 00 en python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jamerrq.py
@@ -0,0 +1,58 @@
+"""
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
+
+# URL del sitio web oficial de Python
+# https://www.python.org/
+
+# Comentario de una línea
+
+"""
+Comentario
+de
+varias
+líneas
+"""
+
+# Variable
+variable = "Hola, Python!"
+
+# Constante
+# Python no tiene constantes, pero se puede simular con variables en mayúsculas
+CONSTANTE = "¡Hola, Python!"
+
+# Tipos de datos primitivos
+cadena = "Cadena de texto"
+entero = 42
+flotante = 3.14159
+booleano = True
+
+# Imprimir por terminal
+print("Variable:", variable)
+print("Constante:", CONSTANTE)
+print("Cadena de texto:", cadena)
+print("Entero:", entero)
+print("Flotante:", flotante)
+print("Booleano:", booleano)
