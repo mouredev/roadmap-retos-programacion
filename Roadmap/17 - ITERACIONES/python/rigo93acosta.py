d6d93bafe379588f3e4be8e02eeace8436d71c8f
riacosta <rigo93acosta@gmail.com>
2024-05-08 19:41:54 -0300

    Created File

---
+++ Roadmap/17 - ITERACIONES/python/rigo93acosta.py
@@ -0,0 +1,11 @@
+'''
+ * EJERCICIO:
+ * Utilizando tu lenguaje, emplea 3 mecanismos diferentes para imprimir
+ * números del 1 al 10 mediante iteración.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Escribe el mayor número de mecanismos que posea tu lenguaje
+ * para iterar valores. ¿Eres capaz de utilizar 5? ¿Y 10?
+ */
+'''
+
