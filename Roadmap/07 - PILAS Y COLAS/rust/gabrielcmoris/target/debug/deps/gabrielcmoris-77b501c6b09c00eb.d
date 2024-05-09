c5725626adfabdb7b8a17ca4dd88e7cec8ebe1f4
gabriel <gabrielcmoris@gmail.com>
2024-05-09 04:22:09 -0400

    deleted extra files

--- Roadmap/07 - PILAS Y COLAS/rust/gabrielcmoris/target/debug/deps/gabrielcmoris-77b501c6b09c00eb.d
+++
@@ -1,5 +0,0 @@
-/home/moris/learning/roadmap-retos-programacion/Roadmap/07 - PILAS Y COLAS/rust/gabrielcmoris/target/debug/deps/gabrielcmoris-77b501c6b09c00eb: src/main.rs
-
-/home/moris/learning/roadmap-retos-programacion/Roadmap/07 - PILAS Y COLAS/rust/gabrielcmoris/target/debug/deps/gabrielcmoris-77b501c6b09c00eb.d: src/main.rs
-
-src/main.rs:
--- Roadmap/07 - PILAS Y COLAS/rust/gabrielcmoris/target/debug/gabrielcmoris.d
+++
@@ -1,1 +0,0 @@
-/home/moris/learning/roadmap-retos-programacion/Roadmap/07\ -\ PILAS\ Y\ COLAS/rust/gabrielcmoris/target/debug/gabrielcmoris: /home/moris/learning/roadmap-retos-programacion/Roadmap/07\ -\ PILAS\ Y\ COLAS/rust/gabrielcmoris/src/main.rs
