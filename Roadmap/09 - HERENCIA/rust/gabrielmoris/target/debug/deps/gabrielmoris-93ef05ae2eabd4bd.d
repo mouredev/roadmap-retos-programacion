f3c25514e42f96d7e725c234e3495d34cb8659a4
gabriel <gabrielcmoris@gmail.com>
2024-05-11 07:13:17 -0400

    #09-rust

--- Roadmap/09 - HERENCIA/rust/gabrielmoris/target/debug/deps/gabrielmoris-93ef05ae2eabd4bd.d
+++
@@ -1,5 +0,0 @@
-/config/learn/roadmap-retos-programacion/Roadmap/09 - HERENCIA/rust/gabrielmoris/target/debug/deps/libgabrielmoris-93ef05ae2eabd4bd.rmeta: src/main.rs
-
-/config/learn/roadmap-retos-programacion/Roadmap/09 - HERENCIA/rust/gabrielmoris/target/debug/deps/gabrielmoris-93ef05ae2eabd4bd.d: src/main.rs
-
-src/main.rs:
--- Roadmap/09 - HERENCIA/rust/gabrielmoris/target/debug/gabrielmoris.d
+++
@@ -1,1 +0,0 @@
-/config/learn/roadmap-retos-programacion/Roadmap/09\ -\ HERENCIA/rust/gabrielmoris/target/debug/gabrielmoris: /config/learn/roadmap-retos-programacion/Roadmap/09\ -\ HERENCIA/rust/gabrielmoris/src/main.rs
