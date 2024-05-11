3b0d9e4bf0a1e525a24c67e1d8927da4739d7514
gabriel <gabrielcmoris@gmail.com>
2024-05-11 03:42:11 -0400

    understand classes and inheritance

---
+++ Roadmap/09 - HERENCIA/rust/gabrielmoris/target/debug/gabrielmoris.d
@@ -0,0 +1,1 @@
+/config/learn/roadmap-retos-programacion/Roadmap/09\ -\ HERENCIA/rust/gabrielmoris/target/debug/gabrielmoris: /config/learn/roadmap-retos-programacion/Roadmap/09\ -\ HERENCIA/rust/gabrielmoris/src/main.rs
