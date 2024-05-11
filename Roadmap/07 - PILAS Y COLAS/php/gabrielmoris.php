4708e1c5dafeea9d51ddea30566cfac257ee6c57
gabriel <gabrielcmoris@gmail.com>
2024-05-11 02:42:39 -0400

    start php

---
+++ Roadmap/07 - PILAS Y COLAS/php/gabrielmoris.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * EXERCISE:
+ * Implement the mechanisms of introduction and retrieval of elements typical of
+ * stacks (stacks - LIFO) and queues (queue - FIFO) using an array
+ * or list (depending on the possibilities of your language).
+ */
+
+class Stack
+{
+}
+
+ 
+/*
+ * EXTRA DIFFICULTY (optional):
+ * - Using the stack implementation and text strings, simulate the forward/back
+ *   mechanism of a web browser. Create a program in which you can navigate to a page or tell it
+ *   that you want to move forward or backward, showing in each case the name of the web.
+ *   The words "forward", "backward" trigger this action, the rest is interpreted as
+ *   the name of a new web.
+ * - Using the queue implementation and text strings, simulate the mechanism of a
+ *   shared printer that receives documents and prints them when indicated.
+ *   The word "print" prints an element from the queue, the rest of the words are
+ *   interpreted as document names.
+ */
