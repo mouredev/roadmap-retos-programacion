73efccf5a4398d29ad89e001e67af23d81e27362
kenysdev <kenysdev@hotmail.com>
2024-05-12 14:36:49 -0500

    fix-19-vb.net-ENUMERACIONES

--- Roadmap/19 - ENUMERACIONES/vb.net/kenysdev.vb
+++ Roadmap/19 - ENUMERACIONES/vb.net/kenysdev.vb
@@ -9,8 +9,14 @@
 ' https://learn.microsoft.com/es-es/dotnet/visual-basic/language-reference/statements/enum-statement
 ' https://learn.microsoft.com/es-es/dotnet/api/system.enum?view=net-8.0
 
-
 Module Program
+    '* EJERCICIO 1:
+    '* Empleando tu lenguaje, explora la definición del tipo de dato
+    '* que sirva para definir enumeraciones (Enum).
+    '* Crea un Enum que represente los días de la semana del lunes
+    '* al domingo, en ese orden. Con ese enumerado, crea una operación
+    '* que muestre el nombre del día de la semana dependiendo del número entero
+    '* utilizado (del 1 al 7).
 
     Enum Weekday
         MONDAY = 1
@@ -39,6 +45,19 @@ Module Program
             Return 0
         End If
     End Function
+
+    '* EJERCICIO 2:
+    '* Crea un pequeño sistema de gestión del estado de pedidos.
+    '* Implementa una clase que defina un pedido con las siguientes características:
+    '* - El pedido tiene un identificador y un estado.
+    '* - El estado es un Enum con estos valores: PENDIENTE, ENVIADO, ENTREGADO y CANCELADO.
+    '* - Implementa las funciones que sirvan para modificar el estado:
+    '*   - Pedido enviado
+    '*   - Pedido cancelado
+    '*   - Pedido entregado
+    '*   (Establece una lógica, por ejemplo, no se puede entregar si no se ha enviado, etc...)
+    '* - Implementa una función para mostrar un texto descriptivo según el estado actual.
+    '* - Crea diferentes pedidos y muestra cómo se interactúa con ellos. 
 
     Enum Status
         PENDING
@@ -93,7 +112,6 @@ Module Program
             Console.WriteLine($"Invalid operation, {Info()}")
         End Sub
 
-
     End Class
 
     Sub Main()
@@ -133,4 +151,5 @@ Module Program
         Console.WriteLine(libro2.Info())
         Console.WriteLine(libro3.Info())
     End Sub
+    
 End Module
