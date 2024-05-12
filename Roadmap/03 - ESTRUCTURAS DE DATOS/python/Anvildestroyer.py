3e5468f8a9454e4a30709351cce701b9a37c0d0c
AnvilDestroyer <seguridad.infomadrid@gmail.com>
2024-05-12 22:06:10 +0200

    03 - ESTRUCTURAS DE DATOS

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/Anvildestroyer.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/Anvildestroyer.py
@@ -1,10 +1,3 @@
-"""
- * EJERCICIO:
- * - Muestra ejemplos de creación de todas las estructuras soportadas por defecto
- *   en tu lenguaje.
- * - Utiliza operaciones de inserción, borrado, actualización y ordenación.
-"""
-
 # Tipo Listas
 lista = [1, 2, 3, 4]
 lista = list("1234")
@@ -97,19 +90,6 @@ print(type(d1))
 
 
 #Extra
-"""
-* DIFICULTAD EXTRA (opcional):
- * Crea una agenda de contactos por terminal.
- * - Debes implementar funcionalidades de búsqueda, inserción, actualización
- *   y eliminación de contactos.
- * - Cada contacto debe tener un nombre y un número de teléfono.
- * - El programa solicita en primer lugar cuál es la operación que se quiere realizar,
- *   y a continuación los datos necesarios para llevarla a cabo.
- * - El programa no puede dejar introducir números de teléfono no númericos y con más
- *   de 11 dígitos (o el número de dígitos que quieras).
- * - También se debe proponer una operación de finalización del programa.
-
-"""
 contactos =  {}
 
 def menu_usuario():
