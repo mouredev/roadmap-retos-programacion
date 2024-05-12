f3c784f3474db7f32f8c685ee7e783268a899f08
Miguel Ángel Delgado <migue@miguelandelgado.com>
2024-05-12 12:08:52 +0200

    #19 - php
    Ejercicio terminado

--- Roadmap/19 - ENUMERACIONES/php/miguelex.php
+++ Roadmap/19 - ENUMERACIONES/php/miguelex.php
@@ -25,11 +25,13 @@ for ($i = 1; $i <= 7; $i++) {
 
 // Extra
 
-enum Status: int {
-    case PENDIENTE = 1;
-    case ENVIADO = 2;
-    case ENTREGADO = 3;
-    case CANCELADO = 4;
+echo "\n\n EJERCICIO EXTRA \n\n"; 
+
+enum Status {
+    case PENDIENTE;
+    case ENVIADO;
+    case ENTREGADO;
+    case CANCELADO;
 }
 class Parcel {
 
@@ -41,8 +43,62 @@ class Parcel {
         $this->status = $status;
     }
 
+    function __toString(){
+        return "Paquete $this->id: {$this->status->name}\n";
+    }
 
+    function sendPackage($id) {
+        if ($this->status == Status::PENDIENTE){
+            echo "El paquete $id ya ha sido enviado\n";
+            $this->status = Status::ENVIADO;
+            return;
+        }
+
+        echo "El paquete $id no se encuentra preparado para el envío o ya no está en el sistema\n";
+    }
+
+    function deliverPackage($id) {
+        if ($this->status == Status::ENVIADO){
+            echo "El paquete $id ha sido entregado\n";
+            $this->status = Status::ENTREGADO;
+            return;
+        }
+
+        echo "El paquete $id no se encuentra en estado de envío\n";
+    }
+
+    function cancelPackage($id) {
+        if ($this->status == Status::PENDIENTE){
+            echo "El paquete $id ha sido cancelado\n";
+            $this->status = Status::CANCELADO;
+            return;
+        }
+
+        echo "El paquete $id no se encuentra en estado de envío\n";
+    }
+    
 }
+
+$parcel1 = new Parcel(1, Status::PENDIENTE);
+$parcel2 = new Parcel(2, Status::PENDIENTE);
+$parcel3 = new Parcel(3, Status::PENDIENTE);
+
+echo $parcel1;
+echo $parcel2;
+echo $parcel3;
+
+$parcel1->sendPackage(1);
+$parcel2->deliverPackage(2);
+$parcel3->cancelPackage(3);
+
+$parcel1->deliverPackage(1);
+$parcel2->sendPackage(2);
+$parcel3->sendPackage(3);
+
+echo $parcel1;
+echo $parcel2;
+echo $parcel3;
+
 
 
 
