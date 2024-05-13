c2f6e8ed28a7ea74176fd811b805cc05f065a18d
gabriel <gabrielcmoris@gmail.com>
2024-05-13 01:42:10 -0400

    Start exercise

---
+++ Roadmap/11 - MANEJO DE FICHEROS/rust/gabrielmoris/src/main.rs
@@ -0,0 +1,31 @@
+/*
+ * IMPORTANT: You should only upload the code file as part of the exercise.
+ *
+ * EXERCISE:
+ * Develop a program capable of creating a file named after
+ * your GitHub username and has the .txt extension.
+ * Add several lines to that file:
+ * - Your name.
+ * - Age.
+ * - Favorite programming language.
+ * Print the content.
+ * Delete the file.
+ */
+fn exercise() {
+    println!("Exercise");
+}
+
+/* EXTRA DIFFICULTY (optional):
+ * Develop a sales management program that stores its data in a
+ * .txt file.
+ * - Each product is saved on a line of the file in the following way:
+ *   [product_name], [quantity_sold], [price].
+ * - Following that format, and through terminal, it should allow adding, consulting,
+ *   updating, deleting products and exiting.
+ * - It should also have options to calculate the total sale and by product.
+ * - The exit option deletes the .txt.
+ */
+
+fn main() {
+    exercise();
+}
---
+++ Roadmap/11 - MANEJO DE FICHEROS/rust/gabrielmoris/target/debug/gabrielmoris.d
@@ -0,0 +1,1 @@
+/config/learn/roadmap-retos-programacion/Roadmap/11\ -\ MANEJO\ DE\ FICHEROS/rust/gabrielmoris/target/debug/gabrielmoris: /config/learn/roadmap-retos-programacion/Roadmap/11\ -\ MANEJO\ DE\ FICHEROS/rust/gabrielmoris/src/main.rs
