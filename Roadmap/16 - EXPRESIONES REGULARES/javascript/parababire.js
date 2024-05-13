27a2678b114f7e2c918721a688a6ae27bd605e5d
Parababire <angelenarvaezm@gmail.com>
2024-05-13 12:49:12 -0400

    #16 - Javascript

---
+++ Roadmap/16 - EXPRESIONES REGULARES/javascript/parababire.js
@@ -0,0 +1,17 @@
+const string = 'ferrocarril';
+const regex = /roca/;
+const resultado = regex.test(string);
+const ocurrencia = string.match(regex)
+const someText = 'El siguiente número primo es el 11, por lo que tachamos todos los múltiplos de 11, que son el 22, 33, 44, 55, 66, 77, 88, y el 99.';
+const matchNum = /\d{2,}/g;
+
+/* console.log(resultado);
+console.log(ocurrencia[0]);
+console.log(someText.match(matchNum));
+ */
+//Extra
+
+const mail = 'angelenarvaezm@gmail.com';
+const regexMail = /\w+([.][\w]+)*@\w+([.][\w]+)*\.[a-zA-Z]{2,5}/;
+console.log(regexMail.test(mail));
+/* Debo buscar prohibir espacios \s */
