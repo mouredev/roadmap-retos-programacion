4260ea8b5f5ab3643944c8cdefc8abe339dc18f9
Johnny S. Bermúdez Soria <johnny.sbsoria@gmail.com>
2024-05-15 01:24:26 +0200

    #00 - Javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Johnnysbers.js
@@ -0,0 +1,49 @@
+// EJERCICIO:
+// - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// Comentario en una línea
+
+/*
+Comentario
+en varias
+líneas
+*/
+
+// - Crea una variable (y una constante si el lenguaje lo soporta).
+
+let myName  = 'Johnny';
+var myColor = 'Rojo';
+const myAge = 28;
+
+// - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+// String
+let cadenaDeTexto = 'Hola mundo';
+
+// Number
+let numeroEntero  = 10;
+let numeroDecimal = 15.75;
+
+// BigInt
+let numeroGrande = 10n;
+
+// Boolean
+let boleanoVerdadero = true;
+let boleanoFalso     = false;
+
+// Undefined
+let indefinido = undefined;
+
+// Null
+let nulo = null;
+
+// Symbol
+let simbolo = Symbol('id');
+
+// - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+console.log('¡Hola, JavaScript!');
