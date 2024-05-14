e99829430bff02c8b679f30baf643b835b7179b2
JoseC4str0 <80718589+JoseC4str0@users.noreply.github.com>
2024-05-14 17:42:30 -0600

    JoseC4str0.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/JoseC4str0.py
@@ -0,0 +1,56 @@
+# Este es un comentario
+
+# Sitio oficial del lenguaje con el que comenzaré a aprender
+# https://www.python.org/
+
+# Comentario en una línea
+"""
+Esto también es un comentario pero solo que en varias líneas
+como muestra realizaré
+saltos
+de
+línea
+"""
+
+'''Esto es otra forma de agregar comentarios pero con comillas simples y puede funcionar en varias líneas'''
+
+my_var = "Esto es una variable"
+my_var = "Modifique el valor de la variable"
+
+# Python no tiene constantes, no es aplicable para este lenguaje
+# Comunmente si se quiere definir una constante en python, se hace en mayúsculas y con ello se interpreta constante
+
+MY_CONSTANT = "VALOR 1"
+
+# Ejemplo
+print(MY_CONSTANT)
+
+# Tipos de datos primitivos
+
+# Entero
+my_int = 20
+
+# Decimales (float)
+my_decimal = 1.5
+
+# Bool (0/1)
+
+my_bool = True
+my_bool = False
+
+# String
+
+my_string = "Mi cadena de texto"
+my_other_string = 'Otra cadena de texto'
+sitio_web_lng= "https://www.python.org/"
+
+print("Mi lenguaje es: Python, puedes encontrar mas información en ",sitio_web_lng)
+
+print("Validaremos los tipos de datos de nuestras variables")
+print(type(my_int))
+print(type(my_decimal))
+print(type(my_string))
+
+
+
+
