89d6fe2b320f85e987ae524479186bcffacfceed
riacosta <rigo93acosta@gmail.com>
2024-05-17 18:05:35 -0300

    Init

---
+++ Roadmap/19 - ENUMERACIONES/python/rigo93acosta.py
@@ -0,0 +1,23 @@
+'''
+ * EJERCICIO:
+ * Empleando tu lenguaje, explora la definición del tipo de dato
+ * que sirva para definir enumeraciones (Enum).
+ * Crea un Enum que represente los días de la semana del lunes
+ * al domingo, en ese orden. Con ese enumerado, crea una operación
+ * que muestre el nombre del día de la semana dependiendo del número entero
+ * utilizado (del 1 al 7).
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un pequeño sistema de gestión del estado de pedidos.
+ * Implementa una clase que defina un pedido con las siguientes características:
+ * - El pedido tiene un identificador y un estado.
+ * - El estado es un Enum con estos valores: PENDIENTE, ENVIADO, ENTREGADO y CANCELADO.
+ * - Implementa las funciones que sirvan para modificar el estado:
+ *   - Pedido enviado
+ *   - Pedido cancelado
+ *   - Pedido entregado
+ *   (Establece una lógica, por ejemplo, no se puede entregar si no se ha enviado, etc...)
+ * - Implementa una función para mostrar un texto descriptivo según el estado actual.
+ * - Crea diferentes pedidos y muestra cómo se interactúa con ellos. 
+'''
+
