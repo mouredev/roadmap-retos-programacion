6fdfbd3c2b1351223a4d20148a7f27b3418adf1f
gabriel <gabrielcmoris@gmail.com>
2024-05-18 17:15:55 -0400

    manage files in rust

--- Roadmap/11 - MANEJO DE FICHEROS/rust/gabrielmoris/src/main.rs
+++ Roadmap/11 - MANEJO DE FICHEROS/rust/gabrielmoris/src/main.rs
@@ -11,8 +11,39 @@
  * Print the content.
  * Delete the file.
  */
-fn exercise() {
-    println!("Exercise");
+use std::fs::{File, OpenOptions};
+// use std::io::prelude::*;
+use std::io::{Error, Read, Write};
+
+fn create_file(filename: &str) -> Result<(), Error> {
+    let file_name = format!("{}.txt", filename);
+    let _ = File::create(file_name);
+    Ok(())
+}
+
+fn write_file(filename: &str, contents: &str) -> Result<(), Error> {
+    let file_name = format!("{}.txt", filename);
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(file_name)?;
+
+    file.write_all(contents.as_bytes())?;
+    Ok(())
+}
+
+fn read_file(filename: &str) -> Result<String, Error> {
+    let file_name = format!("{}.txt", filename);
+    let mut file = File::open(file_name)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+fn delete_file(filename: &str) -> Result<(), Error> {
+    let file_name = format!("{}.txt", filename);
+    std::fs::remove_file(file_name)?;
+    Ok(())
 }
 
 /* EXTRA DIFFICULTY (optional):
@@ -27,5 +58,28 @@ fn exercise() {
  */
 
 fn main() {
-    exercise();
+    match create_file("gabrielcmoris") {
+        Ok(()) => println!("File Created sucessfully"),
+        Err(err) => println!("There was an error: {}", err),
+    }
+
+    match write_file(
+        "gabrielcmoris",
+        "Name: Gabriel\nAge: 34\nFavorite programming language: Assembly",
+    ) {
+        Ok(()) => println!("File writted sucessfully"),
+        Err(err) => println!("There was an error: {}", err),
+    }
+
+    match read_file("gabrielcmoris") {
+        Ok(file) => println!("File contents {:?}", file),
+        Err(err) => println!("There was an error: {}", err),
+    }
+
+    match delete_file("gabrielcmoris") {
+        Ok(()) => println!("File deleted sucessfully"),
+        Err(err) => println!("There was an error: {}", err),
+    }
 }
+
+// std::fs::remove_file("filename.txt")?;
