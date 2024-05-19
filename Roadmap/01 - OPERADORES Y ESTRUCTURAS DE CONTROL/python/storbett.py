615ccad92f21faf12026e24dc916efe50deb5cec
Simon <simontorbett@gmail.com>
2024-05-19 18:26:09 -0400

    .

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/storbett.py
@@ -0,0 +1,50 @@
+
+
+#operadore aritmeticos
+print (f"suma: 10+3 = {10+3}")
+print (f"resta: 10-3 = {10-3}")
+print (f"mulriplicacion: 10*3 = {10*3}")
+print (f"division: 10/3 = {10/3}")
+print (f"modulo: 10%3 = {10%3}")
+print (f"exponente: 10**3 = {10**3}")
+print (f"division entera: 10//3 = {10//3}")
+
+#operadores de comparacion
+
+print (f"igualdad 10 == 3 es {10 == 3}")
+print (f"desigualdad 10 != 3 es {10 != 3}")
+print (f"mayor que 10 > 3 es {10 > 3}")
+print (f"menor que 10 < 3 es {10 < 3}")
+print (f"mayor o igual que 10 >= 3 es {10 > 3}")
+print (f"menor o igual que 10 <= 3 es {10 < 3}")
+
+#operadores logicos
+
+print (f"AND &&: 10 + 3 == 13 and 5 -1 == 4 es {10 + 3 == 13 and 5 - 1 == 4}")
+print (f"OR: 10 + 3 == 13 or 5 -1 == 4 es {10 + 3 == 13 or 5 - 1 == 4}")
+print (f"NOT !: not 10 + 3 == 14 es {not 10 + 3 == 14}")
+
+
+# operadores de asignacion 
+my_number = 11 #asignacion
+print (my_number)
+my_number += 1
+print (my_number)
+my_number -= 1
+print (my_number)
+my_number *= 2
+print (my_number)
+my_number /= 2
+print (my_number)
+my_number %= 2
+print (my_number)
+my_number **= 1
+print (my_number)
+my_number //= 1
+
+# operadores de identidad 
+my_new_number = my_number
+print (f"my_number is my_new_number es {my_number is my_new_number}")
+print (f"my_number is not my_new_number es {my_number is not my_new_number}")
+
+#operadores de pertenecia 
