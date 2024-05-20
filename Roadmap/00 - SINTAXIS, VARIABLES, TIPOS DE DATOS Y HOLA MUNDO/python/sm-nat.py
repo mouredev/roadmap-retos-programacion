89b9fabf74b7f7d5f7a97f69e3bf9ccdaf361ac7
Natalia SM <fu.pro.nat@gmail.com>
2024-05-20 21:52:31 +1000

    primer ejercicio

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/sm-nat.py
@@ -0,0 +1,33 @@
+#Pagina oficial de python https://www.python.org/
+
+"""
+para comentar bloques de codigo
+se puede utilizar comillas dobles triples 
+"""
+
+'''
+o puedes utilizar comillas
+simples como estas
+'''
+
+# tambien se utiliza el  # para comentar lineas de codigo
+
+#una variables:
+variable = "su valor puede cambiar"
+
+#una constante:
+CONSTANTE = "su valor no debe cambiar y se usa  mayusculas para declararlas"
+
+#Tipos de datos
+
+enteros = 1
+flotantes = 1.0
+complejos = 1 + 2j  
+booleanos = True
+strings = "hola mundo"
+listas = [1,2,3,4,5]
+tuplas = (1,2,3,4,5)
+diccionarios = {"nombre":"juan","apellido":"perez"}
+conjuntos = {1,2,3,4,5}
+
+print("¡Hola, Python!")
