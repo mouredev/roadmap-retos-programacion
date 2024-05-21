37dd7a3672f91d176cc94f8958377603978233ad
Natalia SM <fu.pro.nat@gmail.com>
2024-05-21 19:06:37 +1000

    operadores aritmeticos

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sm-nat.py
@@ -0,0 +1,23 @@
+# operadores Aritméticos
+
+suma = 2 + 2
+print(suma)
+
+resta = 5 - 2
+print(resta)
+
+multiplicacion = 5 * 5
+print(multiplicacion)
+
+division = 10 / 2
+print(division)
+
+division_entera = 32 // 4 # devuelve el numero entero del cociente
+print(division_entera)
+
+modulo = 5 % 3 # Devuelve el resto de la división del primer operando por el segundo
+print(modulo)
+
+exponencial = 5 ** 2
+print(exponencial)
+
