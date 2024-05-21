0e9a05aa4cd808b711badd3a1393ebfd3d60c6ff
Ramón Almeida Sánchez <ramonalmeidabkb@gmail.com>
2024-05-21 23:52:20 +0100

    #01 - Python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/ramon-almeida.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/ramon-almeida.py
@@ -13,7 +13,8 @@ print(resto_división)
 
 """ Crea un programa que imprima por consola todos los números comprendidos
  entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
-"""
+
+ """
 
 result = []
 
