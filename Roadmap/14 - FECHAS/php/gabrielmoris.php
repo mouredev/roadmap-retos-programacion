153d8310d91019063615f8f9fb6d2acd694ab646
gabriel <gabrielcmoris@gmail.com>
2024-05-22 09:51:57 -0400

    #14 - php

--- Roadmap/14 - FECHAS/php/gabrielmoris.php
+++ Roadmap/14 - FECHAS/php/gabrielmoris.php
@@ -11,23 +11,23 @@
 date_default_timezone_set('Europe/Berlin'); // This will set the date zone in general
 echo date(DATE_RSS) . "\n"; // Output: Mon, 22 May 2023 15:02:34 +0000
 echo date(DATE_ATOM) . "\n"; // Output: 2023-05-22T15:02:34+00:00
-
+$count = 0;
 do {
     echo "\033c";
     $currentDateTime = date('D d.m.Y H:i:s');
     echo "Current date and time: " . $currentDateTime . "\n";
     $BDDateTime = new DateTime('1989-09-17 03:45:00');
     $formatedBD = $BDDateTime->format("D d.m.Y H:i:s");
-    echo  "You born " . $formatedBD . "\n";
+    echo  "I born " . $formatedBD . "\n";
     echo "\n\n";
     $currDate = new DateTime($currentDateTime);
     $timedifference = $BDDateTime->diff($currDate);
 
-    echo "You are " . round($timedifference->days / 365, 1) . " years old.\n";
+    echo "I am " . round($timedifference->days / 365, 1) . " years old.\n";
 
-
+    $count += 1;
     sleep(1);
-} while (true);
+} while ($count < 5);
 
 
 /* EXTRA DIFFICULTY (optional):
@@ -40,3 +40,17 @@ do {
 * Name of the month.
 * (whatever you can think of...)
 */
+
+echo "My BD in different formats:\n";
+
+$BDDateTime = new DateTime('1989-09-17 03:45:00');
+echo "Date " . $BDDateTime->format('d.m.Y') . "\n";
+echo "Hour " . $BDDateTime->format('H:i:s') . "\n";
+echo "Day of the Year " . $BDDateTime->format('z') . "\n";
+echo "Day of the week " . $BDDateTime->format('l') . "\n";
+echo "Name of the Month " . $BDDateTime->format('F') . "\n";
+echo "ISO format " . $BDDateTime->format('c') . "\n";
+$now = new DateTime();
+$interval = $BDDateTime->diff($now);
+echo "Time Passed: " . floor($interval->format('%R%a') / 365) . " Years \n";
+echo "I born a " . $BDDateTime->format('l, jS F Y \a\t g:ia') . "\n";
