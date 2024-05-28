d4bbae6b15bedfbec89d1416cf516c60111585c4
Simon <simontorbett@gmail.com>
2024-05-27 20:36:36 -0400

    .py

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/storbett.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/storbett.py
@@ -28,10 +28,32 @@ print(mi_tupla)
 print(type(mi_tupla)) 
 
 
-# set 
+# set #no se duplican los datos#
+#no se puede ordenar#
+
 
 mi_set = {"simon", "torbett", "storbett", "36"}
 print(mi_set)
 mi_set.add("simontorbett@gmail.com")
+mi_set.remove("storbett") #elimicacion
 print(mi_set)
+
 print(type(mi_set))
+
+#diccionario
+
+mi_diccionario: dict = {
+    "nombre": "simon",
+    "apellido": "torbett",
+    "edad": "33"
+}
+
+mi_diccionario["email"] = "simontorbett@gmail.com" # insercion
+print(mi_diccionario)
+del mi_diccionario["apellido"] # elimimcacion
+print(mi_diccionario)
+mi_diccionario["edad"] = "34" #actualizacion
+print(mi_diccionario)
+mi_diccionario = dict(sorted(mi_diccionario.items()))
+print(mi_diccionario)
+print(type(mi_diccionario))
