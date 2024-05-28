729ab7b9ffc9e70827295fe4d9be4a45038d2480
Simon <simontorbett@gmail.com>
2024-05-28 19:54:50 -0400

    .

--- Roadmap/03 - ESTRUCTURAS DE DATOS/python/storbett.py
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/python/storbett.py
@@ -57,3 +57,58 @@ print(mi_diccionario)
 mi_diccionario = dict(sorted(mi_diccionario.items()))
 print(mi_diccionario)
 print(type(mi_diccionario))
+
+# extra
+
+def mi_agenda():
+
+    agenda = {}
+
+    def insert_contact():
+        phone = input("Introduce el teléfono del contacto: ")
+        if phone.isdigit() and len(phone) > 0 and len(phone) <= 11:
+            agenda[name] = phone
+        else:
+            print(
+                "Debes introducir un número de teléfono un máximo de 11 dígitos.")
+
+    while True:
+
+        print("")
+        print("1. Buscar contacto")
+        print("2. Insertar contacto")
+        print("3. Actualizar contacto")
+        print("4. Eliminar contacto")
+        print("5. Salir")
+
+        option = input("\nSelecciona una opción: ")
+
+        match option:
+            case "1":
+                name = input("Introduce el nombre del contacto a buscar: ")
+                if name in agenda:
+                    print(
+                        f"El número de teléfono de {name} es {agenda[name]}.")
+                else:
+                    print(f"El contacto {name} no existe.")
+            case "2":
+                name = input("Introduce el nombre del contacto: ")
+                insert_contact()
+            case "3":
+                name = input("Introduce el nombre del contacto a actualizar: ")
+                if name in agenda:
+                    insert_contact()
+                else:
+                    print(f"El contacto {name} no existe.")
+            case "4":
+                name = input("Introduce el nombre del contacto a a eliminar: ")
+                if name in agenda:
+                    del agenda[name]
+                else:
+                    print(f"El contacto {name} no existe.")
+            case "5":
+                print("Saliendo de la agenda.")
+                break
+            case _:
+                print("Opción no válida. Elige una opción del 1 al 5.")
+mi_agenda()
