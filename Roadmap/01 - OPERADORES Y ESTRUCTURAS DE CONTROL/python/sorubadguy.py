933bf07dbe410c2ede74070174965c5d8b3ad36c
Grull <lucasmartinez33@gmail.com>
2024-05-29 02:12:59 -0300

    #01 - python

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sorubadguy.py
@@ -0,0 +1,14 @@
+num1 = 2
+num2 = 10
+num3 = 100
+#Operadores Aritmeticos
+
+print(f"suma: {num1} + {num2} = {num1 + num2}")
+print(f"resta: {num2} - {num1} = {num2 - num1}")
+print(f"multiplicacion: {num1} * {num2} = {num1 * num2}")
+print(f"divicion: {num2} / {num1} = {num2 / num1}")
+print(f"suma: {num1} + {num2} = {num1 + num2}")
+print(f"modulo: {num2} % {num1} = {num2 % num1}")
+print(f"exponente: {num1} ** {num2} = {num1 ** num2}")
+print(f"division entera: {num3} // {num2} = {num3 // num2}")
+
