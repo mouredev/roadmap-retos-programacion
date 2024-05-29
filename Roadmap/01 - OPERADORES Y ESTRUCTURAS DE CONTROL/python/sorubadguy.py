39c35ee1d5006714b1b5727b57ca59e3c9d1e628
Grull <lucasmartinez33@gmail.com>
2024-05-29 02:17:40 -0300

    Operadores comparacion

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sorubadguy.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sorubadguy.py
@@ -12,3 +12,12 @@ print(f"modulo: {num2} % {num1} = {num2 % num1}")
 print(f"exponente: {num1} ** {num2} = {num1 ** num2}")
 print(f"division entera: {num3} // {num2} = {num3 // num2}")
 
+#Operadores de comparacion
+
+print(f"igual que: {num1} == {num2} = {num1 == num2}")
+print(f"distinto que: {num1} != {num2} = {num1 != num2}")
+print(f"mayor que: {num1} > {num2} = {num1 > num2}")
+print(f"menor que: {num1} < {num2} = {num1 < num2}")
+print(f"mayor o igual que: {num1} >= {num2} = {num1 >= num2}")
+print(f"menor o igual que: {num1} <= {num2} = {num1 <= num2}")
+
