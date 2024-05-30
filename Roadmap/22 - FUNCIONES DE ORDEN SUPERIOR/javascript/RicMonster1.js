f8ff4b8776112db54916365f14da478f93647f18
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-05-30 09:15:12 -0400

    #22 - JavaScript

--- Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/javascript/RicMonster1.js
+++ Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/javascript/RicMonster1.js
@@ -14,34 +14,21 @@ Array.prototype.customMap = function (callback) {
 
 console.log(arr.customMap((n) => n * 2));
 
-/*
- * DIFICULTAD EXTRA (opcional):
- * Dada una lista de estudiantes (con sus nombres, fecha de nacimiento y
- * lista de calificaciones), utiliza funciones de orden superior para
- * realizar las siguientes operaciones de procesamiento y análisis:
- * - Promedio calificaciones: Obtiene una lista de estudiantes por nombre
- *   y promedio de sus calificaciones.
- * - Mejores estudiantes: Obtiene una lista con el nombre de los estudiantes
- *   que tienen calificaciones con un 9 o más de promedio.
- * - Nacimiento: Obtiene una lista de estudiantes ordenada desde el más joven.
- * - Mayor calificación: Obtiene la calificación más alta de entre todas las
- *   de los alumnos.
- * - Una calificación debe estar comprendida entre 0 y 10 (admite decimales).
- */
-
+//EXTRA
 let myStudents = [];
 let bestStudents = [];
 
 class Student {
 	constructor(name, grades, birthDate) {
 		this.name = name;
-		this.grades = grades;
 		this.birthDate = birthDate;
+		this.grades = grades;
 		this.age = this.getAge();
 		this.average = this.getAverage();
+
 		myStudents.push(this);
 
-		if (this.getAverage() >= 9) {
+		if (this.average >= 9) {
 			bestStudents.push(this);
 		}
 	}
@@ -57,44 +44,72 @@ class Student {
 
 	getAge() {
 		let now = new Date();
-		let age = Math.floor(
-			(now.getTime() - this.birthDate.getTime()) / (365 * 24 * 60 * 60 * 1000)
-		);
+		let age =
+			(now.getTime() - this.birthDate.getTime()) / (365 * 24 * 60 * 60 * 1000);
 		return age;
 	}
 }
 
 new Student('Sophie', [8, 8, 7, 9], new Date(2002, 8, 21));
+new Student('Rebeca', [8, 9, 10, 9], new Date(2001, 4, 16));
 new Student('David', [7, 6, 9, 8], new Date(2003, 0, 15));
 new Student('Eliezer', [4, 6, 7, 5], new Date(2005, 1, 21));
-new Student('Rebeca', [8, 9, 10, 9], new Date(2001, 4, 16));
 new Student('Robert', [10, 10, 10, 9], new Date(2002, 0, 16));
 
 function getHighestAverage() {
 	let result;
 	let average = 0;
 	bestStudents.forEach((element) => {
-		if (element.getAverage() > average) {
+		if (element.average > average) {
 			result = element;
 		}
 	});
 	return result;
 }
-
 let highestAverage = getHighestAverage();
 
-function sortByAge() {}
+//Pendiente por revisar
+function sortByAge() {
+	for (let i = 0; i < myStudents.length; i++) {
+		for (let j = i + 1; j < myStudents.length; j++) {
+			if (myStudents[i].age > myStudents[j].age) {
+				let change = myStudents[i];
+				myStudents[i] = myStudents[j];
+				myStudents[j] = change;
+			}
+		}
+	}
+
+	return myStudents;
+}
+
+let sortedByAge = sortByAge();
+
+let sortedByAverage = myStudents.sort(function (a, b) {
+	return a.average - b.average;
+});
+
+console.log('\nLISTA DE PROMEDIOS');
+
+sortedByAverage.customMap((element) => {
+	console.log(`\n${element.name}: ${element.average}`);
+});
+
+console.log('\nMEJORES ESTUDIANTES');
+
+bestStudents.customMap((element) => {
+	console.log(`\n${element.name}: ${element.average}`);
+});
+
+console.log('\nLISTA DE NACIMIENTOS');
+
+sortedByAge.customMap((element) => {
+	console.log(`\n${element.name}: ${element.birthDate.toLocaleString()}`);
+	console.log(`${Math.floor(element.age)} años`);
+});
+
+console.log('\nMEJOR PROMEDIO DE LA CLASE');
 
 console.log(
-	`\n${highestAverage.name} ha obtenido el mayor promedio de la clase: ${highestAverage.average}\n`
+	`\nFelicidades a ${highestAverage.name}. Con un ${highestAverage.average} se ha coronado como el mejor promedio de la clase`
 );
-
-console.log(myStudents.find((element) => element.name === 'David'));
-
-myStudents.customMap((element) => {
-	console.log(`\nEl promedio de ${element.name} es de ${element.average}`);
-});
-
-myStudents.customMap((element) => {
-	console.log(`\nEl nacimiento de ${element.name} fue el ${element.birthDate}`);
-});
