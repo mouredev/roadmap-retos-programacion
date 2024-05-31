79a1bbec5bab9cdddb085121a7f042bd98fc6353
Simon <simontorbett@gmail.com>
2024-05-30 21:01:57 -0400

    .

---
+++ Roadmap/04 - CADENAS DE CARACTERES/python/storbett.py
@@ -0,0 +1,33 @@
+#operaciones
+
+
+s1 = "hola"
+s2 = "python"
+
+#concatenacion
+print (s1 + ", " + s2 + "!")
+
+#repeticion
+
+print (s1 * 3)
+
+#idexacion
+
+print (s1[0] + s1[1] + s1[2] + s1[3])
+
+#longitud
+
+print(len(s1) + len(s2))
+
+#slicing (porcion)
+
+print( s2[2:5])
+print( s2[2:])
+print(s2[0:2])
+print(s2[:2])
+
+#busqueda
+
+print ("a" in s1)
+
+#
