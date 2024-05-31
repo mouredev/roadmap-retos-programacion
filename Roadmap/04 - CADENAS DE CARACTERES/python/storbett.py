dcf9caca7fc092d83e8f0ede4a0fa6c7273160fc
Simon <simontorbett@gmail.com>
2024-05-30 21:22:47 -0400

    .

--- Roadmap/04 - CADENAS DE CARACTERES/python/storbett.py
+++ Roadmap/04 - CADENAS DE CARACTERES/python/storbett.py
@@ -30,4 +30,36 @@ print(s2[:2])
 
 print ("a" in s1)
 
-#
+#reemplazo
+
+print(s1.replace("o", "a"))
+
+# Division
+
+print(s2.split("t"))
+
+# mayusculas y minisculas
+
+print(s2.upper())
+print(s2.lower())
+print("simon torbett".title())
+print("simon torbett".capitalize())
+
+# elimicacion del espacio al principio y al finas
+
+print(" simon torbett ".strip() + "lo lograras")
+
+#busqueda al principio y al final
+
+print(s1.startswith("ho"))
+print(s1.startswith("py"))
+
+s3 = "simon torbett"
+# Busqueda de posicion
+
+print("simon torbett @storbett".find("torbett"))
+
+#busaqueda de ocurrencias
+
+print(s3.lower().count("t"))
+
