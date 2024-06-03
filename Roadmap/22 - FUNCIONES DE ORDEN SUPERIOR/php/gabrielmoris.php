b755290cf600dd1044c0cd43571d551392ed0edf
gabriel <gabrielcmoris@gmail.com>
2024-06-03 04:50:35 -0400

    start

---
+++ Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/php/gabrielmoris.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * EXERCISE:
+ * Explore the concept of higher-order functions in your language
+ * by creating simple examples (of your choice) that demonstrate their operation.
+ */
+
+ 
+
+ /*
+ * EXTRA DIFFICULTY (optional):
+ * Given a list of students (with their names, date of birth, and
+ * list of grades), use higher-order functions to perform the following
+ * processing and analysis operations:
+ * - Grade average: Get a list of students by name and their average grade.
+ * - Top students: Get a list with the names of students who have an average
+ *   grade of 9 or higher.
+ * - Birth date: Get a list of students ordered from youngest to oldest.
+ * - Highest grade: Get the highest grade among all students.
+ * - A grade must be between 0 and 10 (decimal values allowed).
+ */
