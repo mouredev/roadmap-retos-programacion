e7ef34ac10d11491929036847022572cac357e84
Javi <Jav.molh@gmail.com>
2024-06-03 01:13:10 -0300

    Extra, Reto #11

--- Roadmap/11 - MANEJO DE FICHEROS/python/Jav-mol.py
+++ Roadmap/11 - MANEJO DE FICHEROS/python/Jav-mol.py
@@ -19,3 +19,29 @@ if os.path.exists(file):
         
 # --- Extra ---
 
+# Crear Fichero
+#with open('Jav-mol.txt', 'w') as file:
+#   
+#    file.write(f'[Jabon], [10], [23] \n')
+#    file.write(f'[Arroz], [20], [10] \n')
+#    file.write(f'[Fideos], [20], [10]')
+
+
+with open('Jav-mol.txt', 'a+') as file1:
+    file1.seek(0)
+    texto = file1.read()
+    
+    product = input('Ingrese el producto: ')
+    
+    
+    inicio = texto.find(product) - 1
+    
+    print(inicio)
+    
+    
+    fin = inicio + len(product) + 14
+    
+    print(fin)
+    
+    lista = texto[inicio:fin] #.split(', ')
+    print(lista)
