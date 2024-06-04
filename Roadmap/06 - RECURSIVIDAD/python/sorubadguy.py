b77f13ebd58a3d03657b4e6d9e72705c67fc7bf8
Grull <lucasmartinez33@gmail.com>
2024-06-04 15:01:11 -0300

    06 - python factorial finalizado

--- Roadmap/06 - RECURSIVIDAD/python/sorubadguy.py
+++ Roadmap/06 - RECURSIVIDAD/python/sorubadguy.py
@@ -2,7 +2,7 @@
 *Recursividad
 """
 
-def numeros(numero):
+def numeros(numero: int):
     if(numero<100):
         numero += 1
         numeros(numero)
@@ -10,3 +10,19 @@ def numeros(numero):
     
 
 numeros(0)
+
+"""
+!Extra
+"""
+
+def factorial(numero: int, resultado = 1):
+    if(numero <= 0):
+        print(resultado)
+    else:
+        resultado *= numero
+        numero -= 1
+        factorial(numero, resultado)
+
+factorial(int(input("de que numero entero desea calcular el factorial?: ")))
+
+#def fibonacci(posicion: int, resultado)
