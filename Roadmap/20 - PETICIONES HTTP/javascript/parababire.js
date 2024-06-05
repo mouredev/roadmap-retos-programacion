26b29a75beffb4cb66d74c02dfa7580c21c21ba0
Parababire <angelenarvaezm@gmail.com>
2024-06-05 09:02:28 -0400

    #20 - Javascript

--- Roadmap/20 - PETICIONES HTTP/javascript/parababire.js
+++ Roadmap/20 - PETICIONES HTTP/javascript/parababire.js
@@ -1,24 +1,10 @@
-async function extraerCotizacion() {
-  try {
-    const URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
-    const RES_API = await fetch(URL);
-    const JSON = await RES_API.json();
-    console.log(`La cotización del bitcoin es: ${JSON.bpi.USD.rate_float} $`);
-  } catch (error) {
-    console.error("TypeError");
-  }
-}
-
-extraerCotizacion();
-
-fetch('https://api.coindesk .com/v1/bpi/currentprice.json')
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
   .then(response => {
-      if (response.status === 200) {
-        return response.json();
-      } else {
-        console.log(error);
-      }
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
     }
   )
   .then(data => console.log(`La cotización del bitcoin es: ${data.bpi.USD.rate_float}$`))
-  .catch(error=> console.log("Hay un error que no está entre 200 y 50x"));
+  .catch(error=> console.log(error));
