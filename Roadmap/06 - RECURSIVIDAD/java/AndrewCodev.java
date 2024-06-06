a315d8c37193c3dac4821dc2be91e46aad4e4a4d
AndrewCodev <andrewcodev@gmail.com>
2024-06-06 01:20:31 -0500

    #06 - Java
    Se corrige el punto del ejercicio con el factorial validando el ingreso de números negativos y retirando el return de la primera función

--- Roadmap/06 - RECURSIVIDAD/java/AndrewCodev.java
+++ Roadmap/06 - RECURSIVIDAD/java/AndrewCodev.java
@@ -1,52 +1,55 @@
-package ejercicio06;
-
 public class AndrewCodev {
-	
-	//Listar números del 100 al 0
-	public static int listarNumeros(int numero) {
-		if(numero == 0) {
-			return 0;
-		}else {
+
+	// Listar números del 100 al 0
+	public static void listarNumeros(int numero) {
+		if (numero >= 0) {
 			System.out.println(numero);
-			return listarNumeros(numero - 1);
+			listarNumeros(numero - 1);
 		}
 	}
-	
-	//Calcular el factorial de un número
-	public static int factorial(int numero) {		
-		if(numero == 0) {
+
+	// Calcular el factorial de un número
+	public static int factorial(int numero) {
+		if (numero < 0) {
+			System.out.println("\nEl número no es valido para realizar la función factorial");
+			return 0;
+		} else if (numero == 0) {
 			return 1;
-		}else {
+		} else {
 			return numero * factorial(numero - 1);
 		}
 	}
-	
-	//Buscar elemento de la serie de Fibonacci segun la posición enviada
+
+	// Buscar elemento de la serie de Fibonacci segun la posición enviada
 	public static int posicionFibonacci(int posicion) {
-		if(posicion == 0) {
+		if (posicion == 0) {
 			return 0;
-		}else if(posicion == 1){
+		} else if (posicion == 1) {
 			return 1;
-		}else {
-			return posicionFibonacci(posicion - 1) + posicionFibonacci(posicion -2);
-		}		
+		} else {
+			return posicionFibonacci(posicion - 1) + posicionFibonacci(posicion - 2);
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		//Impresión del 100 al 0;
+		// Impresión del 100 al 0;
 		int numero = 100;
-		int impresion = listarNumeros(numero);
-		System.out.println(impresion);
-		
-		//Factorial
+		listarNumeros(numero);
+
+		// Factorial
 		int numero1 = 5;
 		int factorial = factorial(numero1);
-		System.out.println("\nEl factorial de: "+numero1+" es: "+factorial);
-		
-		//Posicion en la serie de Fibonacci
-		int posicion  = 6;
-		int elemento = posicionFibonacci(posicion);
-		System.out.println("\nLa posición: "+posicion+" en la serie de Fibonacci "
-				+ "corresponde al elemento: "+elemento);
+		if (factorial > 0) {
+			System.out.println("\nEl factorial de: " + numero1 + " es: " + factorial);
+		}
+		// Posicion en la serie de Fibonacci
+		int posicion = -6;
+		if (posicion < 0) {
+			System.out.println("\nEl número no es valido para realizar la función factorial");
+		} else {
+			int elemento = posicionFibonacci(posicion);
+			System.out.println("\nLa posición: " + posicion + " en la serie de Fibonacci " + "corresponde al elemento: "
+					+ elemento);
+		}
 	}
 }
