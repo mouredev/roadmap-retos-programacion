6cc18789dddb0a5bf8a420fd2783c11d8b4bf5e3
Javi <Jav.molh@gmail.com>
2024-06-06 12:47:41 -0300

    Retoques del ejercicio

--- Roadmap/11 - MANEJO DE FICHEROS/python/Jav-mol.py
+++ Roadmap/11 - MANEJO DE FICHEROS/python/Jav-mol.py
@@ -16,26 +16,30 @@ file = 'Jav-mol.txt'
 
 if os.path.exists(file):
     os.remove(file)
+
         
 # --- Extra ---
 
 import os
 index = None
+caracteres = lambda:print('-'*25)
 
 def delete_file():
     file = 'Jav-mol.txt'
     if os.path.exists(file):
-        os.remove(file)
-
+        #os.remove(file)
+        pass
     return True
 
 def add_product():
     
     with open('Jav-mol.txt', 'a+') as file:
-    
+        
+        caracteres()
         product = input('Ingrese el producto: ')
         stock = input('Ingrese la cantidad: ')
         precio = input('Ingrese el precio: ')
+        caracteres()
         
         file.write(f'[{product}], [{stock}], [${precio}]' + '\n')
 
@@ -43,20 +47,22 @@ def add_product():
 def listening_product():
     with open('Jav-mol.txt', 'r') as file:
         texto = file.read()
+        caracteres()
         print(texto)
-
-
+        caracteres()
+        
 list_to_str = lambda lista:f'{lista[0]}, {lista[1]}, {lista[2]}'
-
 def update_product(products, index, lista):
         total = input('Ingrese la cantidad vendida: ')
         products[1] = f'[{total}]'
         price = input('Ingrese el precio: ')
         products[2] = f'[${price}]'
         
-        lista_products = list_to_str(products)
-        lista[index] = lista_products
+        lista_str_products = list_to_str(products)
+        lista[index] = lista_str_products
         
+        print(lista_str_products)
+        caracteres() 
         return lista 
         
 def edit_product():    
@@ -64,6 +70,8 @@ def edit_product():
         texto = file.read()
         text_list = texto.split('\n')
         lista_final = None
+        
+        caracteres()
         product = input('Ingrese el producto: ')
         
         for num, lista in enumerate(text_list):
@@ -71,10 +79,11 @@ def edit_product():
                 index = num
                 products = lista.split(', ')
 
-                lista_final = update_product(products, index, lista)
+                lista_final = update_product(products, index, text_list)
             
         if not lista_final:
             print('Producto no encontrado')
+            caracteres()
         else:            
             with open('Jav-mol.txt', 'w') as file:
                 for produc in lista_final:
@@ -94,28 +103,37 @@ def delete_product():
         
         if index >= 0:
             deleted = text_list.pop(index)
+            print(deleted)
             with open('Jav-mol.txt', 'w') as file:
                 for produc in text_list:
                     if produc == '':
                         continue
                     file.write(f'{produc}\n')
+            caracteres()
                     
         else:
             print('Producto no encontrado')
+            caracteres()
 
 
+delete_caract = lambda x: int(x.replace('[', '').replace(']', '').replace('$', ''))
 def ventas_por_producto():    
     with open('Jav-mol.txt', 'r') as file:
         texto = file.read()
         text_list = texto.split('\n')
+        
+        caracteres()
         product = input('Ingrese el producto: ')        
         
         for lista in text_list:
             if product in lista:
                 products = lista.split(', ')
 
-                venta_total = int(products[1][1:3]) * int(products[2][2:4])
-        print(venta_total)
+                venta_total = delete_caract(products[1]) * delete_caract(products[2])
+        print(f'${venta_total}')
+        caracteres()
+
+
 
 def ventas_total():    
     with open('Jav-mol.txt', 'r') as file:
@@ -127,8 +145,11 @@ def ventas_total():
             if lista:
                 products = lista.split(', ')
 
-                venta_total += int(products[1][1:3]) * int(products[2][2:4])
+                venta_total += delete_caract(products[1]) * delete_caract(products[2])
+
+        caracteres()        
         print(venta_total)
+        caracteres()
 
 menu = """1-Agregar Producto
 2-Listar Productos
@@ -141,7 +162,7 @@ menu = """1-Agregar Producto
 functions = {
     '1':add_product,
     '2':listening_product,
-    '3':update_product,
+    '3':edit_product,
     '4':delete_product,
     '5':ventas_por_producto,
     '6':ventas_total
