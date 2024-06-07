aacb70077a4db3fcd0fa694baebff7fa673abe56
Grull <lucasmartinez33@gmail.com>
2024-06-07 10:33:01 -0300

    08 - python clases ejemplo

--- Roadmap/08 - CLASES/python/sorubadguy.py
+++ Roadmap/08 - CLASES/python/sorubadguy.py
@@ -4,23 +4,25 @@ import time
 """
 
 #?Ejemplo de definicion de clase
-class Ejemplo:
+class Perro:
 
-    #?atributo de clase
+    #?Atributo general de la clase
+    tipo = "Perro"
     
     #?Inicializador de la clase
-    def __init__(self) -> None:
-        self.hora = time.time_ns()
+    def __init__(self, nombre: str):
+        self.nombre = nombre #?Atributo unico para cada instancia
+        self.trucos = [] #?Si la lista estuviese como atributo general, todas las instancias de esta clase compartirian la misma lista
 
-    #?Funcion propia de la clase
-    def mostrar_ejemplo(self):
-        print("Hola Soy una clase")
+    def agregar_truco(self, truco):
+        self.trucos.append(truco)
 
-    def mostrar_hora(self):
-        print(self.hora)
+perro1 = Perro("Gero")
+perro2 = Perro("Mily")
 
-#!Instancia de una clase
-#?Inicializo la instancia
-x = Ejemplo()
-x.mostrar_hora()
-x.mostrar_ejemplo
+perro1.agregar_truco("saltar")
+perro1.agregar_truco("voltereta")
+perro2.agregar_truco("dar la pata")
+
+print(f"{perro1.nombre} puede hacer los siguientes trucos: {perro1.trucos}")
+print(f"{perro2.nombre} puede hacer los siguientes trucos: {perro2.trucos}")
