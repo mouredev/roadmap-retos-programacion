859f0973753e282f3a5fb1e6ea0ff12526387416
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-06-07 12:10:32 -0400

    #23 - JavaScript

--- Roadmap/23 - SINGLETON/javascript/RicJDev.js
+++ Roadmap/23 - SINGLETON/javascript/RicJDev.js
@@ -20,6 +20,12 @@ console.log(anotherVillain.name);
 
 //EXTRA
 class UserSession {
+	app = 'Ric-JS-Sandbox';
+
+	user = {
+		instances: 0,
+	};
+
 	constructor() {
 		if (typeof UserSession.instance === 'object') {
 			return UserSession.instance;
@@ -28,4 +34,51 @@ class UserSession {
 		UserSession.instance = this;
 		return this;
 	}
+
+	init(name, username, userID, email) {
+		if (this.user.instances === 0) {
+			this.user.name = name;
+			this.user.username = username;
+			this.user.userID = userID;
+			this.user.email = email;
+			this.user.instances++;
+			return this.user;
+		}
+	}
+
+	getData() {
+		let result = [];
+		Object.keys(this.user).forEach((element) => {
+			result.push(this.user[element]);
+		});
+		return result;
+	}
+
+	close() {
+		return (this.user = {
+			instances: 0,
+		});
+	}
 }
+
+let session = new UserSession();
+
+console.log(session);
+
+console.log(session.getData());
+
+session.init('Fred', 'CrazyFred4564', 7880003, 'crazyfred@gmail.com');
+
+console.log(session);
+
+session.close();
+
+console.log(session.getData());
+
+session.init('Fred', 'CrazyFred4564', 7880003, 'crazyfred@gmail.com');
+
+console.log(session.getData());
+
+session.init('Sarah', 'SunshineSarah2435', 6550099, 'sarahmilanesa@gmail.com');
+
+console.log(session.getData());
