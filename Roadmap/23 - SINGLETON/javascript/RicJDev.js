c2dccf483ab2a44aee6ba73898076af1e23db401
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-06-07 19:49:45 -0400

    #23 - JavaScript

--- Roadmap/23 - SINGLETON/javascript/RicJDev.js
+++ Roadmap/23 - SINGLETON/javascript/RicJDev.js
@@ -47,11 +47,7 @@ class UserSession {
 	}
 
 	getData() {
-		let result = [];
-		Object.keys(this.user).forEach((element) => {
-			result.push(this.user[element]);
-		});
-		return result;
+		console.table(this.user);
 	}
 
 	close() {
@@ -61,24 +57,34 @@ class UserSession {
 	}
 }
 
+console.log('\nIniciamos la sesión, pero no se ha ingresado ningún usuario');
 let session = new UserSession();
-
 console.log(session);
+session.getData();
 
-console.log(session.getData());
-
+console.log('\nIngresamos un usuario y mostramos sus datos');
 session.init('Fred', 'CrazyFred4564', 7880003, 'crazyfred@gmail.com');
-
 console.log(session);
+session.getData();
 
+console.log('\nCerramos la instancia del usuario');
 session.close();
+console.log(session);
+session.getData();
 
-console.log(session.getData());
-
+console.log(
+	'\nIngresamos al usuario. Cuando ya existe un instancia de este no es posible ingresar otro usuario a menos que se cierre la instancia'
+);
 session.init('Fred', 'CrazyFred4564', 7880003, 'crazyfred@gmail.com');
-
-console.log(session.getData());
+session.getData();
 
 session.init('Sarah', 'SunshineSarah2435', 6550099, 'sarahmilanesa@gmail.com');
+session.getData();
 
-console.log(session.getData());
+console.log('\nY eso hacemos acá');
+session.close();
+console.log(session);
+session.getData();
+
+session.init('Sarah', 'SunshineSarah2435', 6550099, 'sarahmilanesa@gmail.com');
+session.getData();
