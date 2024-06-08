d9fd6b9a021b7aafaa355d3b54b6b80a1acab806
Grull <lucasmartinez33@gmail.com>
2024-06-08 12:45:42 -0300

    08 - python herencia

--- Roadmap/08 - CLASES/python/sorubadguy.py
+++ Roadmap/08 - CLASES/python/sorubadguy.py
@@ -7,7 +7,7 @@ import time
 class Perro:
 
     #?Atributo general de la clase
-    tipo = "Perro"
+    
     
     #?Inicializador de la clase
     def __init__(self, nombre: str):
@@ -26,3 +26,22 @@ perro2.agregar_truco("dar la pata")
 
 print(f"{perro1.nombre} puede hacer los siguientes trucos: {perro1.trucos}")
 print(f"{perro2.nombre} puede hacer los siguientes trucos: {perro2.trucos}")
+
+#?Herencia, raza tiene las mismas caracteristicas de perro, ademas de las suyas propias
+
+class Raza(Perro):
+
+    def __init__(self, nombre: str, color_pelo: str, tipo_pelo: str, altura: float, raza = "puro perro"):
+        super().__init__(nombre)
+        self.raza = raza
+        self.color_pelo = color_pelo
+        self.tipo_pelo = tipo_pelo
+        self.altura = altura
+
+    def mostrar_raza(self):
+        print(f"Raza: {self.raza}\nTipo de Pelo: {self.tipo_pelo}\nColor del Pelo: {self.color_pelo}\nAltura: {self.altura} mt(s)")
+
+perro3 = Raza("pancho", "marron", "corto", 1.2)
+perro3.agregar_truco("Hacerce el muertito")
+perro3.mostrar_raza()
+print(perro3.trucos) #?caracteristica de la clase perro, heredada por raza
