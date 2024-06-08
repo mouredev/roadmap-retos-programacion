e1c1a5b08b9f44f3128cd42c9af06460712b4f06
Natalia SM <fu.pro.nat@gmail.com>
2024-06-08 23:21:34 +1000

    terminado

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/sm-nat.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/sm-nat.py
@@ -65,9 +65,29 @@ def mi_funcion():
     
 mi_funcion()
 
+#EXTRA
+"""Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
+ * - La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
+ *   - Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
+ *   - Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
+ *   - Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
+ *   - La función retorna el número de veces que se ha impreso el número en lugar de los textos."""
 
-
-
+def ejercicio_extra(txt_1,txt_2):
+    count = 0
+    for number in range(1,101):
+        if number % 3 == 0 and number % 5 == 0:
+            print(txt_1 + txt_2)
+        elif number % 3 == 0:
+            print(txt_1)
+        elif number % 5 == 0:
+            print(txt_2)
+        else:
+            print(number)
+            count += 1
+    return count
+    
+print(ejercicio_extra("hola","mundo"))
 
 
 
