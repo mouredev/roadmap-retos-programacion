ba4ffd1e32b71c7547250ce9306f0eb8ec9add95
Natalia SM <fu.pro.nat@gmail.com>
2024-06-11 00:22:04 +1000

    me falta el extra

--- Roadmap/05 - VALOR Y REFERENCIA/python/sm-nat.py
+++ Roadmap/05 - VALOR Y REFERENCIA/python/sm-nat.py
@@ -42,3 +42,11 @@ mi_lista = [1, 2, 3]
 print(f"Lista antes de la función: {mi_lista}")
 modificar_lista(mi_lista)
 print(f"Lista después de la función: {mi_lista}")
+
+#EXTRA
+"""Crea dos programas que reciban dos parámetros (cada uno) definidos como variables anteriormente.
+Cada programa recibe, en un caso, dos parámetros por valor, y en otro caso, por referencia.
+Estos parámetros los intercambia entre ellos en su interior, los retorna, y su retorno
+se asigna a dos variables diferentes a las originales. A continuación, imprime el valor de las
+variables originales y las nuevas, comprobando que se ha invertido su valor en las segundas.
+Comprueba también que se ha conservado el valor original en las primeras."""
