80b8ac39e1bc75c9959e960d2f83f72f2311053d
Parababire <angelenarvaezm@gmail.com>
2024-06-10 12:56:54 -0400

    #20 - Javascript

--- Roadmap/20 - PETICIONES HTTP/javascript/parababire.js
+++ Roadmap/20 - PETICIONES HTTP/javascript/parababire.js
@@ -1,4 +1,9 @@
-fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+//Ejecicio
+
+const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+async function showResponse() {
+  await fetch(url)
   .then(response => {
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
@@ -8,3 +13,26 @@ fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
   )
   .then(data => console.log(`La cotización del bitcoin es: ${data.bpi.USD.rate_float}$`))
   .catch(error=> console.log(error));
+}
+
+//showResponse();
+
+//Extra
+
+const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
+let pokemonName;
+fetch(pokeApiUrl)
+.then(response => {
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  return response.json();
+  }
+)
+.then(data => {
+  pokemonName = data;
+})
+.then(() => {
+  console.log(pokemonName.species.name);
+})
+.catch(error=> console.log(error))
