dc0deb419342d96561220669d368efe3f1fedb39
qwik-zgheib <qwikzgheib@gmail.com>
2024-06-10 08:03:09 -0500

    feat: #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/qwik-zgheib.py
@@ -0,0 +1,16 @@
+# visite oficial page https://www.python.org/
+
+# comment in one line
+
+"""
+comment in multiple lines
+"""
+
+number_int: int = 10
+number_float: float = 10.5
+CONSTANT: float = 3.14
+string: str = "Hello, Python!"
+boolen: bool = True
+
+
+print(f"{string}")
