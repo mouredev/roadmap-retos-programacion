164a37eb477c5ae719eaf888957aff54bb396ae8
Natalia SM <fu.pro.nat@gmail.com>
2024-06-10 13:30:32 +1000

    extra terminado

--- Roadmap/04 - CADENAS DE CARACTERES/python/sm-nat.py
+++ Roadmap/04 - CADENAS DE CARACTERES/python/sm-nat.py
@@ -49,3 +49,40 @@ print(otra_cadena.strip())
 
 #Capitalización
 print(otra_cadena.title())
+
+#EXTRA
+"""Crea un programa que analice dos palabras diferentes y realice comprobaciones
+ * para descubrir si son:
+ * - Palíndromos
+ * - Anagramas
+ * - Isogramas"""
+
+def main():
+    palabra = input("Escribe tu palabra para ver si es palíndromo: ").lower()
+    palabra1 = input("Escribe tu primera palabra: ").lower()
+    palabra2 = input("Escribe tu segunda palabra: ").lower()
+
+    def es_palindromo(palabra):
+        if palabra == palabra[::-1]:
+            print(f"{palabra} es palíndromo!")
+        else:
+            print(f"{palabra} no es palíndromo")
+    
+    def es_anagrama(palabra1, palabra2):
+        if sorted(palabra1) == sorted(palabra2):
+            print(f"{palabra1} y {palabra2} son anagramas!")
+        else:
+            print(f"{palabra1} y {palabra2} no son anagramas")
+    
+    def es_isograma(palabra):
+        if len(palabra) == len(set(palabra)):
+            print(f"{palabra} es isograma!")
+        else:
+            print(f"{palabra} no es isograma")
+    
+    es_palindromo(palabra)
+    es_anagrama(palabra1, palabra2)
+    es_isograma(palabra1)
+    es_isograma(palabra2)
+    
+main()
