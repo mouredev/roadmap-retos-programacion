b1849ab8cfa6171bcb50fa32781b4b019de6cad7
Lucas <lucasmartinez33@gmail.com>
2024-06-12 19:45:04 -0300

    09 - gerente de proyecto

--- Roadmap/09 - HERENCIA/python/sorubadguy.py
+++ Roadmap/09 - HERENCIA/python/sorubadguy.py
@@ -45,3 +45,48 @@ gato.imprimir_sonido()
 """
 !Extra
 """
+
+class Empleado():
+
+    def __init__(self, carga_horaria: int, paga: float, id: int, nombre: str) -> None:
+        self.carga_horaria = carga_horaria
+        self.paga = paga
+        self.nombre = nombre
+        self.id = id
+    pass
+
+class Gerente(Empleado):
+
+    def imprimir_gerente(self):
+        print(self.id)
+        print(self.nombre)
+        print(self.paga)
+        print(self.horario)
+
+    def imprimir_tarea(self):
+        print(f"{self.nombre} es gerente general")
+
+class Gerente_Proyecto(Empleado):
+    
+    def agregar_proyecto(self, proyecto: list):
+        self.proyecto = []
+        self.proyecto.append(proyecto)
+    
+    def quitar_proyecto(self):
+
+        salir = False
+        while(not salir):
+            print("Seleccione el proyecto finalizado")
+            for i in range(len(self.proyecto)):
+                print(f"{i}: {self.proyecto[i]}")
+            
+            try:
+                self.proyecto.pop(int(input("Ingrese el proyecto finalizado: ")))
+            except:
+                print("Opcion incorrecta")
+            finally:
+                salir = True
+
+
+class Programador(Empleado):
+    pass
