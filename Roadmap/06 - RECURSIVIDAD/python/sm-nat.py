29fe5903d96f624b3d2c597c9c05d2f9ad358f5f
Natalia SM <fu.pro.nat@gmail.com>
2024-06-13 00:02:03 +1000

    extra

--- Roadmap/06 - RECURSIVIDAD/python/sm-nat.py
+++ Roadmap/06 - RECURSIVIDAD/python/sm-nat.py
@@ -5,3 +5,22 @@ def funcion_recursiva(n):
         print(n)
         funcion_recursiva(n-1)
 funcion_recursiva(100)
+
+#EXTRA
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1) 
+
+print(factorial(5))  
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+print(fibonacci(10))  
+
