33db72909f6e04cc77268ab68a9df9fc2ad539d8
Natalia SM <fu.pro.nat@gmail.com>
2024-06-13 12:44:43 +1000

    pila

---
+++ Roadmap/07 - PILAS Y COLAS/python/sm-nat.py
@@ -0,0 +1,14 @@
+#Pila (Stack - LIFO)
+pila = []
+
+pila.append(1)
+pila.append(2)
+pila.append(3)
+
+print("Pila después de los push:", pila)  
+
+ultimo_elemento = pila.pop()
+
+print("Elemento eliminado (pop):", ultimo_elemento) 
+print("Pila después del pop:", pila) 
+
