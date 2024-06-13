f1ae811f73efcb4481e13ae2231c4be621339819
Natalia SM <fu.pro.nat@gmail.com>
2024-06-13 12:48:28 +1000

    cola

--- Roadmap/07 - PILAS Y COLAS/python/sm-nat.py
+++ Roadmap/07 - PILAS Y COLAS/python/sm-nat.py
@@ -12,3 +12,16 @@ ultimo_elemento = pila.pop()
 print("Elemento eliminado (pop):", ultimo_elemento) 
 print("Pila después del pop:", pila) 
 
+#Cola (Queue - FIFO)
+cola = []
+
+cola.append(1)
+cola.append(2)
+cola.append(3)
+
+print("Cola después de los enqueue:", cola) 
+
+primer_elemento = cola.pop(0)
+
+print("Elemento eliminado (dequeue):", primer_elemento)  
+print("Cola después del dequeue:", cola)  
