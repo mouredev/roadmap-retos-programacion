9c6fcbca627c17d20e879275c6652e7f5d00dec0
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-06-13 11:09:29 -0400

    #24 - JavaScript

--- Roadmap/24 - DECORADORES/javascript/RicJDev.js
+++ Roadmap/24 - DECORADORES/javascript/RicJDev.js
@@ -1,16 +1,50 @@
-class Hamburger {
-	basicIngredients = ['bread roll', 'beef', 'tomato', 'lettuce', 'cheese', 'onion'];
-}
+//EJERCICIO
+class BasicPizza {
+	constructor(size) {
+		this.price = this.pricesList[size];
+	}
 
-class Cheeseburger extends Hamburger {
-	constructor() {
-		super();
-		this.basicIngredients.push('more cheese');
+	toppings = ['salsa', 'mozzarella', 'pepperoni'];
+
+	//Precios en dólares, jajs
+	pricesList = {
+		small: '$5.00',
+		medium: '$9.00',
+		large: '$12.00',
+		extralarge: '$16.00',
+	};
+
+	getPrice() {
+		console.log(this.price);
 	}
 }
 
-let burger1 = new Hamburger();
-console.log(burger1);
+class HawaianPizza extends BasicPizza {
+	constructor(size) {
+		super(size);
+		this.toppings.push('jamón', 'piña');
+		this.price = this.pricesList[size];
+	}
 
-let burger2 = new Cheeseburger();
-console.log(burger2);
+	pricesList = {
+		small: '$6.70',
+		medium: '$10.50',
+		large: '$13.50',
+		extralarge: '$16.30',
+	};
+}
+
+class PizzaDecorator extends BasicPizza {
+	constructor(pizza) {
+		super(pizza.size, pizza.toppings);
+		this.pizza = pizza;
+	}
+
+	getPrice() {
+		this.pizza.getPrice();
+	}
+}
+
+let pizza1 = new HawaianPizza('small');
+
+pizza1.getPrice();
