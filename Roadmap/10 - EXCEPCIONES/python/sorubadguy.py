f0831ced42819f2dd8a15fd43a44e86183f466bc
Grull <lucasmartinez33@gmail.com>
2024-06-15 12:59:41 -0300

    10 - python

--- Roadmap/10 - EXCEPCIONES/python/sorubadguy.py
+++ Roadmap/10 - EXCEPCIONES/python/sorubadguy.py
@@ -3,7 +3,7 @@
 """
 
 try: #?Intenta realizar la/s tarea/s contenidas
-    a = int(input("Ingrese un numero: "))
+    a = 1#int(input("Ingrese un numero: "))
 except Exception as ex:#?Si no las puede llevar a cavo, se ejecuta lo que contenga el except
     print("No es un numero")
     print(f"{ex} \n{type(ex).__name__}")
@@ -23,3 +23,18 @@ if a < 0:
 !Extra
 """
 
+def parametros(params: list):
+
+    if(len(params) > 3 or len(params) < 1):
+        raise IndexError()
+    elif(type(params[2]) == str):
+        raise TypeError()
+    else:
+        try:
+            print(f"{params[0]} / {params[1]} = {params[0]/params[1]}")
+        except Exception as ex:
+            print(f"Ocurrio un error: {type(ex).__name__}")
+
+    
+
+parametros([2,0,3])
