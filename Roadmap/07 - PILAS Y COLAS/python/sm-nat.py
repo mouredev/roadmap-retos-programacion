6fe3d88882d1ab64c122b0c05182fa592eddc542
Natalia SM <fu.pro.nat@gmail.com>
2024-06-15 21:11:01 +1000

    extra

--- Roadmap/07 - PILAS Y COLAS/python/sm-nat.py
+++ Roadmap/07 - PILAS Y COLAS/python/sm-nat.py
@@ -25,3 +25,55 @@ primer_elemento = cola.pop(0)
 
 print("Elemento eliminado (dequeue):", primer_elemento)  
 print("Cola después del dequeue:", cola)
+
+#EXTRA
+"""Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+ *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+ *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+ *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+ *   el nombre de una nueva web.
+ * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+ *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+ *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+ *   interpretan como nombres de documentos."""
+
+
+def navegador_web():
+    historial = []  
+
+    while True:
+        comando = input("Introduce una página o 'adelante' o 'atrás' o 'salir': ").strip().lower()
+        if comando == "salir":
+            print("Saliendo...")
+            break
+        elif comando == "adelante":
+           pass
+        elif comando == "atrás":
+            if len(historial) > 0:
+                historial.pop()
+            else:
+                historial.append(comando)
+                print("Página actual:", historial.len(historial[-1]))
+            
+        else:
+            historial.append(comando)
+            print("Página actual:", comando)
+
+navegador_web()
+
+#Impresora compartida
+
+def impresora_compartida():
+    cola_imprimir = []
+    while True:
+        comando = input("Introduce un documento o 'imprimir' o 'salir': ")
+        if comando.lower() == "salir":
+            print("Saliendo...")
+            break
+        elif comando.lower() == "imprimir":
+            if len(cola_imprimir) > 0:
+                print("Imprimiendo:", cola_imprimir.pop(0))
+            else:
+                cola_imprimir.append(comando)
+        else:
+            print("No hay documentos en la cola")
