4f6bc9a7123d4895b57184da8f7b4282147650e7
Jose Manuel Bono Campos <122403351+BonoStudent@users.noreply.github.com>
2024-06-15 15:54:19 +0200

    Ejercicio 00 con Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/TuTrolloLove.java
@@ -0,0 +1,22 @@
+public class TuTrolloLove {
+    public static void main(String[] args) {
+        // https://www.java.com/es/
+        // First way to create comments
+        /* Second way to create comments */
+        /** Third way to create comments (documentation) */
+
+        String example = "Hello World!";
+        final String constVar = "Hello World!";
+
+        byte byteVar = 1;
+        short shortVar = 2;
+        int intVar = 3;
+        long longVar = 4;
+        float floatVar = 5.5f;
+        double doubleVar = 6.6;
+        char charVar = 'A';
+        boolean boolVar = true;
+
+        System.out.println("¡Hola, Java!");
+    }
+}
