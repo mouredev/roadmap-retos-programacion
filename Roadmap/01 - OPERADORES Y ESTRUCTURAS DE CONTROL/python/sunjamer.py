6286a38a83fd9eab6326dcd09d0568e7782cd55c
Pedro <sunjamer@yahoo.es>
2024-06-17 03:04:07 +0200

    añadir fichero a #01 - Python

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sunjamer.py
@@ -0,0 +1,27 @@
+"""
+operadores
+"""
+
+# operadores aritméticos
+
+print(f"Suma 10 + 3 = {10 + 3}")
+print(f"resta 8 - 4 = {8 - 4}")
+print(f"multiplicar 2 * 10 = {2 * 10}")
+print(f"división de 14 / 5 = {14 / 5}")
+print(f"módulo de 14 / 5 = {14 % 5}")
+print(f"exponente de 2 ** 5 = {2 ** 5}")
+print(f"division entera 14 // 5 = {14 // 5}")
+
+# operadores de comparación
+
+print(f"igualdad 8 == 4 es {8 == 4}")
+print(f"es diferente 14 != 5 = {14 != 5}")
+print(f"es 14 > 5 {14 > 5}")
+print(f"es 14 < 5 {14 < 5}")
+print(f"es 14 >= 5 {14 >= 5}")
+print(f"es 14 <= 5 {14 <= 5}")
+
+
+# operadores de asignación
+
+print(f"")
