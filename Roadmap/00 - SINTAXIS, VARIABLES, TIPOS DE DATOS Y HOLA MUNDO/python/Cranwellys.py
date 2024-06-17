735a67eaac7807467e764a3f1f3622654e40e271
Cranwellys <cranwellys@hotmail.com>
2024-06-17 03:49:04 +0200

    Create Cranwellys.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Cranwellys.py
@@ -0,0 +1,37 @@
+# https://www.python.org/
+
+# Comentario en una línea se comienza con el símbolo de almohadilla
+
+"""
+Para crear comentarios
+en varias líneas se utiliza
+tres dobles comillas al principio y al final
+"""
+
+'''
+También se puede crear comentarios
+en varias líneas utilizando
+tres simples comillas al principio y al final
+'''
+
+variable = "variable"
+variable = "otro valor de variable"
+
+CONSTANTE = "Constante"  # No existen constantes en Python, pero por convención se escribe con MAYUSCULA para indicar que es una constante y tenerlo en cuenta
+
+# - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+# número entero
+my_int = 1
+
+# número decimal
+my_float = 1.5
+
+# valor booleano
+my_bool = True
+my_bool = False
+
+# cadena de texto
+my_string = "mi cadena de texto"
+
+# Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, python!")
