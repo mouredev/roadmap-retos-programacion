5a031f671fe94f05a7990f14d25c99f3eecddbf4
Parababire <angelenarvaezm@gmail.com>
2024-06-18 08:24:17 -0400

    #21 - Javascript

--- Roadmap/21 - CALLBACKS/javascript/parababire.js
+++ Roadmap/21 - CALLBACKS/javascript/parababire.js
@@ -2,45 +2,47 @@
 
 /* Las callback brillan en el mundo real cuando son usadas por funciones asyncronas */
 
-/* function myFunction() {
+function myFunction() {
   console.log('Hello World');
-}*/
+}
 /* La función setTimeout nos permite emular un proceso asyncrono que al finalizar llama a nuestra callback */
-/*setTimeout(myFunction, 3000); */
+setTimeout(myFunction, 3000);
 
 //Extra
 
-function orderProcess(dishName, confirmCallbak, readyCallback, deliveredCallback) {
-  confirmCallbak(dishName);
-  readyCallback();
-  deliveredCallback();
+async function orderProcess(dishName, confirmCallbak, readyCallback, deliveredCallback) {
+  await confirmCallbak(dishName).then(orderHandle);
+  await readyCallback(dishName).then(orderHandle);
+  await deliveredCallback(dishName).then(orderHandle);
 }
 
-async function confirmOrder(orden) {
-  let pedidoRecibido = new Promise(function(resolve) {
-    setTimeout(function() {
-      resolve(`Orden de ${orden} Recibida`);
-    }, 1000);
+function orderHandle(result) {
+  return console.log(result);
+}
+
+async function confirmOrder(order) {
+  return new Promise(function(resolve) {
+    resolve(`Orden de ${order} Recibida`);
   })
-  console.log(await pedidoRecibido);
 }
 
-async function orderReady() {
-  let ordenLista = new Promise(function(resolve) {
+async function orderReady(order) {
+  return new Promise(function(resolve) {
     setTimeout(function() {
-      resolve('Orden lista');
-    }, 3000);
+      resolve(`${order} lista`);
+    }, Math.floor((Math.random() * 10000) + 1000));
   })
-  console.log(await ordenLista);
 }
 
-async function orderDelivered() {
-  let ordenEntregada = new Promise(function(resolve) {
+async function orderDelivered(order) {
+  return new Promise(function(resolve) {
     setTimeout(function() {
-      resolve('Orden Entregada');
-    }, 5000);
+      resolve(`${order} entregada`);
+    }, Math.floor((Math.random() * 10000) + 1000));
   })
-  console.log(await ordenEntregada);
 }
 
-orderProcess('Salmón con ensalada', confirmOrder, orderReady, orderDelivered);
+orderProcess('Pizza Margarita', confirmOrder, orderReady, orderDelivered);
+orderProcess('Pizza 4 Quesos', confirmOrder, orderReady, orderDelivered);
+orderProcess('Pizza Napolitana', confirmOrder, orderReady, orderDelivered);
+orderProcess('Pizza Fungi', confirmOrder, orderReady, orderDelivered);
