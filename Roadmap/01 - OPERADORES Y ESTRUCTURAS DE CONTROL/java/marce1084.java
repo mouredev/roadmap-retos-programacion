c3daeef9ae3097afecdcb23ea120d1745a5608be
marce1084 <marcelo.grra@gmail.com>
2024-06-19 20:52:03 -0300

    #01 - Java

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/marce1084.java
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/java/marce1084.java
@@ -72,7 +72,7 @@ public class marce1084 {
 
         //Excepciones
         try {
-            System.out.println(10 / 0);
+            System.out.println(10/0);
         } catch (ArithmeticException e) {
             System.out.println("Se produjo error por la división");
         } finally {
