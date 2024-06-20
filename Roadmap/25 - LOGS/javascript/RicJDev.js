e50622cb2ffce1ab8cc51a623f54a3d27af2d992
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-06-20 09:06:18 -0400

    #25 - JavaSript

--- Roadmap/25 - LOGS/javascript/RicJDev.js
+++ Roadmap/25 - LOGS/javascript/RicJDev.js
@@ -2,22 +2,32 @@
 const { error } = require('node:console');
 const fs = require('node:fs');
 
+const logFile = 'RicJDev_log.txt';
+
 function addToLog(fun, severity) {
 	const message = Object.freeze({
 		INFO: `${fun.name} se ha ejecutado`,
-		TRACE: '',
-		DEBUG: `${fun.name} ha arrojado un valor inesperado`,
+		TRACE: `Revise los parámetros de ${fun.name}`,
+		DEBUG: `${fun.name} ha arrojado un valor inesperado. ${fun}`,
 		WARN: `${fun.name} no se ejecutó correctamente`,
 		ERROR: `ha ocurrido un error al ejecutar ${fun.name}`,
-		FATAL: 'ha petado esto, tío',
+		FATAL: `esto ha petado al ejecutar ${fun.name}`,
 	});
 
-	const logContent = `${new Date().toLocaleString()} [${severity}]: ${message[severity]}\n`;
-
 	return function (...arg) {
-		fs.appendFile('logs.txt', logContent, (error) => {
+		let logContent = `${new Date().toLocaleString()} [${severity}]: ${message[severity]}. Parámetros: ${JSON.stringify(
+			arg
+		)}\n`;
+
+		fs.appendFile(logFile, logContent, (logError) => {
 			if (error) {
-				console.log(error);
+				fs.appendFile(
+					logFile,
+					`${new Date().toLocaleString()}: Error al escribir en el archivo de registro: ${logError}\n`,
+					(error) => {
+						console.log(error);
+					}
+				);
 			}
 		});
 
@@ -26,27 +36,34 @@ function addToLog(fun, severity) {
 }
 
 function infoFunction() {
-	console.log('Hola. Soy una funcion');
+	console.log('Hola. Soy una función y solo me estoy ejecutando');
 }
 
 function errorFunction() {
-	console.log('Soy una funcion que da error. Jejeje');
+	console.log('Soy una función que da error. Jejeje');
 }
 
 function fatalFunction() {
-	console.log('Revisa el log');
+	console.log('Algo salió terriblemente mal');
+}
+
+function traceFunction(any, callback) {
+	callback(any);
 }
 
 infoFunction = addToLog(infoFunction, 'INFO');
-
 errorFunction = addToLog(errorFunction, 'ERROR');
-
 fatalFunction = addToLog(fatalFunction, 'FATAL');
+traceFunction = addToLog(traceFunction, 'TRACE');
 
 infoFunction();
 
 errorFunction();
 
 fatalFunction();
+
+traceFunction('Hola', (greeting) => {
+	console.log(greeting);
+});
 
 //EXTRA
