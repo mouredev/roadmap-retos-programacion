68f9801f1086d7eec40e0e87828d672113107529
SaicoBys <jacobmoronta0000@gmail.com>
2024-06-20 23:40:19 -0400

    #00 - python

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/saicobys.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/saicobys.py
@@ -1,16 +1,3 @@
-
-"""
- * EJERCICIO 1:
- * - Crea un comentario en el código y coloca la URL del sitio web oficial del
- *   lenguaje de programación que has seleccionado.
- * - Representa las diferentes sintaxis que existen de crear comentarios
- *   en el lenguaje (en una línea, varias...).
- * - Crea una variable (y una constante si el lenguaje lo soporta).
- * - Crea variables representando todos los tipos de datos primitivos
- *   del lenguaje (cadenas de texto, enteros, booleanos...).
- * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
- """
-
 # Python: https://www.python.org/
 """
 Este es un comentario de varias líneas
@@ -21,12 +8,12 @@ Este es un comentario de varias líneas
 variable_prueba = 420 # Python no tiene constantes
 
 # Tipos de datos primitivos
-string = "Hola chat"
-integer = 69
-float = 4.20
+string = "Hola Jacob"
+integer = 99
+float = 2.20
 boolean = True
 NoneType = None
-complex_number = 50 + 9j
+complex_number = 1 + 4j
 
 # imprimir por terminal
-print("¡Hola, Jacob!")
+print("¡Hola, Python!")
