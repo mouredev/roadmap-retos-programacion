4ac7e353acfc3ed9731241571630c0d6ece608e3
Simon Guzman <siguzman@unicauca.edu.co>
2024-06-21 15:52:05 -0500

    #[00] - [JAVA]

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/simonguzman.java
@@ -0,0 +1,50 @@
+
+public class simonguzman {
+    //https://dev.java/
+
+    //Comentario en una linea
+
+    /**
+     * 
+     * Comentario en varias lineas(Documentacion)
+     */
+
+    /*
+    Documentacion en varias lineas
+     */ 
+    public static void main(String[] args) {
+        
+        //Datos primitivos y creacion de variables
+        //Variables con valores numericos
+        byte Byte = 127;
+        short corto = 1;
+        int entero = 2;
+        long numerogrande = 3;
+        float numeroFlotante = 3.3f;
+        double numeroDoble = 3.3;
+
+        //Representaciones de valores numericos
+
+        //Numero decimal
+        int decimalValue = 16;
+        //Numero hexadecimal
+        int hexadecimalValue = 0x1a;
+        //Numero binario
+        int binaryValue = 0b11010;
+
+        //Expresiones para punto flotante
+        double double1 = 123.4;
+
+        //Notaciones diferentes
+        double double2 = 1.234e2;
+        float f1 = 123.4f; 
+
+        //Variables de texto(Lineas de caracteres y cadenas de texto)
+        String cadena = "Texto";
+        char Caracter = 'a';
+        boolean result = true;
+
+        System.out.println("¡Hola, java!");
+    }
+
+}
