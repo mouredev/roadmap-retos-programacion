ba506c8e6b54d20e8bc8706549f93d93aca8c9ae
Matiascba27 <matiasnunez1969@gmail.com>
2024-06-23 13:58:28 -0300

    Agregando comentarios de una y varias lineas

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Matiascba27.js
@@ -0,0 +1,12 @@
+// link de la documentacion oficial de Javascript https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// este es un comentario de una sola linea
+
+/* este 
+es
+un
+comentario
+de
+varias
+lineas
+*/
