3526d5adc3476471dfe016010f2bcc946a4a0313
Grull <lucasmartinez33@gmail.com>
2024-06-24 14:21:10 -0300

    12 - python primera parte finalizada

--- Roadmap/12 - JSON Y XML/python/sorubadguy.py
+++ Roadmap/12 - JSON Y XML/python/sorubadguy.py
@@ -2,28 +2,49 @@
 *JSON y XML
 """
 import json
+from xml.dom.minidom import Element
+import xml.etree.ElementTree as xml
 from os import remove
 
 #!Json
 #?persona es un objeto de python (diccionario)
 persona = {
     "nombre" : "sorubadguy",
-    "edad" : 30,
+    "edad" : "30",
     "f_nac" : "27/03/94",
     "leng_progra" : "python"
 }
 
 #?convierto a persona en un JSON
-with open("archivo.json", "w") as j:
-    json.dump(persona, j)
+def crear_json(persona):
+    with open("archivo.json", "w") as j:
+        json.dump(persona, j)
 
-#?leer y mostrar el archivo.json
-with open("archivo.json", "r") as j:
-    #?Leo el archivo Json
-    objson = json.load(j)
+    #?leer y mostrar el archivo.json
+    with open("archivo.json", "r") as j:
+        #?Leo el archivo Json
+        objson = json.load(j)
+
+    print(objson)
+    print(type(objson))
+
+    remove("archivo.json")
+
+#!XML
+
+def crear_xml(persona):
+    pers = xml.Element("informacion")
+    nombre = xml.SubElement(pers, "nombre").text = persona["nombre"]
+    edad = xml.SubElement(pers, "edad").text = persona["edad"]
+    nacimiento = xml.SubElement(pers, "fecha_nacimiento").text = persona["f_nac"]
+    progra = xml.SubElement(pers, "Lenguaje_programacion").text = persona["leng_progra"]
     
-print(objson)
-print(type(objson))
+    xml.dump(pers)
+    datos = xml.ElementTree(pers)
+    datos.write("persona.xml")
+    
+crear_xml(persona)
 
-remove("archivo.json")
-
+with open("persona.xml", "r") as datos_xml:
+    print(datos_xml.read())
+remove("persona.xml")
