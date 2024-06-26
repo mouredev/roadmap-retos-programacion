de32f20891f8675f12a636ce5faa394521b5119d
Pedro <sunjamer@yahoo.es>
2024-06-26 02:05:53 +0200

    #01 sunjamer.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sunjamer.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/sunjamer.py
@@ -4,6 +4,8 @@ operadores
 
 # operadores aritméticos
 
+print ("aqui operadors")
+
 print(f"Suma 10 + 3 = {10 + 3}")
 print(f"resta 8 - 4 = {8 - 4}")
 print(f"multiplicar 2 * 10 = {2 * 10}")
@@ -29,7 +31,7 @@ print(f"OR es 7 + 5 = 12 OR 9 - 6 == 4 {7 + 5 == 12 or 9 - 6 == 4}")
 print(f"NOT 7 + 5 = 12 or 9 - 6 == 3 {not (9 + 6 == 3 or 7 + 5 == 12)}")
 
 # operadores de asignación
-
+print ("aqui asignacion")
 my_variable = 33   # asignación de valor a variable
 print(my_variable)
 my_variable+=3
@@ -63,8 +65,47 @@ print (7 <= 5)
 print (7 == 5)
 print (7 != 5)
 
-# ejercicios
 
+# estructuras de control
+
+# while
+
+index = 0
+while index <= 10:
+    if index == 5 or index == 7:
+        index+=1
+        continue
+    print (index)
+    index+=1
+
+
+
+
+# for
+
+my_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in my_numbers:
+    print (number)
+
+print ("salimos del for")
+
+lenguaje = "Python"
+for letra in lenguaje:
+    print (letra)
+print ("salimos del segundo for")
+
+
+
+
+
+
+
+
+
+
+
+# ejercicios
+"""
 my_age = 46
 my_weight = 70.3
 my_complex_variable = 5j+1
@@ -76,7 +117,7 @@ height = int(input())
 area = 0.5*base*height
 print(f"El area del triangulo es {area}")
 
-
+"""
 
 
 
