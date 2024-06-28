2625b1f138872244890babe4f7fbb0bd20fb96eb
AlbaBP <albabrox@ucm.es>
2024-06-28 16:26:27 +0200

    #[00] - [JAVASCRIPT]

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/albrox.js
@@ -0,0 +1,30 @@
+// Sitio oficial JavaScript: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// 1. Comentarios
+
+// Comentario de una línea
+
+/*
+Comentario
+de
+varias
+líneas
+*/
+
+// 2. Variables
+
+// Declaración de variables
+var x = 5;
+let y = 10;
+const z = 15;
+
+// 3. Tipos de datos
+let a = 5; // Number
+let b = "Hola"; // String
+let c = true; // Boolean
+let d = [1, 2, 3]; // Array
+let e = {nombre: "Albrox", edad: 25}; // Object
+
+// 4. Hola mundo
+let language = "JavaScript";
+console.log("Hola mundo desde " + language + "!");
