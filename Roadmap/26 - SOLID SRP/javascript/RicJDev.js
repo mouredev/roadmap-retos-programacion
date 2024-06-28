38da3a1fd5c5454d79325b25a34d07a0f39795ef
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-06-27 23:31:30 -0400

    #26 - JavaScript

--- Roadmap/26 - SOLID SRP/javascript/RicJDev.js
+++ Roadmap/26 - SOLID SRP/javascript/RicJDev.js
@@ -1,146 +1,84 @@
 //EJERCICIO
-//Código sin seguir el principio de Responsabilidad Única
-class CarNoSRP {
-	constructor(model, year, price) {
-		this.model = model;
-		this.year = year;
-		this.price = price;
-
-		this.stock = 2;
-		this.status = this.setStatus();
-	}
-
-	//Esta sería la simulación de una lógica que le muestre información al cliente y le permita adquirir un carro
-	describe() {
-		console.log(`${this.model} (${this.year}): $${this.price.toLocaleString()}`);
-	}
-
-	getStatus() {
-		console.log(`El modelo ${this.model} está ${this.status}`);
-	}
-
-	buyCar(payment) {
-		if (this.status === 'disponible') {
-			if (this.validatePayment(payment)) {
-				console.log(`Felicidades! Ha adquirido un ${this.model} totalmente nuevo!`);
-				this.stock--;
-				this.setStatus();
-			} else {
-				console.log('Debe abonar el monto total del modelo para comprarlo');
-			}
-		} else {
-			console.log('Modelo no disponible');
-		}
-	}
-
-	/*
-	Esta sería la simulación de una lógica que procese los pagos, gestione inventarios
-	o realice cualquier otra función que no interesa o no conviene que sepa el cliente
-	*/
-	validatePayment(payment) {
-		return payment >= this.price;
-	}
-
-	setStatus() {
-		if (this.stock > 0) {
-			this.status = 'disponible';
-		} else {
-			this.status = 'agotado';
-		}
-
-		return this.status;
-	}
-
-	getStock() {
-		console.log(`Inventario actual del modelo ${this.model}: ${this.stock}`);
-	}
-}
-
 /*
-Esto está mal por varias razones:
+CUANDO NO SE SIGUE EL PRINCIPIO DE RESPONSABILIDAD ÚNICA:
 
-- Se complica la tarea de implementar procesos en otras clases si los necesitan.
-- De la misma manera la clase vendrá con procesos que no necesita en todos los casos que será implementada.
+- Se complica la tarea de implementar procesos en otras clases.
+- De la misma manera la clase vendrá con métodos que no necesitará en todos los casos que será implementada.
 - Complica el mantenimiento, al juntar demasiados procesos.
 - Hace más difícil definir los test unitarios.
 - El código es algo más ilegible, pues no queda claro para qué es la clase.
 
-En general, el principio de responsabilidad única busca simplificar y facilitar el desarrollo,
-lo cual es todavía más útil si se trabaja con código que luego será leído y editado por otras personas.
+En general, este principio busca simplificar y facilitar el desarrollo, sobre todo si se trabaja con código que luego será leído y editado por otras personas.
 */
 
-let car1 = new CarNoSRP('Chevrolet Tracker', 2022, 20190);
-car1.describe();
-car1.getStatus();
-car1.buyCar(20000);
-car1.buyCar(20200);
-car1.getStock();
-car1.buyCar(20190);
-car1.getStock();
-car1.buyCar(30000);
-car1.buyCar(30000);
-car1.getStock();
-
-//Código siguiendo el principio de Responsabilidad Única
-//Lógica de negocio
-console.clear();
-
-const Inventory = {
-	list: [],
-
-	search: function (itemName) {
-		let result = undefined;
-
-		this.list.forEach((element) => {
-			if (element[itemName]) {
-				result = element;
-			}
-		});
-
-		return result;
-	},
-
-	displayInventory: function () {
-		console.log('\nINVENTARIO');
-		this.list.forEach((element) => {
-			console.log(element);
-		});
-	},
-};
-
-class Item {
-	constructor(itemName, quantity = 0) {
-		this[itemName] = quantity;
-		Inventory.list.push(this);
+class UsersNoSRP {
+	constructor(username, password, email, userID) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.userID = userID;
 	}
 
-	addQuantity(itemName, addNum) {
-		return (this[itemName] = +addNum);
+	registerUser() {
+		//Registrar el usuario
 	}
 
-	substractQuantity(iteName, substractNum) {
-		return (this[iteName] = -substractNum);
+	validateUser() {
+		//Validar la contraseña del usuario
+	}
+
+	sendEmail() {
+		//Enviar un email al ususario
 	}
 }
-
-class Car {
-	constructor(model, year, price) {
-		this.model = model;
-		this.year = year;
-		this.fullName = `${model} ${year}`;
-		this.price = price;
-		this.stock = new Item(this.fullName, 5);
-	}
-}
-
-new Car('Ram Pickup', 2021, 40150);
-new Car('Chevrolet Silverado', 2022, 43990);
-
-console.log(Inventory.search('Ram Pickup 2021'));
-//Lógica de cliente
 
 /*
- * DIFICULTAD EXTRA (opcional):
+PRINCIPIO DE RESPONSABILIDAD ÚNICA (SRP):
+
+"Una clase debe hacer una cosa y, por lo tanto, debe tener una sola razón para cambiar"
+
+Esto quiere decir que cada clase tiene un objetivo y una sola razón de ser. El único motivo por el que se debería modificar una clase es que este objetivo o la manera en que debe cumplirse sean modificados también.
+*/
+
+class User {
+	constructor(username, password, email, userID) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.userID = userID;
+	}
+
+	//Lógica que necesite la creación de un usuario
+}
+
+class UserRegister {
+	constructor(user) {
+		this.user = user;
+	}
+
+	//Registrar al usuario
+}
+
+class UserValidation {
+	constructor(user) {
+		this.user = user;
+	}
+
+	//Validar contraseñas o permisos del usuario
+}
+
+class EmailSender {
+	constructor(emailAddress, emailObject, emailContent) {
+		this.emailAddress = emailAddress;
+		this.emailObject = emailObject;
+		this.emailContent = emailContent;
+	}
+
+	//Lógica para crear y enviar un correo
+}
+
+/*
+* DIFICULTAD EXTRA (opcional):
  * Desarrolla un sistema de gestión para una biblioteca. El sistema necesita
  * manejar diferentes aspectos como el registro de libros, la gestión de Users
  * y el procesamiento de préstamos de libros.
@@ -160,3 +98,69 @@ console.log(Inventory.search('Ram Pickup 2021'));
  * 2. Refactoriza el código: Separa las responsabilidades en diferentes clases
  * siguiendo el Principio de Responsabilidad Única.
  */
+
+class Library {
+	userDataBase = [];
+	bookDataBase = [];
+
+	registerNewBook(bookID, title, author, avalaibleCopies) {
+		let newBook = {
+			bookID: bookID,
+			title: title,
+			author: author,
+			avalaibleCopies: avalaibleCopies,
+		};
+
+		this.bookDataBase.push(newBook);
+	}
+
+	registerNewUser(name, userID, emailAddress) {
+		let newUser = {
+			name: name,
+			userID: userID,
+			emailAddress: emailAddress,
+		};
+
+		this.userDataBase.push(newUser);
+	}
+
+	search(elementID, searchType) {
+		let result;
+
+		if (searchType === 'book') {
+			this.bookDataBase.forEach((element) => {
+				if (element.bookID === elementID) {
+					result = element;
+				}
+			});
+		} else if (searchType === 'user') {
+			this.userDataBase.forEach((element) => {
+				if (element.userID === elementID) {
+					result = element;
+				}
+			});
+		}
+
+		return result;
+	}
+
+	lendBook(userID, bookID) {
+		let bookToLend = this.search(bookID, 'book');
+		let userLender = this.search(userID, 'user');
+
+		if (bookToLend.avalaibleCopies > 0) {
+			console.log(
+				`Se le ha prestado el libro \"${bookToLend.title}\" de ${bookToLend.author} a ${userLender.name}`
+			);
+			bookToLend.avalaibleCopies--;
+		} else {
+			console.log('No hay copias disponibles');
+		}
+	}
+}
+
+let myLibrary = new Library();
+
+myLibrary.registerNewBook(1, 'Dracula', 'Bram Stoker', 3);
+myLibrary.registerNewUser('Juan', 1, 'juanloquillo16@gmail.com');
+myLibrary.lendBook(1, 1);
