2053f7b4a106bacbe5de08318a65a3328247cd6e
Ldflui <luisnajarrito11@gmail.com>
2024-06-30 14:47:06 -0500

    00-SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Ldflui.js
@@ -0,0 +1,46 @@
+/* ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+
+// Documentación oficial de JavaScript --> https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//comentario en una linea
+/*esto en
+un comentaario
+en multiples
+lineas*/
+
+let mascota = "bady";
+const PI = 3.1416;
+
+let num = 1;
+let nombre = "Luis";
+let boleano = true;
+let numeroEnteroGrande= 2332322332n;
+let decimal = 1.23456;
+let nulo = null;
+let vacio=undefined;
+let simbolo = Symbol;
+
+console.log("!Hola, JavaScript¡");
+console.log(num,nombre,boleano,numeroEnteroGrande,decimal,nulo,vacio,simbolo); 
+console.log(decimal);
