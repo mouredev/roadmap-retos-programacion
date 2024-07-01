b343f88957a00a13574fa1de555c4da8e27df436
Arkadio147 <scite33@gmail.com>
2024-07-01 14:11:59 +0200

    #00 - Python - Arkadio147

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Arkadio147.py
@@ -0,0 +1,18 @@
+# https://www.python.org/
+# Comentarios en una línea
+'''Comentarios
+multilinea'''
+"""Segunda forma de comentarios
+multilinea"""
+variable_1 = "Python"
+var_string = "string de prueba"
+var_integer = 124
+var_float = 1.2
+var_bool = True
+var_lista = [1, 2, 3, 4, 5]
+var_tupla = ("cosa", "animal", "fruta")
+var_conjunto = {var_string, var_integer, var_float}
+var_dict = {"1": 1,
+            "2": 2,
+            "3": 3}
+print(f"¡Hola, {variable_1}!")
