9bf5af5a8e89e3b8262b9d2eca9bd9d29ebe1276
Parababire <angelenarvaezm@gmail.com>
2024-07-05 12:17:40 -0400

    #23 - Javascript

--- Roadmap/23 - SINGLETON/javascript/parababire.js
+++ Roadmap/23 - SINGLETON/javascript/parababire.js
@@ -7,7 +7,7 @@ class Person {
     if (typeof Person.instance === 'object') { /* Si la clase Person existe se devuelve la instancia creada */
       return Person.instance;
     } 
-    /* En caso contrario se establecen las propiedades */
+    /* Caso contrario se establecen las propiedades */
     this.name = name;
     this.age = age;
 
@@ -18,5 +18,48 @@ class Person {
 
 let person1 = new Person('José', 44);/* El objeto creado solo tendrá una instancia */
 let person2 = new Person('María', 30);
-console.log(person1);
-console.log(person2);
+console.log(person1); // Person { name: 'José', age: 44 }
+console.log(person2); // Person { name: 'José', age: 44 }
+
+// Extra
+
+class UserSession {
+  constructor() {
+
+    if (!!UserSession.instance) {
+      return UserSession.instance;
+    }
+
+    this.user = null;
+
+    UserSession.instance = this;
+  }
+
+  setUser(id, username, name, mail) {
+    this.user = {id, username, name, mail};
+  }
+  
+  getUser() {
+    return `${this.user.id} ${this.user.username} ${this.user.name} ${this.user.mail}`;
+  }
+
+  clearUser() {
+    this.user.id = null;
+    this.user.username = null;
+    this.user.name = null;
+    this.user.mail = null;
+  }
+}
+
+let newSession = new UserSession();
+newSession.setUser(212, 'parababire', 'Angel', 'angelnarvaez@gmail.com');
+console.log(newSession.getUser());
+
+let newSession2 = new UserSession();
+console.log(newSession2.getUser());
+
+let newSession3 = new UserSession();
+newSession3.clearUser();
+console.log(newSession3.getUser());
+
+console.log(newSession2.getUser() === newSession.getUser()); // true
