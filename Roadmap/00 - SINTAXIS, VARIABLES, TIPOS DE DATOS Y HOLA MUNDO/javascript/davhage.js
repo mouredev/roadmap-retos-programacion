99c889a6bcbb572c89dfa78b1048e289e48a503a
David Hage <davhage@icloud.com>
2024-07-04 18:08:35 -0600

    01 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/davhage.js
@@ -0,0 +1,20 @@
+//?https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//* Comentario de una línea
+/*
+    Comentario de
+    varias líneas
+*/
+
+let variable = 'Hola';
+const constante = 'Mundo';
+
+let name = 'David';
+let age = 33;
+let student = true;
+let city = null;
+let centimeterToTheMoon = 38440000000n;
+let array = [1, 2, 3];
+let objeto = {id: 1, name: 'david', email: 'davidhage@hotmail.com'};
+
+console.log('Hola javascript!');
