33a5526e8bef14e2ae7a7d5028f2dcad21cc17ac
riacosta <rigo93acosta@gmail.com>
2024-07-09 11:07:08 -0300

    Finished

--- Roadmap/21 - CALLBACKS/python/rigo93acosta.py
+++ Roadmap/21 - CALLBACKS/python/rigo93acosta.py
@@ -28,4 +28,34 @@ def greet_callback(names: str):
 
 greeting_process('Rigo', greet_callback)
 
+'''
+Extra
+'''
+import random
+import time
+import threading
 
+def order_process(dish_name: str, confirm_callbackm, ready_callback, delivered_callback):
+    def process():   
+        confirm_callbackm(dish_name)
+        time.sleep(random.randint(1, 10))    
+        ready_callback(dish_name)
+        time.sleep(random.randint(1, 10))
+        delivered_callback(dish_name)
+    
+    threading.Thread(target=process).start()
+
+
+def confirm_order(dish_name: str):
+    print(f"Tu pedido {dish_name} ha sido confirmado.")
+
+def order_ready(dish_name: str):
+    print(f"Tu pedido {dish_name} está listo.")
+
+def order_delivered(dish_name: str):
+    print(f"Tu pedido {dish_name} ha sido entregado.")
+
+order_process("Pizza Barbacoa", confirm_order, order_ready, order_delivered)
+order_process("Pizza 4 Quesos", confirm_order, order_ready, order_delivered)
+order_process("Pizza Margarita", confirm_order, order_ready, order_delivered)
+order_process("Pizza con Piña", confirm_order, order_ready, order_delivered)
