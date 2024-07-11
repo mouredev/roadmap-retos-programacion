fbe3c069f9216c9cdab9dbbb33f9108f7bf2d44e
riacosta <rigo93acosta@gmail.com>
2024-07-11 19:03:25 -0300

    Finished basic

--- Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/python/rigo93acosta.py
+++ Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/python/rigo93acosta.py
@@ -20,3 +20,54 @@
  */
 '''
 
+"""
+Ejercicio
+"""
+
+# Funcion con argumento
+def apply_func(func, x):
+    return func(x)
+
+print(apply_func(len, "Hola mundo"))
+
+# Retorno de funcion
+
+def apply_multiplier(n):
+    def multiplier(x):
+        return x * n
+    return multiplier
+
+x = apply_multiplier(2)
+print(x(5))
+
+# Sistema
+numbers = [3, 4, 1, 5, 2]
+
+# map()
+def applay_double(n):
+    return n*2
+
+print(list(map(applay_double, numbers)))
+
+# filter()
+def is_even(n):
+    return n % 2 == 0
+
+print(list(filter(is_even, numbers)))
+
+# sorted()
+print(sorted(numbers))
+print(sorted(numbers, reverse=True))
+print(sorted(numbers, key=lambda x: -x))
+
+# reduce 
+from functools import reduce
+def sum(x, y):
+    return x + y
+
+print(reduce(sum, numbers))
+
+"""
+Extra
+"""
+
