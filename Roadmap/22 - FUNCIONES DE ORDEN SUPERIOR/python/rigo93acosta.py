4b5d6f14c51a42608850a7adb60f1f0fd296832e
riacosta <rigo93acosta@gmail.com>
2024-07-11 16:38:24 -0300

    Init Reto

---
+++ Roadmap/22 - FUNCIONES DE ORDEN SUPERIOR/python/rigo93acosta.py
@@ -0,0 +1,22 @@
+'''
+## 22 FUNCIONES DE ORDEN SUPERIOR
+/*
+ * EJERCICIO:
+ * Explora el concepto de funciones de orden superior en tu lenguaje 
+ * creando ejemplos simples (a tu elección) que muestren su funcionamiento.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Dada una lista de estudiantes (con sus nombres, fecha de nacimiento y 
+ * lista de calificaciones), utiliza funciones de orden superior para 
+ * realizar las siguientes operaciones de procesamiento y análisis:
+ * - Promedio calificaciones: Obtiene una lista de estudiantes por nombre
+ *   y promedio de sus calificaciones.
+ * - Mejores estudiantes: Obtiene una lista con el nombre de los estudiantes
+ *   que tienen calificaciones con un 9 o más de promedio.
+ * - Nacimiento: Obtiene una lista de estudiantes ordenada desde el más joven.
+ * - Mayor calificación: Obtiene la calificación más alta de entre todas las
+ *   de los alumnos.
+ * - Una calificación debe estar comprendida entre 0 y 10 (admite decimales).
+ */
+'''
+
