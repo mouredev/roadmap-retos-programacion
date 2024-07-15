594fdf35b12da1292cd8dbef726057531f3bf313
riacosta <rigo93acosta@gmail.com>
2024-07-15 19:12:41 -0300

    Init Excercise

---
+++ Roadmap/23 - SINGLETON/python/rigo93acosta.py
@@ -0,0 +1,16 @@
+## 23 - SINGLETOn
+'''
+ * EJERCICIO:
+ * Explora el patrón de diseño "singleton" y muestra cómo crearlo
+ * con un ejemplo genérico.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Utiliza el patrón de diseño "singleton" para representar una clase que
+ * haga referencia a la sesión de usuario de una aplicación ficticia.
+ * La sesión debe permitir asignar un usuario (id, username, nombre y email),
+ * recuperar los datos del usuario y borrar los datos de la sesión.
+ */
+'''
+
+## Ejercicio
+
