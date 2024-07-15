b12b3aea2dd1e8b187bbf6975688e1fc3a659560
riacosta <rigo93acosta@gmail.com>
2024-07-15 19:29:49 -0300

    End Extra

--- Roadmap/23 - SINGLETON/python/rigo93acosta.py
+++ Roadmap/23 - SINGLETON/python/rigo93acosta.py
@@ -35,5 +35,47 @@ singleton_2 = Singleton()
 
 print(singleton_1 is singleton_2) # Es la misma clase
 
+'''
+Extra
+'''
 
+class UserSession:
 
+    _instance = None
+    id: str = None
+    username: str = None
+    name: str = None
+    email: str = None
+
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(UserSession, cls).__new__(cls)
+        
+        return cls._instance
+    
+    def set_user(self, id, username, name, email):
+        self.id = id
+        self.username = username
+        self.name = name
+        self.email = email
+
+    def get_user(self):
+        return f"{self.id}, {self.username}, {self.name}, {self.email}"
+    
+    def clear_user(self):
+        self.id = None
+        self.username = None
+        self.name = None
+        self.email = None
+
+sesssion1 = UserSession()
+print(sesssion1)
+sesssion1.set_user(1, "rigo93", "Rigo", "rigo93acosta@gmail.com")
+print(sesssion1.get_user())
+
+sesssion2 = UserSession()
+print(sesssion2.get_user())
+
+sesssion3 = UserSession()
+sesssion3.clear_user()
+print(sesssion3.get_user())
