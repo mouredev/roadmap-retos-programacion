91fa02b1d75fc73964d30cb3ebfdb51dd123b363
Yuzu <Yuzu_0204@outlook.com>
2024-07-15 14:20:14 -0400

    feat: Add Python exercise solution #00
    The code adds the Python exercise #00 solution that includes creating comments, variables, and printing a message.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Yuzu02.py
@@ -0,0 +1,36 @@
+"""
+EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+"""
+#? https://www.python.org/
+
+# Comentario de una sola linea
+
+"""
+Comentario de
+multiples
+lineas
+en Python
+"""
+
+# Variable
+variable = 'valor'
+
+# Constante
+CONSTANTE = 'valor constante que no cambia durante la ejecución'
+
+# Tipos de datos primitivos
+entero = 5
+flotante = 5.2
+booleano = True
+cadena = 'valor de cadena'
+nada = None
+
+print('¡Hola, Python! :)')
