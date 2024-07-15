02c1c6f1f21bd9225deac8e8d02415121d124975
sepujas <sepujas.hilti@gmail.com>
2024-07-15 18:57:49 -0500

    sepujas - #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/sepujas.py
@@ -0,0 +1,44 @@
+# Sepujas - #00 - Python
+
+'''
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+'''
+#Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+# https://www.python.org/
+
+#Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+# One line
+'''
+Multiples
+lines
+'''
+
+#Variable
+my_first_variable = 0
+
+# Constants
+MY_FIRST_CONSTANTS = 'X'
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+
+my_string = 'Hi, I am a string'
+my_integer = 1
+my_float = 1.5
+my_boolean = True
+my_language = 'Python'
+
+print('Hola Python')
+print('Hola', my_language)
+print(f'hola {my_language}')
+
