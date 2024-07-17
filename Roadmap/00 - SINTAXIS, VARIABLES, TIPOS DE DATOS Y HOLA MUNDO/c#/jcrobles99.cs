6920191a315ae6348782286ccd9ba76e655816a0
Juan Robles <jcroblesor@gmail.com>
2024-07-16 18:16:15 -0600

    variables enteras

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/jcrobles99.cs
@@ -0,0 +1,49 @@
+/*
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+using system
+namespace reto00
+{
+    // Ejercicio #00.1
+    // https://dotnet.microsoft.com/es-es/languages/csharp
+
+    // Ejercicio #00.2: Comentario en una línea
+    /*
+        Comentario
+        múltiples
+        líneas
+    */
+    int x = /*comentario en linea de codigo*/ 1;
+
+    // Ejercicio #00.3
+    //********Tipos enteros************
+    sbyte sb = 0; //Variable tipo sbyte (8 bits con signo)
+
+    
+    int y = 0; // Declaracion de variable (tipo int)
+    const double pi = 3.14159265; //Declaracion de constante (tipo double)
+
+    // Ejercicio #00.4
+    int i = 0; //Variable tipo int (entero)
+    float f = 0.0f; //Variable tipo float (decimales)(4 bytes)(presicion 6 a 9 digitos aprox.)
+    double d = 0.0; //Variable tipo double (decimales)(8 bytes)(presicion 15 a 17 digitos aprox.)
+    decimal m = 0.0m; //Variable tipo decimal (decimales)(16 bytes)(presicion 28 a 29 digitos aprox.)
+    string cadena = "Variable para cadena de texto" //Variable tipo string (cadenas de texto)
+
+
+    
+}
+    
+
+ 
