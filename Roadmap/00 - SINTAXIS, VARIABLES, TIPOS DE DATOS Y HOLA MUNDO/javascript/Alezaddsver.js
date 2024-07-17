96a26339ca05848088368b2698cde19b4f73cdc4
Alezaddsver <139552510+Alezaddsver@users.noreply.github.com>
2024-07-17 15:10:38 +0200

    Alezaddsver.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Alezaddsver.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/en-US/docs/Web/javascript//
+
+// Comentario en una linea//
+
+/* Comentario de 
+
+
+
+
+
+diversas y largas lineas*/
+
+let name = "JavaScript"
+const constant = "Buenos dias"
+let string = "String"
+let number = 5
+let boolean = true
+let null = null
+let undefined = undefined
+console.log("¡Hola,"+" "+ name +"!")
