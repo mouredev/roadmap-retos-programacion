1975eaa0bdc414ef9fb79714b39ccfe7981ae2e5
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-07-18 07:45:11 -0400

    #07 - TypeScript

--- Roadmap/07 - PILAS Y COLAS/typescript/RicJDev.ts
+++ Roadmap/07 - PILAS Y COLAS/typescript/RicJDev.ts
@@ -26,19 +26,53 @@ queue.shift()
 console.log('Después de eliminar un elemento:', queue)
 
 //EXTRA
-import * as readline from 'readline'
+import * as readline from 'node:readline/promises'
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 })
 
-//Sistema de navegacion
+//Sistema de navegación
 function browser(): void {
-	const stack = []
+	const pagesStack: string[] = []
+
+	function menu(): void {
+		if (pagesStack.length > 0) {
+			console.log(`\nActualmente estas en ${pagesStack[pagesStack.length - 1]}`)
+		} else {
+			console.log('\nEstas en la pagina de inicio')
+		}
+
+		rl.question(
+			'Elige una opcion\n1. Avanzar\n2. Retroceder\n3. Salir del navegador\nIr a otra pagina '
+		).then((answer) => {
+			if (answer === '1') {
+				menu()
+			} else if (answer === '2') {
+				pagesStack.pop()
+				menu()
+			} else if (answer === '3') {
+				console.log('\nSaliendo del navegador...')
+				printer()
+			} else {
+				pagesStack.push(answer)
+				menu()
+			}
+		})
+	}
+
+	menu()
 }
+
+browser()
 
 //Impresora compartida
 function printer(): void {
 	const queue = []
+
+	console.log('Hola')
+	rl.close()
+
+	function menu() {}
 }
