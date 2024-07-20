a700bd6d2d517189843017e9e0ea5186453e3ed8
jrgranadosb <jrgranadosb@msn.com>
2024-07-19 18:35:18 -0600

    Create jrgranados.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jrgranados.py
@@ -0,0 +1,27 @@
+# https://python.org
+
+# comentario en una linea
+
+""" 
+Esto tambien es un comentario en varias lineas
+"""
+
+'''
+ Esta es otra forma de comentarios en varias lineas
+'''
+
+#Crear variables
+my_var1 = "my texto texto"
+MY_CONSTANTE = "esta es una constante"
+
+# Tipos datos primitivos
+
+#Enteros
+my_int = 1
+my_float = 1.1
+my_boolean = True
+my_boolean = False
+my_string = "abcdef"
+
+#imprimir texto 
+print("!Hola!, Python")
