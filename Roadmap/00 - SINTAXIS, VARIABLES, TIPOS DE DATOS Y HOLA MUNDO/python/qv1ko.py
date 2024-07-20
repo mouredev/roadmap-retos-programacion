a3ea1a12f35319c1534a8daecab04d0359b57fa0
Qv1ko <vicgarmur947@gmail.com>
2024-07-20 16:39:15 +0200

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/qv1ko.py
@@ -0,0 +1,17 @@
+# #00 - Sintaxis, variables, tipos de datos y hola mundo
+
+# www.python.org
+
+# Comment
+"""
+Comment
+"""
+
+var1 = 1
+VAR2 = 2
+
+var3 = "Python"
+var4 = 4
+var5 = 5.5
+
+print("¡Hola, " + var3 + "!")
