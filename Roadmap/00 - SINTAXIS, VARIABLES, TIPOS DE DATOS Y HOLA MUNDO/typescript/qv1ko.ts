e3cbacd3b111847ecb26e1eb8174b415e4686b7e
Qv1ko <vicgarmur947@gmail.com>
2024-07-20 17:18:18 +0200

    #00 - TypeScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/typescript/qv1ko.ts
@@ -0,0 +1,15 @@
+// #00 Sintaxis, variables, tipos de datos y hola mundo
+
+// www.typescriptlang.org
+
+/** Comment */
+// Comment
+
+var var1: number = 1;
+const VAR2: string = "TypeScript";
+
+var var2: string = 'TypeScript';
+var var3: number = 3.3;
+var var4: boolean = true;
+
+console.log("¡Hola, " + VAR2 + "!");
