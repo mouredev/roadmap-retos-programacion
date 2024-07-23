8e3f1a95388c3a0c2dcddb2891c20e40fb49d0aa
Marcos <sirluamark@gmail.com>
2024-07-23 16:00:22 +0200

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Lumanet.py
@@ -0,0 +1,26 @@
+# Python: https://www.python.org/
+
+# Comentario en una línea
+
+"""
+Comentario
+de varias
+líneas
+"""
+
+'''
+Comentario 
+de varias
+líneas también
+'''
+
+variable = "Valor de la variable"
+
+CONSTANTE = "Valor de la constante"
+
+cadena = "Cadena de texto"
+entero = 7
+boleano = True
+float = 7.77
+
+print("¡Hola, Python!")
