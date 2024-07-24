1a72829a0f9c48c6f0149a22a9c45463dabd8678
Jesus Alvarado <a20211688@pucp.edu.pe>
2024-07-23 20:19:13 -0500

    #00 - java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/JesusAP346.java
@@ -0,0 +1,29 @@
+public class JesusAP346 {
+    public static void main(String[] args) {
+        // https://www.java.com/es/
+
+        // comentario en una línea
+
+        /*
+        comentario
+        en
+        varias
+        lineas
+        */
+
+        int variable = 2;
+        final double cte = 3.141592;
+
+
+
+        int entero = 1;
+        double decimal = 1.1;
+        float flotante = 1.1f;
+        String cadenaDetexto = "hola gente";
+        char char_ = 'a' ;
+        boolean booleano = false;
+        System.out.println("¡Hola, java!");
+
+
+    }
+}
