bfbf97c06db62ce6add47e1f60fa84ebdb3bea9e
Jesus Antonio Escamilla <JesusAEEscamilla@outlook.com>
2024-07-25 04:14:09 -0600

    #06 - Java & Python "Extra"

--- Roadmap/06 - RECURSIVIDAD/python/JesusAntonioEEscamilla.py
+++ Roadmap/06 - RECURSIVIDAD/python/JesusAntonioEEscamilla.py
@@ -11,6 +11,7 @@ def imprimir_números(n):
     # Aquí es donde realiza la Recursividad
     imprimir_números(n - 1)
 
+print("Recursividad")
 imprimir_números(100)
 
 
@@ -18,4 +19,22 @@ imprimir_números(100)
 """
 EXTRA
 """
-# Pendiente
+# Factorial
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print("Factorial")
+print(factorial(5))
+
+# Fibonacci
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fibonacci(n - 1 ) + fibonacci(n - 2)
+
+print("Fibonacci")
+print(fibonacci(10))
