6b2a8d311a925bb5a9d0a5bb7f2c2a27ff307520
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-07-25 00:14:23 -0400

    30 - JavaScript

---
+++ Roadmap/30 - SOLID DIP/javascript/RicJDev.js
@@ -0,0 +1,17 @@
+/*
+ * EJERCICIO:
+ * Explora el "Principio SOLID de Inversión de Dependencias (Dependency Inversion
+ * Principle, DIP)" y crea un ejemplo simple donde se muestre su funcionamiento 
+ * de forma correcta e incorrecta.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un sistema de notificaciones.
+ * Requisitos:
+ * 1. El sistema puede enviar Email, PUSH y SMS (implementaciones específicas).
+ * 2. El sistema de notificaciones no puede depender de las implementaciones específicas.
+ * Instrucciones:
+ * 1. Crea la interfaz o clase abstracta.
+ * 2. Desarrolla las implementaciones específicas.
+ * 3. Crea el sistema de notificaciones usando el DIP.
+ * 4. Desarrolla un código que compruebe que se cumple el principio.
+ */
