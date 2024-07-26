183e3b5430e0b3b8c77c3e4c2be76855a1635d79
antonioverdugo <antonioverdugogarcia@gmail.com>
2024-07-26 12:05:23 +0200

    reto 00 antonioverdugo

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/antonioverdugo.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/antonioverdugo.js
@@ -1,3 +1,6 @@
+// Lenguaje javascript 
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
 //Comentario en una linea
 
 /*
@@ -5,3 +8,14 @@ Comentario
 en varias
 lineas
 */
+
+//Variable
+
+let variable = "hola mundo desde let"; //Recomendable utilizar let para las variables
+var variable2 = "hola mundo desde var"; //var es menos recomendable la diferencia es que var es mas global
+
+//Constante
+
+const constante = "Antonio"; 
+
+//Tipos de datos primitivos
