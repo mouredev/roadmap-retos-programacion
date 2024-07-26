1132906937508d1781859b0f717c1278cafc60c6
Maxycarp89 <maxicostilla21@gmail.com>
2024-07-26 15:45:46 -0300

    #00 - Javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Maxycarp89.js
@@ -0,0 +1,20 @@
+// Documentación oficial de JavaScript --> https://developer.mozilla.org/es/docs/Web/JavaScript
+
+//comentario de una sola linea
+/* comentario
+de 
+varias
+lineas */
+
+// Variables
+var variable1 = "Hola";
+let variable2 = 42;
+const variable3 = true;
+let variable4 = null;
+let variable5 = undefined;
+let variable6 = { nombre: "Juan", edad: 30 };
+let variable7 = [1, 2, 3, 4, 5];
+
+
+
+console.log("Hola JavaScript");
