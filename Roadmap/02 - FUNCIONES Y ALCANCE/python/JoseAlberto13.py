3661e97696b5de9e2e068eb6329d4e037e69c204
Jose Figueroa <josearies.9811@gmail.com>
2024-07-27 02:13:06 -0400

    #02-python

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/JoseAlberto13.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/JoseAlberto13.py
@@ -28,11 +28,13 @@ def saludo():
 
 saludo()
 
+
 # Con retorno
 def return_saludo():
         return "Hola, Función con retorno"
 
 print(return_saludo())
+
 
 # Con un argumento
 def arg_saludo(nombre):
@@ -40,11 +42,13 @@ def arg_saludo(nombre):
     
 arg_saludo("José Alberto")
 
+
 # Con varios argumentos
 def arg_personal(nombre, edad, sexo, nacionalidad):
       print(f"Nombre: {nombre}. Edad: {edad}. Sexo: {sexo}. Nacionalidad: {nacionalidad}")
     
-arg_personal("José", 25, "Masculino", "Venezolana")
+arg_personal("José", 26, "Masculino", "Venezolana")
+
 
 # Con un argumento predeterminado (si es que no se diera el argumento del parámetro)
 def default_arg_saludo(nombre="Usuario"):
@@ -53,10 +57,90 @@ def default_arg_saludo(nombre="Usuario"):
 default_arg_saludo()
 default_arg_saludo("Figueroa")
 
+
 # Con argumentos y return
 def args_return(saludo, persona):
       return f"{saludo}, {persona}"
 
-print(args_return(default_arg_saludo(),arg_saludo("José A.")))
+print(args_return("Hola!","José A."))
 
-# Funciones dentro de una Funcion
+
+# Con retorno de valores separados
+def multiple_return_saludo():
+      return "Hola", "Python"
+
+saludo, name = multiple_return_saludo()
+print(saludo)
+print(name)
+
+
+# Con un número variable de argumentos
+def variable_arg_saludo(*nombres):
+      for nombres in nombres:
+            print(f"Hola, {nombres}!")
+
+variable_arg_saludo("Mundo", "Python", "Git", "José", "MoureDev")
+
+
+# Con un número variable de argumentos con palabra clave
+def variable_key_arg(**nombres):
+      for key, value in nombres.items():
+            print(f"{key}: '{value}'")
+
+variable_key_arg(
+      lenguaje = "Python",
+      nombre = "José",
+      ocupacion = "Estudiante",
+      alias = "JoseAlberto13",
+      años = 26
+)
+
+
+# Funciones dentro de Funciones
+def outer_function():
+      def inner_function():
+            print("inner function: Función dentro de la función")
+      inner_function()
+
+outer_function()
+
+
+# Funciones dentro del lenguaje
+print(len("José"))
+print(type(25.5))
+print(sorted({4,2,7,1,5,6,3}))
+
+
+# Variables locales y globales
+variable_global = "Esto es global!"
+print(variable_global)
+
+def conVaribale_Global():
+      variable_local = "Esto es local!"
+      print(f"Funciónes: local={variable_local} global={variable_global}")
+
+conVaribale_Global()
+
+print(variable_global)
+# print(variable_local) no funciona fuera de la funcion definida
+
+
+"""
+DIFICULTAD EXTRA
+"""
+
+def lafuncion(texto1, texto2) -> int:
+      count = 0
+      for i in range(1,101):
+            if i % 5 == 0 and i % 3 == 0:
+                  print(texto1 + texto2)
+            elif i % 3 == 0:
+                  print(texto1)
+            elif i % 5 == 0:
+                  print(texto2)
+            else:
+                  print(i)
+                  count += 1
+      return count
+
+print(lafuncion("Texto #1","Texto #2"))
