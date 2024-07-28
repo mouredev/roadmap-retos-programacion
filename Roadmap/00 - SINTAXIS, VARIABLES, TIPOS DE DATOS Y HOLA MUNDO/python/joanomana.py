29d1b6262bdc1a91884b6b7c82adc52e2ae05039
joanomana <joansebastianomasu@gmail.com>
2024-07-28 09:43:39 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/joanomana.py
@@ -0,0 +1,51 @@
+"""
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
+#Comentario de una linea 
+#Sitio web de python: https://www.python.org/
+
+"""
+    Este es un comenratario
+    de varias lineas
+    con comillas dobles    
+"""
+'''
+Este es un comentario
+de varias lineas con comillas simples
+'''
+#Crear variable y constante
+mi_variable = "Hola"
+MI_VARIABLE = 1
+
+#Variables con tipos de datos
+my_int = 10
+my_float = 10.5
+my_str = "Python"
+my_bool = True
+
+#Imprimir por terminal
+print("!Hola, Python!")
+
+
