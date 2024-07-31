362b68d6d2546c0caf9aec99d7332f2e957ca7cf
3flip <162455028+3flip@users.noreply.github.com>
2024-07-31 18:06:46 +0200

    Create 3flip.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/3flip.js
@@ -0,0 +1,32 @@
+* - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+
+  /Esta es la web de JavaScript  https://developer.mozilla.org/es/docs/Web/JavaScript /
+  
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje 
+  /Comentaeio de una línea/
+  /*Comentarios
+  de varias
+  líneas*/
+  
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+
+   let mensaje; /Declaracióm de variable
+   const nacimiento = "03.05.1993" ;
+  
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+
+   Undefined: typeof instance === "undefined"
+  Boolean: typeof instance === "boolean"  representa una entidad lógica y puede tener dos valores: true y false.
+  Number: typeof instance === "number"    ECMAScript tiene dos tipos numéricos integrados: Number y BigInt
+  String: typeof instance === "string"    El tipo String de JavaScript se utiliza para representar datos textuales
+  BigInt: typeof instance === "bigint"    El tipo BigInt es un primitivo numérico en JavaScript que puede representar números enteros con precisión arbitraria.
+  Symbol: typeof instance === "symbol"    Un símbolo es un valor primitivo único e inmutable y se puede utilizar como clave de una propiedad de objeto
+
+  
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+    console.log("!hola, JavaScript¡");
+ *
