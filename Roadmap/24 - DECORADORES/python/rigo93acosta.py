1d67761c62fc274a274e7d4b3e75a0b7d10cb42a
riacosta <rigo93acosta@gmail.com>
2024-07-31 09:29:48 -0300

    Init Reto

---
+++ Roadmap/24 - DECORADORES/python/rigo93acosta.py
@@ -0,0 +1,27 @@
+# #24 PATRONES DE DISEÑO: DECORADORES
+'''
+/*
+ * EJERCICIO:
+ * Explora el concepto de "decorador" y muestra cómo crearlo
+ * con un ejemplo genérico.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un decorador que sea capaz de contabilizar cuántas veces
+ * se ha llamado a una función y aplícalo a una función de tu elección.
+ */
+'''
+
+## Ejercicio
+
+def example_function():
+    pass
+
+def example_function_2():
+    pass
+
+def example_function_3():
+    pass
+
+example_function()
+example_function_2()
+example_function_3()
