00753963415e1cc1f13636f079f5c9abc6bc93c1
Bastian Alquinta <bastian.alquinta.l@gmail.com>
2024-08-01 14:52:27 -0400

    Se agregó archivo Para Reto LSP

---
+++ Roadmap/28 - SOLID LSP/python/BastianAlq.py
@@ -0,0 +1,20 @@
+# /*
+#  * EJERCICIO:
+#  * Explora el "Principio SOLID de Sustitución de Liskov (Liskov Substitution Principle, LSP)" 
+#  * y crea un ejemplo simple donde se muestre su funcionamiento
+#  * de forma correcta e incorrecta.
+#  *
+#  * DIFICULTAD EXTRA (opcional):
+#  * Crea una jerarquía de vehículos. Todos ellos deben poder acelerar y frenar, así como
+#  * cumplir el LSP.
+#  * Instrucciones:
+#  * 1. Crea la clase Vehículo.
+#  * 2. Añade tres subclases de Vehículo.
+#  * 3. Implementa las operaciones "acelerar" y "frenar" como corresponda.
+#  * 4. Desarrolla un código que compruebe que se cumple el LSP.
+#  */
+
+
+# ---------------------------------------
+# | LISKOV SUBSTITUTION PRINCIPLE (OCP)  |  Información extraída de -> https://devexpert.io/principios-solid-guia-gratis/ 
+# ---------------------------------------
