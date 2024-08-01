c403ad7ff0364a888c996c93b981460ed777ae33
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-08-01 11:07:01 -0400

    31 - JavaScript

--- Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/javascript/RicJDev.js
+++ Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/javascript/RicJDev.js
@@ -1,23 +1,13 @@
+//@RicJDev
+//EJERCICIO
 /*
 
-	Se ejecuta en Node.js
+* PARTE I: modelado de jugadores, simulacion de los juegos y sistema de registro de eventos
 
-	- Se ha importado el modulo Readline
-	- Se ha instalado Picocolors, una libreria para colorear el texto de la terminal
-
-	@RicJDev
+	- Se ejecuta en Node.js
 
 */
 
-const readline = require('readline')
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
-
-const pc = require('picocolors')
-
-//EJERCICIO
 class Player {
   constructor(name, country) {
     this.name = name
@@ -30,11 +20,11 @@ class GameSimulator {
     this.players = []
   }
 
-  addPlayer(player) {
+  add(player) {
     this.players.push(player)
   }
 
-  simulateGame() {
+  play() {
     if (this.players.length >= 3) {
       this.players.sort(() => Math.random() - 0.5)
 
@@ -58,7 +48,9 @@ class EventRegister {
   events = []
 
   search(discipline) {
-		this.events.find((event) => event.discipline == discipline)
+    let result = this.events.find((event) => event.discipline == discipline)
+
+    return result
   }
 
   add(event) {
@@ -67,41 +59,96 @@ class EventRegister {
   }
 
   finish(event) {
-		let result = this.search(event)
+    let result = this.search(event)
     result.status = 'finished'
   }
 }
 
-//Menu
+class InformService {
+  constructor() {
+    this.data = []
+  }
+
+  add(data) {
+    this.data.push(data)
+  }
+
+  display() {
+    console.log(this.data)
+  }
+}
+
+/*
+
+* PARTE II: implementacion de la parte I en la terminal
+
+	- Se ha importado el modulo Readline para tener input en terminal
+	- Se ha instalado Picocolors, una libreria para colorear el texto de la terminal
+
+*/
+
+const readline = require('readline')
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const pc = require('picocolors')
+
+const eventRegister = new EventRegister()
+const informService = new InformService()
+
+//Funciones del menu
 function createEvent() {
   rl.question(pc.blue('\nIndique la disciplina del evento a registrar: '), (discipline) => {
+    eventRegister.add(new Event(discipline))
+
     main()
   })
 }
 
 function registerParticipant() {
-  rl.question(pc.green('\nIndique el evento para registrar el participante: '), (discipline) => {
-    rl.question(pc.green('Indique el nombre del participante: '), (name) => {
-      rl.question(pc.green('Indique el pais del participante: '), (country) => {
-        main()
+  rl.question(pc.blue('\nIndique el evento para registrar el participante: '), (discipline) => {
+    const event = eventRegister.search(discipline)
+
+    if (event) {
+      rl.question(pc.green('Indique el nombre del participante: '), (name) => {
+        rl.question(pc.magenta('Indique el pais del participante: '), (country) => {
+          event.simulator.add(new Player(name, country))
+
+          main()
+        })
       })
-    })
+    } else {
+      console.log(pc.red('Evento no registrado'))
+
+      main()
+    }
   })
 }
 
 function simulateEvent() {
   rl.question(pc.blue('\nIndique el evento a simular: '), (discipline) => {
+    const event = eventRegister.search(discipline)
+
+    if (event) {
+      let results = event.simulator.play()
+      informService.add(results)
+    } else {
+      console.log(pc.red('Evento no registrado'))
+    }
+
     main()
   })
 }
 
 function generateInform() {
-  //...
+  informService.display()
   main()
 }
 
 function exit() {
-  console.log(pc.green('\nSaliendo del programa...'))
+  console.log(pc.red('\nSaliendo del programa...'))
 
   setTimeout(() => {
     console.clear()
@@ -113,6 +160,12 @@ function exit() {
 function main() {
   console.log(pc.underline('\nSIMULADOR DE JUEGOS OLIMPICOS'))
 
+  console.log(
+    pc.yellow(
+      '\n1. Registrar evento\n2. Registrar participantes\n3. Simular evento\n4. Generar informes\n5. Salir del programa'
+    )
+  )
+
   const actions = new Map([
     ['1', createEvent],
     ['2', registerParticipant],
@@ -121,16 +174,16 @@ function main() {
     ['5', exit],
   ])
 
-  console.log(
-    pc.yellow(
-      '\n1. Registrar evento\n2. Registrar participantes\n3. Simular evento\n4. Generar informes\n5. Salir del programa'
-    )
-  )
-
   rl.question(pc.green('\nSeleccione una opcion (1 - 5) '), (option) => {
-    const method = actions.get(option) || main
-    method()
+    const action =
+      actions.get(option) ||
+      function () {
+        console.log(pc.red('Opcion no valida'))
+        main()
+      }
+
+    action()
   })
 }
 
-rl.close()
+main()
