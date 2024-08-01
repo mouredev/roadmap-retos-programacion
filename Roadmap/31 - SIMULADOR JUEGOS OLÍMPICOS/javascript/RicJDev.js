c189396c5e5aa294fc68fb06b971041300ce9bf4
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-07-31 20:01:52 -0400

    31 - JavaScript

--- Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/javascript/RicJDev.js
+++ Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/javascript/RicJDev.js
@@ -1,49 +1,23 @@
 /*
- * EJERCICIO:
- * ¡Los JJOO de París 2024 han comenzado!
- * Crea un programa que simule la celebración de los juegos.
- * El programa debe permitir al usuario registrar eventos y participantes,
- * realizar la simulación de los eventos asignando posiciones de manera aleatoria
- * y generar un informe final. Todo ello por terminal.
- * Requisitos:
- * 1. Registrar eventos deportivos.
- * 2. Registrar participantes por nombre y país.
- * 3. Simular eventos de manera aleatoria en base a los participantes (mínimo 3).
- * 4. Asignar medallas (oro, plata y bronce) basándose en el resultado del evento.
- * 5. Mostrar los ganadores por cada evento.
- * 6. Mostrar el ranking de países según el número de medallas.
- * Acciones:
- * 1. Registro de eventos.
- * 2. Registro de participantes.
- * 3. Simulación de eventos.
- * 4. Creación de informes.
- * 5. Salir del programa.
- */
 
-//Se ejecuta en Node.js importando el módulo ReadLine
+	Se ejecuta en Node.js
+
+	- Se ha importado el modulo Readline
+	- Se ha instalado Picocolors, una libreria para colorear el texto de la terminal
+
+	@RicJDev
+
+*/
+
 const readline = require('readline')
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 })
+
+const pc = require('picocolors')
+
 //EJERCICIO
-//Modelado de eventos
-const events = []
-
-class Event {
-	constructor(discipline) {
-		this.discipline = discipline
-		this.participants = []
-
-		events.push(this)
-	}
-
-	addParticipant(participant) {
-		this.participants.push(participant)
-	}
-}
-
-//Modelado de participantes
 class Participant {
 	constructor(name, country) {
 		this.name = name
@@ -51,73 +25,89 @@ class Participant {
 	}
 }
 
-//Funciones del menu
-function searchEvent(discipline) {
-	const result = events.find((event) => event.discipline === discipline)
+class Event {
+	constructor(discipline) {
+		this.discipline = discipline
+	}
+}
 
-	return result
+class EventRegister {
+	events = []
+
+	search(discipline) {
+		this.events.find((event) => event.discipline === discipline)
+	}
+
+	addEvent(discipline) {
+		let event = new Event(discipline)
+		event.status = 'unfinished'
+
+		this.events.push(event)
+	}J
+}
+
+class GameSimulator {
+	constructor() {
+		this.players = []
+	}
+
+	addPlayer(player) {
+		this.players.push(player)
+	}
+
+	simulateGame() {
+		if (this.players.length >= 3) {
+			this.players.sort(() => Math.random() - 0.5)
+
+			this.players[0].medal = 'gold'
+			this.players[1].medal = 'silver'
+			this.players[2].medal = 'bronze'
+
+			return this.players
+		}
+	}
+}
+
+//Menu
+function createEvent() {
+	rl.question(pc.blue('\nIndique la disciplina del evento a registrar: '), (discipline) => {
+		main()
+	})
 }
 
 function registerParticipant() {
-	rl.question('Indique el evento para registrar el participante ', (discipline) => {
-		const event = searchEvent(discipline)
-
-		rl.question('Indique el nombre del participante ', (name) => {
-			rl.question('Indique el pais del participante ', (country) => {
-				const participant = new Participant(name, country)
-				event.addParticipant(participant)
-
+	rl.question(pc.green('\nIndique el evento para registrar el participante: '), (discipline) => {
+		rl.question(pc.green('Indique el nombre del participante: '), (name) => {
+			rl.question(pc.green('Indique el pais del participante: '), (country) => {
 				main()
 			})
 		})
 	})
 }
 
-function createEvent() {
-	rl.question('Indique la disciplina del evento a registrar ', (discipline) => {
-		new Event(discipline)
-
-		main()
-	})
-}
-
 function simulateEvent() {
-	rl.question('Indique el evento a simular ', (discipline) => {
-		const event = searchEvent(discipline)
-
-		if (event.participants.length > 3) {
-			//...
-		} else {
-			console.log('No hay suficientes participantes para la simulacion')
-		}
-
+	rl.question(pc.blue('\nIndique el evento a simular: '), (discipline) => {
 		main()
 	})
 }
 
 function generateInform() {
+	//...
 	main()
 }
 
 function exit() {
-	console.log('\nSaliendo del programa...')
-	rl.close()
-}
+	console.log(pc.green('\nSaliendo del programa...'))
 
-function display() {
-	console.log('\n\nOpcion para mostrar listados solo para el desa')
-
-	events.forEach((element) => {
-		console.log(element.discipline)
-		console.log(element.participants)
-	})
-
-	main()
+	setTimeout(() => {
+		console.clear()
+		rl.close()
+	}, 300)
 }
 
 //Menu de opciones
 function main() {
-	console.log('\nSIMULADOR DE JUEGOS OLIMPICOS')
+	console.log(pc.underline('\nSIMULADOR DE JUEGOS OLIMPICOS'))
 
 	const actions = new Map([
 		['1', createEvent],
@@ -125,17 +115,18 @@ function main() {
 		['3', simulateEvent],
 		['4', generateInform],
 		['5', exit],
-		['d', display],
 	])
 
 	console.log(
-		'1. Registrar evento\n2. Registrar participantes\n3. Simular evento\n4. Generar informes\n5. Salir del programa'
+		pc.yellow(
+			'\n1. Registrar evento\n2. Registrar participantes\n3. Simular evento\n4. Generar informes\n5. Salir del programa'
+		)
 	)
 
-	rl.question('Seleccione una opcion (1 - 5) ', (option) => {
+	rl.question(pc.green('\nSeleccione una opcion (1 - 5) '), (option) => {
 		const method = actions.get(option) || main
 		method()
 	})
 }
 
-main()
+rl.close()
