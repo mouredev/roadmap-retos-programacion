59f50ea87c87e3c7cdb9b945e0b1fab8e1be2bb3
Francisco <cuminifrancisco@gmail.com>
2024-08-02 08:36:15 -0300

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/FranciscoCuminiLondero.js
@@ -0,0 +1,15 @@
+// https://www.javascript.com/
+/*https://www.javascript.com/*/
+
+const MI_NOMBRE = "Francisco Cumini";
+let edad = 23;
+
+let MyString = "Francisco";
+let MyNumber = 1;
+let MyBoolean = true;
+let MyNull = null;
+let MyUndefined = undefined;
+let MySymbol = Symbol();
+let MyBigInt = BigInt(32);
+
+console.log("¡Hola, JavaScript!");
