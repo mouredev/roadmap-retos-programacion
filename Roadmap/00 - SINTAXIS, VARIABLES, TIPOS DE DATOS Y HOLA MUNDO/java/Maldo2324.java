3f0a5cf9b83ac387b95aa977e258b059a68aae2d
Maldo2324 <maldonadom.josea@gmail.com>
2024-08-05 15:38:56 -0500

    #00-Maldo2324

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Maldo2324.java
@@ -0,0 +1,35 @@
+public class Maldo2324 {
+    public static void main(String[] args) {
+
+        // Sitio web del lenguaje de programacion usado "https://www.java.com/"
+
+        // SINTAXIS PARA CREAR COMENTARIOS EN EL LENGUAJE
+
+    // Utilizamos // para comentarios en una sola linea
+
+    /* De esta forma podemos demostrar
+    comentarios de multiples lineas
+     */
+
+    //Crea una variable y una constante
+        int variable = 1;
+        final int constante= 2;
+
+    //Crea una variable representando todos los tipos de datos primitivos
+        byte maxByte = 127;
+        short maxShort = 32767;
+        int maxInt = 2147483647;
+        long maxLong = 9223372036854775807L;
+        float puntoFlotante = 10.5F;
+        double decimales = 12.5;
+        char unCaracter = 'x';
+        boolean soloDosValores = true; // o False
+
+        System.out.println("¡Hola, Java!");
+        
+
+
+
+
+    }
+}
