0213d509fea4841a90c86d1471ea3c9faf258c86
culebropalido <culebro@localhost.localdomain>
2024-08-06 19:14:40 +0200

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/culebropalido.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env  python3
+
+#   https://www.python.org/ Web oficial de python
+
+#   Esto es un comentario de una sola linea
+
+'''
+    Esto es un comnentario con varias lineas,
+    en python podemos hacerlo englobando el texto 
+    entre tres comillas. 
+    Con confundir con docstrings.
+'''
+variable = 1    # Asignamos el valor 1 a la variable
+
+#   En python no existen constantes
+
+# A continuación se crean variables primitivas de python
+
+entero = 10     #  Variable tipo integer
+
+decimal = 44.5    # Variable tipo float
+
+complejo = e = 1 +2j    # Varibale tipo complex
+
+cadena = "Hola que tal" # Variable tipo cadena de texto
+
+booleana = True #   Variable tipo booleana
+
+#   Imprimir el mensaje indicado
+print("¡Hola, Python!")
