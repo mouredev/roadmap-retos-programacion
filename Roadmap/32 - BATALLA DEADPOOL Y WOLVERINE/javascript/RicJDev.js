6bc30471533b7bc38c7a7814838bdeb912fd8d93
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-08-08 21:31:13 -0400

    32 - JavaScript

--- Roadmap/32 - BATALLA DEADPOOL Y WOLVERINE/javascript/RicJDev.js
+++ Roadmap/32 - BATALLA DEADPOOL Y WOLVERINE/javascript/RicJDev.js
@@ -1,77 +1,10 @@
-//EJERCICIO
-//Modelado de personajes
-class Character {
-  constructor(name, hp, attackRange = { min: 0, max: 50 }, defenseRate) {
-    this.name = name
-    this.hp = hp
-
-    if (this.hp > 1500) {
-      console.log(pc.gray('Se ha establecido un máximo de 1500 hp.'))
-      this.hp = 1500
-    } else if (hp < 500 || isNaN(hp)) {
-      console.log(pc.gray('Se ha establecido un mínimo de 500 hp.'))
-      this.hp = 500
-    }
-
-    this.attackRange = attackRange
-    this.defenseRate = defenseRate
-  }
-
-  attactk() {
-    let damage = Math.floor(
-      Math.random() * (this.attackRange.max - this.attackRange.min + 1) + this.attackRange.min
-    )
-
-    return damage
-  }
-
-  defense(damageReceived) {
-    let result = Math.floor(Math.random() * 100 + 1)
-
-    if (result > 100 - this.defenseRate) {
-      damageReceived = 0
-    }
-
-    this.hp -= damageReceived
-
-    if (this.hp < 0) {
-      this.hp = 0
-    }
-  }
-
-  get lifeBar() {
-    let bars = Math.floor(this.hp / 25)
-
-    if (this.hp === 0) {
-      return pc.gray('-'.repeat(20))
-    }
-
-    return pc.green('|'.repeat(bars))
-  }
-}
-
-let test = new Character('Juan', 1300, { min: 5, max: 90 }, 0)
-console.log(test.hp)
-console.log(test.lifeBar)
-
-for (let i = 0; i < 10; i++) {
-  let attack = test.attactk()
-
-  if (attack === test.attackRange.max) {
-    console.log(pc.red('CRITICAL:'), attack)
-  }
-
-  test.defense(attack)
-}
-
-console.log(test.hp)
-console.log(test.lifeBar)
-
 /*
+  EJERCICIO:
 
-  - Se ejecuta en Node.js, importando el modulo Readline/promises (https://nodejs.org/api/readline.html)
-  - Se ha instalado Picocolors, un libreria para colorear texto en terminal (https://www.npmjs.com/package/picocolors)
+- Se ejecuta en Node.js, importando el modulo Readline/promises (https://nodejs.org/api/readline.html)
+- Se ha instalado Picocolors, un libreria para colorear texto en terminal (https://www.npmjs.com/package/picocolors)
 
+  @RicJDev
 */
 
 import * as readline from 'node:readline/promises'
@@ -82,37 +15,158 @@ const rl = readline.createInterface({
   output: process.stdout,
 })
 
-let Deadpool
-let Wolverine
+//Modelado de personajes
+class Character {
+  constructor(name, hp, attackRange = { min: 0, max: 50 }, defenseRate) {
+    this.name = name
+    this.hp = hp
 
+    if (this.hp > 2000) {
+      console.log(pc.gray('Se ha establecido en el máximo de 2000 hp.'))
+      this.hp = 2000
+    } else if (hp < 500 || isNaN(hp)) {
+      console.log(pc.gray('Se ha establecido en el mínimo de 500 hp.'))
+      this.hp = 500
+    }
+
+    this.canAttack = true
+
+    this.attackRange = attackRange
+    this.defenseRate = defenseRate
+  }
+
+  attack() {
+    let damage = 0
+
+    if (this.canAttack) {
+      damage = Math.floor(
+        Math.random() * (this.attackRange.max - this.attackRange.min + 1) + this.attackRange.min
+      )
+    }
+
+    return damage
+  }
+
+  defense(damage) {
+    let result = Math.floor(Math.random() * 100 + 1)
+
+    if (result > 100 - this.defenseRate) {
+      damage = 0
+    }
+
+    this.hp -= damage
+
+    if (this.hp < 0) {
+      this.hp = 0
+    }
+
+    return damage
+  }
+
+  get lifeBar() {
+    let bars = pc.green('|'.repeat(Math.floor(this.hp / 25)))
+
+    if (this.hp === 0) {
+      bars = pc.gray(' - ')
+    }
+
+    return pc.bgBlack(`${bars}`)
+  }
+}
+
+//Funciones del simulador
+function display(playerA, playerB) {
+  console.log(`\n${playerA.name} (${playerA.hp}):\n${playerA.lifeBar} `)
+  console.log(`\n${playerB.name} (${playerB.hp}):\n${playerB.lifeBar} `)
+}
+
+function simulateAtack(playerA, playerB) {
+  let message
+
+  if (playerA.canAttack) {
+    let defenseResult = playerB.defense(playerA.attack())
+
+    if (defenseResult > 0) {
+      message = `ataque efectivo! ${playerB.name} ${pc.magenta(`-${defenseResult} hp`)}`
+
+      if (defenseResult === playerA.attackRange.max) {
+        message += pc.magenta(' [CRITICAL]')
+
+        playerB.canAttack = false
+      }
+    } else {
+      message = `${playerB.name} ha esquivado el ataque`
+    }
+  } else {
+    message = `recibió un golpe crítico. No puede atacar`
+
+    playerA.canAttack = true
+  }
+
+  console.log(`\nTurno de ${playerA.name}: ${message}\n`)
+}
+
+let turn = true
+
+function simulateTurn(playerA, playerB) {
+  display(playerA, playerB)
+
+  if (turn) {
+    simulateAtack(playerA, playerB)
+
+    turn = false
+  } else {
+    simulateAtack(playerB, playerA)
+
+    turn = true
+  }
+}
+
+function simulateBattle(playerA, playerB) {
+  let battle = setInterval(() => {
+    console.clear()
+    console.log(pc.underline('\nBATALLA EN CURSO!'))
+
+    simulateTurn(playerA, playerB)
+
+    if (playerA.hp === 0 || playerB.hp === 0) {
+      clearInterval(battle)
+
+      console.clear()
+      console.log(pc.underline('BATALLA FINALIZADA!'))
+
+      display(playerA, playerB)
+
+      if (playerA.hp === 0) {
+        console.log(`\n${playerB.name} ha ganado!\n`)
+      } else if (playerB.hp === 0) {
+        console.log(`\n${playerA.name} ha ganado!\n`)
+      }
+    }
+  }, 1100)
+}
+
+//Implementacion en terminal
 async function main() {
   console.clear()
+  console.log(pc.underline('\nBIENVENIDO AL SIMULADOR!\n'))
 
-  console.log(pc.underline('\nBIENVENIDO AL SIMULADOR!'))
-  console.log(' ')
-
+  //Deadpool
   let deadpoolHP = await rl.question('Indique la cantidad de vida para Deadpool. ')
   deadpoolHP = parseInt(deadpoolHP)
 
-  Deadpool = new Character('Deadpool', deadpoolHP, { min: 10, max: 100 }, 25)
+  const Deadpool = new Character(pc.red('Deadpool'), deadpoolHP, { min: 10, max: 100 }, 25)
 
+  //Wolverine
   let wolverineHP = await rl.question('Indique la cantidad de vida para Wolverine. ')
   wolverineHP = parseInt(wolverineHP)
 
-  Wolverine = new Character('Wolverine', wolverineHP, { min: 10, max: 120 }, 20)
+  const Wolverine = new Character(pc.yellow('Wolverine'), wolverineHP, { min: 10, max: 120 }, 20)
 
   console.log(pc.gray('Cargando...'))
-
-  function display() {
-    console.clear()
-    console.log(`\n${pc.red(Deadpool.name)} (${Deadpool.hp}):\n${Deadpool.lifeBar} `)
-    console.log(`\n${pc.yellow(Wolverine.name)} (${Wolverine.hp}):\n${Wolverine.lifeBar} `)
-  }
-
-  setTimeout(display, 1000)
+  simulateBattle(Deadpool, Wolverine)
 
   rl.close()
 }
 
-//main()
-rl.close()
+main()
