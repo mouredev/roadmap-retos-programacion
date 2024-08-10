b76d9e177dc3dc4aeafad04226fd62b804f0e6db
VolumiDev <diego.martga@gmail.com>
2024-08-10 16:53:58 +0200

    Complete el ejercicio con mas tipos de funciones que nos habia contemplado

--- Roadmap/02 - FUNCIONES Y ALCANCE/kotlin/VolumiDev.kt
+++ Roadmap/02 - FUNCIONES Y ALCANCE/kotlin/VolumiDev.kt
@@ -18,7 +18,12 @@ fun restar(){
 fun hello(cadena : String){
   println("Hello $cadena !!!")
 }
+// Funcion con parametros predeterminado
+fun predet_fun(cad: String = "Este es el valor predeterminado"){
+  println("$cad")
+}
 
+// creamos una funcion dentro de otra
 fun crear_funciones(){
   fun sum(){
     println("Este mensaje esta dentro de un funcion que ha sido crada dentro de otra")
@@ -27,6 +32,21 @@ fun crear_funciones(){
   println("Estamos dentro de la funcion 1")
   sum()
 }
+
+// Funciones de extension
+fun String.combi() : String {
+  var cadena = this.uppercase()
+  cadena = cadena.reversed()
+  return cadena
+}
+
+// Funciones de orden superior
+fun calcular(a: Int, b: Int, operacion: (Int, Int) -> Int): Int {
+  return operacion(a, b)
+}
+
+// Funciones lambdas 
+val multiplicacion = {a: Int, b: Int -> a * b}
 
 
   // Dificultad extra del ejercicio 02
@@ -43,11 +63,11 @@ fun extra(cad1 : String, cad2 : String) : Int {
     var indice : Int = 0
     while(indice <= 100){
       when{
-        indice % 3 == 0 -> println(cad1)
-        indice % 5 == 0 -> println(cad2)
-        (indice % 5 == 0) && (indice % 3 == 0) -> {println("$cad1 $cad2")}
+        indice % 5 == 0 && indice % 3 == 0 -> {print(" $cad1$cad2")}
+        indice % 3 == 0 -> print(" $cad1")
+        indice % 5 == 0 -> print(" $cad2")
         else -> {
-          println(indice)
+          print(" $indice")
           cont++
         }
       }
@@ -58,19 +78,30 @@ fun extra(cad1 : String, cad2 : String) : Int {
 
 
 fun main() {
-  val cad : String = "todo en minus para probar"
+  var cad : String = "todo en minus para probar"
   val resultado = sumar(3, 5)
   println("El resultado de la suma es: $resultado") // Salida: El resultado de la suma es: 8
   restar()
+  predet_fun()
+  predet_fun("Este es el mensaje que le hemos pasado por parametro")
   hello("Kotlin")
   crear_funciones()
   println(variable_global)
 
   //Ejemplo de funcion que esta establecida en el lenguaje
   println(cad.uppercase())
+
+  // Vamos a probar una funcion de extension
+  var cadena1 = "Funciones de extension"
+  cadena1 = cadena1.combi()
+  println("$cadena1")
   
-  println("Se han impreso ${ extra("Primera cadena de texto", "Segunda cadena de texto") } numeros")
+  print("Se han impreso ${ extra("fizz", "buzz") } numeros")
 
+  println("FUNCIONES DE ORDEN SUPERIOR")
+  println("Este es el resultado de una funcion de orden superior: ${calcular(5, 4) {x, y -> x - y}}")
 
-
+  println("FUNCIONES LAMBDA")
+  println("Este es el resultado de una funcion lambda: ${multiplicacion(4, 3)}")
+  
 }
