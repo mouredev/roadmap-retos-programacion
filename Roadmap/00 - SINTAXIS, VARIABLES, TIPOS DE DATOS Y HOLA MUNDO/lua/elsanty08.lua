7582d31784597e9453bb88fc85632c04399b82d0
elsanty08 <105884031+elsanty08@users.noreply.github.com>
2024-08-11 14:34:42 -0400

    elsanty08.lua

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/lua/elsanty08.lua
@@ -0,0 +1,29 @@
+-- https://lua.org/
+
+-- esta sintaxis se utiliza para comentar en una linea
+
+--[[ 
+esta sintaxis se utiliza para comentar en varias lineas
+]]
+
+local miVariable = "mimismo"
+--[[ 
+no utilizamos constante. Por ejemplo en lua para roblox studio :) utilizamos la definicion de constante para objetos que
+no debemos eliminar por que nos interesa mantener con el mismo nombre o lugar y usamos: screaming snake case como ejemplo 
+en la linea: 16
+]]
+
+local BARRIL_AGUA = Game.Workspace...ETC -- para declarar el objeto en el espacio de trabajo
+
+local miVariableBooleana = true and false or nil
+
+local miVariableEntero = 1 
+
+local miVariableString = "texto" -- puedes usar comillas simples o dobles 
+
+local function (nombreFuncion)
+  --cuerpo de la funcion
+end
+
+print("¡Hola, lua!")
+
