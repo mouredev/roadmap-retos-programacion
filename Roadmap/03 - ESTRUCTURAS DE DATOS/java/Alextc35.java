a6d702a33d4aada1265b7375f6c31b268bd4d285
Alextc35 <119206974+Alextc35@users.noreply.github.com>
2024-08-13 18:04:33 +0200

    Update Alextc35.java

--- Roadmap/03 - ESTRUCTURAS DE DATOS/java/Alextc35.java
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/java/Alextc35.java
@@ -574,20 +574,6 @@ public class Alextc35 {
         menu(miAgenda);
         sc.close();
     }
-    /*
-     * DIFICULTAD EXTRA (opcional):
-     * Crea una agenda de contactos por terminal.
-     * - Debes implementar funcionalidades de búsqueda, inserción, actualización y
-     * eliminación de contactos.
-     * - Cada contacto debe tener un nombre y un número de teléfono.
-     * - El programa solicita en primer lugar cuál es la operación que se quiere
-     * realizar, y a continuación los datos necesarios para llevarla a cabo.
-     * - El programa no puede dejar introducir números de teléfono no númericos y
-     * con más de 11 dígitos.
-     * (o el número de dígitos que quieras)
-     * - También se debe proponer una operación de finalización del programa.
-     */
-
     // AGENDA
     // menú
     public static void menu(HashMap<String, Integer> map) {
