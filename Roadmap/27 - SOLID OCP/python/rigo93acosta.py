20a100af3da453bbb9e6436e0281f29f3971e895
riacosta <rigo93acosta@gmail.com>
2024-08-13 21:57:21 -0300

    End Reto

--- Roadmap/27 - SOLID OCP/python/rigo93acosta.py
+++ Roadmap/27 - SOLID OCP/python/rigo93acosta.py
@@ -42,3 +42,55 @@ class Triangle(Form):
 Extra
 '''
 
+from abc import ABC, abstractmethod
+
+class Operation(ABC):
+    @abstractmethod
+    def execute(self, a, b):
+        pass
+
+class Sum(Operation):
+    def execute(self, a, b):
+        return a + b
+    
+class Subtraction(Operation):
+    def execute(self, a, b):
+        return a - b
+    
+class Multiplication(Operation):
+    def execute(self, a, b):
+        return a * b
+    
+class Division(Operation):
+    def execute(self, a, b):
+        return a / b
+
+class Power(Operation):
+    def execute(self, a, b):
+        return a ** b
+
+class Calculator:
+
+    def __init__(self) -> None:
+        self.operations = {}
+
+    def add_operation(self, name, operation):
+        self.operations[name] = operation
+
+    def calculate(self, name, a, b):
+        if name not in self.operations:
+            raise ValueError(f'Operation {name} not found')
+        return self.operations[name].execute(a, b)
+
+calculator = Calculator()
+calculator.add_operation('sum', Sum())
+calculator.add_operation('subtraction', Subtraction())
+calculator.add_operation('multiplication', Multiplication())
+calculator.add_operation('division', Division())
+calculator.add_operation('power', Power())
+
+print(calculator.calculate('sum', 2, 3))
+print(calculator.calculate('subtraction', 15, 3))
+print(calculator.calculate('multiplication', 2, 3))
+print(calculator.calculate('division', 10, 5))
+print(calculator.calculate('power', 2, 3))
