14d13e41ea37448ff867ea6ed007f62f6fcf9ea7
Rafa Canosa <rafacv23@gmail.com>
2024-08-14 16:26:50 +0200

    feat: create Rafacv23.js

---
+++ Roadmap/33 - RESCATANDO A MICKEY/javascript/Rafacv23.js
@@ -0,0 +1,19 @@
+/* 
+    Creado por Rafa Canosa
+    Github: https://github.com/Rafacv23
+    Website: https://www.rafacanosa.dev
+*/
+
+// Posibles casillas para el tablero de juego 6x6
+
+const board = [
+  ["🐭", "⬜", "⬛", "⬜", "⬜", "⬜"],
+  ["⬛", "⬜", "⬜", "⬜", "⬛", "⬜"],
+  ["⬛", "⬛", "⬛", "⬛", "⬛", "⬜"],
+  ["⬜", "⬜", "🚪", "⬛", "⬛", "⬜"],
+  ["⬜", "⬛", "⬛", "⬛", "⬜", "⬜"],
+  ["⬜", "⬜", "⬜", "⬜", "⬜", "⬛"],
+]
+
+// Función para que el jugador pueda moverse
+const playerMove = (input) => {}
