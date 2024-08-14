cf6b5e9dd3827d5221e7feecce4dd3c1c95e3537
riacosta <rigo93acosta@gmail.com>
2024-08-13 21:36:27 -0300

    Init Reto

---
+++ Roadmap/27 - SOLID OCP/python/rigo93acosta.py
@@ -0,0 +1,22 @@
+'''
+ * EJERCICIO:
+ * Explora el "Principio SOLID Abierto-Cerrado (Open-Close Principle, OCP)" 
+ * y crea un ejemplo simple donde se muestre su funcionamiento
+ * de forma correcta e incorrecta.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Desarrolla una calculadora que necesita realizar diversas operaciones matemáticas. 
+ * Requisitos:
+ * - Debes diseñar un sistema que permita agregar nuevas operaciones utilizando el OCP.
+ * Instrucciones:
+ * 1. Implementa las operaciones de suma, resta, multiplicación y división.
+ * 2. Comprueba que el sistema funciona.
+ * 3. Agrega una quinta operación para calcular potencias.
+ * 4. Comprueba que se cumple el OCP.
+ */
+'''
+
+'''
+Basic
+'''
+
