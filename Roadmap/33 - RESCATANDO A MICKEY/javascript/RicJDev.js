d76cf474c05dac22523b170b49420409b5dcabd6
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-08-15 12:12:13 -0400

    33 - JavaScript

--- Roadmap/33 - RESCATANDO A MICKEY/javascript/RicJDev.js
+++ Roadmap/33 - RESCATANDO A MICKEY/javascript/RicJDev.js
@@ -1,28 +1,3 @@
-/*
- * EJERCICIO:
- * ¡Disney ha presentado un montón de novedades en su D23!
- * Pero... ¿Dónde está Mickey?
- * Mickey Mouse ha quedado atrapado en un laberinto mágico
- * creado por Maléfica.
- * Desarrolla un programa para ayudarlo a escapar.
- * Requisitos:
- * 1. El laberinto está formado por un cuadrado de 6x6 celdas.
- * 2. Los valores de las celdas serán:
- *    - ⬜️ Vacío
- *    - ⬛️ Obstáculo
- *    - 🐭 Mickey
- *    - 🚪 Salida
- * Acciones:
- * 1. Crea una matriz que represente el laberinto (no hace falta
- * que se genere de manera automática).
- * 2. Interactúa con el usuario por consola para preguntarle hacia
- * donde se tiene que desplazar (arriba, abajo, izquierda o derecha).
- * 3. Muestra la actualización del laberinto tras cada desplazamiento.
- * 4. Valida todos los movimientos, teniendo en cuenta los límites
- * del laberinto y los obtáculos. Notifica al usuario.
- * 5. Finaliza el programa cuando Mickey llegue a la salida.
- */
-
 const maze = [
   [1, 0, 1, 1, 2, 1],
   [0, 0, 0, 1, 0, 1],
