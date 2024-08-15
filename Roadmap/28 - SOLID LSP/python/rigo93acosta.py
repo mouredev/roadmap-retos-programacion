09c450b41c5a99d435d931f346cc449ad15c2cd6
rigo93acosta <rigo93acosta@gmail.com>
2024-08-15 09:44:39 -0300

    End reto

--- Roadmap/28 - SOLID LSP/python/rigo93acosta.py
+++ Roadmap/28 - SOLID LSP/python/rigo93acosta.py
@@ -52,3 +52,57 @@ print(chicken.move())
 Extra
 '''
 
+class Vehicle:
+    
+    def __init__(self, speed=0) -> None:
+        self.speed = speed
+
+    def accelerate(self, increment) -> str:
+        self.speed += increment
+        return f"Velocidad {self.__class__.__name__}: {self.speed} km/h"
+
+    def brake(self, decrement) -> str:
+        self.speed -= decrement
+        if self.speed < 0:
+            self.speed = 0
+        return f"Velocidad {self.__class__.__name__}: {self.speed} km/h"
+    
+class Car(Vehicle):
+    def accelerate(self, increment) -> str:
+        print(f"{self.__class__.__name__} acelerando")
+        return super().accelerate(increment)
+    
+    def brake(self, decrement) -> str:
+        print(f"{self.__class__.__name__} frenando")
+        return super().brake(decrement)
+
+class Bicycle(Vehicle):
+    def accelerate(self, increment) -> str:
+        print(f"{self.__class__.__name__} acelerando")
+        return super().accelerate(increment)
+    
+    def brake(self, decrement) -> str:
+        print(f"{self.__class__.__name__} frenando")
+        return super().brake(decrement)
+
+
+class Motorcycle(Vehicle):
+    def accelerate(self, increment) -> str:
+        print(f"{self.__class__.__name__} acelerando")
+        return super().accelerate(increment)
+    
+    def brake(self, decrement) -> str:
+        print(f"{self.__class__.__name__} frenando")
+        return super().brake(decrement)
+
+car = Car()
+print(car.accelerate(10))
+print(car.brake(5))
+
+bicycle = Bicycle()
+print(bicycle.accelerate(3))
+print(bicycle.brake(1))
+
+motorcycle = Motorcycle()
+print(motorcycle.accelerate(5))
+print(motorcycle.brake(2))
