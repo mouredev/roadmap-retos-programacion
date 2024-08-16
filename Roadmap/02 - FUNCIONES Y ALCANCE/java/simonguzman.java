e185bc42cd60f5ab14306635da77904bebcb99d9
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-15 21:17:20 -0500

    Terminado el ejercicio #2

--- Roadmap/02 - FUNCIONES Y ALCANCE/java/simonguzman.java
+++ Roadmap/02 - FUNCIONES Y ALCANCE/java/simonguzman.java
@@ -2,20 +2,26 @@ import java.math.*;
 import java.util.Scanner;
 
 public class simonguzman {
+
+    //Variable global utilizada para el calculo del numero par e impar
     static int validarParImpar = 2;
     public static void main(String[] args) {
-        
+        //Prueba del ejercicio extra
         int numImpreso = ejercicioExtra("multiplo de 3", "multiplo de 5");
         System.out.println("Numero de veces que se ha impreso numero en lugar de las cadenas de texto: " + numImpreso);
-        //iniciarCalculadora();
+        
+        //Prueba de la calculadora en la que se encuentran todas las funciones que se puedian de requerimiento
+        iniciarCalculadora();
     }
 
+    //Funcion sin parametros ni retorno
     public static void presentation(){
         System.out.println("*************************");
         System.out.println("Bienvenido a la calculadora");
         System.out.println("*************************");
     }
 
+    //Funcion sin parametros ni retorno
     public static void menu(){
         System.out.println("**********MENU**********");
         System.out.println("1. Suma");
@@ -31,21 +37,25 @@ public class simonguzman {
         System.out.println("11. Salir");
     }
 
+    //Funcion con retorno y 2 parametros
     public static double suma(double num1, double num2){
         double sum = num1 + num2;
         return sum;
     }
 
+    //Funcion con retorno y 2 parametros
     public static double resta(double num1, double num2){
         double res = num1 - num2;
         return res;
     }
 
+    //Funcion con retorno y 2 parametros
     public static double multiplicacion(double num1, double num2){
         double multi = num1 * num2;
         return multi;
     }
 
+    //Funcion con retorno y 2 parametros
     public static double division(double num1, double num2){
         double div = 0;
         if(num2 == 0){
@@ -56,6 +66,7 @@ public class simonguzman {
         return div;
     }
 
+    //Funcion con retorno y 2 parametros
     public static double modulo(double num1, double num2){
         double mod = 0;
         if(num2 == 0){
@@ -66,6 +77,7 @@ public class simonguzman {
         return mod;
     }
 
+    //Funcion con retorno y 1 parametros
     public static int factorial(int num){
         int factorial = 1;
         if (num < 0){
@@ -80,6 +92,7 @@ public class simonguzman {
         return factorial;
     }
 
+    //Funcion con 1 parametro pero sin retorno
     public static void numeroPrimo(int num1){
         boolean numprimo = true;
         for (int i = 2; i < num1; i++){
@@ -95,16 +108,19 @@ public class simonguzman {
         }
     }
 
+    //Funcion con 2 parametros, retorno y utilizando funciones del lenguaje
     public static double potenciacion(int base, int exponente){
         double resultado = Math.pow(base, exponente);
         return resultado;
     }
 
+    //Funcion con 1 parametro, retorno y utilizando funciones del lenguaje
     public static double raizCuadrada(int num){
         double raiz = Math.sqrt(num);
         return raiz;
     }
 
+    //Fncion con 1 prametro y sin retorno
     public static void numParImpar(int num){
         if(num % validarParImpar == 0){
             System.out.println("El numero "+num+" es par");
@@ -113,6 +129,7 @@ public class simonguzman {
         }
     } 
     
+    //Funcion con 2 parametros y sin retorno, utilizando las funciones creadas anteriormente
     public static void menuOperaciones(int opcion, Scanner scanner){
         switch (opcion) {
             case 1:
@@ -186,6 +203,7 @@ public class simonguzman {
         }
     }
 
+    //Funcion con 1 parametro y sin retonro utilizando las funciones creadas con anterioridad
     public static void menuCompleto(Scanner scanner){
         int opcion = 0;
         do{
@@ -198,12 +216,14 @@ public class simonguzman {
         }while(opcion != 11);
     }
 
+    //Funcion sin parametros ni retorno, utilizando funciones creadas con anterioridad
     public static void iniciarCalculadora(){
         Scanner scanner = new Scanner(System.in);
         menuCompleto(scanner);
         scanner.close();
     }
 
+    //Ejercicio extra propuesta en la actividad, 2 parametros de texto con un retorno entero
     public static int ejercicioExtra(String text1, String text2){
         int count = 0;
         for(int i=0; i<=100; i++){
