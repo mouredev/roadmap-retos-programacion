e584e2979c0caf334a223f7e91cb17a7133b5671
riacosta <rigo93acosta@gmail.com>
2024-08-18 12:59:51 -0300

    Init Extra

--- Roadmap/30 - SOLID DIP/python/rigo93acosta.py
+++ Roadmap/30 - SOLID DIP/python/rigo93acosta.py
@@ -22,3 +22,68 @@
 EJERCICIO
 '''
 
+## SIN DIP
+## Sistema muy acoplado
+
+class Switch():
+
+    def turn_on(self):
+        print("Enciende la lámpara")
+
+    def turn_off(self):
+        print("Apaga la lámpara")
+
+class Lamp():
+
+    def __init__(self) -> None:
+        self.switch = Switch()
+
+    def operate(self, command):
+        if command == "on":
+            self.switch.turn_on()
+        elif command == "off":
+            self.switch.turn_off()
+    
+lamp = Lamp()
+lamp.operate("on")
+lamp.operate("off")
+
+## CON DIP
+
+class AbstractSwitch():
+    
+    def turn_on(self):
+        pass
+
+    def turn_off(self):
+        pass
+
+class LampSwitch(AbstractSwitch):
+    
+    def turn_on(self):
+        print("Enciende la lámpara")
+
+    def turn_off(self):
+        print("Apaga la lámpara")
+
+
+class Lamp():
+
+    def __init__(self, switch: AbstractSwitch) -> None:
+        self.switch = switch
+
+    def operate(self, command):
+        if command == "on":
+            self.switch.turn_on()
+        elif command == "off":
+            self.switch.turn_off()
+
+lamp = Lamp(LampSwitch())
+lamp.operate("on")
+lamp.operate("off")
+
+'''
+DIFICULTAD EXTRA
+'''
+
+
