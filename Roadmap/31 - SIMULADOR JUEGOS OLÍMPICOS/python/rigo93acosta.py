18df917868ebcc0abd6f1584ebd15738ac029161
riacosta <rigo93acosta@gmail.com>
2024-08-18 13:07:24 -0300

    Init reto

---
+++ Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/python/rigo93acosta.py
@@ -0,0 +1,26 @@
+'''
+/*
+ * EJERCICIO:
+ * ¡Los JJOO de París 2024 han comenzado!
+ * Crea un programa que simule la celebración de los juegos.
+ * El programa debe permitir al usuario registrar eventos y participantes,
+ * realizar la simulación de los eventos asignando posiciones de manera aleatoria
+ * y generar un informe final. Todo ello por terminal.
+ * Requisitos:
+ * 1. Registrar eventos deportivos.
+ * 2. Registrar participantes por nombre y país.
+ * 3. Simular eventos de manera aleatoria en base a los participantes (mínimo 3).
+ * 4. Asignar medallas (oro, plata y bronce) basándose en el resultado del evento.
+ * 5. Mostrar los ganadores por cada evento.
+ * 6. Mostrar el ranking de países según el número de medallas.
+ * Acciones:
+ * 1. Registro de eventos.
+ * 2. Registro de participantes.
+ * 3. Simulación de eventos.
+ * 4. Creación de informes.
+ * 5. Salir del programa.
+ */
+'''
+
+
+
