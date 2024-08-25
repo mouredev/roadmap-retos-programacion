935b4d6474e1c234cc21d4c1bd4dda501e8bca9a
juan <jhernandezsanchezagesta@gmail.com>
2024-08-25 02:04:33 +0200

    #00 - java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/juanhdezz.java
@@ -0,0 +1,23 @@
+/*
+ * Hola Mundo en Java
+ * Pagina web de la doaumentacion: https://docs.oracle.com/javase/7/docs/api/
+ * 
+ */
+// Esta es otra manera de hacer comentarios en java
+
+public class juanhdezz {
+    int variable = 0;
+    double variable2 = 0.0;
+    String variable3 = "¡Hola ,Java!";
+    boolean variable4 = true;
+    char variable5 = 'a';
+    float variable6 = 0.0f;
+    long variable7 = 0;
+    short variable8 = 0;
+    byte variable9 = 0; 
+
+    public static void main(String[] args) {
+        String variable3 = "¡Hola ,Java!";
+        System.out.println(variable3);
+    }
+  }
