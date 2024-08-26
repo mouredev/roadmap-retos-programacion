7d76a6261d02d9e778070421fdd63623d01a1729
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-26 16:20:26 -0500

    Ejericicio adicional historial web terminado, falta el de la impresora con colas

--- Roadmap/07 - PILAS Y COLAS/java/simonguzman.java
+++ Roadmap/07 - PILAS Y COLAS/java/simonguzman.java
@@ -40,11 +40,89 @@ public class simonguzman {
         System.out.println(cola.peek());
         //Tamaño de la cola
         System.out.println(cola.size());
+
+        navegadorWeb();
+    }
+
+    public static void navegadorWeb(){
+        Stack<String> historial = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+        int index = -1;
+        String opcion;
+        do{
+            System.out.println("Ingrese pagina, adelante, atras, o salir: ");
+            opcion = sc.next();
+            if(opcion.equals("pagina")){
+                index = ingresarPagina(historial);
+            }else{
+                index = opcionesNavegador(historial, opcion, index);
+            }
+        }while(!opcion.equals("salir"));
+        
+    }
+
+    public static int opcionesNavegador(Stack<String> pila, String opcion, int index){
+        switch (opcion) {
+            case "adelante":
+            index = accionAdelante(pila, index);
+                break;
+            case "atras":
+            index = accionDetras(pila, index);
+            break;
+            case "salir":
+            System.out.println("Saliendo del programa.....");
+            break;
+            default:
+            System.out.println("Opcion no valida");
+                break;
+        }
+        return index;
+    }
+
+    public static int ingresarPagina(Stack<String> pila){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese la url de la pagina: ");
+        String pagina = sc.next();
+        pila.push(pagina);
+        System.out.println("Has navegado a: "+pagina);
+        return pila.size()-1;
+    }
+
+    public static int accionAdelante(Stack<String> pila, int index){
+        if(validarPilaVacia(pila)){
+            if(index < pila.size() - 1){
+                index++;
+                System.out.println("Link de pagina: "+pila.get(index));
+            }else{
+                System.out.println("No hay mas historial hacia adelante\n");
+                //index = pila.size() - 1;
+            }
+        }else{
+            System.out.println("No hay historial");
+        }
+        return index;
+    }
+
+    public static int accionDetras(Stack<String> pila, int index){
+        if(validarPilaVacia(pila)){
+            if(index > 0){
+                index--;
+                System.out.println("Link de pagina: "+pila.get(index));
+            }else{
+                System.out.println("No hay mas historial hacia adelante\n");
+            }
+        }else{
+            System.out.println("No hay ningun historial");
+        }
+        return index;
     }
 
     public static boolean validarColaVacia(Queue<String> cola){
         return !cola.isEmpty();
     }
 
+    public static boolean validarPilaVacia(Stack<String> pila){
+        return !pila.isEmpty();
+    }
 
 }
