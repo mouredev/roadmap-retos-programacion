10fae68eb313fa4a179681160101f5252470bd25
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-28 14:57:13 -0500

    Ejercicio #10 completado

--- Roadmap/10 - EXCEPCIONES/java/simonguzman.java
+++ Roadmap/10 - EXCEPCIONES/java/simonguzman.java
@@ -5,7 +5,7 @@ public class simonguzman{
     public static void main(String[] args) {
 
         //Manejo de excepciones basicas
-        try {
+        /*try {
             //Excepcion forzada: Division por cero
             int result = 10 / 0;
         } catch (ArithmeticException e) {
@@ -26,7 +26,41 @@ public class simonguzman{
             throw new customException("Mensaje personalizado para una excepcion personalizada");
         } catch (customException e) {
             System.out.println("Se ha producido una excepcion personalizada..."+e.getMessage());
+        }*/
+
+        executeProcessParameters();
+    }
+
+    static void executeProcessParameters(){
+        try {
+            processParameters(10, 2, "test");
+        } catch (ArithmeticException e){
+            System.out.println("ERROR: Excepcion aritmetica..."+e.getMessage());
+        } catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("ERROR: Indice fuera de los limites..."+e.getMessage());
+        } catch(customException e){
+            System.out.println("ERROR: Excepcion general..."+e.getMessage());
+        }finally{
+            System.out.println("La ejecucion a finalizado.");
         }
+    }
+
+    static void processParameters(int num1, int num2, String str) throws ArithmeticException, ArrayIndexOutOfBoundsException, customException{
+        if (num2 == 0){
+            throw new ArithmeticException("ERROR: Division por cero");
+        }
+        int result = num1/num2;
+        System.out.println("Resultado: "+result);
+
+        if(str.length() < 3){
+            throw new ArrayIndexOutOfBoundsException("Cadena de texto muy corta");
+        }
+
+        if(str.equals("test")){
+            throw new customException("Cadena no permitida");
+        }
+
+        System.out.println("Parametros procesados correctamente");
     }
 
     static class customException extends Exception {
