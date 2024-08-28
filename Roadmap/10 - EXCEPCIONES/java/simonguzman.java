70e04d4b7f00a3e929b3741fca9562880708dce1
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-28 14:42:24 -0500

    manejo de excepciones personalizadas

--- Roadmap/10 - EXCEPCIONES/java/simonguzman.java
+++ Roadmap/10 - EXCEPCIONES/java/simonguzman.java
@@ -3,7 +3,10 @@ import java.util.List;
 
 public class simonguzman{
     public static void main(String[] args) {
+
+        //Manejo de excepciones basicas
         try {
+            //Excepcion forzada: Division por cero
             int result = 10 / 0;
         } catch (ArithmeticException e) {
             System.out.println("ERROR: Division por cero..."+e.getMessage());
@@ -16,6 +19,23 @@ public class simonguzman{
             String item = list.get(1);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("ERROR: Indice fuera de los limites..."+e.getMessage());
+        }
+
+        //Manejo de excepciones personalizadas
+        try {
+            throw new customException("Mensaje personalizado para una excepcion personalizada");
+        } catch (customException e) {
+            System.out.println("Se ha producido una excepcion personalizada..."+e.getMessage());
+        }
+    }
+
+    static class customException extends Exception {
+        public customException(){
+
+        }
+
+        public customException(String message){
+            super(message);
         }
     }
 }
