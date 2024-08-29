951162987882cca2ba9a0c3e8b2929a090cbe21f
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-28 21:22:39 -0500

    Leer fichero completado

--- Roadmap/11 - MANEJO DE FICHEROS/java/simonguzman.java
+++ Roadmap/11 - MANEJO DE FICHEROS/java/simonguzman.java
@@ -1,12 +1,16 @@
 import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 
 public class simonguzman {
     
     public static void main(String[] args) {
         createAndWriteFile();
+        readFile();
     }
 
+    //Crear y escribrir el fichero
     static void createAndWriteFile(){
         try (FileWriter writer = new FileWriter("simonguzman.txt")){
             writer.write("Nombre: Simon\n");
@@ -16,4 +20,16 @@ public class simonguzman {
             System.out.println("ERROR: No se puede crear o escribir el archivo...."+e.getMessage());
         }
     }
+
+    static void readFile(){
+        try (BufferedReader reader = new BufferedReader(new FileReader("simonguzman.txt"))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR: No se pudo leer el archivo..."+e.getMessage());
+        }
+    }
+
 }
