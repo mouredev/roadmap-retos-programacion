69ad7c76225e47a5c34379172594931aeee2b63c
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-29 17:08:36 -0500

    Añadir y consultar productos completado y probado

--- Roadmap/11 - MANEJO DE FICHEROS/java/simonguzman.java
+++ Roadmap/11 - MANEJO DE FICHEROS/java/simonguzman.java
@@ -1,13 +1,16 @@
 import java.io.FileWriter;
 import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.util.Scanner;
 
 public class simonguzman {
     
     public static void main(String[] args) {
         testFile();
+        salesManager();
     }
 
 
@@ -18,7 +21,7 @@ public class simonguzman {
             menu();
             System.out.println("Ingrese una opcion:");
             opcion = sc.nextInt();
-            opcionsMenu(opcion);
+            opcionsMenu(opcion, sc);
         }while(opcion != 5);
     }
 
@@ -31,10 +34,10 @@ public class simonguzman {
         System.out.println("5. Salir");
     }
 
-    static void opcionsMenu(int opcion){
+    static void opcionsMenu(int opcion, Scanner sc){
         switch (opcion) {
             case 1:
-                addProducts();
+                addProducts(sc);
                 break;
             case 2:
                 consultProducts();
@@ -54,13 +57,33 @@ public class simonguzman {
         }
     }
 
-    static void addProducts(){
-
+    static void addProducts(Scanner sc){
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("ventas.txt", true))){
+                System.out.println("Ingrese el nombre del producto: ");
+                String nameProduct = sc.next();
+                System.out.println("Ingrese la cantidad vendida: ");
+                int cantProduct = sc.nextInt();
+                System.out.println("Ingrese el precio del producto: ");
+                double priceProduct = sc.nextDouble();
+                sc.nextLine();
+                writer.write(nameProduct + " , "+cantProduct+" , "+priceProduct);
+                writer.newLine();
+                System.out.println("Producto añadido correctamente");
+        } catch (Exception e) {
+            System.out.println("ERROR: No se ha podido añadir el producto");
+        }
     }
 
     static void consultProducts(){
-
-    }
+        try (BufferedReader reader = new BufferedReader(new FileReader("ventas.txt"))){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR: No se pudo consultar los productos..."+e.getMessage());
+        }
+    }   
 
     static void updateProducts(){
 
@@ -71,6 +94,12 @@ public class simonguzman {
     }
 
     static void exit(){
+        File file = new File("ventas.txt");
+        if(file.delete()){
+            System.out.println("Fichero eliminado.");
+        }else{
+            System.out.println("ERROR: No se pudo eliminar el archivo.");
+        }
         System.out.println("Saliendo del programa...");
     }
 
