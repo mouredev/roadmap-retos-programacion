79015d9ff6d3eefe1d7b9cf1936742c55f62985a
SimonGuzman <siguzman@unicauca.edu.co>
2024-08-30 20:45:09 -0500

    Simulacion de JSON, funcion crear completado

--- Roadmap/12 - JSON Y XML/java/simonguzman.java
+++ Roadmap/12 - JSON Y XML/java/simonguzman.java
@@ -1,29 +1,31 @@
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
+
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.List;
+import java.util.Arrays;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 import java.io.File;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-
 
 public class simonguzman {
+
     public static void main(String[] args) {
         createXMLFile();
         readXMLFile();
         deleteXMLFile();
+        createPropertiesFile();
     }
 
     static void createXMLFile(){
@@ -107,6 +109,23 @@ public class simonguzman {
             System.out.println("Archivo XML borrado correctamente.");
         }else{
             System.out.println("ERROR: No se pudo borrar el archivo XML.");
+        }
+    }
+
+    static void createPropertiesFile(){
+        Properties properties = new Properties();
+
+        properties.setProperty("nombre", "Simon");
+        properties.setProperty("edad", "22");
+        properties.setProperty("fecha_de_nacimiento", "2001-11-28");
+
+        properties.setProperty("lenguajes", "Java,Python");
+
+        try (FileOutputStream output = new FileOutputStream("datos.properties")){
+            properties.store(output, "Datos del usuario en formato JSON simulado");
+            System.out.println("Archivo 'datos.properties' creado");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
