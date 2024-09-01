c042caccebc30112c1bca89668430e57ccc6f561
Paloma <paloma.ditex@gmail.com>
2024-08-31 23:00:46 -0400

    001-Js-SingularPigeon, 002-Js-SingularPigeon

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/SingularPigeon.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/SingularPigeon.js
@@ -80,3 +80,16 @@ console.log(x);
 // devuelve una cadena que indica el tipo de operando
 console.log(typeof 35); // number
 console.log(typeof "cadena"); // string
+
+/*
+Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ */
+for ( let i = 10; i <= 55; i++) {
+  if ( i % 2 === 0 && i !== 16 && i % 3 !== 0) {
+    console.log(i);
+    //
+  }
+   
+}
+
