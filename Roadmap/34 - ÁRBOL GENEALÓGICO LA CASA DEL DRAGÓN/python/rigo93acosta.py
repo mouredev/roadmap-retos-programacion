3528a10146fd69ed0ad058f5b9345c0e7ece884c
rigo93acosta <rigo93acosta@gmail.com>
2024-09-02 13:19:50 -0300

    Init Reto

---
+++ Roadmap/34 - ÁRBOL GENEALÓGICO LA CASA DEL DRAGÓN/python/rigo93acosta.py
@@ -0,0 +1,28 @@
+'''
+/*
+ * EJERCICIO:
+ * ¡La Casa del Dragón ha finalizado y no volverá hasta 2026! 
+ * ¿Alguien se entera de todas las relaciones de parentesco
+ * entre personajes que aparecen en la saga?
+ * Desarrolla un árbol genealógico para relacionarlos (o invéntalo).
+ * Requisitos:
+ * 1. Estará formado por personas con las siguientes propiedades:
+ *    - Identificador único (obligatorio)
+ *    - Nombre (obligatorio)
+ *    - Pareja (opcional)
+ *    - Hijos (opcional)
+ * 2. Una persona sólo puede tener una pareja (para simplificarlo).
+ * 3. Las relaciones deben validarse dentro de lo posible.
+ *    Ejemplo: Un hijo no puede tener tres padres.
+ * Acciones:
+ * 1. Crea un programa que permita crear y modificar el árbol.
+ *    - Añadir y eliminar personas
+ *    - Modificar pareja e hijos
+ * 2. Podrás imprimir el árbol (de la manera que consideres).
+ * 
+ * NOTA: Ten en cuenta que la complejidad puede ser alta si
+ * se implementan todas las posibles relaciones. Intenta marcar
+ * tus propias reglas y límites para que te resulte asumible.
+ */
+'''
+
