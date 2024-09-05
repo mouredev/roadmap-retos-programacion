1a9fbbab3fa2a681bb11f6f5fa7da724457b3086
SimonGuzman <siguzman@unicauca.edu.co>
2024-09-04 20:43:50 -0500

    Reto #16 completado

--- Roadmap/16 - EXPRESIONES REGULARES/java/simonguzman.java
+++ Roadmap/16 - EXPRESIONES REGULARES/java/simonguzman.java
@@ -4,6 +4,9 @@ import java.util.regex.Pattern;
 public class simonguzman {
     public static void main(String[] args) {
         extractNumbersOfText();
+        validateEmail();
+        validatePhoneNumber();
+        validateUrl();
     }
 
     static void extractNumbersOfText(){
@@ -15,4 +18,35 @@ public class simonguzman {
             System.out.println("Numero encontrado: "+matcher.group());
         }
     } 
+
+    static void validateEmail(){
+        String email = "siguzman@unicauca.edu.co";
+        String pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"; //Expresion regular para validaar el email
+        if (Pattern.matches(pattern, email)){
+            System.out.println("Email valido");
+        }else{
+            System.out.println("Email no valido");
+        }
+    }
+
+    static void validatePhoneNumber(){
+        String number = "+1 123 456 7890";
+        String pattern = "^\\+?\\d{1,3}?[- .]?\\(?(?:\\d{2,3})\\)?[- .]?\\d\\d\\d[- .]?\\d\\d\\d\\d$"; // expresión regular para validar número de teléfono
+        if (Pattern.matches(pattern, number)){
+            System.out.println("Número de teléfono válido");
+        }else{
+            System.out.println("Numero de telefono no valido");
+        }
+    }
+
+    static void validateUrl(){
+        String url = "https://www.example.com/path/to/resource";
+        String pattern = "^((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&//=]*)$"; // expresión regular para validar URL
+        if(Pattern.matches(pattern, url)){
+            System.out.println("Url valido.");
+        }else{
+            System.out.println("Url no valido");
+        }
+
+    }
 }
