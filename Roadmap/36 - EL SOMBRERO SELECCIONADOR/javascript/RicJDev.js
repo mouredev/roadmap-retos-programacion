d2f6b5736b6b11d302cd72e8084ce4d55981709e
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-09-05 15:28:04 -0400

    36 - JavaScript

--- Roadmap/36 - EL SOMBRERO SELECCIONADOR/javascript/RicJDev.js
+++ Roadmap/36 - EL SOMBRERO SELECCIONADOR/javascript/RicJDev.js
@@ -1,8 +1,7 @@
 // Modelado de preguntas y cuestionario
 
 class Question {
-  constructor(title, id = 0) {
-    this.id = id
+  constructor(title) {
     this.title = title
     this.options = {
       A: null,
@@ -19,7 +18,7 @@ class Question {
       const voidOption = Object.keys(this.options).find((option) => this.options[option] === null)
       this.options[voidOption] = option
     } else {
-      console.log(`Se han llenado todas las opciones. No se pudo agregar "${option}"`)
+      console.warn(`Se han llenado todas las opciones. No se pudo agregar "${option}"`)
     }
   }
 
@@ -27,7 +26,7 @@ class Question {
     if (this.options[letter]) {
       this.options[letter] = newOption
     } else {
-      console.log('Está intentando acceder a una opción inexistente')
+      console.warn('Está intentando acceder a una opción inexistente')
     }
   }
 }
@@ -35,32 +34,21 @@ class Question {
 class Questionary {
   constructor() {
     this.questions = {}
-  }
-
-  addQuestion(id, question) {
-    if (this.questions[id]) {
-      console.log('Esta pregunta ya existe en el cuestionario.')
-    } else {
-      this.questions[id] = question
-      question.id = id
+    this.results = {
+      A: 0,
+      B: 0,
+      C: 0,
+      D: 0,
     }
   }
 
-  printQuestion(id) {
-    const question = this.questions[id]
-
-    for (const letter in question.options) {
-        console.log(` - ${letter}. ${question.options[letter] || 'Opción vacía'}`)
-    }
+  addQuestion(title) {
+    const id = this.questionQuantity + 1
+    this.questions[id] = new Question(title)
   }
 
-  printAllQuestions() {
-    for (const id in this.questions) {
-      const currentQuestion = this.questions[id]
-
-      console.log(`\n${id}. ${currentQuestion.title}\n`)
-      this.printQuestion(id)
-    }
+  get questionQuantity() {
+    return Object.keys(this.questions).length
   }
 }
 
@@ -68,7 +56,7 @@ class Questionary {
 
 const questionary = new Questionary()
 
-questionary.addQuestion(1, new Question('¿Qué parte de un sitio web te llama más la atención?'))
+questionary.addQuestion('¿Qué parte de un sitio web te llama más la atención?')
 
 questionary.questions[1].addOption('El diseño visual y la interfaz de usuario.')
 questionary.questions[1].addOption(
@@ -80,11 +68,9 @@ questionary.questions[1].addOption(
 )
 
 questionary.addQuestion(
-  2,
-  new Question(
-    'Si tuvieras que elegir entre crear una nueva aplicación móvil o diseñar una base de datos, ¿cuál preferirías?'
-  )
+  'Si tuvieras que elegir entre crear una nueva aplicación móvil o diseñar una base de datos, ¿cuál preferirías?'
 )
+
 questionary.questions[2].addOption('Crear una aplicación móvil con una interfaz intuitiva.')
 questionary.questions[2].addOption(
   'Diseñar una base de datos eficiente para almacenar grandes cantidades de información.'
@@ -92,13 +78,13 @@ questionary.questions[2].addOption(
 questionary.questions[2].addOption('Ambas opciones me parecen igualmente interesantes.')
 questionary.questions[2].addOption('Ninguna de las opciones me llama la atención.')
 
-questionary.addQuestion(3, new Question('¿Qué te resulta más fácil de entender?'))
+questionary.addQuestion('¿Qué te resulta más fácil de entender?')
 questionary.questions[3].addOption('Diagramas de flujo y diseños visuales.')
 questionary.questions[3].addOption('Código y algoritmos.')
 questionary.questions[3].addOption('Prototipos y maquetas.')
 questionary.questions[3].addOption('Gráficos y estadísticas.')
 
-questionary.addQuestion(4, new Question('¿Qué tipo de problemas te gusta resolver?'))
+questionary.addQuestion('¿Qué tipo de problemas te gusta resolver?')
 questionary.questions[4].addOption(
   'Problemas relacionados con la estética y la experiencia del usuario.'
 )
@@ -107,23 +93,22 @@ questionary.questions[4].addOption(
   'Problemas relacionados con la portabilidad y la compatibilidad en diferentes dispositivos.'
 )
 questionary.questions[4].addOption(
-  'D',
   'Problemas relacionados con la extracción de información útil de grandes conjuntos de datos.'
 )
 
-questionary.addQuestion(5, new Question('¿Qué herramienta te resulta más interesante?'))
+questionary.addQuestion('¿Qué herramienta te resulta más interesante?')
 questionary.questions[5].addOption('Photoshop o Figma.')
 questionary.questions[5].addOption('Python o Java.')
 questionary.questions[5].addOption('XCode o Android Studio.')
 questionary.questions[5].addOption('SQL o Tableau.')
 
-questionary.addQuestion(6, new Question('¿Qué te gustaría hacer en tu tiempo libre?'))
+questionary.addQuestion('¿Qué te gustaría hacer en tu tiempo libre?')
 questionary.questions[6].addOption('Diseñar interfaces de usuario para diferentes aplicaciones.')
 questionary.questions[6].addOption('Desarrollar videojuegos o aplicaciones web.')
 questionary.questions[6].addOption('Crear aplicaciones móviles para diferentes plataformas.')
 questionary.questions[6].addOption('Analizar datos y crear visualizaciones.')
 
-questionary.addQuestion(7, new Question('¿Qué tipo de proyectos te motivan más?'))
+questionary.addQuestion('¿Qué tipo de proyectos te motivan más?')
 questionary.questions[7].addOption('Proyectos que tienen un impacto visual y estético.')
 questionary.questions[7].addOption(
   'Proyectos que requieren resolver problemas complejos y optimizar el rendimiento.'
@@ -133,10 +118,8 @@ questionary.questions[7].addOption(
 )
 questionary.questions[7].addOption('Proyectos que utilizan datos para tomar decisiones informadas.')
 
-questionary.addQuestion(
-  8,
-  new Question('¿Qué habilidad consideras más importante para un desarrollador?')
-)
+questionary.addQuestion('¿Qué habilidad consideras más importante para un desarrollador?')
+
 questionary.questions[8].addOption('La creatividad y el sentido del diseño.')
 questionary.questions[8].addOption('La capacidad de resolver problemas y pensar de forma lógica.')
 questionary.questions[8].addOption(
@@ -144,13 +127,13 @@ questionary.questions[8].addOption(
 )
 questionary.questions[8].addOption('La capacidad de analizar datos y extraer insights.')
 
-questionary.addQuestion(9, new Question('¿Qué tipo de equipo te gustaría formar parte?'))
+questionary.addQuestion('¿Qué tipo de equipo te gustaría formar parte?')
 questionary.questions[9].addOption('Un equipo de diseño y UX.')
 questionary.questions[9].addOption('Un equipo de desarrollo backend.')
 questionary.questions[9].addOption('Un equipo de desarrollo móvil.')
 questionary.questions[9].addOption('Un equipo de ciencia de datos.')
 
-questionary.addQuestion(10, new Question('¿Qué te gustaría lograr a largo plazo en tu carrera?'))
+questionary.addQuestion('¿Qué te gustaría lograr a largo plazo en tu carrera?')
 questionary.questions[10].addOption(
   'Crear productos digitales con una interfaz de usuario excepcional.'
 )
@@ -161,3 +144,57 @@ questionary.questions[10].addOption(
 questionary.questions[10].addOption(
   'Utilizar datos para resolver problemas del mundo real y tomar decisiones estratégicas.'
 )
+
+/*
+  * Creando la "app" en la terminal *
+
+- Se ejecuta en Node.js, importando el módulo Readline/promises (https://nodejs.org/api/readline.html)
+- Se ha instalado Picocolors, un librería para colorear texto en la terminal (https://www.npmjs.com/package/picocolors)
+
+*/
+
+import * as readline from 'readline/promises'
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+import pc from 'picocolors'
+
+console.clear()
+
+const name = await rl.question('Dime tu nombre, querido alumno. ')
+
+for (const id in questionary.questions) {
+  const question = questionary.questions[id]
+
+  let message = pc.gray('¡Descubre a cuál casa perteneces!')
+  let answer = ''
+
+  while (!Object.keys(question.options).includes(answer.toUpperCase())) {
+    console.clear()
+    console.log(`Bienvenido seas, ${pc.bold(name)}. ${message}`)
+
+    console.log(`\n${pc.blue(id)}. ${question.title}\n`)
+
+    for (const letter in question.options) {
+      console.log(` - ${pc.green(letter)}. ${question.options[letter] || 'Opción vacía'}`)
+    }
+
+    answer = await rl.question(pc.gray('\nIndica la opción de tu preferencia. '))
+
+    message = pc.yellow('Elije una opción válida, por favor')
+  }
+
+  questionary.results[answer.toUpperCase()]++
+}
+
+console.clear()
+console.log('\nEl cuestionario ha terminado!\nEstos son los resultados:')
+
+console.log(`Total de respuestas A: ${questionary.results.A} ${pc.blue('(frontend)')}.`)
+console.log(`Total de respuestas B: ${questionary.results.B} ${pc.blue('(backend)')}.`)
+console.log(`Total de respuestas C: ${questionary.results.C} ${pc.blue('(mobile)')}.`)
+console.log(`Total de respuestas D: ${questionary.results.D} ${pc.blue('(data)')}`)
+
+rl.close()
