77d8b6af039cc35d7dbde5c25146928ab774a137
gabriel <gabrielcmoris@gmail.com>
2024-09-06 11:33:08 +0200

    finish agenda challenge

--- Roadmap/03 - ESTRUCTURAS DE DATOS/go/gabrielmoris.go
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/go/gabrielmoris.go
@@ -16,9 +16,25 @@ import (
 	"sync"
 )
 
+type ContactAgenda struct {
+	name        string
+	phoneNumber string
+}
+
+const (
+    ADD_CONTACT    = 1
+    FIND_CONTACT   = 2
+    UPDATE_CONTACT = 3
+    DELETE_CONTACT = 4
+    EXIT           = 5
+  )
+  
+
 func main(){
 	dataStructures()
-	challenge()
+    // CHALLENGE
+    agenda := make(map[string]ContactAgenda)
+	challenge(agenda)
 }
 
 func dataStructures() {
@@ -123,9 +139,9 @@ func receiver(ch chan int, wg *sync.WaitGroup) {
         fmt.Println("Received:", value)
     }
 }
-
 ////////////////////// CHALLENGE //////////////////////
- /*
+
+/*
  * EXTRA DIFFICULTY (optional):
  * Create a contact book via terminal.
  * - You must implement functionalities for searching, inserting, updating,
@@ -138,22 +154,15 @@ func receiver(ch chan int, wg *sync.WaitGroup) {
  * - An operation to end the program should also be proposed.
  */
 
- func challenge(){
-     type Contact struct {
-         Name     string
-         Phone    string
-     }
-     
-    agenda := make(map[string]Contact)
-
-    fmt.Println("======== AGENDA ========")
-    fmt.Println("1. Add Contact")
-    fmt.Println("2. Find Contact")
-    fmt.Println("3. Update Contact")
-    fmt.Println("4. Delete Contact")
-    fmt.Println("5. Exit")
-    fmt.Println("========================")
-    for{
+ func challenge(agenda map[string]ContactAgenda) {
+	fmt.Println("======== AGENDA ========")
+	fmt.Println("1. Add Contact")
+	fmt.Println("2. Find Contact")
+	fmt.Println("3. Update Contact")
+	fmt.Println("4. Delete Contact")
+	fmt.Println("5. Exit")
+	fmt.Println("========================")
+	for {
 
 		input := getInput("Enter Option: ")
 		option, err := strconv.Atoi(input)
@@ -163,21 +172,23 @@ func receiver(ch chan int, wg *sync.WaitGroup) {
 			continue
 		}
 
-        switch option{
-        case 1:
-            addContact(agenda)
-        case 2:
-            fmt.Printf("Find contact")
-        case 3:
-            fmt.Printf("Update contact")
-        case 4:
-            fmt.Printf("Delete contact")
-        case 5:
-            fmt.Printf("Good Bye\n")
-            return
-        }
+		switch option {
+		case 1:
+			addContact(agenda)
+		case 2:
+			findContact(agenda)
+		case 3:
+			editContact(agenda)
+		case 4:
+			deleteContactAgenda(agenda)
+		case 5:
+			fmt.Println("Good Bye")
+			return
+        default:
+            fmt.Println("Please, write a possible option.")
+		}
 
-    }
+	}
 }
 
 func getInput(prompt string) string {
@@ -187,21 +198,63 @@ func getInput(prompt string) string {
 	return strings.TrimSpace(input)
 }
 
-
-
-func addContact(agenda map[string]Contact) {
-	name := getInput("Enter contact name: ")
-	phone :=  getInput("Enter phone: ")
-    if len(phone) > 11 && !isNumeric(phone) {
-        fmt.Println("Invalid input. Please enter a number.")
-	    return
-    }
-
-	agenda[name] = Contact{name: name, phoneNumber: phone}
-	fmt.Println("Contact added successfully!")
-}
-
 func isNumeric(s string) bool {
 	_, err := strconv.Atoi(s)
 	return err == nil
 }
+
+func validatePhone(phone string) bool {
+    // Implement phone number validation logic here (e.g., length, format)
+    return len(phone) < 11 && len(phone) > 2 && isNumeric(phone)
+  }
+
+func addContact(agenda map[string]ContactAgenda) {
+	name := getInput("Enter contact name: ")
+	phone := getInput("Enter phone: ")
+
+	if !validatePhone(phone) {
+		fmt.Println("Invalid input. Please enter a valid phone number.")
+		return
+	}
+
+	agenda[name] = ContactAgenda{name: name, phoneNumber: phone}
+	fmt.Println("Contact added successfully!")
+}
+
+func findContact(agenda map[string]ContactAgenda){
+    search := getInput("Enter contact name: ")
+    contact, found := agenda[search]
+
+    if !found {
+      fmt.Println("Contact not found.")
+      return
+    }
+
+    fmt.Println("________________________")
+    fmt.Println("Your contact: ", contact.name, "\nPhone: ", contact.phoneNumber)
+}
+
+func editContact(agenda map[string]ContactAgenda){
+    search := getInput("Enter contact name: ")
+    contact, found := agenda[search]
+
+    if !found {
+      fmt.Println("Contact not found.")
+      return
+    }
+
+    newPhone := getInput("Enter new contact phone: ")
+
+    contact.phoneNumber = newPhone
+    agenda[search] = contact
+    fmt.Println("Contact edited successfully!\n", contact.name, "\nPhone: ", contact.phoneNumber)
+}
+
+func deleteContactAgenda(agenda map[string]ContactAgenda){
+    search := getInput("Enter contact name: ")
+    found := agenda[search]
+    delete(agenda, found.name)
+
+    fmt.Println("Contact deleted successfully!")
+}
+
