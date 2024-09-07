f7d8c179ccf1ce195c2c38d78b5c66c1d33af82a
Lance <155090153+1ceL4nc3@users.noreply.github.com>
2024-09-07 19:47:00 +0100

    L4nce.c

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c/L4nce.c
@@ -0,0 +1,13 @@
+https://www.cprogramming.com/
+
+Types of comments
+"// //" = // comment // 
+"/* */" = /*comment*/
+
+integer 
+int a = 2;
+constant value = const + type of variable + name of variable
+const int n = 5;
+
+char x = 'c';
+float y = 2.35*32;
