a523f2ce129372ac6022d35bdf61226ac08a4f04
riacosta <rigo93acosta@gmail.com>
2024-09-07 10:54:05 -0300

    Init Reto

---
+++ Roadmap/35 - REPARTIENDO LOS ANILLOS DE PODER/python/rigo93acosta.py
@@ -0,0 +1,19 @@
+'''
+/*
+ * EJERCICIO:
+ * ¡La temporada 2 de "Los Anillos de Poder" está a punto de estrenarse! 
+ * ¿Qué pasaría si tuvieras que encargarte de repartir los anillos
+ * entre las razas de la Tierra Media?
+ * Desarrolla un programa que se encargue de distribuirlos.
+ * Requisitos:
+ * 1. Los Elfos recibirán un número impar.
+ * 2. Los Enanos un número primo.
+ * 3. Los Hombres un número par.
+ * 4. Sauron siempre uno.
+ * Acciones:
+ * 1. Crea un programa que reciba el número total de anillos
+ *    y busque una posible combinación para repartirlos.
+ * 2. Muestra el reparto final o el error al realizarlo.
+ */
+'''
+
