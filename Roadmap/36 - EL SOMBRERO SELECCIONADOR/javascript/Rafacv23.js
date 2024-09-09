78c71cffa15d4e501e0be047dff4d5223c06e4eb
Rafa Canosa <rafacv23@gmail.com>
2024-09-09 16:13:56 +0200

    add readline to receive inputs and pointsPerHouse

--- Roadmap/36 - EL SOMBRERO SELECCIONADOR/javascript/Rafacv23.js
+++ Roadmap/36 - EL SOMBRERO SELECCIONADOR/javascript/Rafacv23.js
@@ -1,118 +1,234 @@
+const readline = require("readline")
+
 const preguntas = [
   {
     pregunta: "¿Qué parte de un proyecto disfrutas más?",
-    respuestas: [
-      "A) Crear interfaces interactivas y atractivas",
-      "B) Diseñar la estructura de datos y las bases de datos",
-      "C) Desarrollar aplicaciones móviles",
-      "D) Analizar grandes volúmenes de datos",
+    opciones: [
+      {
+        respuesta: "Crear interfaces interactivas y atractivas",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Diseñar la estructura de datos y las bases de datos",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Desarrollar aplicaciones móviles",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Implementar sistemas de seguridad y control de acceso",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Cuál es tu lenguaje de programación favorito?",
-    respuestas: ["A) JavaScript", "B) Python", "C) Swift", "D) R"],
+    opciones: [
+      {
+        respuesta: "JavaScript",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Python",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Swift",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "R",
+        casa: "Data",
+      },
+    ],
   },
   {
     pregunta: "¿Qué herramienta te resulta más útil en tu trabajo diario?",
-    respuestas: [
-      "A) React o Vue",
-      "B) Node.js",
-      "C) Flutter",
-      "D) Jupyter Notebooks",
+    opciones: [
+      {
+        respuesta: "Visual Studio Code",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Git y GitHub",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Docker y Kubernetes",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Jupyter Notebooks",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Cómo prefieres gestionar tus proyectos?",
-    respuestas: [
-      "A) A través de metodologías ágiles (Scrum, Kanban)",
-      "B) Con enfoque en arquitectura de microservicios",
-      "C) Aplicando patrones de diseño para aplicaciones móviles",
-      "D) Mediante análisis y reportes de datos",
+    opciones: [
+      {
+        respuesta: "Utilizando versiones de control",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Utilizando un herramienta de planeamiento",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Utilizando un herramienta de gestión de proyectos",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Utilizando un herramienta de planificación",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Qué parte de la depuración de código disfrutas más?",
-    respuestas: [
-      "A) Encontrar errores visuales en las interfaces",
-      "B) Corregir problemas de rendimiento en el servidor",
-      "C) Arreglar fallos en la experiencia de usuario móvil",
-      "D) Optimizar algoritmos de procesamiento de datos",
+    opciones: [
+      {
+        respuesta: "Encontrar errores visuales en las interfaces",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Corregir problemas de rendimiento en el servidor",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Desarrollar nuevas características y funciones",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Optimizar algoritmos de procesamiento de datos",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Cuál sería tu rol ideal en un equipo de desarrollo?",
-    respuestas: [
-      "A) Diseñador/Desarrollador Frontend",
-      "B) Arquitecto de Soluciones Backend",
-      "C) Desarrollador de Aplicaciones Móviles",
-      "D) Científico de Datos",
+    opciones: [
+      {
+        respuesta: "Desarrollador/Diseñador Frontend",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Arquitecto de Soluciones Backend",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Desarrollador de Aplicaciones Móviles",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Científico de Datos",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Cómo prefieres aprender nuevas tecnologías?",
-    respuestas: [
-      "A) Experimentando con nuevas librerías y frameworks de frontend",
-      "B) Trabajando con nuevas tecnologías de servidores y bases de datos",
-      "C) Explorando nuevas plataformas de desarrollo móvil",
-      "D) Analizando nuevos algoritmos y técnicas de procesamiento de datos",
+    opciones: [
+      {
+        respuesta:
+          "Experimentando con nuevas librerías y frameworks de frontend",
+        casa: "Frontend",
+      },
+      {
+        respuesta:
+          "Trabajando con nuevas tecnologías de servidores y bases de datos",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Explorando nuevas plataformas de desarrollo móvil",
+        casa: "Mobile",
+      },
+      {
+        respuesta:
+          "Analizando nuevos algoritmos y técnicas de procesamiento de datos",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Qué tipo de proyectos te motivan más?",
-    respuestas: [
-      "A) Aplicaciones web interactivas y atractivas",
-      "B) Sistemas robustos y escalables en la nube",
-      "C) Aplicaciones móviles con excelente experiencia de usuario",
-      "D) Proyectos de análisis y visualización de datos",
+    opciones: [
+      {
+        respuesta: "Crear interfaces web interactivas y atractivas",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Desarrollar sistemas robustos y escalables en la nube",
+        casa: "Backend",
+      },
+      {
+        respuesta:
+          "Desarrollar aplicaciones móviles con excelente experiencia de usuario",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Proyectos de análisis y visualización de datos",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Qué tecnología te gustaría dominar en el futuro?",
-    respuestas: [
-      "A) WebAssembly y animaciones avanzadas en la web",
-      "B) Inteligencia Artificial y Machine Learning en servidores",
-      "C) Realidad aumentada en aplicaciones móviles",
-      "D) Big Data y análisis de grandes volúmenes de información",
+    opciones: [
+      {
+        respuesta: "WebAssembly y animaciones avanzadas en la web",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Inteligencia Artificial y Machine Learning en servidores",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Realidad aumentada en aplicaciones móviles",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Big Data y análisis de grandes volúmenes de información",
+        casa: "Data",
+      },
     ],
   },
   {
     pregunta: "¿Cómo te gusta colaborar en un equipo?",
-    respuestas: [
-      "A) Diseñando y mejorando la experiencia de usuario",
-      "B) Integrando servicios y APIs en el backend",
-      "C) Asegurando la mejor experiencia en aplicaciones móviles",
-      "D) Proporcionando análisis detallados a partir de los datos",
+    opciones: [
+      {
+        respuesta: "Diseñando y mejorando la experiencia de usuario",
+        casa: "Frontend",
+      },
+      {
+        respuesta: "Integrando servicios y APIs en el backend",
+        casa: "Backend",
+      },
+      {
+        respuesta: "Asegurando la mejor experiencia en aplicaciones móviles",
+        casa: "Mobile",
+      },
+      {
+        respuesta: "Proporcionando análisis detallados a partir de los datos",
+        casa: "Data",
+      },
     ],
   },
 ]
 
-const casas = [
-  {
-    casa: "Frontend",
-    color: "#f2f2f2",
-    icono:
-      "https://www.iconfinder.com/icons/464586/code_icon_flat_web_development_programming_icon/size_64/png",
-    tema: "Diseño y desarrollo de interfaces web",
-  },
-  {
-    casa: "Backend",
-    color: "#e3e3e3",
-    icono:
-      "https://www.iconfinder.com/icons/1438014/server_icon_flat/size_64/png",
-    tema: "Desarrollo de sistemas backend",
-  },
-  {
-    casa: "Mobile",
-    color: "#cccccc",
-    icono:
-      "https://www.iconfinder.com/icons/1832874/mobile_app_icon_flat/size_64/png",
-    tema: "Desarrollo de aplicaciones móviles",
-  },
-  {
-    casa: "Data",
-    color: "#ffffff",
-    icono:
-      "https://www.iconfinder.com/icons/2518380/database_icon_flat/size_64/png",
-    tema: "Análisis y visualización de datos",
-  },
-]
+const puntosCasas = {
+  Frontend: 0,
+  Backend: 0,
+  Mobile: 0,
+  Data: 0,
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve))
+
+// When done reading prompt, exit program
+rl.on("close", () => process.exit(0))
