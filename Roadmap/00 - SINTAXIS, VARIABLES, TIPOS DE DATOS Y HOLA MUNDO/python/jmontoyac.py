864a68fd1c79ac644e57903031ec63f626d344ab
Jesus Montoya <jmontoyac@gmail.com>
2024-09-08 20:54:45 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jmontoyac.py
@@ -0,0 +1,21 @@
+# https://www.python.org/
+
+# Python solo tiene comentarios de una linea
+# Para agregar comentarion multi lineas, se agregan
+# mas comentarios de una linea
+
+# Variable en Python
+primera_variable = 0
+
+# Constante en Python
+DURACION_MILISEGUNDOS = 300
+
+# Tipos de datos primitivos
+variable_string_comillas_simples = 'String de comillas simples'
+Variable_string_comillas_dobles = "String de comillas dobles"
+variable_int = 3600
+variable_float = 5.5
+variable_boolean = True
+variable_nula = None
+
+print("¡Hola Python!")
