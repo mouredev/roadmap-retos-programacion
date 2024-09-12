dd8717d6a6806c3be016edcabe870644212ab66a
Jazmin <jazminmattachavez@gmail.com>
2024-09-12 11:42:33 +0200

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/jazmindfm.js
@@ -0,0 +1,12 @@
+// Página web oficial de JavaScript https://developer.mozilla.org/en-US/docs/Web/JavaScript
+/* 
+Esto también es un 
+comentario :D 
+*/
+
+let texto = "Esto es una cadena de texto";
+let numeros = 12323;
+const boleano = false;
+let array = [2,43,54,6];
+
+console.log("¡Hola, JavaScript!");
