630258a9e02d5f9004b254ff1e62b777b9f6fe97
SimonGuzman <siguzman@unicauca.edu.co>
2024-09-13 14:23:45 -0500

    Ejemplo de log completado, manejo de handler para poder mostrar todos los niveles

--- Roadmap/25 - LOGS/java/simonguzman.java
+++ Roadmap/25 - LOGS/java/simonguzman.java
@@ -1,11 +1,51 @@
 
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class simonguzman{
     public static void main(String[] args) {
-        sintaxisLogger();
+        //sintaxisLogger();
+        exampleLog();
     }
+
+    /************************ ejemplo del log************************/
+    public static void exampleLog(){
+        Logger logger = Logger.getLogger(simonguzman.class.getName());
+        logger.setLevel(Level.ALL);
+
+        // Configuracion del handler para que se acepten todos los niveles
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.ALL);
+        logger.addHandler(handler);
+
+        logger.info("Inicio del programa.");
+        logger.config("Configuracion de la aplicacion completada.");
+
+        try {
+            int result = split(10, 2, logger);
+            logger.info("Resultado de la division: "+result);
+
+            result = split(10, 0, logger);
+            logger.info("Resultado de la segunda division: "+result);
+
+        } catch (ArithmeticException e) {
+            logger.severe("ERROR: Division por cero..."+e.getMessage());
+        }
+
+        logger.fine("Operaciones menores completadas.");
+        logger.finer("Nivel de depuracion mas detallado");
+        logger.finest("Nivel de depuracion mas minucioso");
+        logger.warning("Advertencia: fin del programa");
+    }
+    
+    public static int split(int num1, int num2, Logger logger){
+        logger.fine("Dividiendo "+num1+" entre "+num2);
+        return num1 / num2;
+    }
+
+
+    /************************ Sintaxis del log************************/
     static void sintaxisLogger(){
         //Creacion del logger
         Logger logger = Logger.getLogger(simonguzman.class.getName());
