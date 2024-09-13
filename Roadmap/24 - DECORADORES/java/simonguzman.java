3a3250ba7beb213afb6e57f148c935cf8b2f5430
SimonGuzman <siguzman@unicauca.edu.co>
2024-09-13 11:23:56 -0500

    Ejemplo adicional aplicando el patron decorator

--- Roadmap/24 - DECORADORES/java/simonguzman.java
+++ Roadmap/24 - DECORADORES/java/simonguzman.java
@@ -1,14 +1,80 @@
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class simonguzman {
     public  static void main(String[] args) {
         genericExample();
+        example();
     }
 
+    /***************************** Ejemplo de implemetacion mas complejo *****************************/
+    static void example(){
+        UserService userService = new LoginAttemptsDecorator(new BasicUserService(), 3);
+        userService.login("user1", "password");
+        userService.login("user1", "wrongpassword");
+        userService.login("user1", "wrongpassword");
+        userService.login("user1", "wrongpassword");
+        userService.login("user1", "wrongpassword");
+    }
+    static interface UserService{
+        boolean login(String username, String password);
+    }
+
+    static class BasicUserService implements UserService{
+        private Map<String, String> userDatabase = new HashMap<>();
+
+        public BasicUserService(){
+            userDatabase.put("user1", "password");
+            userDatabase.put("user2", "12345");
+        }
+
+        @Override
+        public boolean login(String username, String password){
+            String storedPassword = userDatabase.get(username);
+            if(storedPassword != null && storedPassword.equals(password)){
+                System.out.println("Inicio de sesion exitoso para "+username);
+                return true;
+            }else{
+                System.out.println("Credenciales incorrectas para "+username);
+                return false;
+            }
+        }
+    }
+
+    static class LoginAttemptsDecorator implements UserService{
+        private UserService userService;
+        private Map<String, Integer> loginAttempts;
+        private int maxAttempts;
+
+        public LoginAttemptsDecorator(UserService userService, int maxAttempts) {
+            this.userService = userService;
+            this.maxAttempts = maxAttempts;
+            this.loginAttempts = new HashMap<>();
+        }
+
+        @Override
+        public boolean login(String username, String password) {
+            if(loginAttempts.getOrDefault(username, 0) >= maxAttempts){
+                System.out.println("Cuenta bloqueada para "+username+" por exceder los intentos permitidos");
+                return false;
+            }
+
+            boolean success = userService.login(username, password);
+            if(!success){
+                loginAttempts.put(username, loginAttempts.getOrDefault(username, 0)+1);
+                System.out.println("Intentos fallidos para "+username+":" + loginAttempts.get(username));
+            }else{
+                loginAttempts.put(username, 0);
+            }
+            return success;
+        }
+        
+    }
 
     /***************************** Ejemplo generico *****************************/
-
-    
     static void genericExample(){
         Component component = new ConcreteComponent();
         Component decorated = new ConcreteDecorator(component);
