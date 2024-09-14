7a542ff6e571513acfe838e1964a6b871aa57ef2
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-09-13 20:51:06 -0400

    37 - JavaScript

--- Roadmap/37 - OASIS VS LINKIN PARK/javascript/RicJDev.js
+++ Roadmap/37 - OASIS VS LINKIN PARK/javascript/RicJDev.js
@@ -1,114 +1,33 @@
-/*
-fetch('https://accounts.spotify.com/api/token', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-  },
-  body: new URLSearchParams({
-    grant_type: 'client_credentials',
-    client_id: clientId,
-    client_secret: clientSecret,
-  }),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    const artistId = '4Z8W4fKeB5YxbusRsdQVPb'
+// Creamos una función para obtener el token de acceso y le pasaremos el client_id y secret_client como parámetros.
 
-    const url = `https://api.spotify.com/v1/artists/${artistId}`
-
-    fetch(url, {
-      method: 'GET',
-      headers: {
-        Authorization: `${data.token_type} ${data.access_token}`,
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok')
-        }
-        return response.json()
-      })
-      .then((dataArtist) => {
-        console.log(dataArtist)
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-      })
+async function getToken(clientId, clientSecret) {
+  const accessToken = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'client_credentials',
+      client_id: clientId,
+      client_secret: clientSecret,
+    }),
   })
-  .catch((error) => {
-    console.error('Error:', error)
-  })
-*/
+    .then((response) => response.json())
+    .catch((err) => console.log(err))
 
-// import * as readline from 'readline/promises'
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// })
-
-// async function getToken(clientId, clientSecret) {
-//   fetch('https://accounts.spotify.com/api/token', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     body: new URLSearchParams({
-//       grant_type: 'client_credentials',
-//       client_id: clientId,
-//       client_secret: clientSecret,
-//     }),
-//   })
-//     .then((response) =>
-//       response
-//         .json()
-//         .then((data) => {
-//           return `${data.token_type} ${data.access_token}`
-//         })
-//         .catch((err) => console.log(err))
-//     )
-//     .catch((err) => console.log(err))
-// }
-
-// const clientId = await rl.question('Ingrese su client ID. ')
-// const clientSecret = await rl.question('Ingrese su client secret ID. ')
-
-// const token = await getToken(clientId, clientSecret).catch((err) => console.log(err))
-
-// const artistId = '4Z8W4fKeB5YxbusRsdQVPb'
-
-// const url = `https://api.spotify.com/v1/artists/${artistId}`
-
-// fetch(url, {
-//   method: 'GET',
-//   headers: {
-//     Authorization: token,
-//   },
-// })
-//   .then((response) => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok')
-//     }
-//     return response.json()
-//   })
-//   .then((dataArtist) => {
-//     console.log(dataArtist)
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error)
-//   })
-
-// rl.close()
+  return accessToken
+}
 
 /*
+* IMPORTANTE *
 
-* El archivo client.js (que no estará disponible dentro del repo) contiene las ID's de mi cuenta.
+Estoy importando mis ID's del archivo RicJDev_client.js (que no está disponible dentro del repo).
+Si desea ejecutar este código deberá pasar sus propias ID's como parámetros de la función getToken().
 
-* Para ejecutar el programa deberá crear las variables clientId y clientSecret y asignarles
-* sus propias ID's o pasarlas como parámetros de la función getToken().
+Para obtenerlas puede revisar esta sección en su cuenta de Spotify para desarrolladores: https://developer.spotify.com/dashboard
 
 */
-import { clientId, clientSecret } from './client.js'
 
-console.log(clientId)
-console.log(clientSecret)
+import { myClientId, myClientSecret } from './RicJDev_client.js'
+
+const token = await getToken(myClientId, myClientSecret)
