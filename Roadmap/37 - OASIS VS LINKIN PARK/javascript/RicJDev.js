4e4d1c3940c2b5abe0b3e76905d9c78a8168ed69
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-09-16 09:18:17 -0400

    37 - JavaScript

--- Roadmap/37 - OASIS VS LINKIN PARK/javascript/RicJDev.js
+++ Roadmap/37 - OASIS VS LINKIN PARK/javascript/RicJDev.js
@@ -3,7 +3,6 @@
  *
  * @param {string} clientId
  * @param {string} clientSecret
- * @returns {Promise<object>}
  *
  * @see https://developer.spotify.com/dashboard
  */
@@ -36,39 +35,72 @@ Si desea ejecutar este código deberá usar sus propias id's.
 import { myClientId, myClientSecret } from './RicJDev_client.js'
 
 const token = await getToken(myClientId, myClientSecret)
+const GET_requestMethod = {
+  method: 'GET',
+  headers: {
+    Authorization: `${token.token_type} ${token.access_token}`,
+  },
+}
 
 /**
  * Retorna la información sobre un artista utilizando la API de Spotify.
  *
  * @param {string} artistId se obtiene de la página del artista en Spotify (https://open.spotify.com/intl-es/artist/ `artistId`).
- * @param {object} token token de acceso. Use `getToken()` para obetenerlo.
  * @returns {Promise<object>}
+ *
+ * @see https://developer.spotify.com/dashboard
  */
 
-async function getArtistData(artistId, token) {
-  const artistData = await fetch(`https://api.spotify.com/v1/artists/${artistId}`, {
-    method: 'GET',
-    headers: {
-      Authorization: `${token.token_type} ${token.access_token}`,
-    },
-  }).then((response) => response.json())
+async function getArtistData(artistId) {
+  const artistData = await fetch(
+    `https://api.spotify.com/v1/artists/${artistId}`,
+    GET_requestMethod
+  ).then((response) => response.json())
+
+  return artistData
+}
+
+/**
+ * Retorna la data de los albums de un artista utilizando la API de Spotify.
+ *
+ * @param {string} artistId se obtiene de la página del artista en Spotify (https://open.spotify.com/intl-es/artist/ `artistId`).
+ *
+ * @see https://developer.spotify.com/dashboard
+ */
+
+async function getArtistAlbumsData(artistId) {
+  const artistData = await fetch(
+    `https://api.spotify.com/v1/artists/${artistId}/albums?include_groups=album,single&market=VE&limit=50`,
+    GET_requestMethod
+  ).then((response) => response.json())
 
   return artistData
 }
 
 // Almacenamos las id's de Oasis y Linkin Park.
 
-const OasisId = '2DaxqgrOhkeH0fpeiQq2f4'
-const LinkinParkId = '6XyY86QOPPrYVGvF9ch6wz'
+const Oasis = {
+  id: '2DaxqgrOhkeH0fpeiQq2f4',
 
-const OasisData = await getArtistData(OasisId, token)
-const LinkinParkData = await getArtistData(LinkinParkId, token)
+  data: null,
 
-console.log('\nDatos de Oasis')
-console.log(OasisData)
+  topTracks: null,
 
-console.log('\nDatos de Linkin Park')
-console.log(LinkinParkData)
+  albums: null,
+}
+
+const LinkinPark = {
+  id: '6XyY86QOPPrYVGvF9ch6wz',
+
+  data: null,
+
+  topTracks: null,
+
+  albums: null,
+}
+
+Oasis.data = await getArtistData(Oasis.id, token)
+LinkinPark.data = await getArtistData(Oasis.id, token)
 
 /*
 TODO: 
