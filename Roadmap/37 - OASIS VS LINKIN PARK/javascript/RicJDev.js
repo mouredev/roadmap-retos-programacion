1be4bc54acbcac74fbdcd36034bbd1e2592d5873
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-09-17 10:16:07 -0400

    37 - JavaScript

--- Roadmap/37 - OASIS VS LINKIN PARK/javascript/RicJDev.js
+++ Roadmap/37 - OASIS VS LINKIN PARK/javascript/RicJDev.js
@@ -31,18 +31,18 @@ async function getToken(clientId, clientSecret) {
   return token.access_token
 }
 
-// Nos almacenamos el metodo de autenticacion que usaremos para las peticiones.
+// Nos almacenamos el método de autenticación que usaremos para las peticiones.
 
 const accessToken = await getToken(CLIENT_ID, CLIENT_SECRET)
 const authorization = { headers: { Authorization: `Bearer  ${accessToken}` } }
 
-// Creamos una funcion para aprovechar el sitema de busqueda de la API.
+// Creamos una función para aprovechar el sitema de búsqueda de la API.
 
 async function getArtistId(artist) {
   const url = `https://api.spotify.com/v1/search?q=${artist}&type=artist&limit=1`
   const results = await fetch(url, authorization).then((response) => {
-    if (!response.ok) {
-      throw new Error(`Error al obtener el artista`)
+    if (response.status !== 200) {
+      throw new Error(`Error al obtener el artista ${artist}`)
     }
 
     return response.json()
@@ -55,7 +55,7 @@ async function getArtistId(artist) {
   return results.artists.items[0].id
 }
 
-// E iremos almacenando la informacion de nuestros artistas.
+// E iremos almacenando la información de nuestros artistas.
 
 const LinkinPark = {
   id: await getArtistId('Linkin Park'),
@@ -65,7 +65,7 @@ const Oasis = {
   id: await getArtistId('Oasis'),
 }
 
-// Creamos una funcion que nos retorne los datos del artista desde la API.
+// Creamos una función que nos retorne los datos del artista desde la API.
 
 async function getArtistData(artistId) {
   const url = `https://api.spotify.com/v1/artists/${artistId}`
@@ -87,7 +87,7 @@ async function getArtistData(artistId) {
 LinkinPark.data = await getArtistData(LinkinPark.id)
 Oasis.data = await getArtistData(Oasis.id)
 
-// Creamos otra funcion para obtener la cancion mas escuchada de la semana.
+// Creamos otra función para obtener la canción más escuchada de la semana.
 
 async function getArtistTopTrack(artistId) {
   const url = `https://api.spotify.com/v1/artists/${artistId}/top-tracks?market=VE`
@@ -109,6 +109,6 @@ async function getArtistTopTrack(artistId) {
 LinkinPark.topTrack = await getArtistTopTrack(LinkinPark.id)
 Oasis.topTrack = await getArtistTopTrack(Oasis.id)
 
-// Aqui empezamos a comparar.
+// Aquí empezamos a comparar.
 
 //TODO
