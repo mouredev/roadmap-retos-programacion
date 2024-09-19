6fe7506ef52b57f1e107a510446f03c1f5a68003
lswebdevelops <sardanhaluciano9690@gmail.com>
2024-09-19 14:31:58 -0300

    adding my first challenge

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/lswebdevelops.py
@@ -0,0 +1,22 @@
+#https://www.python.org/
+
+#that is just a comment
+"""
+this is one other comment
+using many lines
+"""
+
+'''
+this is also a comment 
+more lines here
+
+'''
+
+# variables: 
+
+that_is_a_variable = "My User"
+my_user = "lswebdevelops"
+
+CONSTANTE_LANGUAGE = "Python"
+
+print(CONSTANTE_LANGUAGE)
