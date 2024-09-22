d921d2565f19bc438428b37f4ccfea82f481bbc1
CluvAcademy <hello@cluvacademy.com>
2024-09-22 17:13:40 -0500

    #00 - Python, Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Raghnus.py
@@ -0,0 +1,26 @@
+# URL: https://www.python.org
+
+#Comentario de una linea
+
+"""
+Comentario
+de
+varias lineas
+"""
+
+#Variable
+x = 5
+
+#Constante
+VARIABLE_CONSTANTE = 'Constante'
+
+#Tipos primitivos del lenguaje
+cadena = "Python"               #String
+entero = 8                      #Int
+flotante = 9.05                 #Float
+booleano = True                 #Boolean
+
+#Imprimir mensaje
+print("Hola,", cadena)
+
+
