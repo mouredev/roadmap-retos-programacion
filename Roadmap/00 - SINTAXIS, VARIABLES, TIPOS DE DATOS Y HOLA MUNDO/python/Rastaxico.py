2f8912abc7cf9c71725770d88764a54c32c7de74
Rastaxico <rastaxico1125@gmail.com>
2024-09-22 20:42:35 -0500

    Create Rastaxico.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rastaxico.py
@@ -0,0 +1,46 @@
+# Ejercicio realizado por Rastaxico#
+
+#Ruta de la pagina oficial del lenguaje de programacion que escogí, en este caso Python 
+# https://www.python.org/   
+
+# Asi se hace un comentario de una sola linea
+
+'''
+Y asi se hace 
+un comentario 
+que necesite varias lineas
+'''
+
+""" Asi tambien 
+se podria 
+hacer un comentario que 
+contenga varias
+lineas """
+
+# Ejemplos de como crear una variable
+
+mi_variable = "esta es mi variable"
+dato = 9
+
+# Crear una constante
+
+MI_CONSTANTE = "esta es mi constante"
+
+# crear variables con cada tipo de datos primitivos
+
+# Strings o Cadenas de texto
+nombre_github = "rastaxico"
+
+# Integers o Numeros enteros
+edad_años = 42
+
+#Floats o Flotantes
+altura = 1,72
+
+# Booleanos o de verdad
+deseas_programar = True
+
+# Imprimir por terminal el texto “! Hola, [y el nombre de tu lenguaje]¡”
+#print("!Hola, Python¡")
+
+print("Hola, soy", nombre_github, "y uso Python")
