53206226d4583e02df3effb5fb1d2aa84d86c34c
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-09-25 08:18:57 -0400

    39 - JavaScript

--- Roadmap/39 - BATMAN DAY/javascript/RicJDev.js
+++ Roadmap/39 - BATMAN DAY/javascript/RicJDev.js
@@ -9,10 +9,9 @@
 // Nos creamos una función que generará un día de Batman para el año que le pasemos como parámetro.
 
 function getBatmanDay(year = 2014) {
-  let week = 0
-
-  let day = 1
-  let date = new Date(year, 8, day)
+  let week = 0,
+    day = 1,
+    date = new Date(year, 8, day)
 
   while (date.getDay() !== 6 || week !== 3) {
     day++
@@ -43,4 +42,42 @@ for (let i = 0; i < GothamCity.length; i++) {
   GothamCity[i] = Array(20).fill(0)
 }
 
-// TODO: lo que falta del reto 2
+/*
+    Creamos una función que reciba los reportes de amenazas.
+
+    Debido a que en JavaScript no hay tuplas, las simularemos con arrays.
+    Cada uno tendrá tres valores: [coordenada y, coordenada x, nivel de amenaza]
+
+    Usaremos esto para ubicar y asignar el valor de cada amenaza dentro de nuestro GothamCity.
+*/
+
+function updateCityStatus(reports) {
+  reports.forEach((tuple) => {
+    GothamCity[tuple[0]][tuple[1]] = tuple[2]
+  })
+}
+
+const reports = [
+  [4, 5, 9],
+  [2, 8, 5],
+  [4, 10, 8],
+  [0, 19, 10],
+  [1, 16, 8],
+  [15, 1, 9],
+  [12, 2, 9],
+  [2, 5, 5],
+  [17, 9, 6],
+  [1, 16, 7],
+  [2, 9, 6],
+  [7, 15, 8],
+  [1, 9, 2],
+  [17, 3, 4],
+]
+
+updateCityStatus(reports)
+
+// Una vez tengamos esto, podemos 'escanear' la ciudad para ver dónde debemos concentrar los recursos.
+
+function scanCity() {
+  // TODO
+}
