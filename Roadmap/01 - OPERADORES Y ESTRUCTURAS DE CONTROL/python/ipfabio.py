7bc242280cfa21147a6c8951e8c53f6c14614099
Fabio <fabioperrone95@hotmail.com>
2024-09-25 15:22:20 -0300

    #01 - Python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/ipfabio.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/ipfabio.py
@@ -121,3 +121,10 @@ except:
     print("Se ha producido un error")
 finally:
     print("Hemos terminado el manejo de excepciones")
+
+# Dificultad Extra
+#  * Crea un programa que imprima por consola todos los números comprendidos
+#  * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+for i in range(10,56):
+    if i % 2 == 0 and i != 16 and i % 3 != 0:
+        print(i)
