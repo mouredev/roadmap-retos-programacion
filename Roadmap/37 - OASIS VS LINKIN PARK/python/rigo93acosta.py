1d856fcf723722784fdd4491e94ca3d097bf2d7b
riacosta <rigo93acosta@gmail.com>
2024-09-25 20:37:58 -0300

    init reto

---
+++ Roadmap/37 - OASIS VS LINKIN PARK/python/rigo93acosta.py
@@ -0,0 +1,20 @@
+'''
+/*
+ * EJERCICIO:
+ * ¡Dos de las bandas más grandes de la historia están de vuelta!
+ * Oasis y Linkin Park han anunciado nueva gira, pero, ¿quién es más popular?
+ * Desarrolla un programa que se conecte al API de Spotify y los compare.
+ * Requisitos:
+ * 1. Crea una cuenta de desarrollo en https://developer.spotify.com.
+ * 2. Conéctate al API utilizando tu lenguaje de programación.
+ * 3. Recupera datos de los endpoint que tú quieras.
+ * Acciones:
+ * 1. Accede a las estadísticas de las dos bandas.
+ *    Por ejemplo: número total de seguidores, escuchas mensuales,
+ *    canción con más reproducciones...
+ * 2. Compara los resultados de, por lo menos, 3 endpoint.
+ * 3. Muestra todos los resultados por consola para notificar al usuario.
+ * 4. Desarrolla un criterio para seleccionar qué banda es más popular.
+ */
+'''
+
