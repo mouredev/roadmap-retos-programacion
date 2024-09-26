3a5a7c523eaac7ad9fb238c27fada0a6407bdaf3
melhk2 <melissaherrada25@gmail.com>
2024-09-26 21:02:20 +0200

    00# - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/melhk2.java
@@ -0,0 +1,19 @@
+
+
+public class melhk2 {
+
+    //la url mas importante de este lenguaje es su API: https://docs.oracle.com/javase/8/docs/api/
+    int num = 0;
+    final long nuevo = 10;
+    String nombre = "melissa";
+    boolean hola = true;
+    char tipo = 'c';
+    double externo = 1.5;
+    long ano = 1000;
+    short num2 = 15;
+    float externo1 = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Hola, Java!");
+    }
+}
