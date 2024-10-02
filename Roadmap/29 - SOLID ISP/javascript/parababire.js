297d31790a54c2a03b90f01866bad295dbe43b80
Parababire <angelenarvaezm@gmail.com>
2024-10-02 10:32:36 -0400

    #29 - Javascript

---
+++ Roadmap/29 - SOLID ISP/javascript/parababire.js
@@ -0,0 +1,58 @@
+// Incorrecto
+
+class Entity {
+  constructor(name, attackDamage, health) {
+    this.name = name
+    this.attackDamage = attackDamage
+    this.health = health
+  }
+  move() {
+    console.log(`${this.name} moved`)
+  }
+  attack(targetEntity) {
+    console.log(`${this.name} attacked ${targetEntity.name} for ${this.attackDamage} damage`)
+    targetEntity.takeDamage(this.attackDamage)
+  }
+  takeDamage(amount) {
+    this.health -= amount
+    console.log(`${this.name} has ${this.health} remaining`)
+  }
+}
+
+class Character extends Entity {
+  
+}
+
+class Wall extends Entity {
+  constructor(name, health) {
+    super(name, 0, health)
+  }
+  move() {
+    return null
+  }
+  attack() {
+    return null
+  }
+}
+
+class Turret extends Entity {
+  constructor(name, attackDamage) {
+    super(name, attackDamage, -1)
+  }
+  move() {
+    return null
+  }
+  takeDamage() {
+    return null
+  }
+}
+
+const turret = new Turret('Turret', 5)
+const character = new Character('Character', 3, 100)
+const wall = new Wall('Wall', 100)
+
+turret.attack(character)
+character.move()
+character.attack(wall)
+
+//Correcto
