498e35648b03bcc881611a4c0d5bcbd567b2316c
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2024-10-03 16:23:56 -0400

    40 - JavaScript

--- Roadmap/40 - FORTNITE RUBIUS CUP/javascript/RicJDev.js
+++ Roadmap/40 - FORTNITE RUBIUS CUP/javascript/RicJDev.js
@@ -24,27 +24,145 @@ async function getAuthToken(clientId, clientSecret) {
 
 const accessToken = await getAuthToken(CLIENT_ID, CLIENT_SECRET)
 
-const authorization = {
+const requestBody = {
   headers: {
     'Client-ID': CLIENT_ID,
     Accept: 'application/vnd.twitchtv.v5+json',
-    Authorization: 'Bearer ' + accessToken,
+    Authorization: `Bearer ${accessToken}`,
   },
 }
 
-async function searchChannel(channel) {
-  const url = `https://api.twitch.tv/helix/search/channels?query=${channel}`
+async function getChannelData(name) {
+  const url = `https://api.twitch.tv/helix/search/channels?query=${name}&first=1`
 
-  const results = await fetch(url, authorization).then((response) => {
+  const results = await fetch(url, requestBody).then((response) => {
     if (response.status !== 200)
-      throw new Error(`Error al obtener informacion del canal ${channel}.`)
+      throw new Error(`Error al obtener informacion del canal buscado: ${name}.`)
 
     return response.json()
   })
 
-  return results
+  return { name: results.data[0].display_name, id: results.data[0].id }
 }
 
-const midu = await searchChannel('midudev')
+async function getTotalFollowers(broadcasterId) {
+  const url = `https://api.twitch.tv/helix/channels/followers?broadcaster_id=${broadcasterId}`
 
-console.log(midu)
+  const results = await fetch(url, requestBody).then((response) => {
+    if (response.status !== 200)
+      throw new Error(`Error al obtener seguidores del canal con la id: ${broadcasterId}.`)
+
+    return response.json()
+  })
+
+  return results.total
+}
+
+const ari = {
+  data: null,
+  followers: null,
+  createdDate: null,
+}
+
+ari.data = await getChannelData('Ari Gameplays')
+ari.followers = await getTotalFollowers(ari.data.id)
+
+console.log(ari)
+
+const participants = [
+  'Abby',
+  'Ache',
+  'Adri Contreras',
+  'Agustin',
+  'Alexby',
+  'Ampeter',
+  'Ander',
+  'Ari Gameplays',
+  'Arigelli',
+  'Auronplay',
+  'Axozer',
+  'Beniju',
+  'By Calitos',
+  'Byviruzz',
+  'Carrerra',
+  'Celopan',
+  'Cheeto',
+  'Crystalmolly',
+  'Dario Eme Hache',
+  'Dheylo',
+  'DJMarrio',
+  'Doble',
+  'Elvisa',
+  'Elyas360',
+  'Folagor',
+  'Grefg',
+  'Guanyar',
+  'Hika',
+  'Hiper',
+  'Ibai',
+  'Ibelky',
+  'Illojuan',
+  'Imantado',
+  'Irina Isasia',
+  'Jesskiu',
+  'Jopa',
+  'JordiWild',
+  'Kenai Souza',
+  'Keroro',
+  'Kidd Keo',
+  'Kiko Rivera',
+  'Knekro',
+  'KronnoZomber',
+  'Leviathan',
+  'Lit Killah',
+  'Lola Lolita',
+  'Luh',
+  'Luzu',
+  'Mangel',
+  'Mayichi',
+  'Melo',
+  'MissaSinfonia',
+  'Mixwell',
+  'Mr.Jagger',
+  'Nate Gentile',
+  'Nexxuz',
+  'Nia',
+  'Nil Ojeda',
+  'Nissaxter',
+  'Ollie',
+  'Outsconsumer',
+  'OrsloK',
+  'Papi Gavi',
+  'Paracetarol',
+  'Patica',
+  'Paula Gonu',
+  'Pausenpaii',
+  'Perxitaa',
+  'Plex',
+  'Polispol',
+  'Quackity',
+  'Recuerdop',
+  'Reven',
+  'Rivers',
+  'RoberTPG',
+  'Roier',
+  'Rojuu',
+  'Rubius',
+  'Shadoune',
+  'Silithur',
+  'Spoksponha',
+  'Spreen',
+  'Spursiti',
+  'Staxx',
+  'Suzyroxx',
+  'Vicens',
+  'Vituber',
+  'Werlyb',
+  'Xavi',
+  'Xcry',
+  'Xokas',
+  'Xraas',
+  'Zarcort',
+  'Zeling',
+  'Zorman',
+]
