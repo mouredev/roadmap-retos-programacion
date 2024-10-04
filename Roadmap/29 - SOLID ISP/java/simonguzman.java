2b17f6f1ba629807f889052ed22016b94737f60f
SimonGuzman <siguzman@unicauca.edu.co>
2024-10-04 14:03:06 -0500

    ejemplo con isp

--- Roadmap/29 - SOLID ISP/java/simonguzman.java
+++ Roadmap/29 - SOLID ISP/java/simonguzman.java
@@ -1,6 +1,7 @@
 public class simonguzman {
     public static void main(String[] args) {
         exampleNoIsp();
+        exampleIsp();
     }
 
     /******************************** Ejercicio adicional con isp ********************************/
@@ -8,7 +9,41 @@ public class simonguzman {
     /******************************** Ejercicio adicional sin isp ********************************/
 
     /******************************** Ejemplo con isp ********************************/
+    static void exampleIsp(){
+        Workable robotIsp = new RobotIsp();
+        robotIsp.work();
 
+        Employee employee = new Employee();
+        employee.work();
+        employee.eat();
+    }
+
+    static interface Workable {
+        void work();
+    }
+
+    static interface Eatable {
+        void eat();
+    }
+
+    static class RobotIsp implements Workable {
+        @Override
+        public void work() {
+            System.out.println("El robot está trabajando.");
+        }
+    }
+
+    static class Employee implements Workable, Eatable {
+        @Override
+        public void work() {
+            System.out.println("El empleado está trabajando.");
+        }
+
+        @Override
+        public void eat() {
+            System.out.println("El empleado está comiendo.");
+        }
+    }
     /******************************** Ejemplo sin isp ********************************/
     static void exampleNoIsp(){
         Worker robot = new Robot();
