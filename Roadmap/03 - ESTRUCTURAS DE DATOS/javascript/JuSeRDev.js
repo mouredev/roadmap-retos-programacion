c4ac5e76ceb6f537b2b25fc448c94e9e5d749a0f
JuSeRDev <juserdev@gmail.com>
2024-10-05 10:03:31 -0500

    #03 - JuSeRDev

--- Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/JuSeRDev.js
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/javascript/JuSeRDev.js
@@ -82,67 +82,105 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
 const contactos = new Map()
 
 const agregarContacto = ()=>{
+    console.log("")
     rl.question("Escriba el nombre: ",(nombre)=>{
         rl.question("Escriba el celular: ", (telefono)=>{
-            contactos.set(nombre, telefono)
-            console.log(`contacto agregado: ${nombre} - ${telefono}`)
-            console.log("Volveras al menu principial")
-            agenda()
+            const telefonoValido = /^\d{10}$/.test(telefono)
+            if (telefonoValido) {
+                console.log("")
+                contactos.set(nombre, telefono)
+                console.log("CONTACTO AGREGADO")
+                console.log("")
+                console.log(`Nombre: ${nombre}`)
+                console.log(`Telefono: ${telefono}`)
+                console.log("")
+                console.log("Volveras al menu principial")
+                console.log("")
+                agenda()
+            }else {
+                console.log("El numero de telefono debe tener 10 digitos")
+                console.log("")
+                agregarContacto()
+            }
         })
     })
    
 }
 
 const BuscarContacto = ()=>{
+    console.log("")
     rl.question("Escribe el nombre del contacto que desdeas buscar: ", (nombre)=>{
         if (contactos.has(nombre)) {
             const telefono = contactos.get(nombre)
-            console.log(`Contacto encontrado: ${nombre} - ${telefono}`);
+            console.log("")
+            console.log("CONTACTO EXISTENTE")
+            console.log("")
+            console.log(`Nombre: ${nombre}`)
+            console.log(`Telefono: ${telefono}`)
+            console.log("")
+            agenda()
         } else{
             console.log(`El contacto ${nombre} no existe`);
+            console.log("")
+            console.log("Volveras al menu principial")
+            console.log("")
+            agenda()
         }
-        agenda()
     })
 }
 
 const actualizarContacto = ()=>{
+    console.log("")
     rl.question("Escribe el nombre del contacto que quieres actualizar: ",(nombre)=>{
         if (contactos.has(nombre)) {
             rl.question("Escrible en telefono actulizado: ",(telefono)=>{
+                console.log("")
                 contactos.set(nombre,telefono)
                 console.log("Contacto actualizado exitosamente")
-                console.log(`Contacto: ${nombre} - ${telefono}`)
+                console.log(`Contacto: ${nombre}`)
+                console.log(`Telefono: ${telefono}`)
+                console.log("");
+                console.log("Volveras al menu principial")
+                console.log("")
                 agenda()
             })
         }else{
             console.log("El contacto no se puede actualizar por que no existe existe")
+            console.log("")
             agenda()
         }
     })
 }
 
 const eliminarContacto = ()=>{
+    console.log("")
     rl.question("escribe el nombre del contacto que deseas eliminar: ", (nombre)=>{
         if (contactos.has(nombre)) {
+            const telefono = contactos.get(nombre)
+            console.log(`Nombre: ${nombre} - ${telefono}`)
             contactos.delete(nombre)
             console.log("contacto eliminado");
+            console.log("Volveras al menu principial")
+            console.log("")
             agenda()
         } else {
             console.log("El contacto no esta en la base de datos");
+            console.log("")
         }
     })
 }
 
 const mostrarContactos = ()=>{
+    console.log("")
     console.table(contactos)
     agenda()
 }
 
 const salir = ()=>{
+    console.log("")
     console.log("saliste de la aplicacion");
     rl.close()
     process.exit(0)
@@ -150,32 +188,63 @@ const salir = ()=>{
 
 
 const agenda = ()=>{
-    console.log("escribe la opcion que deseas")
-    console.log("1. agregar contacto")
+    console.log("")
+    console.log("/--------AGENDA--------/")
+    console.log("")
+    console.log("Escribe la opcion que deseas:")
+    console.log("1. Agregar contacto")
     console.log("2. Buscar contacto")
-    console.log("3. actualizar contacto")
-    console.log("4. eliminar contacto")
-    console.log("5. mostrar contactos existentes")
-    console.log("6. salir")
-    
-    rl.question("Que vas a hacer?", (respuesta)=>{
-        if (respuesta === "1") {
-            agregarContacto()
-        } else if (respuesta === "2") {
-            BuscarContacto()
-        }else if (respuesta === "3") {
-            actualizarContacto()
-        }else if(respuesta === "4"){
-            eliminarContacto()
-        }else if(respuesta === "5"){
-            mostrarContactos()
-        }else if(respuesta === "6"){
-            salir()
-        }else{
-            console.log("No seleccionaste una opcion valida")
-            agenda()
+    console.log("3. Actualizar contacto")
+    console.log("4. Eliminar contacto")
+    console.log("5. Mostrar contactos existentes")
+    console.log("6. Salir")
+
+    rl.question("que vas a hacer?",(respuesta)=>{
+        switch (respuesta) {
+            case "1":
+                agregarContacto()
+                break;
+            case "2":
+                BuscarContacto()
+                break;
+            case "3":
+                actualizarContacto()
+                break;
+            case "4":
+                eliminarContacto()
+                break;
+            case "5":
+                mostrarContactos()
+                break;
+            case "6":
+                salir()
+                break;
+            default:
+                console.log("No seleccionaste una opcion valida")
+                agenda()
+                break;
         }
     })
+
+
+    // rl.question("Que vas a hacer?", (respuesta)=>{
+    //     if (respuesta === "1") {
+    //         agregarContacto()
+    //     } else if (respuesta === "2") {
+    //         BuscarContacto()
+    //     }else if (respuesta === "3") {
+    //         actualizarContacto()
+    //     }else if(respuesta === "4"){
+    //         eliminarContacto()
+    //     }else if(respuesta === "5"){
+    //         mostrarContactos()
+    //     }else if(respuesta === "6"){
+    //         salir()
+    //     }else{
+    //         console.log("No seleccionaste una opcion valida")
+    //         agenda()
+    //     }
+    // })
 }
 
 agenda()
