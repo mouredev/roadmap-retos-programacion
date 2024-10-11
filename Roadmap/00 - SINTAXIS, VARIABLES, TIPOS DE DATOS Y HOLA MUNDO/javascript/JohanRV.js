63f3bd010a9e627436a477d6e95d988e146a9522
JohanRV <172932835+JohanRV@users.noreply.github.com>
2024-10-11 19:10:16 -0400

    #00 - JavaScript

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JohanRV.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JohanRV.js
@@ -18,15 +18,24 @@ let mySecondVariable = "Otro texto"; // segunda forma de crear variables -> es l
 const myConstant = "Texto que no cambia"; // tercera forma de crear variable, funciona como una constante.
 
 // Tipos de datos primitivos
+
+// Boolean
 let myBooleanTrue = true;
 let myBooleanFalse = false;
+// Null
 let myNull = null;
+// Undefined
+let myUndefined = undefined;
+// Number
 let myNumber = 20;
 let myOtherNumber = 3.14;
+// BigInt
 let myBigInt = 9007199254740992n;
+// String
 let myStringOne = "Hola";
 let myStringTwo = "Hola";
 let myStringThree = `Hola`;
+// Symbol
 let mySymbol = Symbol("identificador");
 
 console.log("Hola, JavaScript");
