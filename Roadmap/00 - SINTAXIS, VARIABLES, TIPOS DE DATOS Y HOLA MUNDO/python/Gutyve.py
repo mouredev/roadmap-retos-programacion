28c21f7e81727b66413b2281c5d2a93dd24a5d64
Gutyve <martingutierrezvega16@gmail.com>
2024-10-12 16:41:02 -0500

    gutyve.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Gutyve.py
@@ -0,0 +1,15 @@
+"" ESTO es un comentario de Python ""
+""" esto es un comentario en la plataforma 
+de python demostrando que se puede 
+escribir en varias lineas """ 
+
+
+#https://www.python.org/
+
+
+My_variable = " variable "
+My_variable = " segunda variable "
+
+MY_VARIABLE = "Constante"
+
+
