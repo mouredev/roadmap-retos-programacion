7590a5e993e01ac88db79512d37f2c964be37078
riacosta <rigo93acosta@gmail.com>
2024-10-12 11:18:09 -0300

    Reto 2 end

--- Roadmap/39 - BATMAN DAY/python/rigo93acosta.py
+++ Roadmap/39 - BATMAN DAY/python/rigo93acosta.py
@@ -37,26 +37,91 @@
 
 # When Saturday 3th Week September
 from datetime import datetime, timedelta
+import math
 
 ## Reto 1
-year_of_creation: int = 1939
-anniversary: int = year_of_creation + 85
-batman_day_ann_dates = []
-while anniversary <= year_of_creation + 100:
-    september = datetime(anniversary, 9, 1)
-    first_saturday = 5 - september.weekday() \
-                        if september.weekday() <= 5 \
-                        else 12 - september.weekday()
-    third_saturday = september + timedelta(days=first_saturday + 14)
+# year_of_creation: int = 1939
+# anniversary: int = year_of_creation + 85
+# batman_day_ann_dates = []
+# while anniversary <= year_of_creation + 100:
+#     september = datetime(anniversary, 9, 1)
+#     first_saturday = 5 - september.weekday() \
+#                         if september.weekday() <= 5 \
+#                         else 12 - september.weekday()
+#     third_saturday = september + timedelta(days=first_saturday + 14)
 
-    batman_day_ann_dates.append(
-        (
-            anniversary, 
-            anniversary - year_of_creation, 
-            third_saturday.strftime("%d-%m-%Y")
-        )
-    )
-    anniversary += 1
+#     batman_day_ann_dates.append(
+#         (
+#             anniversary, 
+#             anniversary - year_of_creation, 
+#             third_saturday.strftime("%d-%m-%Y")
+#         )
+#     )
+#     anniversary += 1
 
-for year, anniversary, date in batman_day_ann_dates:
-    print(f"Batman Day {anniversary}º Aniversario: {date}")
+# for year, anniversary, date in batman_day_ann_dates:
+#     print(f"Batman Day {anniversary}º Aniversario: {date}")
+
+## Reto 2
+sensors = [
+    (2, 3, 7),
+    (4, 3, 8),
+    (2, 2, 7),
+    (10, 12, 8),
+    (11, 12, 8),
+    (10, 11, 8),
+    (15, 18, 4)
+]
+
+map_gotham = [[0 for _ in range(20)] for _ in range(20)]
+threshold = 20
+all_alarm = []
+
+def update_map(map_gotham, sensors):
+    map_gotham_tmp = map_gotham.copy()
+    for x, y, threat in sensors:
+        map_gotham_tmp[x][y] = threat
+    
+    return map_gotham_tmp
+
+def print_map(map_gotham):
+    for row in map_gotham:
+        print(row)
+
+def sum_subgrid(values):
+    values = [value for sublist in values for value in sublist]
+    return sum(values)
+
+def select_alarm(alarms):
+    alarm = max(alarms, key=lambda x: x[2])
+    return alarm
+
+def batcave_security(map_gotham):
+    for y in range(1, 19):
+        for x in range(1, 19):
+            values = [
+                map_gotham[y-1][x-1:x+2],
+                map_gotham[y][x-1:x+2],
+                map_gotham[y+1][x-1:x+2]
+            ]
+            
+            all_alarm.append((y, x, sum_subgrid(values)))
+                
+    alarm = select_alarm(all_alarm)
+    if not alarm:
+        print("No hay amenazas.")
+    else:
+        center_coor = (alarm[0], alarm[1])
+        distance = abs(alarm[0]) + abs(alarm[1])
+        protocol = "activar" \
+                    if alarm[2] > threshold \
+                    else "no activar"
+             
+        print(f"Máxima ameneza en {center_coor}" +\
+            f" con {alarm[2]} de amenaza," +\
+                f" distancia {distance:.2f} u.a," +\
+                f" {protocol} protocolo de seguridad.")
+
+map_gotham = update_map(map_gotham, sensors)   
+print_map(map_gotham)
+batcave_security(map_gotham)
