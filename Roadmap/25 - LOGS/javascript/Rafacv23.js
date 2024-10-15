ec7c4457b4b98fb1a378526febc69d7e54b63293
Rafa Canosa <rafacv23@gmail.com>
2024-10-15 16:36:16 +0200

    first part of the exercise, show different logs about your language

---
+++ Roadmap/25 - LOGS/javascript/Rafacv23.js
@@ -0,0 +1,16 @@
+// Hecho por @Rafacv23 | https://github.com/Rafacv23 | https://twitter.com/rafacanosa  | https://www.rafacanosa.dev
+
+// Tipos de logs que hay en JavaScript
+console.log("Esto es un console.log() sirve para mostrar mensajes por consola.")
+console.warn(
+  "Esto es un console.warn(), sirve para mostrar mensajes de aviso ante posibles errores o áreas a las que haya que prestar especial atención."
+)
+console.error(
+  "Esto es un console.error(), sirve para mostrar errores por consola."
+)
+console.info(
+  "Esto es un console.info(), sirve para aportar información adicional a un mensaje."
+)
+console.debug(
+  "Esto es un console.debug(), sirve para mostrar un mensaje por consola pero con el modo debug activado."
+)
