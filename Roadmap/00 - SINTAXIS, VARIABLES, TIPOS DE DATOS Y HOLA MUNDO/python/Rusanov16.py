90b747ae46f48b4f8443ee0b5533e9a078e6d874
russanov <carlosrusaisidro@gmail.com>
2024-10-19 21:55:02 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Rusanov16.py
@@ -0,0 +1,45 @@
+"""
+EJERCICIO 00:
+    - Crea un comentario en el código y coloca la URL del sitio web oficial del
+    lenguaje de programación que has seleccionado.
+    - Representa las diferentes sintaxis que existen de crear comentarios
+    en el lenguaje (en una línea, varias...).
+    - Crea una variable (y una constante si el lenguaje lo soporta).
+    - Crea variables representando todos los tipos de datos primitivos
+    del lenguaje (cadenas de texto, enteros, booleanos...).
+    - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+"""
+
+#*-------------------------------------------------------------------------------------------------------------#
+
+# * - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+# https://www.python.org
+
+#*-------------------------------------------------------------------------------------------------------------#
+
+#  * - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+# Con el # se puede escribir un comentario de una linea.
+
+"""
+Otra forma de escribir comentarios, en este caso en varias líneas. Además, tambien se puede escribir comentarios en varias 
+lineas utilizando las comillas simples: '''
+"""
+#*-------------------------------------------------------------------------------------------------------------#
+
+#  * - Crea una variable (y una constante si el lenguaje lo soporta).
+variable = "2" #Variable de tipo String (Cadena)
+CONSTANTE_PI = 3.141592 #Para declarar una constante en python se debe declarar la variable en mayúsculas
+
+#*-------------------------------------------------------------------------------------------------------------#
+
+#  * - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+variable_string = "Github"
+variable_integer = 35
+variable_booleano = True
+variable_float = 0.9873
+
+#*-------------------------------------------------------------------------------------------------------------#
+
+#  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("Hola Mundo desde Python!!")
