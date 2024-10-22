dde352091abb0afe949dab5dbb19c693fe683098
Guido Saez <guido.saez93@gmail.com>
2024-10-22 16:25:41 -0300

    Solucion ejercicio #00 por Guido Saez

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/GuidoSaez.js
@@ -0,0 +1,39 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Esto es un comentario de una sola linea
+
+/* Esto
+es un
+comentario 
+de varias
+lineas
+*/
+
+let variable = "variable";
+const CONSTANTE = "constante";
+
+// ---------------------------------------------------------
+
+// Tipos de datos primitivos:
+
+// String
+let cadena = "cadena de texto";
+
+// Number
+let numero = 12;
+let numero2 = 2.4;
+
+// Boolean
+let boleano = true;
+let booleano2 = false;
+
+// Null
+let nulo = null;
+
+// Undefined
+let indefinido = undefined;
+
+// -------------------------------------------------------------
+
+console.log("¡Hola, JavaScript!");
+
