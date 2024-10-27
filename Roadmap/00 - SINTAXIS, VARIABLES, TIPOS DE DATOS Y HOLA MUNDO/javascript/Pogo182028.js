337d3ec066ee337f06087aa509462a761065e6c9
Dunston Madariaga <dunston.madariaga@gmail.com>
2024-10-27 14:10:23 -0400

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Pogo182028.js
@@ -0,0 +1,25 @@
+// https://retosdeprogramacion.com/roadmap
+
+// Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// Comentarios de una línea: En estos solo escribes en esta línea, no puedes bajar a la siguiente.
+/*
+    Comentarios de multiples líneas:
+    Con estos tipos de comentarios puedes escribir
+    en multiples líneas.
+*/
+
+// Crea una variable (y una constante si el lenguaje lo soporta).
+var a;
+let b;
+const c = 0;
+
+// Crea variables representando todos los tipos de datos primitivos
+// del lenguaje (cadenas de texto, enteros, booleanos...).
+let numero = 182028; // Todos los numeros, ya sean enteros, reales, etc. JavaScript los trata como numero igualmente
+let numero2 = 12.82028;
+let cadenaDeTexto = "¡Hola Mundo!";
+let booleanos = true;
+
+// Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("¡Hola, JavaScript!");
