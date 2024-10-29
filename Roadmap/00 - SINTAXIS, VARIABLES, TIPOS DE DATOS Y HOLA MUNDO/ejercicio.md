d38d9934177f399e58aa7a3490b15c9e7b606af0
Fredy <fredyfrasser@hotmail.com>
2024-10-28 19:41:02 -0500

    #00 - javascript

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ejercicio.md
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ejercicio.md
@@ -22,7 +22,7 @@
  * - Crea una variable (y una constante si el lenguaje lo soporta).
  * - Crea variables representando todos los tipos de datos primitivos
  *   del lenguaje (cadenas de texto, enteros, booleanos...).
- * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ * -    
  *
  * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
  * debemos comenzar por el principio.
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/F2Design.js
@@ -0,0 +1,28 @@
+/* https://developer.mozilla.org/es/docs/Web/JavaScript */
+
+// Comentario en una linea
+
+/*
+Comentario en 
+varias lineas
+*/
+
+// Variable
+let myName = "Fredy";
+
+// Constante
+const myName = "Fredy";
+
+// Tipos de datos primitivos de JavaScript
+typeof "Hola" // String
+typeof 42 // Number
+typeof true // Boolean
+typeof null // Object
+typeof undefined // Undefined
+typeof Symbol // Symbol
+typeof 23n // bigint (grande)
+
+// Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("¡Hola, JavaScript!");
+
+
