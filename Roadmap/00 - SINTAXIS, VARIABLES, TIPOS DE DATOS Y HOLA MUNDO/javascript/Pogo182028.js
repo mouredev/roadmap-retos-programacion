0e4c5a2558c9adf0fe2db3e71dbd0ede941836d7
Dunston Madariaga <dunston.madariaga@gmail.com>
2024-10-29 14:17:54 -0400

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Pogo182028.js
@@ -0,0 +1,17 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+// comentarios de una sola línea (solo se puede comentar en esta línea)
+
+/* 
+    comentarios en bloque
+    (se puede comentar en varias líneas)
+*/
+
+let variable;
+const variableConstante = 100;
+
+let cadena = "Esto es una cadena";
+let numero = 182028;
+let booleano = true;
+
+console.log("¡Hola, JavaScript!");
