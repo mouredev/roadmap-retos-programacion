02fc344991a9a678be7273e1d6e04442a636d550
Jackziel-Sumoza <sjackziel1234@gmail.com>
2024-11-01 22:00:57 +0200

    feat: :sparkles: operadores aritmeticos
    suma, resta, división, división entera, multiplicación, modulo o resto, potencia.

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/Jackziel-Sumoza.py
@@ -0,0 +1,30 @@
+""" /*
+ * EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */ """
+
+num_1 = 24
+num_2 = 54
+num_3 = 2.45
+
+#* Aritméticos
+print(f"Esto es una suma: {num_1 + num_2}")
+print(f"Esto es una resta: {num_1 - num_2} \n")
+print(f"Esto es una división: {num_1 / num_3} \n")
+print(f"Esto es una división entera: {num_1 // num_3} \n")
+print(f"Esto es una multiplicación: {num_1 * num_2} \n")
+print(f"Esto es modulo o resto: {num_1 % num_3} \n")
+print(f"Esto es una potencia: {num_1 ** num_3} \n")
