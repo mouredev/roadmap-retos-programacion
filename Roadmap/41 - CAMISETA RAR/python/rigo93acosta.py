ca1a5c73953c58fb4e855fd0ff112ce90a1291a2
riacosta <rigo93acosta@gmail.com>
2024-11-04 19:28:16 -0300

    Init Project

---
+++ Roadmap/41 - CAMISETA RAR/python/rigo93acosta.py
@@ -0,0 +1,12 @@
+"""
+/*
+ * EJERCICIO:
+ * ¿Has visto la camiseta.rar?
+ * https://x.com/MoureDev/status/1841531938961592740
+ *
+ * Crea un programa capaz de comprimir un archivo 
+ * en formato .zip (o el que tú quieras).
+ * - No subas el archivo o el zip.
+ */
+"""
+
