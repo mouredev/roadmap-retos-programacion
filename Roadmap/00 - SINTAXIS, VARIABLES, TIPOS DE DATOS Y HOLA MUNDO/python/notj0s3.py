969e21910c2aa556eaf99fcfa75b2355c1653926
NewJ0S3 <alexme30@gmail.com>
2024-11-04 16:58:08 +0100

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/notj0s3.py
@@ -0,0 +1,23 @@
+#https://wiki.python.org/moin/SpanishLanguage
+
+#Este es un comentario en python en una linea
+
+"""
+Este es un
+comentario en python
+en varias lineas
+"""
+
+#Mis primeras variables
+mi_primera_variable = "Varaible1"
+MI_PRIMERA_CONSTANTE = "Constante de mentiritas"
+
+#Datos primitivos
+string = "Cadena de texto"
+int = 1
+float = 3.14
+boolean1 = True
+boolean2 = False
+
+# Aquí comienza este nuevo camino
+print("¡Hola, Python!")
