f1db63565fb7b3a170aff1258e3edde59722e7fe
Mirko <ruhlmirkojoel@gmail.com>
2024-11-04 23:29:38 -0300

    Primer commit

---
+++ Roadmap/05 - VALOR Y REFERENCIA/python/RuhlMirko.py
@@ -0,0 +1,15 @@
+
+
+print("Asignacion por valor:")
+first_int = 123
+print(f"a: {first_int}")
+second_int = 321
+print(f"b: {second_int}")
+second_int = first_int
+print(f"b: {second_int}")
+first_int = 234
+print(f"a: {first_int}")
+
+print("Asignacion por referencia:")
+
+
