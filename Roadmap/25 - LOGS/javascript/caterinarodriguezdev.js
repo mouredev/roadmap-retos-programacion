f834d3a295a7ccb83ccad3862567687739ebfde2
caterinarodriguezdev <crbergas@gmail.com>
2024-11-12 22:48:26 +0100

    #25 - javascript

--- Roadmap/25 - LOGS/javascript/caterinarodriguezdev.js
+++ Roadmap/25 - LOGS/javascript/caterinarodriguezdev.js
@@ -13,18 +13,64 @@
  * Utiliza el log para visualizar el tiempo de ejecución de cada tarea.
  */
 
-console.log('Mensaje general 💚');
-console.error('Mensaje de error 🔴');
-console.warn('Mensaje de alerta 🚩');
-console.info('Mensaje informativo ℹ️');
-console.debug('Mensaje de depuración')
-console.trace('Mensaje detallado sobre la ejecución del programa 🔢');
+console.log("Mensaje general 💚");
+console.error("Mensaje de error 🔴");
+console.warn("Mensaje de alerta 🚩");
+console.info("Mensaje informativo ℹ️");
+console.debug("Mensaje de depuración");
+console.trace("Mensaje detallado sobre la ejecución del programa 🔢");
 
-console.group('Lista de mensajes');
-console.log('M1');
-console.log('M2');
-console.log('M3');
+console.group("Lista de mensajes");
+console.log("M1");
+console.log("M2");
+console.log("M3");
 console.groupEnd();
 
-console.time('tiempoEjecucion');
-console.timeEnd('tiempoEjecucion');
+console.time("tiempoEjecucion");
+console.timeEnd("tiempoEjecucion");
+
+console.log("-----------------DIFICULTAD EXTRA-------------");
+
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+let tareas = [];
+
+const gestorTareas = () => {
+  console.log(`Menú:
+        1. Añadir tarea
+        2. Eliminar tarea
+        3. Listar tareas\n`);
+
+  rl.question(
+    "Bienvenido al gestor de tareas, elija una opción -> ",
+    (resp) => {
+      switch (resp) {
+        case "1":
+          rl.question("\n¿Qué tarea quieres añadir? -> ", (resp) => {
+            tareas.push(resp);
+            console.log("Añadiendo tarea: ", resp);
+            gestorTareas();
+          });
+          break;
+        case "2":
+          rl.question("\n¿Qué tarea deseas eliminar? -> ", (resp) => {
+            console.log("\n");
+            tareas = tareas.filter((tarea) => tarea !== resp);
+            console.log("Se ha eliminado la tarea: ", resp);
+            gestorTareas();
+          });
+          break;
+        case "3":
+          tareas.forEach((tarea) => console.log(`- ${tarea}\n`));
+          gestorTareas();
+          break;
+        default:
+          console.log("\nElija una opción disponible");
+          gestorTareas();
+      }
+    }
+  );
+};
+
+gestorTareas();
