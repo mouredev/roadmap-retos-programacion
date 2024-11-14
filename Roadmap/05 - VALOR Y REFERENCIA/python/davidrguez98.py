11da1c822430074cf5cc51e56d083e8292cf18a5
davidrguez98 <ropeda98@gmail.com>
2024-11-14 19:19:59 +0100

    Update davidrguez98.py

--- Roadmap/05 - VALOR Y REFERENCIA/python/davidrguez98.py
+++ Roadmap/05 - VALOR Y REFERENCIA/python/davidrguez98.py
@@ -27,3 +27,74 @@ Sigue las **[instrucciones](../../README.md)**, consulta las correcciones y apor
 
 > Recuerda que cada semana se publica un nuevo ejercicio y se corrige el de la semana anterior en directo desde **[Twitch](https://twitch.tv/mouredev)**. Tienes el horario en la sección "eventos" del servidor de **[Discord](https://discord.gg/mouredev)**. """
 
+# Tipos de datos por valor
+
+my_int_a = 10
+my_int_b = my_int_a
+my_int_b = 20
+""" my_int_a = 30 """
+print(my_int_a)
+print(my_int_b)
+
+# Tipos de dato por referencia
+
+my_list_a = [10, 20]
+my_list_b = my_list_a
+my_list_b.append(30)
+print(my_list_a)
+print(my_list_b)
+
+#Funciones de datos por valor
+
+my_int_c = 10
+
+def my_int_func(my_int: int):
+    my_int = 20
+    print(my_int)
+
+my_int_func(my_int_c)
+print(my_int_c)
+
+# Funciones con datos por referencia
+
+my_list_c = [10, 20]
+
+def my_list_func(my_list: list):
+    my_list.append(30)
+
+    my_list_d = my_list
+    my_list_d.append(40)
+
+    print(my_list)
+    print(my_list_d)
+
+my_list_func(my_list_c)
+print(my_list_c)
+
+#DIFICULTAD EXTRA
+
+#Por valor
+int_a = 10
+int_b = 20
+
+def value(value_a: int, value_b: int):
+    value_a = int_b
+    value_b = int_a
+    print(value_a, value_b)
+
+value(int_a, int_b)
+print(int_a, int_b)
+
+#Por referencia
+list_a = [10, 20]
+list_b = [30, 40]
+
+def value2(value_la: list, value_lb: list):
+    value_la = list_b
+    value_lb = list_a
+    value_la.append(50)
+    value_lb.append(60)
+    print(value_la, value_lb)
+
+value2(list_a, list_b)
+print(list_a, list_b)
