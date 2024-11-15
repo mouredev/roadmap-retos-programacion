2116191503dc0b78e9df7d6981b5c7b3b20ca3eb
caterinarodriguezdev <crbergas@gmail.com>
2024-11-15 23:38:39 +0100

    #28-javascript

---
+++ Roadmap/28 - SOLID LSP/javascript/caterinarodriguezdev.js
@@ -0,0 +1,28 @@
+/*
+ * EJERCICIO:
+ * Explora el "Principio SOLID de Sustitución de Liskov (Liskov Substitution Principle, LSP)"
+ * y crea un ejemplo simple donde se muestre su funcionamiento
+ * de forma correcta e incorrecta.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea una jerarquía de vehículos. Todos ellos deben poder acelerar y frenar, así como
+ * cumplir el LSP.
+ * Instrucciones:
+ * 1. Crea la clase Vehículo.
+ * 2. Añade tres subclases de Vehículo.
+ * 3. Implementa las operaciones "acelerar" y "frenar" como corresponda.
+ * 4. Desarrolla un código que compruebe que se cumple el LSP.
+ */
+
+class Ball {
+  throw() {
+    console.log(`Throw the ball`);
+  }
+}
+
+class BowlingBall extends Ball {
+  throw() {
+    console.log(`Throw the ball toward pins`);
+  }
+}
+
