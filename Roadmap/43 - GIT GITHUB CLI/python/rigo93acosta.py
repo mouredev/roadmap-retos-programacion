0ce2c532c2425645eda7adc85255b48d5d184051
rigo93acosta <rigo93acosta@gmail.com>
2024-11-20 09:36:34 -0300

    Init Reto

---
+++ Roadmap/43 - GIT GITHUB CLI/python/rigo93acosta.py
@@ -0,0 +1,24 @@
+'''
+ * EJERCICIO:
+ * ¡Me voy de viaje al GitHub Universe 2024 de San Francisco!
+ *
+ * Desarrolla un CLI (Command Line Interface) que permita 
+ * interactuar con Git y GitHub de manera real desde terminal.
+ * 
+ * El programa debe permitir las siguientes opciones:
+ * 1. Establecer el directorio de trabajo
+ * 2. Crear un nuevo repositorio
+ * 3. Crear una nueva rama
+ * 4. Cambiar de rama
+ * 5. Mostrar ficheros pendientes de hacer commit
+ * 6. Hacer commit (junto con un add de todos los ficheros)
+ * 7. Mostrar el historial de commits
+ * 8. Eliminar rama
+ * 9. Establecer repositorio remoto
+ * 10. Hacer pull
+ * 11. Hacer push
+ * 12. Salir
+ *
+ * Puedes intentar controlar los diferentes errores.
+'''
+
