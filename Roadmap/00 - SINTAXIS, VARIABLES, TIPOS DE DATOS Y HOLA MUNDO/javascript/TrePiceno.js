9d8c839c42a6d796f9bce54b1ab392800569e7d6
Jesus <trepiceno@outlook.com>
2024-11-23 00:55:23 -0600

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/TrePiceno.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario en linea
+
+/* Comentario
+en varias
+lineas */
+
+let variable = "Esto es una variable";
+const constante = "Esto es una constante";
+
+let string = "Esto es un string";
+let number = 123;
+let bigInt = 123n; 
+let boolean = true;
+let symbol = Symbol();
+let nulo = null;
+let noDefinido = undefined;
+
+console.log("¡Hola, JavaScript!");
