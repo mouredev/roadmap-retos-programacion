66d92035e4a8c99b9edf2f0ac1219e1c0f6888d8
ariaslopez <87112741+ariaslopez@users.noreply.github.com>
2024-11-24 05:54:13 +0000

    Roadmap Ejercicio 1

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/AriasLopez.java
@@ -0,0 +1,37 @@
+public class AriasLopez{
+    public static void main(String[] args){
+        
+        //https://www.oracle.com/java/
+
+        // Esto es un comentario de una linea
+        /*
+         * Esto es un comentario de 
+         * varias lineas
+         * 
+         */
+
+        int numero = 5;
+            System.out.println("Esta es una variable" + numero);
+        final int constante = 10;
+            System.out.println("Esta es una constante" + constante);
+        
+        // Tipos de datos
+
+        int entero = 1; // Tipo int, almacena numeros enteros
+        float flotante = 2.5f; // Tipo float, almacena numeros decimales
+        double doble = 15.52; // Tipo double, almacena numeros decimales mas grandes
+        String cadena = "esto es un texto"; // Tipo String, almacena cadenas de texto
+        char caracter = 'T'; // Tipo char, almacena un solo carácter
+        boolean Verdadero = true; // Tipo boolean, retorna un valor verdadero
+        boolean falso = false; // Tipo boolean, retorna un valor falso
+
+        System.out.println("entero: " + entero + ", Flotante: " + flotante + ", Doble: " + doble + ", Cadena: " + cadena + ", Char: " + caracter + ", Verdadero: " + Verdadero + ", Falso: " + falso + ".");
+
+        System.out.println("Hola, Java");
+
+
+
+
+
+    }
+}
