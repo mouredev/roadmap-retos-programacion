bfa930f6ea7eed5dc3e87592763af11b9666b01d
rigo93acosta <rigo93acosta@gmail.com>
2024-11-26 19:29:27 -0300

    Init Reto

---
+++ Roadmap/45 - GITHUB OCTOVERSE/python/rigo93acosta.py
@@ -0,0 +1,25 @@
+"""
+/*
+ * EJERCICIO:
+ * GitHub ha publicado el Octoverse 2024, el informe
+ * anual del estado de la plataforma:
+ * https://octoverse.github.com
+ *
+ * Utilizando el API de GitHub, crea un informe asociado
+ * a un usuario concreto.
+ * 
+ * - Se debe poder definir el nombre del usuario
+ *   sobre el que se va a generar el informe.
+ *   
+ * - Crea un informe de usuario basándote en las 5 métricas
+ *   que tú quieras, utilizando la informración que te
+ *   proporciona GitHub. Por ejemplo:
+ *   - Lenguaje más utilizado
+ *   - Cantidad de repositorios
+ *   - Seguidores/Seguidos
+ *   - Stars/forks
+ *   - Contribuciones
+ *   (lo que se te ocurra)
+ */
+"""
+
