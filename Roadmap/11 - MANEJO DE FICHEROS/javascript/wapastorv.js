cd10ee08f336a3f860bc8c924413e3750f6a6407
wapastorv <williampastor@outlook.com>
2024-12-02 21:21:56 -0500

    11 -  Ejercicios de manejo de ficheros en JavaScript

---
+++ Roadmap/11 - MANEJO DE FICHEROS/javascript/wapastorv.js
@@ -0,0 +1,12 @@
+/* IMPORTANTE: Sólo debes subir el fichero de código como parte del ejercicio.
+ * 
+ * EJERCICIO:
+ * Desarrolla un programa capaz de crear un archivo que se llame como
+ * tu usuario de GitHub y tenga la extensión .txt.
+ * Añade varias líneas en ese fichero:
+ * - Tu nombre.
+ * - Edad.
+ * - Lenguaje de programación favorito.
+ * Imprime el contenido.
+ * Borra el fichero.
+*/
