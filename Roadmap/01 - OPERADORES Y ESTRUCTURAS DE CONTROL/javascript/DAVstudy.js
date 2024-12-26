1431802685d5a0a21c9f41d29509bb29ccaa6d5e
Diego Arenas <arenasvega.diego@gmail.com>
2024-12-26 17:34:38 -0300

    part 1

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/DAVstudy.js
@@ -0,0 +1,41 @@
+/*
+ * EJERCICIO:
+ * - Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+ *   Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+ *   (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+ * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+ *   que representen todos los tipos de estructuras de control que existan
+ *   en tu lenguaje:
+ *   Condicionales, iterativas, excepciones...
+ * - Debes hacer print por consola del resultado de todos los ejemplos.
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea un programa que imprima por consola todos los números comprendidos
+ * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+ *
+ * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+ */
+
+// Operadores aritmeticos
+let x = 10
+let y = 5
+
+console.log(`Suma: ${x + y}`)
+console.log(`Resta: ${x - y}`)
+console.log(`Multiplicacion: ${x * y}`)
+console.log(`Division: ${x / y}`)
+console.log(`Modulo: ${x % y}`)
+console.log(`Potencia: ${x ** y}`)
+
+// Operadores de comparacion
+console.log(`Igualdad: ${x == y}`);
+console.log(`Estrictamente igual: ${x === y}`);
+console.log(`Mayor que ${x > y}`);
+console.log(`Menor que ${x < y}`);
+console.log(`Mayor o igual que ${x >= y}`);
+console.log(`Menor o igual que ${x <= y}`);
+console.log(`Distinto: ${x != y}`);
+
+
+
+
