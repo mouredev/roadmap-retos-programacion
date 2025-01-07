b60636eee0f9f55a69ead05c9dfb6b2e1f6e5ff3
DIEGO ARENAS <arenasvega.diego@gmail.com>
2025-01-07 20:51:46 -0300

    #11 - javascript

--- Roadmap/11 - MANEJO DE FICHEROS/javascript/DAVstudy.js
+++ Roadmap/11 - MANEJO DE FICHEROS/javascript/DAVstudy.js
@@ -25,7 +25,11 @@
 // // Importar modulo
 
 const fs = require("fs");
-
+const readline = require("readline");
+const read = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 // // Crear archivo
 // fs.writeFile("DAVstudy.txt", "", (err) => {
 //   if (err) throw err;
@@ -70,18 +74,44 @@ const myEcommerce = {
   currentQuantitySold: 0,
   currentPrice: 0,
   lastLineAdd: "",
-  app: function (params) {},
   createFile: function (nameFile) {
     this.nameFile = `${nameFile}.txt`;
-    fs.writeFileSync(
-      this.nameFile,
-      "nombre_producto,cantidad_vendida,precio\n"
-    );
+    fs.writeFileSync(this.nameFile, "nombre_producto,cantidad_vendida,precio");
+    return "Archivo Creado";
   },
-  saveNewProduct: function (newLine) {
-    fs.appendFileSync(this.nameFile, newLine);
+  addProduct: function (newLine) {
+    fs.appendFileSync(this.nameFile, "\n" + newLine);
+    return "Nuevo producto agregado";
   },
-  updateProduct: function (product) {},
+  updateProduct: function (
+    product,
+    newProduct = "",
+    quantitySold = "",
+    price = ""
+  ) {
+    const dataUpdate = [];
+    const data = fs.readFileSync(this.nameFile, "utf8");
+    const lines = data.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].split(",");
+      if (line[0] === product) {
+        if (newProduct !== "") {
+          line[0] = newProduct;
+        }
+        if (quantitySold !== "") {
+          line[1] = quantitySold;
+        }
+        if (price !== "") {
+          line[2] = price;
+        }
+        dataUpdate.push(line.join(","));
+      } else {
+        dataUpdate.push(lines[i]);
+      }
+    }
+    fs.writeFileSync(this.nameFile, dataUpdate.join("\n"));
+    return "Archivo actualizado";
+  },
   findProduct: function (product) {
     const data = fs.readFileSync(this.nameFile, "utf8");
     const lines = data.split("\n");
@@ -92,13 +122,223 @@ const myEcommerce = {
         this.currentQuantitySold = line[1];
         this.currentPrice = line[2];
         this.indexCurrentProduct = i;
+        return "Producto encontrado";
       }
     }
+    return "El producto no existe";
+  },
+  deleteProduct: function (product) {
+    const dataUpdate = [];
+    const data = fs.readFileSync(this.nameFile, "utf8");
+    const lines = data.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i].split(",");
+      if (line[0] === product) {
+        continue;
+      } else {
+        dataUpdate.push(lines[i]);
+      }
+    }
+    fs.writeFileSync(this.nameFile, dataUpdate.join("\n"));
+    return "Producto eliminado";
+  },
+  calculateSells: function (product) {
+    this.findProduct(product);
+    return Number(this.currentQuantitySold) * Number(this.currentPrice);
   },
 };
 
+// console.log(myEcommerce.createFile("product-solds-price"));
+// console.log(myEcommerce.addProduct("\nToalla,2000,100"));
+// console.log(myEcommerce.addProduct("\nCartas,400,100"));
+// console.log(myEcommerce.findProduct("toalla"));
+// console.log(myEcommerce.currentProduct);
+// console.log(myEcommerce.updateProduct("Toalla", "Toallas", "10", "100"));
+// console.log(myEcommerce.deleteProduct("Cartas"));
+// console.log(myEcommerce.calculateSells("Toallas"));
 
 
-console.log(myEcommerce.createFile('product-solds-price'))
-console.log(myEcommerce.saveNewProduct('toalla,2000,100\n'))
-console.log(myEcommerce.saveNewProduct('cartas,400,100\n'))
+myEcommerce.createFile('data');
+app = () => {
+  
+  read.question(
+    `Ecommerce: 
+      1. Agregar producto
+      2. Actualizar producto
+      3. Buscar producto
+      4. Cantidad de ventas de un producto 
+      5. Eliminar producto
+      0. Cerrar (elimina el archivo)
+      Ingrese su opcion: `,
+    (option = (option) => {
+      switch (option) {
+        case "1":
+          read.question(`Ingrese el nombre del producto: `, (name) => {
+            read.question(`Ingrese la cantidad de ventas: `, (quantitySold) => {
+              read.question(`Ingrese el precio: `, (price) => {
+                const line = [name, quantitySold, price]
+                console.log(myEcommerce.addProduct(line.join(',')))
+                app();
+              });
+            });
+          });
+          break;
+        case "2":
+          read.question(
+            `Ingrese el nombre del producto a modificar`,
+            (name) => {
+              read.question(`Desea cambiar el nombre? `, (answer) => {
+                answer.toLowerCase() === "si"
+                  ? read.question(`Ingrese el nuevo nombre: `, (newName) =>
+                      read.question(
+                        `Desea cambiar la cantidad de ventas? `,
+                        (answer) => {
+                          answer.toLowerCase() === "si"
+                            ? read.question(
+                                `Ingrese la cantidad de ventas: `,
+                                (newQuantity) =>
+                                  read.question(
+                                    `Desea cambiar el precio? `,
+                                    (answer) => {
+                                      answer.toLowerCase() === "si"
+                                        ? read.question(
+                                            `Ingrese el precio: `,
+                                            (newPrice) => {
+                                              console.log(
+                                                myEcommerce.updateProduct(
+                                                  name,
+                                                  newName,
+                                                  newQuantity,
+                                                  newPrice
+                                                )
+                                              )
+                                            })
+                                        : console.log(
+                                            myEcommerce.updateProduct(
+                                              name,
+                                              newName,
+                                              newQuantity
+                                            )
+                                          );
+                                    }
+                                  )
+                              )
+                            : read.question(
+                                `Desea cambiar el precio? `,
+                                (answer) => {
+                                  answer.toLowerCase() === "si"
+                                    ? read.question(
+                                        `Ingrese el precio: `,
+                                        (newPrice) =>
+                                          console.log(
+                                            myEcommerce.updateProduct(
+                                              name,
+                                              newName,
+                                              "",
+                                              newPrice
+                                            )
+                                          )
+                                      )
+                                    : console.log(
+                                        myEcommerce.updateProduct(
+                                          name,
+                                          newName,
+                                          "",
+                                          ""
+                                        )
+                                      );
+                                }
+                              );
+                        }
+                      )
+                    )
+                  : read.question(
+                      `Desea cambiar la cantidad de ventas? `,
+                      (answer) => {
+                        answer.toLowerCase() === "si"
+                          ? read.question(
+                              `Ingrese la cantidad de ventas: `,
+                              (newQuantity) =>
+                                read.question(
+                                  `Desea cambiar el precio? `,
+                                  (answer) => {
+                                    answer.toLowerCase() === "si"
+                                      ? read.question(
+                                          `Ingrese el precio: `,
+                                          (newPrice) =>
+                                            console.log(
+                                              myEcommerce.updateProduct(
+                                                name,
+                                                "",
+                                                newQuantity,
+                                                newPrice
+                                              )
+                                            )
+                                        )
+                                      : console.log(
+                                          myEcommerce.updateProduct(
+                                            name,
+                                            "",
+                                            newQuantity
+                                          )
+                                        );
+                                  }
+                                )
+                            )
+                          : read.question(
+                              `Desea cambiar el precio? `,
+                              (answer) => {
+                                answer.toLowerCase() === "si"
+                                  ? read.question(
+                                      `Ingrese el precio: `,
+                                      (newPrice) =>
+                                        console.log(
+                                          myEcommerce.updateProduct(
+                                            name,
+                                            "",
+                                            "",
+                                            newPrice
+                                          )
+                                        )
+                                    )
+                                  : console.log("No hubo ningun cambio");
+                              }
+                            );
+                      }
+                    );
+              });
+            }
+          );
+          app();
+          break;
+        case '3':
+          read.question(`Ingrese el nombre del producto: `, (name) => {
+            console.log(myEcommerce.findProduct(name))
+            app();
+          });
+          break;
+        case '4':
+            read.question(`Ingrese el nombre del producto: `, (name) => {
+              console.log(myEcommerce.calculateSells(name))
+              app();
+            });
+            break;  
+        case '5':
+          read.question(`Ingrese el nombre del producto: `, (name) => {
+            console.log(myEcommerce.deleteProduct(name))
+            app();
+          });
+          break;
+        case '0':
+            fs.unlinkSync(myEcommerce.nameFile)
+            read.close()
+            break;
+        default:
+          app();      
+          break;
+      }
+    })
+  );
+};
+
+app();
