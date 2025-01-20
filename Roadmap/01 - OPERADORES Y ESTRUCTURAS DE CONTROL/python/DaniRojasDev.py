449ca0be593060b5b2f1b8986e6bd0743b7b2e65
RojasNov <danielrojasberzosa@gmail.com>
2025-01-20 21:21:14 +0100

    #01 DaniRojasDev.py a medias

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/DaniRojasDev.py
@@ -0,0 +1,19 @@
+'''
+Operadores aritméticos
+'''
+
+a = 10
+b = 3
+
+print (f"10 + 4 = {a + b}") #suma
+print (f"10 - 4 = {a - b}")#resta
+print (a * b) #multiplicación
+print (a / b) #división
+print (a % b) #módulo
+print (a ** b) #exponente
+print (a // b) #cociente
+
+'''
+Operadores lógicos
+'''
+
