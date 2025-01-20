c912695387c1cf118f38fc284b45407b1bd2a825
RojasNov <danielrojasberzosa@gmail.com>
2025-01-20 21:26:29 +0100

    #01 DaniRojasDev.py a medias v2

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/DaniRojasDev.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/DaniRojasDev.py
@@ -7,11 +7,11 @@ b = 3
 
 print (f"10 + 4 = {a + b}") #suma
 print (f"10 - 4 = {a - b}")#resta
-print (a * b) #multiplicación
-print (a / b) #división
-print (a % b) #módulo
-print (a ** b) #exponente
-print (a // b) #cociente
+print (f"10 x 4 = {a * b}") #multiplicación
+print (f"10 / 4 = {a / b}") #división
+print (f"10 % 4 = {a % b}")#módulo
+print (f"10 ** 4 = {a ** b}") #exponente
+print (f"10 // 4 = {a // b}") #cociente
 
 '''
 Operadores lógicos
