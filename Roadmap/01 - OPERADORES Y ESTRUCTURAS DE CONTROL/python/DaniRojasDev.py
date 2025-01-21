fe3d56d3750a1b1d5cd494b65c1931d03d02a286
RojasNov <danielrojasberzosa@gmail.com>
2025-01-21 14:59:02 +0100

    #01 DaniRojasDev.py a medias v3

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/DaniRojasDev.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/DaniRojasDev.py
@@ -14,6 +14,35 @@ print (f"10 ** 4 = {a ** b}") #exponente
 print (f"10 // 4 = {a // b}") #cociente
 
 '''
+Operadores de comparación
+'''
+print (f"10 = 3 is {a==b}") # Igualdad
+print (f"10 is diferent than 3 is {a!=b}") # Desigualdad
+print (f"10 is greater than 3 is {a>b}") # Mayor que
+print (f"10 is less than 3 is {a<b}") # Menor que
+print (f"10 is greater or equal than 3 is {a>=b}") # Mayor o igual que
+print (f"3 is less or equal than 10 is {b<=a}") # Menor o igual que
+
+'''
 Operadores lógicos
 '''
+# operador "and"
+print (f"10+3 = 13 y 10-3 = 7 is {a+b==13 and a-b==7}") # Si ambas operaciones son verdad es true, si no false
+print (f"10+3 = 13 y 10-3 = 7 is {a+b==12 and a-b==7}") # Si ambas operaciones son verdad es true, si no false
+
+# operador "or"
+print (f"10+3 = 13 y 10-3 = 7 is {a+b==13 or a-b==7}") # Si una de las operaciones es verdad es true, si no false
+print (f"10+3 = 13 y 10-3 = 7 is {a+b==12 or a-b==7}") # Si una de las operaciones es verdad es true, si no false
+print (f"10+3 = 13 y 10-3 = 7 is {a+b==12 or a-b==7}") # Si una de las operaciones es verdad es true, si no false
+
+# operador "not"
+print (f"10+3 = 13 is not {not a+b==13 }") # Si una de las operaciones es verdad es false, si no true (invierte true por false)
+
+
+
+
+
+
+
+
 
