4d47d4b2ca0b8f63b8b8741acbd6acdeaa110e24
joselin0407 <y.babilona@gmail.com>
2025-01-24 14:26:10 -0500

    joselin0407.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/joselin0407.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/joselin0407.py
@@ -2,4 +2,12 @@
 # Comentarios:
 
 #Con el signo '#', se inicia un comentario y todo lo que se encuentren esa linea corresponde a comentarios del codigo
+"""
+Tambien se pueden realizar comentarios extensos que se ubican dentro del rango de las tres comillas dobles
+"""
 
+'''
+Los comentarios que se encuentran dentro de estas comillas de tres sencillas 
+pueden tambien registrar comentarios de varias lineas
+como este
+'''
