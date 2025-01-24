d00bb22bd7754b0da807edae85639181d15acd0d
LHG-16 <mirandahg7@gmail.com>
2025-01-24 13:51:41 -0600

    Create LHG-16.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/LHG-16.py
@@ -0,0 +1,33 @@
+https://www.python.org/
+
+# Esto es un comentario en una línea
+ """
+ Esto es un comentario en varias líneas
+ y lleva comillas al abrir y 
+ cerrar y solo son anotaciones
+ """
+
+'''
+Esto también es una forma 
+de escribir un comentario
+'''
+Mi_variable = "Mi variable"
+Mi_variable =  "Valor nuevo de Mi variable"
+
+MIS_MAYUSCULÁS = "MIS_MÁYUSCULÁS" # por convención
+
+
+Mi_oni = 1 
+mi_float = 2.5
+mi_bool = True
+mi_bool= False
+mi_string = "una cadena de texto"
+mi_otro_string = 'mi otra cadena de texto'
+print ( "Hola, python!" )
+
+print (type(Mi_oni))
+print (type(mi_float))
+print (type(mi_bool))
+print (type(mi-string))
+print (type(mi_otro_string))
+
