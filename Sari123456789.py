99e8d80818efbca421cdb22856f3b16b24e08c13
SaraVL8 <167925299+Sari123456789@users.noreply.github.com>
2025-01-29 02:45:00 +0100

    Create Sari123456789.py
    Añadido archivo con solución al reto #00 de Python.

---
+++ Sari123456789.py
@@ -0,0 +1,18 @@
+#Esto es un comentario en Python, usando la almohadilla
+'''
+Esto es otro
+comentario en Python
+pero de varias lineas ,
+usando 3 comillas simples al principio y al final
+'''
+
+# WEB OFICIAL PYTHON: https://www.python.org/
+name = "Sara"
+edad = 30
+casada = True
+estatura = 1.63
+lenguaje = "Python"
+lista_compra = ["tomates", "manzanas", "gel"]
+dicc = {"España":"Madrid" , "Italia":"Roma"}
+
+print (f"¡Hola, {lenguaje}!")
