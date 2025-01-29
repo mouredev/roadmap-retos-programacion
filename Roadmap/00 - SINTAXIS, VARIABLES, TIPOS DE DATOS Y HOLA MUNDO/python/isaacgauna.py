9784247bc33b803937f93eb996720e854f296fe1
isaacgauna <36879885+isaacgauna@users.noreply.github.com>
2025-01-29 17:25:06 -0500

    Create isaacgauna.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/isaacgauna.py
@@ -0,0 +1,33 @@
+# documentation website: https://docs.python.org/3/
+
+''' Python is an easy to learn, powerful programming language.
+It has efficient high-level data structures and a simple but effective approach 
+to object-oriented programming.
+'''
+
+"""
+The Python interpreter is easily extended with new functions and data types implemented in C or C++, 
+make it an ideal language for scripting and rapid application development in many areas on most platforms.
+"""
+
+my_variable = "snake case"
+global_variable = "global" #variable outside of a function.
+MY_CONSTANT = "1" # se utilizan mayúsculas para su representación.
+
+var_int = int(3) # números enteros, positivos o negativos.
+var_float = float(3.141592) # números decimales o flotantes.
+var_complex = complex(1j) # real + imaginario
+var_binary = bin("hello word") #retorna el valor binario.
+var_bool = bool(true or false)  #almost any value is evaluated to "True".
+var_string = str("hello world") #cadenas de texto o de caracteres.
+
+list("key1", "key2", "key3") #secuencia mutable, ordenada, puede contener duplicados, se puede acceder al index.
+dict{"key1" : "value1", "key2" : "value2", "key3" : "value3"} #mutable, ordenada index, sin duplicados.
+set{"key1", "key2", "key3"} #no ordenado, es posible añadir o extrar valores, pero no modificarlos.
+tuple("apple", "banana") #colección ordenada e inmutable.
+
+
+print(f"hello, phyton")
+
+
+
