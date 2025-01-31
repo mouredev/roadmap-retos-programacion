5f4fd9f81db33963f7672756423362d2911d958d
ClaiNu04 <claiderman04@gmail.com>
2025-01-31 12:14:48 -0400

    Clainudev ejercicios

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/clainudev.py
@@ -0,0 +1,17 @@
+#https://www.python.org/
+
+"""
+    Este es un comentario
+    de varias lineas
+"""
+
+nombre = "Clainudev"
+
+string = "hola soy una variable string"
+numero = 100
+flotante = 10.5
+booleano = True
+
+nombre_lenguaje = "Python"
+
+print = (f"Mi nombre es {nombre} y mi lenguaje favorito es {nombre_lenguaje}")
