5c502fd65e313cabd1ca2bda35c17bdfcf28fa3a
Bmayo08 <142277228+Bmayo08@users.noreply.github.com>
2025-02-03 22:25:49 -0500

    ejercicio en progreso roadmap 01

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/bmayo08.js
@@ -0,0 +1,24 @@
+let resta= `resta de 10 -5 igual a ${10-5}`
+let suma=` suma de 10 + 5 igual a ${10 + 5}`
+let division= `division de 10/5 igual a${10/5}`
+let multiplicaion= `multiplicacion de 10/5 igual a ${10*5} `
+let modulo=`modulo de 10%5 igual a ${10%5} `
+let exponente=`exponente de 10**5 igual a ${10**5} `
+
+console.log(exponente)
+
+
+let nota1= 3
+let nota2=4
+let nota3=2
+let promedio=(nota1+nota2+nota3)/5
+
+
+if(promedio >= 3){
+    console.log(` aprobo la materia-su promedio es:${promedio}`)
+}else if(promedio < 3 && promedio >2){
+    console.log(`puede rescuperar la mareteria ${promedio}`)
+
+}else{
+    console.log(`reprobo la materia-su promedio es:${promedio}`)
+}
