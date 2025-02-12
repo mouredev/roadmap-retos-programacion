6a4442147f08146f61f4475e7a28792c26d613e9
Brian Silvero <briandavidsilvero@gmail.com>
2025-02-11 21:28:10 -0300

    #3-Python

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/BrianSilvero.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/BrianSilvero.py
@@ -97,6 +97,10 @@ except:
 finally:
     print("Ha finalizado el manejo de excepciones")
     
+    
+"""
+Extra
+"""
 for number in range(10,56):
     if (number % 2 == 0 and number != 16 and number % 3 != 0) or number == 55:
         print(number)
