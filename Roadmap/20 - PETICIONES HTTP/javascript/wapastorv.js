c8a2870de150be6064c011eff728c8b348784754
wapastorv <williampastor@outlook.com>
2025-02-15 21:54:35 -0500

    Actualizar wapastorv.js para implementar la PokéAPI y permitir la búsqueda de Pokémon por nombre o número

--- Roadmap/20 - PETICIONES HTTP/javascript/wapastorv.js
+++ Roadmap/20 - PETICIONES HTTP/javascript/wapastorv.js
@@ -8,7 +8,7 @@
 
 
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
+/*fetch('https://jsonplaceholder.typicode.com/posts/1')
     .then(response => {
         if (!response.ok) {
             throw new Error('Error al realizar la petición: ' + response.status 
@@ -22,6 +22,7 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
     .catch(error => {
         console.log('Error: ' + error);
     });
+*/
 
 /* DIFICULTAD EXTRA (opcional):
 * Utilizando la PokéAPI (https://pokeapi.co), crea un programa por
@@ -32,9 +33,15 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
 * - Muestra los juegos en los que aparece
 * - Controla posibles errores
 */
+const readline = require("readline"); // Importar el módulo readline, que permite leer datos del usuario
+const rl = readline.createInterface({ // Crear una interfaz de lectura
+    input: process.stdin, // Establecer la entrada de la interfaz
+    output: process.stdout // Establecer la salida de la interfaz
+});
+
 
 function getPokemon(pokemon) {
-    fetch(`https://pokeapi.co/api/v2/ability/${pokemon}/`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error al realizar la petición: ' + response.status 
@@ -43,16 +50,49 @@ function getPokemon(pokemon) {
             return response.json();
         })
         .then(data => {
-            console.log(data);
+            console.log(`Nombre: ${data.name}`);
+            console.log(`ID: ${data.id}`);
+            console.log(`Peso: ${data.weight}`);
+            console.log(`Altura: ${data.height}`);
+            console.log('Tipos:');
+            data.types.forEach(type => {
+                console.log(`- ${type.type.name}`);
+            });
+            return fetch(data.species.url);
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al realizar la petición: ' + response.status 
+                    + ' ' + response.statusText) ;
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(`Cadena de evolución: ${data.evolves_from_species ? data.evolves_from_species.name : 'Ninguna'}`);
+            return fetch(data.varieties[0].pokemon.url);
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al realizar la petición: ' + response.status 
+                    + ' ' + response.statusText) ;
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Juegos en los que aparece:');
+            data.game_indices.forEach(game => {
+                console.log(`- ${game.version.name}`);
+            });
         })
         .catch(error => {
             console.log('Error: ' + error);
         });
 }
-console.log('Digite el nombre de un pokemon');
-let pokemon = ReadableByteStreamController;
-getPokemon('charmander');
-getPokemon('pikachu');
-getPokemon('bulbasaur');
+rl.question("Introduce el nombre o número de un Pokémon: ", function(pokemon) {
+    getPokemon(pokemon);
+    rl.close();
+});// Cerrar la interfaz de lectura cuando se introduce un Pokémon
+
+
 
 
