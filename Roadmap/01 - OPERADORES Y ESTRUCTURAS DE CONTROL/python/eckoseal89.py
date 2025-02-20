9f61e2345ac474ba29e33f424d915847b7f7ea12
eckoseal89 <179222816+eckoseal89@users.noreply.github.com>
2025-02-20 18:27:38 +0100

    #01 - Python

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/eckoseal89.py
@@ -0,0 +1,14 @@
+a = 2
+b = 10
+
+print (a + b)
+print (a - b)
+print (a * b)
+print (b / a)
+print (b // a)
+print (b ** a)
+print (a % b)
+
+for x in range(10,100):
+    if x // 2 == 0:
+        print(x)
