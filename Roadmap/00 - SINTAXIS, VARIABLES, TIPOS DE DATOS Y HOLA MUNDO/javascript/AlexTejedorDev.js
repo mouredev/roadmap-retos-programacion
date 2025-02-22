dc46e80c27dc1d93ac4a0f6879ee8678f4b0cd83
Alexander Tejedor <atromero.98@gmail.com>
2025-02-22 07:23:14 -0500

    Agrege mi solucion al reto 00 de la semana

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/AlexTejedorDev.js
@@ -0,0 +1,29 @@
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una sola línea en JavaScript
+
+/*
+Comentario en
+varias líneas
+en JavaScript
+*/
+
+/**
+ * Comentario de documentación
+ * en JavaScript
+ */
+
+// Declaración de variables en JavaScript
+var name = 'AlexTejedorDev';
+let lastName = 'Tejedor';
+const myId = 123456789;
+
+// Tipos de datos primitivos con JavaScript
+let str = 'Esto es un String';
+let num = 26;
+let isTrue = true; //or false
+let nulo = null;
+let und = undefined;
+
+//Imprimir en pantalla un mensaje
+console.log('Hola, JavaScript');
