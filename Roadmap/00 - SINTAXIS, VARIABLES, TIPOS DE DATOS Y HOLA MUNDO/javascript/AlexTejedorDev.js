92f44e6a0d66d7530a869d68e710fbc890967ade
Alexander Tejedor <atromero.98@gmail.com>
2025-02-24 10:08:18 -0500

    Finalizado el primer reto de programación <3

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/AlexTejedorDev.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/AlexTejedorDev.js
@@ -21,9 +21,13 @@ const myId = 123456789;
 // Tipos de datos primitivos con JavaScript
 let str = 'Esto es un String';
 let num = 26;
+let float = 26.5;
+let bigInt = 123456n; 
 let isTrue = true; //or false
 let nulo = null;
 let und = undefined;
+let idUnico = Symbol('Id Unico')
 
 //Imprimir en pantalla un mensaje
 console.log('Hola, JavaScript');
+console.log(typeof(str))
