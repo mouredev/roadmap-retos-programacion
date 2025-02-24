6f237cba63a0585ec8d00b0d9c2e08c3f3987b60
Yimerson2 <yimerson2@gmail.com>
2025-02-24 13:13:17 -0500

    Create Yimerson2.java
    Primer ejercicio de retos de programación

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Yimerson2.java
@@ -0,0 +1,38 @@
+public class Yimerson2 {
+
+  //  https://www.java.com/es/
+
+  // este es un comentario en una linea 
+
+  /*
+  Estes es un comentario
+  en vaias lineas
+  */
+
+}
+
+String myVariable = "esta es una variable"
+
+public class Constante {
+  public static final int myConstante = 10;
+
+  public static void main(String[] Args) {
+    System.out.println("el valor de la constante es. " + myConstante);
+      
+  }
+
+  String myTexto = "cadena de texto";
+  int myNumber = 29;
+  float decimal = 1.99;
+  char Caracter = 'A';
+  boolean myBoolean = true;
+  
+}
+
+public class Main {
+  public static String Lenguaje = "Java";
+
+  public static void main(String[] Args) {
+    System.out.println("¡Hola, " + lenguaje!);
+  }
+}
