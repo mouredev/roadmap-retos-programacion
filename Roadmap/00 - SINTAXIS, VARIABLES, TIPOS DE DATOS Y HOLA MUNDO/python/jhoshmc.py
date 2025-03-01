cc4135ab355368573131b4967505e36a212dc03d
caleb <jhoshmc201@gmail.com>
2025-03-01 12:51:16 -0500

    Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jhoshmc.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jhoshmc.py
@@ -0,0 +1,37 @@
+""""
+ EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+"""
+
+#* ulr= https://www.python.org/
+
+#* Este es un comentario en una sola línea
+
+"""
+no tenia idea que así se hacia un comentario
+con varias líneas en pyton
+que raro :v
+"""
+
+variable= 1
+VARIABLE_CONSTANTE= 3.1416
+
+numeroEntero= 1
+numeroReal= 1.5
+booleano=True
+double= 1.3453355
+caracter= 'a'
+palabra = '!hola pyton!'
+array=[]
+
+print(palabra)
