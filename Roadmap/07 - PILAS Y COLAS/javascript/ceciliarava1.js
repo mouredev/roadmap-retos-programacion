dfb3e098009d0af61cdf6d6c3845f737a7a2862e
Cecilia Rava <ravacecilia1@gmail.com>
2025-03-05 17:14:43 -0300

    07 - Javascript

---
+++ Roadmap/07 - PILAS Y COLAS/javascript/ceciliarava1.js
@@ -0,0 +1,30 @@
+/* Stacks: LIFO. Least In First Out
+   Queue:  FIFO. First In First Out
+*/
+
+let stack = [1, 2]
+stack.push(3)
+// console.log(stack)
+stack.pop()
+// console.log(stack)
+
+let queue = [1, 2]
+queue.push(3)
+// console.log(queue)
+queue.shift()
+// console.log(queue)
+
+
+
+/*
+ * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás de un navegador web. 
+ * - Crea un programa en el que puedas navegar a una página o indicarle que te quieres desplazar 
+ *   adelante o atrás, mostrando en cada caso el nombre de la web.
+ * - Las palabras "adelante", "atrás" desencadenan esta acción
+ * - el resto se interpreta como el nombre de una nueva web.
+ * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+ *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+ * - La palabra "imprimir" imprime un elemento de la cola
+ * - el resto de palabras se interpretan como nombres de documentos.
+ */
+
