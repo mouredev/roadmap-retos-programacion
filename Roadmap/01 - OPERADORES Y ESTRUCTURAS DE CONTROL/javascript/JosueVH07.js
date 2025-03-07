75d1dcbbbe3c8b3175c1120c6655d76240a06f8f
Josue Velazquez <josuevelazquez0704@gmail.com>
2025-03-07 08:34:25 -0600

    01 - JavaScript

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/JosueVH07.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/JosueVH07.js
@@ -138,3 +138,12 @@ const person = {
 let resultado22 = 'name' in person
 console.log(resultado22)
 
+
+
+// Dificukltad extra 
+
+for (let i = 10; i <= 55; i++) {
+    if (i % 2 === 0 && i !== 16 && i % 3 !== 0) {
+        console.log(i)
+    }
+}
