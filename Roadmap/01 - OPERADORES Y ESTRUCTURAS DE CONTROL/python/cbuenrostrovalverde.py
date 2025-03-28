f284a20dadeb3e9608da0db14e7a47da3c970d16
cbuenrostrovalverde <148787241+cbuenrostrovalverde@users.noreply.github.com>
2025-03-28 11:04:21 +0100

    Update cbuenrostrovalverde.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/cbuenrostrovalverde.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/cbuenrostrovalverde.py
@@ -1,5 +1,5 @@
 """
-Operadores
+Operadores aritméticos
 """
 
 suma = 3 + 4
@@ -11,10 +11,14 @@ exponencial = 2 ** 2
 division_entera = 7 // 2
 
 print('El resultado de los operadores aritmeticos son: ')
-print(f'El resultado de la suma es: {suma}')
-print(f'El resultado de la resta es: {resta}')
-print(f'El resultado de la multiplicacion es: {multiplicacion}')
-print(f'El resultado de la division es: {division}')
-print(f'El resultado de la modulo es: {modulo}')
-print(f'El resultado de la exponencial es: {exponencial}')
-print(f'El resultado de la division entera es: {division_entera}')
+print(f'El resultado de la suma 3 + 4 es: {suma}')
+print(f'El resultado de la resta 5 - 2 es: {resta}')
+print(f'El resultado de la multiplicacion 3 * 5 es: {multiplicacion}')
+print(f'El resultado de la division 8 / 3 es: {division}')
+print(f'El resultado de la modulo 8 % 2 es: {modulo}')
+print(f'El resultado de la exponencial es 2 ** 2: {exponencial}')
+print(f'El resultado de la division entera 7 // 2 es: {division_entera}')
+
+"""
+Operadores aritméticos
+"""
