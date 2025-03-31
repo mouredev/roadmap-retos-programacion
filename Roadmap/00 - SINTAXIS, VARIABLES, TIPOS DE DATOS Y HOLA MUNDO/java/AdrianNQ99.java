aaa8dd9cdb21b3377ed152c4ae204b9d3cb4c5e7
Adrian Neuville <aneuville99@gmail.com>
2025-03-31 20:49:49 +0200

    Update AdrianNQ99.java

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/AdrianNQ99.java
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/AdrianNQ99.java
@@ -1,27 +1,22 @@
 public class AdrianNQ99 {
     // Comentario con URL del sitio web oficial de la documentación de Java 8
     // https://docs.oracle.com/javase/8/docs/api/
-
     // Comentario de una sola línea
-
     /*
      * Comentario de varias líneas
      */
     public static void main(String[] args) {
         final String username = "AdrianNQ99"; // Declaración de una constante de tipo String
-        String name = "Adrian"; // Declaración de una variable de tipo String
-        char initial = 'A'; // Declaración de una variable de tipo caracter
-        
-        int age = 21; // 4 bytes de espacio de almacenamiento. Desde -2,147,483,684 hasta 2,147,483,647
-        byte num = 127; // 1 byte de espacio de almacenamiento. Desde - 128 hasta 127
-        long numLong = 1000000000000000000L; // 8 bytes de espacio de almacenamiento. Números enormes. Sufijo L
-        short numShort = 32767;// 2 bytes de espacio de almacenamiento. Desde -32,768 hasta 32,767
-        
-        float height = 1.75f; // 4 bytes de espacio de almacenamiento. Aproximadamente 6 a 7 cifras decimales significativas. Sufijo F
-        double numDouble = 3.14159265359;  // 8 bytes de espacio de almacenamiento. Aproximadament  15 cifras decimales significativas.
 
-        boolean isStudent = true; // True o False
+        String name = "Adrian"; // Declaración de una variable de tipo String
+        int age = 21; // Declaración de una variable de tipo entero
+        float height = 1.75f; // Declaración de una variable de tipo flotante
+        char initial = 'A'; // Declaración de una variable de tipo caracter
+        boolean isStudent = true; // Declaración de una variable de tipo booleano
+        byte num = 127; // Declaración de una variable de tipo byte
+        long numLong = 1000000000000000000L; // Declaración de una variable de tipo long
+        short numShort = 32767; // Declaración de una variable de tipo short
+        double numDouble = 3.14159265359; // Declaración de una variable de tipo double
 
         System.out.println("Hola, Java!"); // Imprime "Hola Java!" en la consola
     }
-}
