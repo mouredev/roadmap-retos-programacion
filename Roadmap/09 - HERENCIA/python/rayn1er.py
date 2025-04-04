ae5ba5ee9575649ffa976068bbda953933f0896d
unknown <raulortizaray@gmail.com>
2025-04-04 14:19:26 -0400

    #09 - python

--- Roadmap/09 - HERENCIA/python/rayn1er.py
+++ Roadmap/09 - HERENCIA/python/rayn1er.py
@@ -11,3 +11,60 @@
 #  * Dependiendo de su labor, tienen propiedades y funciones exclusivas de su
 #  * actividad, y almacenan los empleados a su cargo.
 #  */
+
+class Animal:
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    
+    def greetings(self):
+        return f'Hi, Im {self.name} the {type(self).__name__}, Im {self.age} years old'
+    
+    def communicate():
+        pass
+
+class Dog(Animal):
+    
+    def communicate(self):
+        return "Woof!"
+
+
+class Cat(Animal):
+
+    def communicate(self):
+        return 'Meow!'
+
+
+yoel = Animal('yoel',3)
+marko = Dog('marko',5)
+yuri = Cat("yuri",5)
+print(yoel.greetings())
+print(marko.greetings())
+print(yuri.greetings())
+print(marko.communicate())
+print(yuri.communicate())
+
+#extra
+
+class Employee:
+
+    def __init__(self,name,id):
+        self.name =  name
+        self.id
+
+class Manager(Employee):
+    
+    def coordinate_projects(self):
+        print(f'{self.name} Esta Coordinando los proyectos de la empresa')
+
+class ProjectManager(Employee):
+    
+    def coordinate_projects(self):
+        print(f'{self.name} Esta Coordinando sus proyectos')
+
+class Developer(Employee):
+
+    def code(self):
+        print(f'{self.name} Esta escribiendo codigo')
