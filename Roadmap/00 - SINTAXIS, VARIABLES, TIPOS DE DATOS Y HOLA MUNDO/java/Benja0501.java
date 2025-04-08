126a18d1942ae72e168822d995790d4874d9290f
Benja0501 <G2333300121@unitru.edu.pe>
2025-04-08 10:39:56 -0500

    Solucion reto #01

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Benja0501.java
@@ -0,0 +1,22 @@
+public class Benja0501 {
+    // esto es un comentario de una linea
+    // https://www.java.com/es/
+    /*
+    esto es un comentario de varias lineas
+     */
+    public static void main(String[] args) {
+        var numero = 5;
+        var cadena =  "esto es una cadena";
+        //datos primitivos:
+        int entero = 5;
+        float flotante = 12.2f;
+        double  doble = 54.4;
+        char letra = 'a';
+        long largo = 5454545;
+        byte bit = 1;
+        String palabra = "Hola mundo";
+        boolean booleano = true;
+        System.out.println("¡Hola, Java!");
+
+    }
+}
