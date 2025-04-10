d44b04c46f06f3c1bce92959418b53a8dd022682
unknown <raulortizaray@gmail.com>
2025-04-10 10:40:17 -0400

    #11 - python

--- Roadmap/11 - MANEJO DE FICHEROS/python/rayn1er.py
+++ Roadmap/11 - MANEJO DE FICHEROS/python/rayn1er.py
@@ -49,22 +49,44 @@ while True:
         3 - Consultar producto
         4 - Eliminar producto
         5 - Ver productos
-        6 - Salir del Gestor
+        6 - Ver ventas totales
+        7 - ver ventas por producto
+        8 - Salir del Gestor
 ''')
     option = int(input("Seleccione una opcion -> "))
 
     if option == 1:
         product_name = input("Ingrese el nombre del producto -> ")
-        product_sold = int(input("Ingrese la cantidad de elementos vendidos ->"))
+        product_sold = int(input("Ingrese la cantidad de elementos vendidos -> "))
         product_price = int(input("Ingrese el precio del producto -> "))
         with open('store.txt','a') as store:
-            store.write(f'[{product_name}], [{product_sold}],[{product_price}]')
+            store.write(f'\n[{product_name}], [{product_sold}],[{product_price}]')
             print("Sus elementos han sido agregados a la lista")
+
+    elif option == 2:
+        pass
+    
+    elif option == 3:
+        pass
+
+    elif option == 4:
+        pass
 
     elif option == 5:
         with open('store.txt','r') as store:
             print("Elementos en la lista")
             print(store.read())
+
     elif option == 6:
+        pass
+
+    elif option == 7:
+        pass
+
+    elif option == 8:
         print("Cerrando el gestor, hasta luego!")
         os.remove('store.txt')
+
+    else:
+        print("Seleccione una opcion valida ")
+
