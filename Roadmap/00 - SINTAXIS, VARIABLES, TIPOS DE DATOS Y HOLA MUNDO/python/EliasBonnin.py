8a61ab9bcfb0f8d1c4dc8787a704def9dc010c7e
EliasDev <eliasbonnin99@gmail.com>
2025-04-17 16:03:05 -0300

    Terminamos el reto 01

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EliasBonnin.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EliasBonnin.py
@@ -1,7 +1,7 @@
 # Ejercicio 00
 
 # En el siguiente URL https:/python.org Tenemos la documentacion de phyton
-# Los retos se encuentran en https://retosdeprogramacion.com/
+# Los retos se encuentran en https://retosdeprogramacion.com
 
 # Hola mundo Python
 
