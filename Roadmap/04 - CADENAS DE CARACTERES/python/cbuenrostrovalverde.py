8b762bf33899f38627327628f79101ca5b2b41b5
cbuenrostrovalverde <cbuenrostrovalverde@gmail.com>
2025-04-23 13:20:11 +0200

    File_update

--- Roadmap/04 - CADENAS DE CARACTERES/python/cbuenrostrovalverde.py
+++ Roadmap/04 - CADENAS DE CARACTERES/python/cbuenrostrovalverde.py
@@ -75,4 +75,62 @@ print(cadena3.lower().find('n'))
 
 # Formateo
 print('Saludo: {}, Dirigido: {}'.format(cadena1, cadena2) )
-print(f'Saludo: {cadena1}, Dirigido: {cadena2}' )
+
+# Interpolación
+print(f'Saludo: {cadena1}, lenguaje: {cadena2}!')
+
+# Transformación en lista de caracteres:
+print(list(cadena3))
+
+# Transformación de lista en cadena:
+cadena4 = [cadena1, ' ', cadena2, '!']
+
+print(''.join(cadena4))
+
+# Transformaciones numéricas:
+
+cadena5 = '123456789'
+cadena5 = int(cadena5)
+print(cadena5)
+
+# Comprobaciones varias:
+
+print(cadena1.isalnum())
+print(cadena1.isalpha())
+
+'''
+Extra
+'''
+
+'''Crea un programa que analice dos palabras diferentes y realice comprobaciones
+ * para descubrir si son:
+ * - Palíndromos
+ * - Anagramas
+ * - Isogramas
+ '''
+
+def check(word1: str, word2: str):
+    # Palíndromos: Si inviertes el orden, obtendríamos la misma palabra (radar, asa, etc...)
+    print(f'¿{word1} es una palabra palíndroma?: {word1 == word1[::-1]}')
+    print(f'¿{word2} es una palabra palíndroma?: {word2 == word2[::-1]}')
+
+    # Anagramas: Palabra que resulta de la transposiciónde todas las letras de una palabra. Cambio de orden de las letras.
+    print(f'¿{word1} es una anagrama de {word2}?: {sorted(word1) == sorted(word2)}')
+
+    # Isograma
+    print(f'¿{word1} es una isograma?: {len(word1) == len(set(word1))}')
+    print(f'¿{word2} es una isograma?: {len(word2) == len(set(word2))}')
+
+    word_dict = dict()
+    for word in word1:
+        word_dict[word] = word_dict.get(word, 0) + 1
+
+    isogram = True
+    isogram_len = word_dict.values
+    for word_count in word_dict:
+        if len(word_count) != isogram_len:
+            isogram = False
+            break
+
+# check('roma', 'amor')
+check('radar', 'python')
