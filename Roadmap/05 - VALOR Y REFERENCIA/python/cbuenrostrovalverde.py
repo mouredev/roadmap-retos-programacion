632043fcc2e81abb544aed5c908a30e8c0cf2709
cbuenrostrovalverde <cbuenrostrovalverde@gmail.com>
2025-05-15 18:45:08 +0200

    New exercie

---
+++ Roadmap/05 - VALOR Y REFERENCIA/python/cbuenrostrovalverde.py
@@ -0,0 +1,33 @@
+'''
+ * EJERCICIO:
+ * - Muestra ejemplos de asignación de variables "por valor" y "por referencia", según
+ *   su tipo de dato.
+ * - Muestra ejemplos de funciones con variables que se les pasan "por valor" y 
+ *   "por referencia", y cómo se comportan en cada caso en el momento de ser modificadas.
+ * (Entender estos conceptos es algo esencial en la gran mayoría de lenguajes)
+ *
+ * DIFICULTAD EXTRA (opcional):
+ * Crea dos programas que reciban dos parámetros (cada uno) definidos como variables anteriormente.
+ * - Cada programa recibe, en un caso, dos parámetros por valor, y en otro caso, por referencia.
+ *   Estos parámetros los intercambia entre ellos en su interior, los retorna, y su retorno
+ *   se asigna a dos variables diferentes a las originales. A continuación, imprime el valor de las
+ *   variables originales y las nuevas, comprobando que se ha invertido su valor en las segundas.
+ *   Comprueba también que se ha conservado el valor original en las primeras.
+'''
+
+# Tipos de dato por valor.
+
+my_int_a = 10
+my_int_b = my_int_a
+# my_int_b = 20
+# my_int_a = 30
+print(my_int_a)
+print(my_int_b)
+
+# Tipos de dato por referencia.
+
+my_list_a = [10, 20]
+my_list_b = my_list_a
+my_list_b.append(30)
+print(my_list_a)
+print(my_list_b) # Los datos por referencia no copian el valor, lo que hacen es copiar su dirección de la memoria.
