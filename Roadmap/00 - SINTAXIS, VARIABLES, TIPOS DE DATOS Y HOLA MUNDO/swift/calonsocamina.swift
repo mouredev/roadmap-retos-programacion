c54d1c112523ed5111620169cc52a59f6781df4d
Carlos Alonso Camiña <c.alonso@app-art.gr>
2025-05-21 16:11:31 +0300

    #00 - Swift

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/swift/calonsocamina.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// Swift official website: https://www.swift.org
+
+//One line comment
+
+/*
+ Comments
+ on
+ different
+ lines
+ */
+
+//Variable and constant
+var variable: Int = 0
+let constant: Int = 1
+
+//Types
+var string = "Hello, World!"
+var int = 42
+var boolean = true
+var float: Float = 3.14
+var double: Double = 3.14159
+
+//Print
+let language = "Swift"
+print("Hi, \(language)!")
+
---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/swift/calonsocamina/calonsocamina/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  calonsocamina
+//
+//  Created by Carlos Alonso Camiña on 21/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
