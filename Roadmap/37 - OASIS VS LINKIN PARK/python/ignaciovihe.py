ee03d161812a854c46be7b7fa4eb98bc93dfe831
Ignacio <ignaciovidalhernando@gmail.com>
2025-05-30 06:29:52 +0200

    #37 - Python v.final. Añadido busqueda de artistas y comentarios

--- Roadmap/37 - OASIS VS LINKIN PARK/python/ignaciovihe.py
+++ Roadmap/37 - OASIS VS LINKIN PARK/python/ignaciovihe.py
@@ -17,19 +17,15 @@
 
 import base64
 import requests
-import aiohttp
+import aiohttp #para hacer llamadas a la api de forma asincrona
 import asyncio
 
 CLIENT_ID = "Mi CLIENT_ID"
 CLIENT_SECRET = "Mi CLIENT_SECRET"
 
-id_oasis = "2DaxqgrOhkeH0fpeiQq2f4"
-id_linkin_park = "6XyY86QOPPrYVGvF9ch6wz"
-id_mago_de_oz = "5ZNxiPcbKgaNcBrERMpqeu"
-id_dartagnan= "7Lj8CmxeAuJ2c2I6YxA6AJ"
 
 def get_token():
-
+    # Configuracion de una autenticacion OAuth
     # Codificamos las credenciales para conseguir el token de acceso, el cual usaremos para hacer peticiones get.
     auth_string = f"{CLIENT_ID}:{CLIENT_SECRET}"# Este es el formato de user:pass que se necesita codificar en base 64.
     b64_auth_string = base64.b64encode(auth_string.encode()).decode()#encode para un string a bytes, luego se codifica en base 64 y al final se vuelve a pasar a string.
@@ -48,6 +44,8 @@ def get_token():
 
     # Enviamos la petición POST
     response = requests.post(url, headers=headers, data=data)
+    if response.status_code != 200:
+        raise Exception("Error obteniendo el token de Spotify.")
     token_data = response.json()
 
     return token_data["access_token"]
@@ -55,11 +53,30 @@ def get_token():
 
 
 async def call_api(session, url, headers):
+    #Funcion asincronaque se encarga de hacer el get  con la session y url pasadas
     async with session.get(url, headers=headers) as answer:
         if answer.status != 200:
             return None
         else:
-            return await answer.json()
+            return await answer.json()# Tiene que esperar a que reciba los datos y los genere, por eso el await.
+        
+async def search_artists(token: str, artist_1: str, artist_2: str):
+    url_artist1_query = f"https://api.spotify.com/v1/search?q={artist_1}&type=artist&limit=1"
+    url_artist2_query = f"https://api.spotify.com/v1/search?q={artist_2}&type=artist&limit=1"
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+    async with aiohttp.ClientSession() as session:# Se crea la sesion para llamadas asincronas. Session = request
+        info_artist_1, info_artist_2 = await asyncio.gather(# Lanzamos la llamadas que no dependen entre ellas
+                                                            call_api(session, url_artist1_query, headers), 
+                                                            call_api(session, url_artist2_query, headers),
+                                                            )
+        if info_artist_1 and info_artist_2:
+            return info_artist_1["artists"]["items"][0]["id"], info_artist_2["artists"]["items"][0]["id"]
+        else:
+            raise Exception("Hubo un problema al buscar los ids de los artistas")
 
 
 async def compare_artists(token: str, artist_id1: str, artist_id2):
@@ -86,14 +103,18 @@ async def compare_artists(token: str, artist_id1: str, artist_id2):
             name1, name2 = info_artist_1["name"], info_artist_2["name"]
             followers1, followers2 = info_artist_1["followers"]["total"], info_artist_2["followers"]["total"]
             popularity1, popularity2 = info_artist_1["popularity"], info_artist_2["popularity"]
+        else:
+            raise Exception("Error obteniendo los datos de los artistas.")
 
         if info_tracks_1 and info_tracks_2:
             sorted_tracks1 = sorted(info_tracks_1["tracks"], key=lambda t: t["popularity"], reverse=True)
             sorted_tracks2 = sorted(info_tracks_2["tracks"], key=lambda t: t["popularity"], reverse=True)
-            name_track_1 = sorted_tracks1[1]["name"]
-            name_track_2 = sorted_tracks2[1]["name"]
-            popularity_track1 = sorted_tracks1[1]["popularity"]
-            popularity_track2 = sorted_tracks2[1]["popularity"]
+            name_track_1 = sorted_tracks1[0]["name"]
+            name_track_2 = sorted_tracks2[0]["name"]
+            popularity_track1 = sorted_tracks1[0]["popularity"]
+            popularity_track2 = sorted_tracks2[0]["popularity"]
+        else:
+            raise Exception("Error obteniendo los datos de los tracks.")
 
         points={
             name1: 0,
@@ -118,26 +139,28 @@ async def compare_artists(token: str, artist_id1: str, artist_id2):
         print(f"{more_popular[1]} es más popular según el ranking de Spotify.\n")
         points[more_popular[1]] += 3
 
-        print("Canción más populares:")
+        print("Canciones más populares:")
         print(f"\tCanción de {name1}: {name_track_1} - {popularity_track1}")
         print(f"\tCanción de {name2}: {name_track_2} - {popularity_track2}")
-        more_popular = max((popularity1, name_track_1, name1), (popularity2, name_track_2, name2))
-        print(f"La cancion {more_popular[1]} de {more_popular[2]} es más popular.\n")
+        more_popular = max((popularity_track1, name_track_1, name1), (popularity_track2, name_track_2, name2))
+        print(f"La canción {more_popular[1]} de {more_popular[2]} es más popular.\n")
         points[more_popular[2]] += 1.5
 
         print("RESULTADO FINAL:")
+        print(f"{name1}: {points[name1]}")
+        print(f"{name2}: {points[name2]}")
         if points[name1] > points[name2]:
             print(f"{name1} es más popular")
         else:
             print(f"{name2} es más popular")
 
 
-
-
-
 async def main():
     token = get_token()
-    await compare_artists(token, id_oasis, id_linkin_park)
+    query1 = input("Introduce el nombre del primer artista: ")
+    query2 = input("Introduce el nombre del segundo artista: ")
+    id_artist_1, id_artist_2 = await search_artists(token, query1, query2)
+    await compare_artists(token, id_artist_1, id_artist_2)
 
 asyncio.run(main())
 
