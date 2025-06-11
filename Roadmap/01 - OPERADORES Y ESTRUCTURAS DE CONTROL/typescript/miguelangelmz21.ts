7e26c0af298da1febd80c5205075c4485713e742
miguelangelmz21 <mike21.mzeb@gmail.com>
2025-06-11 16:14:45 -0500

    Refine extra difficulty condition to include only even numbers not equal to 16 or multiples of 3

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/typescript/miguelangelmz21.ts
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/typescript/miguelangelmz21.ts
@@ -102,7 +102,7 @@ try {
 // DIFICULTAD EXTRA
 console.log("Números comprendidos entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3:");
 for (let i = 10; i <= 55; i++) {
-    if(i%3 != 0){
+    if(i%2==0 && i%3 != 0){
         if(i!=16)
             console.log(i);        
     }
