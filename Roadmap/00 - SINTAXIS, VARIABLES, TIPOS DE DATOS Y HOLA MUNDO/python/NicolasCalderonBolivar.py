7732a53706af89c09102822e24501a1bf2b2778d
nicolino3D <ncalderonbolivar@gmail.com>
2025-07-31 23:03:38 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/NicolasCalderonBolivar.py
@@ -0,0 +1,48 @@
+"""
+EJERCICIO:
+
+
+ - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación
+ que has seleccionado.
+
+ - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+ - Crea una variable (y una constante si el lenguaje lo soporta).
+
+ - Crea variables representando todos los tipos de datos primitivos el lenguaje
+ (cadenas de texto, enteros, booleanos...).
+
+ - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+ """
+
+# -----------------------------------------------------------------------------------------------------------------
+
+
+
+# https://www.python.org/
+
+# asi es un comentario de una linea
+""" asi comentarios en bloque y template"""
+
+variable_numerica_entera = int() # 0
+variable_numerica_flotante = float() # 2.5
+variable_texto = str() # ""
+variable_diccionario = dict() # {}
+variable_lista = list() # []
+variable_booleano = bool()  # False
+
+CONSTANTE = None # una Variable constante vacia. se pone en MAYUSCULA porque python no soporta Constantes.
+
+print("¡Hola, python!")
+
+
+
+
+
+
+
+
+
+
+
