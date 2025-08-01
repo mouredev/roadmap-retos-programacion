1ae9d1de3d1bbce5a205928a3dfc5f0cec74b6d2
nicolino3D <ncalderonbolivar@gmail.com>
2025-08-01 14:58:31 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/NicolasCalderonBolivar.py
@@ -0,0 +1,27 @@
+
+# https://www.python.org/
+
+# asi es un comentario de una linea
+""" asi comentarios en bloque y template"""
+
+variable_numerica_entera = int() # 0
+variable_numerica_flotante = float() # 2.5
+variable_texto = str() # ""
+variable_diccionario = dict() # {}
+variable_lista = list() # []
+variable_booleano = bool()  # False
+
+CONSTANTE = None # una Variable constante vacia. se pone en MAYUSCULA porque python no soporta Constantes.
+
+print("¡Hola, python!")
+
+
+
+
+
+
+
+
+
+
+
