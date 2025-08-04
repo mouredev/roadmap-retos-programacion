3dc4451103f794700ba50db1c6e7fbbffc05adb4
Ignacio <ignaciovidalhernando@gmail.com>
2025-08-04 11:31:36 +0200

    #46 - Python. Refactorizado lo que llevo para cumplir mejor solid. Hasta ahora: registro de usuarios  e inicio/fin  de sesión, creacion de nuevos post

--- Roadmap/46 - X VS BLUESKY/python/ignaciovihe.py
+++ Roadmap/46 - X VS BLUESKY/python/ignaciovihe.py
@@ -55,7 +55,7 @@ class Post:
         self.text = text
 
 
-class User_Manager:
+class UserManager:
     def __init__(self) -> None:
         self.users = dict()
 
@@ -67,32 +67,20 @@ class User_Manager:
     def get_user(self, user_name):
         return self.users[user_name]
 
+class SessionManager:
+    def __init__(self, user_manager: UserManager) -> None:
+        self.user_manager = user_manager
 
-class Posts_manager:
-    def __init__(self, user: User) -> None:
-        self.user = user
-
-    def create_post(self):
-        while True:
-            post_text = input("Introduce el texto del post. Debe ser entre 1 y 200 caracteres: ")
-            if 0 < len(post_text) < 200:
-                break
-            else:
-                print(f"Has escrito {len(post_text)} caracteres. Copia el mensaje y vuelve a intenetarlo.")
-                print()
-                print(post_text)
-        new_post = Post(self.user.name, post_text)
-        self.user.posts[new_post.id] = new_post
-        print(f"El ususairo {self.user.name} ha añadidio un nuevo post.")
-
-    def delete_post(self, post_id):
-        pass
-
-
-class Social_Network:
-    def __init__(self) -> None:
-        self.user_manager = User_Manager()
-        self.active_user: User | None = None
+    def register_new_user(self):
+        user_name = ""
+        while not user_name or user_name in self.user_manager.users:
+            user_name = input("Introduce un nombre de usuario nuevo a registrar: ")
+            if not user_name:
+                print("El nombre de usuario no puede estar vacio. Elige otro.")
+            if user_name in self.user_manager.users:
+                print("El nombre de usuario ya existe. Elige otro.")
+        self.user_manager.register_user(user_name)
+        pausar()
 
     def start_new_session(self):
         if not self.user_manager.users:
@@ -118,21 +106,9 @@ class Social_Network:
                     user_name = ""
 
         return user_name
-
-    def register_new_user(self):
-        user_name = ""
-        while not user_name or user_name in self.user_manager.users:
-            user_name = input("Introduce un nombre de usuario nuevo a registrar: ")
-            if not user_name:
-                print("El nombre de usuario no puede estar vacio. Elige otro.")
-            if user_name in self.user_manager.users:
-                print("El nombre de usuario ya existe. Elige otro.")
-        self.user_manager.register_user(user_name)
-        pausar()
-
+    
     def log_in(self):
-
-        while not self.active_user:
+        while True:
             clear_console()
             print("Bienvenido a 'J' la nueva red social de microblogging.")
             print("\t1. Inicia sesión.")
@@ -146,44 +122,91 @@ class Social_Network:
                 case "1":
                     active_name = self.start_new_session()
                     if active_name:
-                        self.active_user = self.user_manager.get_user(active_name)
+                        return self.user_manager.get_user(active_name)
 
                 case "2":
                     self.register_new_user()
 
                 case "3":
-                    return True
+                    return None
+                
+
+class ActiveSession:
+    def __init__(self, user: User) -> None:
+        self.user = user
+        self.posts_manager = PostsManager(self.user)
+
+    def show_menu(self):
+        clear_console()
+        print(f"Sesion iniciada como: {self.user.name}")
+        print()
+        print("\t1. Crear nuevo post.")
+        print("\t2. Borrar post.")
+        print("\t3. Ver post propios.")
+        print("\t4. Ver post de usuarios seguidos.")
+        print("\t5. Seguir usuario.")
+        print("\t6. Dejar de seguir usuario")
+        print("\t7. Cerrar sesión.")
+        print("\t8. Salir")
+
+        option = "0"
+        while option not in map(str, range(1,9)):
+            option = input("Introduce una opcion correcta: ")
+
+        return option
+
+class PostsManager:
+    def __init__(self, user: User) -> None:
+        self.user = user
+
+    def create_post(self):
+        while True:
+            post_text = input("Introduce el texto del post. Debe ser entre 1 y 200 caracteres: ")
+            if 0 < len(post_text) < 200:
+                break
+            else:
+                print(f"Has escrito {len(post_text)} caracteres. Copia el mensaje y vuelve a intenetarlo.")
+                print()
+                print(post_text)
+        new_post = Post(self.user.name, post_text)
+        self.user.posts[new_post.id] = new_post
+        print(f"El ususairo {self.user.name} ha añadidio un nuevo post.")
+        pausar()
+
+    def delete_post(self, post_id):
+        pass
+
+
+class SocialNetwork:
+    def __init__(self) -> None:
+        self.user_manager = UserManager()
+        self.session_manager = SessionManager(self.user_manager)
+        self.active_user: User | None = None
+
+    def log_in(self):
+        self.active_user = self.session_manager.log_in()
+        return self.active_user is None
                 
     def run_program(self):
-        clear_console()
         if self.active_user:
-            print(f"Sesion iniciada como: {self.active_user.name}")
-            print()
-            print("\t1. Crear nuevo post.")
-            print("\t2. Borrar post.")
-            print("\t3. Ver post propios.")
-            print("\t4. Ver post de usuarios seguidos.")
-            print("\t5. Seguir usuario.")
-            print("\t6. Dejar de seguir usuario")
-            print("\t7. Cerrar sesión.")
-            print("\t8. Salir")
+            session = ActiveSession(self.active_user)
+            option = session.show_menu()
 
-            option = "0"
-            while option not in map(str, range(1,9)):
-                option = input("Introduce una opcion correcta: ")
-            
             match option:
                 case "1":
-                    pass
+                    session.posts_manager.create_post()
 
                 case "2":
                     pass
+
+                case "7":
+                    self.active_user = None
 
                 case "8":
                     return True
 
 
-my_social_network = Social_Network()
+my_social_network = SocialNetwork()
 finish = False
 while not finish:
     while not my_social_network.active_user and not finish:
