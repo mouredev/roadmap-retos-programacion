4dc64ea145094af55a1f3ea6ff12240e3a8aa730
vec70r <lizan.2001@gmail.com>
2025-08-12 16:24:10 -0600

    #00 - c

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c/vec70rr.c
@@ -0,0 +1,25 @@
+//No encontré una página oficial de C, asi que pongo la documentacion de Microsoft: https://learn.microsoft.com/es-es/cpp/c-language/
+
+//Comentario de una linea
+/*
+Comentario de 
+varias lineas
+*/
+
+#include <stdio.h>
+
+#define constante 10 // Definicion de una constante
+
+int main(int argc, char const *argv[])
+{
+    int variable; //declaracion de variable
+
+    int entero;
+    double decimal;
+    float flotante;
+    char caracter;
+    char cadena[20];
+    
+    printf("¡Hola, C!");
+    return 0;
+}
