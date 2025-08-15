ecb1254214aacf369bb1434a899531a749b83bd1
Maria Luna <mlunat20@gmail.com>
2025-08-15 15:24:00 -0500

    add class Olympics for add events

--- Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/typescript/marialunatito.ts
+++ Roadmap/31 - SIMULADOR JUEGOS OLÍMPICOS/typescript/marialunatito.ts
@@ -16,3 +16,20 @@ interface IOlympics {
   id: number | undefined;
   name: string | undefined;
 }
+
+class Olympics implements IOlympics {
+  id: number | undefined;
+  name: string | undefined;
+
+  private listEventsWithPlayers: IOlympics[] = [];
+
+  addEvents(data: IOlympics) {
+    this.id = data.id;
+    this.name = data.name;
+
+    this.listEventsWithPlayers.push({
+      id: this.id,
+      name: this.name,
+    });
+  }
+}
