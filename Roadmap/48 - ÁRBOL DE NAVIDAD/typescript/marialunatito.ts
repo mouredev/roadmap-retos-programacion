c0405b688f58211a1c4f61d20183ee6c1d5cf269
Maria Luna <mlunat20@gmail.com>
2025-08-19 16:03:52 -0500

    add options of the menu

--- Roadmap/48 - ÁRBOL DE NAVIDAD/typescript/marialunatito.ts
+++ Roadmap/48 - ÁRBOL DE NAVIDAD/typescript/marialunatito.ts
@@ -25,6 +25,38 @@ function menu() {
     8. Encender (+) las luces (conservando su posición)
     10. Salir del sistema
     `);
+
+  readlline.question("Seleccione una option: ", (option: string) => {
+    switch (option) {
+      case "0":
+        break;
+      case "1":
+        break;
+      case "2":
+        break;
+      case "3":
+        break;
+      case "4":
+        break;
+      case "5":
+        break;
+      case "6":
+        break;
+      case "7":
+        break;
+      case "8":
+        break;
+      case "10":
+        console.log("Ha salido del sistema");
+        readlline.close();
+        break;
+
+      default:
+        console.log(`La opción ${option} es invalida.. vuelva a intentar`);
+        menu();
+        break;
+    }
+  });
 }
 
 class Tree {
