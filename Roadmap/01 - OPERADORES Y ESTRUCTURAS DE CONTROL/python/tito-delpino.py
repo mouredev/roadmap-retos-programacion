5a39329d633b07634a532d341c0af3c4a743244a
Alvaro Del Pino <alvarodelpino88@gmail.com>
2025-08-31 13:46:09 +0200

    de comparacion y logicos en curso

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/tito-delpino.py
@@ -0,0 +1,43 @@
+# EJERCICIO:
+# Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje:
+#  1- Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
+'''Aritmeticos'''
+print(f'suma: 4 + 4 = {4 + 4}')
+print(f'resta: 4 - 4 = {4 - 4}')
+print(f'multiplicacion: 4 * 3 = {4 * 3}')
+print(f'division: 10 / 5 = {10 / 5}')
+print(f'modulo: 10 % 5 = {11 % 5}')
+print(f'division_entera: 7 // 3 = {7 // 3}')
+print(f'potencia: 4 ** 3 = {4 ** 3}')
+
+'''De comparacion'''
+print(f'igualdad: 10 == 5 es {10 == 5}')
+print(f'desigualdad: 10 != 5 es {10 != 5}')
+print(f'mayor que: 20 > 4 es {20 > 4}')
+print(f'menor que: 20 < 4 {20 < 4}')
+print(f'mayor o igual que: 15 >= 14 es {15 >= 14}')
+print(f'menor o igual que: 15 <= 14 es {15 <= 14}')
+
+'''Logicos'''
+'AND, OR Y NOT'
+a = True
+b = False
+
+print(a and b) # False
+print(a and a) # True
+print(a or b)   # True
+print(not a)    # False
+
+
+#  * - Utilizando las operaciones con operadores que tú quieras, crea ejemplos
+#  *   que representen todos los tipos de estructuras de control que existan
+#  *   en tu lenguaje:
+#  *   Condicionales, iterativas, excepciones...
+#  * - Debes hacer print por consola del resultado de todos los ejemplos.
+#  *
+#  * DIFICULTAD EXTRA (opcional):
+#  * Crea un programa que imprima por consola todos los números comprendidos
+#  * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+#  *
+#  * Seguro que al revisar detenidamente las posibilidades has descubierto algo nuevo.
+#  */
