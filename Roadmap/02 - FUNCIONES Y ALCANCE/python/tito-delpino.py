9ce45407f1703dc3c1e22731d470ca353e2dfafc
Alvaro Del Pino <alvarodelpino88@gmail.com>
2025-09-01 17:04:51 +0200

    #02 - Python

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/tito-delpino.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/tito-delpino.py
@@ -68,14 +68,30 @@ funcion_con_variable_local()
 print(f'print desde fuera de la funcion de variable global - {var_global}')
 
 
-#  * DIFICULTAD EXTRA (opcional):
-#  * Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
-#  * - La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
-#  *   - Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
-#  *   - Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
-#  *   - Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
-#  *   - La función retorna el número de veces que se ha impreso el número en lugar de los textos.
-#  *
-#  * Presta especial atención a la sintaxis que debes utilizar en cada uno de los casos.
-#  * Cada lenguaje sigue una convenciones que debes de respetar para que el código se entienda.
-#  */
+""" DIFICULTAD EXTRA (opcional):
+Crea una función que reciba dos parámetros de tipo cadena de texto y retorne un número.
+* La función imprime todos los números del 1 al 100. Teniendo en cuenta que:
+    Si el número es múltiplo de 3, muestra la cadena de texto del primer parámetro.
+    Si el número es múltiplo de 5, muestra la cadena de texto del segundo parámetro.
+    Si el número es múltiplo de 3 y de 5, muestra las dos cadenas de texto concatenadas.
+    La función retorna el número de veces que se ha impreso el número en lugar de los textos.
+"""
+
+def funcion_prueba(string1, string2):
+    contador = 0
+    for num in range(1,101):
+        if num % 3 == 0 and num % 5 == 0:
+            print(string1 + string2)
+        elif num % 3 == 0:
+            print(string1)
+        elif num % 5 == 0:
+            print(string2)
+        else:
+            print(num)
+            contador += 1
+        
+    return contador
+
+
+resultado = funcion_prueba('Fizz', 'Buzz')
+print(resultado)
