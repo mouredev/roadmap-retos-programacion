bbeae2e6ed85187c35bb5ed6b2aede28cb0b732c
jgutierrez9891 <j.gutierrezl@uniandes.edu.co>
2025-08-31 21:58:47 -0500

    #06 - python - add condition factorial function

--- Roadmap/06 - RECURSIVIDAD/python/jgutierrez9891.py
+++ Roadmap/06 - RECURSIVIDAD/python/jgutierrez9891.py
@@ -21,8 +21,9 @@ Extra
 def factorial(x):
     """This is a recursive function
     to find the factorial of an integer"""
-
-    if x == 1:
+    if x <= 0:
+       print("Number must be positive")
+    elif x == 1:
         return 1
     else:
         return (x * factorial(x-1))
