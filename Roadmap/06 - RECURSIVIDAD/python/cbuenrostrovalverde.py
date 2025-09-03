240377c8163d7a7ad065514bf1eed7e5f18b6db3
cbuenrostrovalverde <cbuenrostrovalverde@gmail.com>
2025-09-03 12:02:42 +0200

    Update cbuenrostrovalverde.py

--- Roadmap/06 - RECURSIVIDAD/python/cbuenrostrovalverde.py
+++ Roadmap/06 - RECURSIVIDAD/python/cbuenrostrovalverde.py
@@ -9,3 +9,41 @@ EJERCICIO:
  * - Calcular el valor de un elemento concreto (según su posición) en la 
  *   sucesión de Fibonacci (la función recibe la posición).
 '''
+
+# Función de recursividad
+
+def recursividad(n):
+    if n < 0:
+        return
+    print(n)
+    
+    recursividad(n - 1)
+
+recursividad(100)
+
+
+# Cálculo del Factorial de un numero utilzando la recursividad. 
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+n = int(input("Introduzca el número: "))
+
+print(f'El factorial de {n} es: {factorial(n)}')
+
+# Cálculo sucesión de Fibonacci haciendo uso de la recursividad.
+
+def fibonacci(numero: int)-> int:
+    if numero < 0:
+        raise ValueError('número debe de ser un entero no negativo')
+    if numero == 0:
+        return 0
+    elif numero == 1:
+        return 1
+    else:
+        return fibonacci(numero - 1) + fibonacci(numero - 2)
+
+print(f'F(5): {fibonacci(5)}')
