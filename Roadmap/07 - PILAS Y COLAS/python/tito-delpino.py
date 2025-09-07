0b7880549739cec3509f6a056f2fb0985f9fd747
Alvaro Del Pino <alvarodelpino88@gmail.com>
2025-09-07 23:34:37 +0200

    faltan los extras

---
+++ Roadmap/07 - PILAS Y COLAS/python/tito-delpino.py
@@ -0,0 +1,36 @@
+
+#  * EJERCICIO:
+#  * Implementa los mecanismos de introducción y recuperación de elementos propios de las
+#  * pilas (stacks - LIFO) y las colas (queue - FIFO) utilizando una estructura de array
+#  * o lista (dependiendo de las posibilidades de tu lenguaje).
+
+# pilas
+lista = [1,2,3]
+print(lista)
+elemento = lista.pop() # la funcion pop extrae el ultimo elemento que entro en la pila
+print(lista)
+print(elemento)
+
+# colas
+lista = [1,2,3]
+print(lista)
+elemento = lista.pop(0) # la funcion pop indicando el indice que queremos eliminar de la cola
+print(lista)
+print(elemento)
+
+
+#  * DIFICULTAD EXTRA (opcional):
+#  * - Utilizando la implementación de pila y cadenas de texto, simula el mecanismo adelante/atrás
+#  *   de un navegador web. Crea un programa en el que puedas navegar a una página o indicarle
+#  *   que te quieres desplazar adelante o atrás, mostrando en cada caso el nombre de la web.
+#  *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
+#  *   el nombre de una nueva web.
+
+
+
+
+
+#  * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
+#  *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
+#  *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
+#  *   interpretan como nombres de documentos.
