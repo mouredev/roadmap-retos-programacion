3752b1bd393382335fda09a788b839be5988f462
Alvaro Del Pino <alvarodelpino88@gmail.com>
2025-09-08 10:54:18 +0200

    07 - Python

--- Roadmap/07 - PILAS Y COLAS/python/tito-delpino.py
+++ Roadmap/07 - PILAS Y COLAS/python/tito-delpino.py
@@ -26,11 +26,56 @@ print(elemento)
 #  *   Las palabras "adelante", "atrás" desencadenan esta acción, el resto se interpreta como
 #  *   el nombre de una nueva web.
 
+def navigation():
+    historial = []
 
+    while True:
 
+        accion = input("Agrega una URL o si quieres desplazarte adelante/atras/salir: ")
+        if accion == 'salir':
+            print("Saliendo del programa")
+            break
+        elif accion == 'adelante':
+            pass
+        elif accion == "atras":
+            if len(historial) > 0:
+                historial.pop()
+        else:
+            historial.append(accion)
+
+        if len(historial) > 0:
+            print(f'Pagina actual {historial[len(historial)-1]}')
+        else:
+            print('Estas en la pagina de inicio')
+        
+
+navigation()
 
 
 #  * - Utilizando la implementación de cola y cadenas de texto, simula el mecanismo de una
 #  *   impresora compartida que recibe documentos y los imprime cuando así se le indica.
 #  *   La palabra "imprimir" imprime un elemento de la cola, el resto de palabras se
 #  *   interpretan como nombres de documentos.
+
+def impresora():
+    cola_impresion = []
+    while True:
+        instruccion = input("Agrega un documento a la cola de impresion, o indica si quieres imprimir o salir: ")
+
+        if instruccion == 'imprimir':
+            if len(cola_impresion) >= 1:
+                print(f'imprimiendo documento: {cola_impresion[0]}')
+                cola_impresion.pop(0)
+            else:
+                print('no quedan documentos en cola')
+        elif instruccion == 'salir':
+            break
+        else:
+            cola_impresion.append(instruccion)
+
+        if len(cola_impresion) >= 1:
+            print('Documentos en cola')
+            for doc in cola_impresion:
+                print(doc)
+
+impresora()
