5f084b87aa9b41a591637aadc1580a3ca3dbedb7
CristianPareja <cppareja@puce.edu.ec>
2025-09-09 11:32:48 -0500

    #00-Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/CristianPareja.py
@@ -0,0 +1,21 @@
+# Esto es un comentario 
+#https://www.python.org/
+#primera forma de comentario
+""" segunda forma de comentario 
+continuacion de la segunda forma de comentario"""
+
+''' tercera forma de comentario 
+continuacion de la tercera forma de comentario'''
+
+variable_cristian = "Cristian Pareja"
+variable_python = "Python"
+variable_nombre = "Cualquier nombre"
+MI_CONSTAN = "Valor constante"
+
+variable_cadena_de_texto = "Hola Mundo"
+variable_entero = 5
+variable_float = 5.5
+variable_booleano = True
+
+print("Hola"+" "+variable_python)
+print(variable_entero)
