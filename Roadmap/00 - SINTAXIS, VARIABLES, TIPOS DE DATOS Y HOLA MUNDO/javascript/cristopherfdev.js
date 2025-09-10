aee4e9143fe1b677092c14eff0d23cc1321be3ab
Cristopher Fernandez <cristopherfernandezj@gmail.com>
2025-09-09 21:06:43 -0400

    Ejercicio 00

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/cristopherfdev.js
@@ -0,0 +1,24 @@
+
+ //javascript.com
+ 
+ //Comentario en una línea
+
+ /*Este es 
+ un comentario 
+ en varias líneas*/
+
+ let primeraVariable = "Mi primera variable"; 
+ const primeraConst = "Mi primera constante"; 
+ 
+let mystring = "Mi primer String"
+let mynumber = 90;
+let myboolean = true; 
+let mybooleanf = false; 
+let datanull = null; 
+let bignumber = 342119484746339; 
+let undefinedvariable; 
+ 
+//Imprimir texto 
+console.log ("Hola, JavaScript!"); 
+ 
+ 
