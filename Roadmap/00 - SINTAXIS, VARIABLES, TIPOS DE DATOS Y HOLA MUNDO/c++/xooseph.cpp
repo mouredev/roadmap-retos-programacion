1474c8e4c8d62c402607b4be08c1bbcb8fdc30e8
xooseph <josephlsalgado@outlook.com>
2025-09-22 12:20:46 -0600

    add: Exercise #00 - C++

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c++/xooseph.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+/* Como tal no es la documentación oficial de C++: 
+    https://learn.microsoft.com/es-es/cpp/cpp/?view=msvc-170
+
+    También se tomará como referencia el libro de "Cómo programar C++, novena edición de
+    Deitel"
+*/
+
+// Este es un comentario de una sola línea
+
+/* Este es comentario
+    de varias líneas.
+*/ 
+int main() {
+    // Variable
+    int randomNumber = 45;
+
+    // Constante
+    const double PI = 3.1415;
+
+    // Tipos de datos primitivos
+    int number = 340;
+    float anotherNumber = 23.4;
+    double doubleNumber = 35.4;
+    char character = 'a';
+    bool boolean = false;
+    string text = "Hello World!";
+
+    cout << "Hola, C++!";
+
+    return 0;
+}
