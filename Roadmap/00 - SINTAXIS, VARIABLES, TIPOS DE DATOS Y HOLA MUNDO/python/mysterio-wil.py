7fb77258bc483a97bf8be618ae16f02cccfd7a9a
Wilmer Gulcochia <karlwgs1989@gmail.com>
2025-09-28 07:56:15 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mysterio-wil.py
@@ -0,0 +1,35 @@
+# EJERCICIO:
+# - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+#   https://www.python.org
+
+# - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+# Esto es un comentario de una línea
+
+"""
+Esto es un comentario
+de varias líneas
+"""
+
+'''
+Esto también es
+un comentario de
+varias líneas
+'''
+
+# - Crea una variable (y una constante si el lenguaje lo soporta).
+my_variable = "Mi variable"
+# Python no tiene constantes, pero por convención se usan mayúsculas
+MY_CONSTANT = "Mi constante" 
+
+# - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+my_string = "Cadena de texto"
+my_int = 10
+my_float = 10.5
+my_bool_true = True
+my_bool_false = False
+my_none = None
+my_complex = 1 + 2j
+
+# - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, Python!")
