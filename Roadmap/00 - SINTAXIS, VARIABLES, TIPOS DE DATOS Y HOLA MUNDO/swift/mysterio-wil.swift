36522e3b0adde0850a50dadd30030e5bb088be59
Wilmer Gulcochia <karlwgs1989@gmail.com>
2025-09-28 07:58:01 -0500

    #00 - Swift

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/swift/mysterio-wil.swift
@@ -0,0 +1,29 @@
+// EJERCICIO:
+// - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+//   https://www.swift.org/
+
+import Foundation
+
+// - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// Esto es un comentario de una línea
+
+/*
+Esto es un comentario
+de varias líneas
+*/
+
+// - Crea una variable (y una constante si el lenguaje lo soporta).
+var myVariable = "Mi variable"
+let myConstant = "Mi constante"
+
+// - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+let myString: String = "Cadena de texto"
+let myInt: Int = 10
+let myFloat: Float = 10.5
+let myDouble: Double = 10.555
+let myBool: Bool = true
+let myCharacter: Character = "a"
+
+// - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, Swift!")
