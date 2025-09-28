2ca45a8ff6d396b113e613f8d622c819483f8b82
Wilmer Gulcochia <karlwgs1989@gmail.com>
2025-09-28 07:58:11 -0500

    #00 - TypeScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/typescript/mysterio-wil.ts
@@ -0,0 +1,29 @@
+// EJERCICIO:
+// - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+//   https://www.typescriptlang.org/
+
+// - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+// Esto es un comentario de una línea
+
+/*
+Esto es un comentario
+de varias líneas
+*/
+
+// - Crea una variable (y una constante si el lenguaje lo soporta).
+let myVariable: string = "Mi variable";
+const MY_CONSTANT: string = "Mi constante";
+
+// - Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+let myString: string = "Cadena de texto";
+let myNumber: number = 10;
+let myBoolean: boolean = true;
+let myNull: null = null;
+let myUndefined: undefined = undefined;
+let mySymbol: symbol = Symbol("mi simbolo");
+let myBigInt: bigint = 9007199254740991n;
+
+
+// - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("¡Hola, TypeScript!");
