495758ea1015de17296e8111668aec0b22878864
SirEduardOo <najeraeduardo414@gmail.com>
2025-09-29 09:45:28 -0600

    Create SirEduardo.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/SirEduardo.py
@@ -0,0 +1,38 @@
+"""* EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ """
+ 
+# URL de la pagina oficial de Pyhton https://python.org/
+
+#Comentarios de una sola linea
+"""
+Comentarios
+de
+multiples
+lineas
+"""
+
+#Esta es la formula para declarar variales
+#nombre de la variables + = + contenido de la variable
+name = "Eduardo"
+#En python no hace falta declarar el tipo de dato de la variable
+
+#En python no existen las constantes pero se puden declarar asi por convencion
+CONSTANT = "Esto es una constante"
+
+#Variables representanto cada tipo de datos PRIMITIVOS: Enteros (INT), Decimales o flotantes (floot), Booleanos (boolean) y Cadenas de texto 
+#(String)
+
+int = 23
+float = 170.50
+bool = True
+string = "Eduardo"
+
+print("Hola, Python!")
