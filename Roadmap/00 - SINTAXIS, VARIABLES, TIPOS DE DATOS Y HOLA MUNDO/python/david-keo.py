01deeb784c9aa32bfd2e6ab9d1547f4f73cf9adf
Carlos Guerrero <guerrerodavid42069@gmail.com>
2025-10-19 15:36:29 -0600

    00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/david-keo.py
@@ -0,0 +1,52 @@
+""" /*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */ """
+
+# https://www.python.org/
+
+# Este es un comentario en solo una linea
+
+"""
+Este es un comentario
+En varias líneas
+"""
+
+'''
+Este es un comentario
+En varias líneas también
+'''
+
+my_variable = "variable"
+my_variable = "Nuevo valor de mi variable"
+
+CONSTANTE = "Mi constante"  # Convención: las constantes se escriben en mayúsculas
+
+my_int = 1 #Numeros Enteros
+my_float = 1.5 #Numeros Decimales
+my_bool = True #Booleanos
+my_string = "Hola" #Cadenas de texto
+
+print("Hola Mundo, Python!")
+
+
+
