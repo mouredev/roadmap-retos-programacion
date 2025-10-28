555fd5d9b85a556d33a93f5e48d854c597ee0f1a
smelo0 <smelocv@gmail.com>
2025-10-28 16:14:43 -0500

    Create smelo0.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/smelo0.py
@@ -0,0 +1,31 @@
+""" * EJERCICIO:
+ *   
+ * 
+ *   
+ * 
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!
+ """
+
+# https://www.python.org/ - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+# primera forma de crear comentarios - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+"""
+Segunda forma de crear comentarios
+"""
+'''
+Tercer comentarazao
+'''
+variable = "queso" # - Crea una variable (y una constante si el lenguaje lo soporta).
+CONSTANTE = 77 # no hay constantes en python, pero se usan mayusculas para señalarlas.
+
+my_int = 1
+the_float = 1.5
+one_boolean = True 
+that_string = "la wea fome"
+
+Print("¡Hola, Python!)
+
+
