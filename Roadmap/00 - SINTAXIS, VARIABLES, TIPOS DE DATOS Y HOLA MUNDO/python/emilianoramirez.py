f5cea829db83272226a901e75fd214936d2bffa7
emilianoramirez-droid <brayantijerinramirez@gmail.com>
2025-10-31 02:30:33 -0600

    Create emilianoramirez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/emilianoramirez.py
@@ -0,0 +1,30 @@
+# https://www.python.org/
+
+# comentario en una linea
+"""
+comentario en varias lineas
+"""
+'''
+tambien es un comentario en varias lineas
+'''
+# crear variable
+
+my_variable = "variable" 
+my_variable= "otro valor a mi variable"
+
+#phthon no tiene constantes
+
+#datos primitivos
+my_int = 1
+my_float = 1.25
+my_bool = true
+my_bool = false 
+my_string = "cadena de texto"
+my_other_string= "mi otra cadena"
+
+prin("hola, phyton")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
