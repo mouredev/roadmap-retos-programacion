d634c90eb328191cd8f093d5918af8411da5b3c6
Kuro <joseignacioramirocastro22@gmail.com>
2025-11-07 12:27:44 +0100

    #00 Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Ramirofordev.py
@@ -0,0 +1,26 @@
+# Python: https://www.python.org/
+'''
+Esta es la forma de comentar en varias lineas
+'''
+
+# Y esta es la forma de comentar en una sola linea
+
+x = 5
+PI = 3.1416
+
+# Int
+y = 6
+
+# Float
+n = 1.5
+
+# String
+nombre = "Nacho"
+
+# Boolean
+estudiando = True
+
+# None
+nada = None
+
+print("Hola, Python!")
