6f33d82e17160a1680d21e81ea40ae99a52867d8
Marc0s-ar <marcos00dev@gmail.com>
2025-11-13 09:09:47 -0300

    #00 - java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Marc0s-ar.java
@@ -0,0 +1,34 @@
+public class marcos00 {
+
+    public static void main(String[] args) {
+        //Sitio oficial de java https://www.java.com/es/
+
+        //comentario en una sola linea
+
+        /*
+         * comentario 
+         * en 
+         * varias
+         * lineas
+         */
+
+         String string = "texto"; // this is a variable 
+         final int number = 1; // this is a constant
+
+
+         int n = 1;
+         byte b = 1;
+         String text = "asd";
+         boolean t = true;
+         char a = 'a';
+         long longNumber = 10000000;
+         float f = 2.4f;
+         short s = 1;
+         double d = 1.2;
+        
+
+         String lenguaje = "java";
+         System.out.println("!Hola, " + lenguaje + "¡" );
+
+    }
+}
