03dce6ca77a88a273e3f81e3f42a2b951d6b49ec
javier <203809377+Javi-kl@users.noreply.github.com>
2025-11-14 17:36:41 +0100

    Guardando progreso reto-33-python

--- Roadmap/33 - RESCATANDO A MICKEY/python/javi-kl.py
+++ Roadmap/33 - RESCATANDO A MICKEY/python/javi-kl.py
@@ -2,21 +2,24 @@ import numpy as np
 
 arr = np.array(
     [
-        [0, 0, 0, 0, 0, 0],
-        [2, 2, 2, 2, 2, 2],
+        [0, 0, 0, 1, 0, 0],
+        [2, 2, 0, 2, 0, 2],
         [0, 2, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0],
         [0, 0, 2, 0, 2, 0],
         [0, 0, 0, 0, 0, 3],
     ]
 )
-
-vacio = 0
-inicio = [0, 3]
-obstaculos = 2
-salida = 3
 print(""" ---- RESCATANDO A MICKEY ----""")
-"""NO ESTAS USANDO EL ARRAY PARA NADA MELON"""
+print(
+    """
+Leyenda
+libre = 0
+Mickey = 1
+obstaculos = 2
+salida = 3"""
+)
+# quizas leyenda para el movimiento
 
 
 class Mickey:
@@ -29,22 +32,19 @@ class Mickey:
     # movimientos
     def moverse_abajo(self):
         self.fila += 1
-        return f"\nActual\nFila: {self.fila}\nColumna: {self.columna}"
+        return "\nMickey se ha movido hacia abajo"
 
     def moverse_arriba(self):
         self.fila -= 1
-        return f"\nActual\nFila: {self.fila}\nColumna: {self.columna}"
+        return "\nMickey se ha movido hacia arriba"
 
     def moverse_izquierda(self):
         self.columna -= 1
-        return f"\nActual\nFila: {self.fila}\nColumna: {self.columna}"
+        return "\nMickey se ha movido hacia izquierda"
 
     def moverse_derecha(self):
         self.columna += 1
-        return f"\nActual\nFila: {self.fila}\nColumna: {self.columna}"
-
-    def __str__(self) -> str:
-        return f"\nInicio\nFila: {self.fila}\nColumna: {self.columna}"
+        return "\nMickey se ha movido hacia derecha"
 
 
 class Comprobador:
@@ -55,27 +55,51 @@ class Comprobador:
     def comprobar_salida(self, fila, columna):
         return arr[fila, columna] == 3
 
-    def comprobar_limite_superior(self, fila):
-        return fila == 0
 
-    def comprobar_limite_inferior(self, fila):
-        return fila == 2
-
-    def comprobar_limite_izquierdo(self, columna):
-        return columna == 0
-
-    def comprobar_limite_derecho(self, columna):
-        return columna == 2
-
-
-mickey = Mickey(inicio)  # Pasamos posición de inicio
+mickey = Mickey(posicion=[0, 3])  # Pasamos posición de inicio
 comprobador = Comprobador()
+posicion_anterior = 0
+posicion_actual = 1
+
+
+def intentar_movimiento(direccion, mickey):  # TODO
+    if (
+        direccion == "1"
+        and mickey.fila == 0
+        or comprobador.comprobar_obstaculo(mickey.fila + 1, mickey.columna)
+    ):
+        return False
+
+    elif (
+        direccion == "2"
+        and mickey.fila == 5
+        or comprobador.comprobar_obstaculo(mickey.fila - 1, mickey.columna)
+    ):
+        return False
+
+    elif (
+        direccion == "3"
+        and mickey.columna == 0
+        or comprobador.comprobar_obstaculo(mickey.fila, mickey.columna - 1)
+    ):
+        return False
+
+    elif (
+        direccion == "4"
+        and mickey.columna == 5
+        or comprobador.comprobar_obstaculo(mickey.fila, mickey.columna + 1)
+    ):
+        return False
+
+    else:
+        return True
 
 
 def menu_principal():
     print("\nMickey esta en el inicio del laberinto")
-    print(mickey)
     while True:
+        arr[mickey.fila, mickey.columna] = posicion_actual
+        print(f"\n{arr}\n")
 
         print("1 = abajo")
         print("2 = Arriba")
@@ -86,58 +110,50 @@ def menu_principal():
         opcion = input("Elige siguiente movimiento\n->")
         match opcion:
             case "1":
-                if comprobador.comprobar_limite_inferior(
-                    mickey.fila
-                ):  # Primero comprobar que no esta en el limite. OK
-                    print("Encontro el limite inferior, no puede moverse aqui")
-                elif comprobador.comprobar_obstaculo(
-                    mickey.fila + 1, mickey.columna
-                ):  # Simulamos el movimiento hacia abajo, añádiendo +1 a la fila igual que hace la funcion moverse abajo
-                    print("Encontró un obstaculo, no puede moverse aqui")
+                if not intentar_movimiento("1", mickey):  # TODO
+
+                    print("No puede moverse aqui")
 
                 else:
+                    arr[mickey.fila, mickey.columna] = posicion_anterior
                     print(mickey.moverse_abajo())
-                    print("\nMickey se ha movido hacia abajo")
 
                     if comprobador.comprobar_salida(mickey.fila, mickey.columna):
                         print("Enhorabuena Mickey encontró la salida")
                         break
 
             case "2":
-                if comprobador.comprobar_limite_superior(mickey.fila):
-                    print("Encontro el limite superior, no puede moverse aqui")
-                elif comprobador.comprobar_obstaculo(mickey.fila - 1, mickey.columna):
-                    print("Encontró un obstaculo, no puede moverse aqui")
+                if not intentar_movimiento("2", mickey):  # TODO
+
+                    print("No puede moverse aqui")
 
                 else:
+                    arr[mickey.fila, mickey.columna] = posicion_anterior
                     print(mickey.moverse_arriba())
-                    print("\nMickey se ha movido hacia arriba")
                     if comprobador.comprobar_salida(mickey.fila, mickey.columna):
                         print("Enhorabuena Mickey encontró la salida")
                         break
 
             case "3":
-                if comprobador.comprobar_limite_izquierdo(mickey.columna):
-                    print("Encontro el limite izquierdo, no puede moverse aqui")
-                elif comprobador.comprobar_obstaculo(mickey.fila, mickey.columna - 1):
-                    print("Encontró un obstaculo, no puede moverse aqui")
+                if not intentar_movimiento("3", mickey):  # TODO
+
+                    print("No puede moverse aqui")
 
                 else:
+                    arr[mickey.fila, mickey.columna] = posicion_anterior
                     print(mickey.moverse_izquierda())
-                    print("\nMickey se ha movido hacia izquierda")
                     if comprobador.comprobar_salida(mickey.fila, mickey.columna):
                         print("Enhorabuena Mickey encontró la salida")
                         break
 
             case "4":
-                if comprobador.comprobar_limite_derecho(mickey.columna):
-                    print("Encontro el limite derecho, no puede moverse aqui")
-                elif comprobador.comprobar_obstaculo(mickey.fila, mickey.columna + 1):
-                    print("Encontró un obstaculo, no puede moverse aqui")
+                if not intentar_movimiento("4", mickey):  # TODO
+
+                    print("No puede moverse aqui")
 
                 else:
+                    arr[mickey.fila, mickey.columna] = posicion_anterior
                     print(mickey.moverse_derecha())
-                    print("\nMickey se ha movido hacia derecha")
                     if comprobador.comprobar_salida(mickey.fila, mickey.columna):
                         print("Enhorabuena Mickey encontró la salida")
                         break
