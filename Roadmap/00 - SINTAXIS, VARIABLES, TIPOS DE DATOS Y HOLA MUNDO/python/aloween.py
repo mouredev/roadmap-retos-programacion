2b49d76987f197f155ba8893771bc910c4c540b9
aloween <alonsogz0710@gmail.com>
2025-11-23 16:21:30 +0100

    hola este es un commit de prueba

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/aloween.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/aloween.py
@@ -4,6 +4,7 @@
 comentario en varias lineas
 Hola
 Mundo 
+
 ''''
 mas lineas """
 
@@ -11,19 +12,19 @@ mas lineas """
 #crear variables
 # en python no existe el concepto de constantes, pero en la comunidad se representan declarando una variable en mayusculas'''
 
-x = "mi nombre es Alonso"
+mi_variable= "mi nombre es Alonso"
 
 MY_CONSTANT = "No cambiar el valor de una constante"
-# existen 4 tipos de datos primitivos
 
-int = 10 # int es un numero entero
-float = 2.5 # float es un numero decimal
-boolean = True # boolean es un valor logico (True o False)
-none = None # none es un valor nulo o ausencia de valor 
-string = "Hola Mundo" # string es una cadena de texto
-print("Hola " + x)
+# Existen 4 tipos de datos primitivos!!! 
+my_int: int  = 10 # int es un numero entero ( podemos usar type hints para indicar el tipo de dato)
+my_float: float = 2.5 # float es un numero decimal
+my_oolean = True # boolean es un valor logico (True o False)
+my_none = None # none es un valor nulo o ausencia de valor 
+my_string = "Hola Mundo" # string es una cadena de texto y podemos usar comillas simples o dobles.  
+print("Hola " + mi_variable)
 print("Hola " + "https://www.python.org/")
 
-print(type(int))
-print(type(x))
-print(type(float))
+print(type(my_int))
+print(type(mi_variable))
+print(type(my_float))
