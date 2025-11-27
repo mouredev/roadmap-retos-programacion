a052991a2e7a407fa79bea26716afc6eeb544554
javier <203809377+Javi-kl@users.noreply.github.com>
2025-11-27 19:17:31 +0100

    #35 - python terminado

--- Roadmap/35 - REPARTIENDO LOS ANILLOS DE PODER/python/javi-kl.py
+++ Roadmap/35 - REPARTIENDO LOS ANILLOS DE PODER/python/javi-kl.py
@@ -7,36 +7,39 @@ class Distribuidor:
         self.enanos = 0
 
     def __str__(self) -> str:
-        return f"Sauron {self.sauron}\nHombres {self.hombres}\nEnanos: {self.enanos}\nELfos {self.elfos}"
+        return f"Sauron {self.sauron}\nHombres {self.hombres}\nEnanos: {self.enanos}\nElfos {self.elfos}"
 
     def comprobacion(self):
+        return (
+            sum(([self.sauron, self.hombres, self.enanos, self.elfos]))
+            == self.total_anillos
+        )
 
-        return sum([self.sauron, self.elfos])
-
-    # Deben recibir número par
-    def calcular_hombres(self):
-        for num in range(self.total_anillos):
-            if num % 2 == 0:
-                self.hombres = num
-
-    # Deben recibir número impar
-    def calcular_elfos(self):
-        for num in range(self.total_anillos):
-            if num % 2 != 0:
-                self.elfos = num
-
-    # Deben recibir número primo
-    def calcular_enanos(self):
-        for num in range(self.total_anillos):
-            if num > 1 and num % 1 == 0 and num % num == 0:
-                self.enanos = num
-
-    # Sauron Debe recibir 1
+    def distribuir(self):
+        for i in range(2, self.total_anillos + 1, 2):  # hombres
+            for j in range(1, self.total_anillos + 1, 2):  # elfos
+                for k in range(2, self.total_anillos + 1):  # enanos
+                    if es_primo(k):
+                        self.hombres = i
+                        self.elfos = j
+                        self.enanos = k
+                        if self.comprobacion():
+                            return True
+        return False
 
 
-distribuidor = Distribuidor(11)
-distribuidor.calcular_enanos()
-distribuidor.calcular_hombres()
-distribuidor.calcular_elfos()
-print(distribuidor.comprobacion())
-print(distribuidor)
+def es_primo(num):
+    if num < 2:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+distribuidor = Distribuidor(7)
+if distribuidor.distribuir():
+    print("Distribución con exito: ")
+    print(distribuidor)
+else:
+    print("No se encuentra una solución")
