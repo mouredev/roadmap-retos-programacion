76c5a9d29d8679671b884859d9adc24049a42d34
PaoloF16 <paolo.cafis@gmail.com>
2025-11-29 00:41:29 +0100

    Esta es mi solucion antes de la resolucion

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/PaoloF19.py
@@ -0,0 +1,46 @@
+# Desarrollo del Ejercicio
+#https://www.python.org/
+
+# Para comentario de una sola linea se usa el "#"
+
+
+"""Las triples comillas simples se usan para
+escribir texto em multiples lineas """
+
+
+''' 
+Esta tambien es una linea de texto 
+en multilinea solo con comillas simples 
+'''
+
+#Variables 
+#¿Cómo nombrar variables?
+"""
+Las variables en Python pueden ser nombradas de la siguiente manera:
+1. No pueden comenzar con números
+2. No pueden contener espacios en blanco
+3. No pueden contener caracteres especiales
+4. No pueden ser iguales a palabras reservadas de Python
+"""
+#Por convención se recomienda utiizar snake case para nombrar variables. Esta utiliza guión bajo para separar palabras.
+#Además se recomienda que el nombre sea fácil de comprender:
+#Ejemplo de variable con snake case
+my_user = "Paolo"
+MY_CONSTANT = "12"
+
+
+'''
+Python admite diferentes tipos de variables sean numericas o textuales dentro de tipo de datos numericos tenemos a enteros (int) flotante
+(float) o complejos (complex) que se pueden combinar mediante operadores, una parte es real y una parte es imaginaria
+'''
+# Ejemplos de variables numericos 
+ # Enteros o int 
+my_int = 3
+
+ # Flotantes o float
+my_float = 3,5
+ # Complejos o complex
+my_complex = 3 + 5j
+
+
+print("Hola Python")
