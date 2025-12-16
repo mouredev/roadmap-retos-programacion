2dd8cc8a1fff88b646b9116c2d31ee47d4917425
IvanCalero04 <anleroeno@proton.me>
2025-12-16 22:18:35 +0100

    Add IvanCalero.cs with variable declarations and comments

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/IvanCalero.cs
@@ -0,0 +1,24 @@
+// https://dotnet.microsoft.com/en-us/languages/csharp
+
+// Esto es un comentario de una linea.
+/*
+Y Esto 
+es un comentario 
+en varias lineas
+*/
+
+
+string myString = "Iván"; // Variable
+const string = "Tengo 21 años."; // Constante
+
+// Tipos de variables:
+
+int myInt = 21;
+string myString = "C#";
+float myFloat = 2.3f;
+double myDouble = 2.3;
+bool myBool = true;
+var myVar = "Variable de tipo inferido";
+dynamic myDynamic = 6;
+
+Console.WriteLine($"¡Hola, {myString}!");
