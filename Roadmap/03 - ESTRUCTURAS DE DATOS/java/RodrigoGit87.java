9b9bbaee0dda7970d57f285910f917d1acbbf807
RodrigoGit87 <rodrigollamas87@gmail.com>
2025-12-31 20:39:50 +0100

    #03-JAVA

--- Roadmap/03 - ESTRUCTURAS DE DATOS/java/RodrigoGit87.java
+++ Roadmap/03 - ESTRUCTURAS DE DATOS/java/RodrigoGit87.java
@@ -1,15 +1,3 @@
-/*
- * DIFICULTAD EXTRA (opcional):
- * Crea una agenda de contactos por terminal.
- * - Debes implementar funcionalidades de búsqueda, inserción, actualización y eliminación de contactos.
- * - Cada contacto debe tener un nombre y un número de teléfono.
- * - El programa solicita en primer lugar cuál es la operación que se quiere realizar, y a continuación
- *   los datos necesarios para llevarla a cabo.
- * - El programa no puede dejar introducir números de teléfono no numéricos y con más de 11 dígitos.
- *   (o el número de dígitos que quieras)
- * - También se debe proponer una operación de finalización del programa.
- */
-
 import java.util.*;
 
 public class RodrigoGit87 {
