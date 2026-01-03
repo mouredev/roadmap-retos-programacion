b4bd77ba875f4402bec04ce1602850c8eccb7b3f
Fran <franbaviano@gmail.com>
2026-01-03 12:18:01 +0100

    Ejercicio 00 en Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/FranDev200.java
@@ -0,0 +1,41 @@
+public class FranDev200 {
+
+    static void main() {
+
+        // - Crea un comentario en el código y coloca la URL del sitio web oficial del
+        // lenguaje de programación que has seleccionado.
+
+        /*
+        - PÁGINA WEB OFICIAL DE JAVA:
+          https://www.java.com/es/
+         */
+
+        // - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+        // Comentario en una linea
+        /*
+            Comentario
+            en
+            varias lineas.
+         */
+
+        // - Crea una variable (y una constante si el lenguaje lo soporta).
+        int variable1;
+        final String CONSTANTE;
+
+        // - Crea variables representando todos los tipos de datos primitivos
+        // del lenguaje (cadenas de texto, enteros, booleanos...).
+
+        int entero;
+        double decimal;
+        boolean bool;
+        char caracter;
+        String cadenaCaracteres;
+
+        // - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+        System.out.println("¡Hola, Java!");
+
+    }
+
+}
