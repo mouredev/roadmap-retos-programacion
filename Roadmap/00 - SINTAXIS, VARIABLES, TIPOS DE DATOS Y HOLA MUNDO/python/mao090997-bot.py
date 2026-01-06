7969adb4891636d84b0a50abcdb1fbafa2d37060
Oscar <mao090997@gmail.com>
2026-01-05 19:04:08 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mao090997-bot.py
@@ -0,0 +1,36 @@
+#https://www.python.org/
+
+#comentario en una linea
+
+"""
+Esto tambien es 
+un comentario en 
+varias lineas
+
+"""
+
+'''
+Hola soy oscar,
+ mira que estamos aprendiendo
+
+'''
+
+oscar_variable = "la vida es bonita"
+
+MI_CONSTANTE = "En python se puede en mayuscula indicar que es constante"
+
+#Con type te dirá que tipo de dato es cada uno. 
+print(type(25))
+print(type(4.5))
+print(type(None))
+print(type(True))
+print(type(4j))
+print(type(False))
+print(type("Hola mundo"))
+print(type([]))
+print(type(()))
+print(type({}))
+
+print("!Hola Python")
+      
+
