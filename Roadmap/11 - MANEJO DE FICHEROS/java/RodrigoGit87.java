0fc15772f6f19055c9d5d404fbb86a9c832edcaa
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-01-28 16:02:21 +0100

    Añado case 2 y 5

--- Roadmap/11 - MANEJO DE FICHEROS/java/RodrigoGit87.java
+++ Roadmap/11 - MANEJO DE FICHEROS/java/RodrigoGit87.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 
 // ------- EXTRA -------
 public class RodrigoGit87 {
-    void main(String[] args) {
+    public static void main(String[] args) {
         String seller = "RodrigoGit87";
         File user = new File(seller);
 
@@ -22,9 +22,9 @@ public class RodrigoGit87 {
 
         while (opcion != 6) {
             System.out.println("\n--- GESTIÓN DE VENTAS ---");
-            System.out.println("1 - Añadir producto");
-            System.out.println("2 - Consultar producto");
-            System.out.println("3 - Actualizar producto");
+            System.out.println("1 - Añadir productos");
+            System.out.println("2 - Consultar productos");
+            System.out.println("3 - Actualizar productos");
             System.out.println("4 - Eliminar productos");
             System.out.println("5 - Calcular venta total");
             System.out.println("6 - SALIR ");
@@ -44,10 +44,10 @@ public class RodrigoGit87 {
                     sc.nextLine(); // depurar buffer
 
                     try (FileWriter writer = new FileWriter(user, true)) { // necesitamos que el programa sea capaz de
-                                                                           // "añadir"nuevos
+                                                                           // "añadir" nuevos
                         // productos sin borrar los anteriores. La clase FileWriter tiene un constructor
                         // especial para esto:
-                        // new FileWriter(instanciaDeFile, true);
+                        // new FileWriter(instanciaDeFile, true); <- en false, sobreescribiría.
                         writer.write(name + ", " + amount + ", " + value + "\n");
                         System.out.println(" datos añadidos");
 
@@ -55,8 +55,51 @@ public class RodrigoGit87 {
                         System.err.println(" error ocurrido: " + e);
                     }
                     break;
+                case 2:
+                    System.out.println("\n Consulta de productos ");
+                    // Usamos try-with-resources para que el Scanner se cierre solo
+                    try (Scanner lector = new Scanner(user)) { // Scanner(user) leerá el objeto instanciado user. de la
+                                                               // clase File
+                        while (lector.hasNextLine()) {
+                            String linea = lector.nextLine();
+                            System.out.println(linea);
+                        }
+                    } catch (IOException e) {
+                        System.out.println("No se pudo leer el archivo o aún no existe.");
+                    }
+                    break;
 
-                // TODO cases 2,3,4,5,6
+                case 5:
+                    double totalGeneral = 0;
+                    System.out.println(" Cálculo de Ventas ");
+                    try (Scanner filelector = new Scanner(user)) {
+                        if (!user.exists()) {
+                            System.out.println("No hay ventas regsitradas");
+                            break;
+                        }
+                        while (filelector.hasNextLine()) {
+                            String line = filelector.nextLine();
+                            String[] parts = line.split(", ");
+
+                            if (parts.length == 3) {
+                                String nombre = parts[0];
+                                int cantidad = Integer.parseInt(parts[1]);
+                                double precio = Double.parseDouble(parts[2]);
+
+                                double subtotal = (cantidad * precio);
+                                totalGeneral += subtotal;
+
+                                System.out.println("Producto: " + nombre + " / Subtotal: " + subtotal);
+                            }
+                            System.out.println("----------------------------");
+                            System.out.println("VENTA TOTAL GLOBAL: " + totalGeneral);
+                        }
+                    } catch (IOException e) {
+                        System.err.println(" error ocurrido: " + e.getMessage());
+                    }
+                    break;
+
+                    // TODO cases 3,4,6
                 case 6:
                     // Lógica para borrar archivo y salir
                     System.out.println("Saliendo...");
