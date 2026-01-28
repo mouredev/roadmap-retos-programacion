57663323aa927c1b56e201b7ab6bdab454f36634
RodrigoGit87 <rodrigollamas87@gmail.com>
2026-01-28 20:13:16 +0100

    Añado ultimos cases ( 3 y 4)

--- Roadmap/11 - MANEJO DE FICHEROS/java/RodrigoGit87.java
+++ Roadmap/11 - MANEJO DE FICHEROS/java/RodrigoGit87.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /* --- CONCEPTOS BÁSICOS EN MANEJO DE FICHEROS ---
@@ -16,15 +17,18 @@ public class RodrigoGit87 {
         String seller = "RodrigoGit87";
         File user = new File(seller);
 
-        // SEleccion de opciones del menu
+        // -----SEleccion de opciones del menu --------
         Scanner sc = new Scanner(System.in);
         int opcion = 0;
 
-        while (opcion != 6) {
+        while (opcion != 6) { // El programa no termina hasta pulsar el 6
             System.out.println("\n--- GESTIÓN DE VENTAS ---");
             System.out.println("1 - Añadir productos");
             System.out.println("2 - Consultar productos");
-            System.out.println("3 - Actualizar productos");
+            System.out.println("3 - Actualizar productos"); // Nota: Check case 3 y 4; podrian mejorar la logica, para
+                                                            // establecer todos los productos a .toLowerCase y luego
+                                                            // .contains para que el usuario no tenga q poner el nombre
+                                                            // exacto.
             System.out.println("4 - Eliminar productos");
             System.out.println("5 - Calcular venta total");
             System.out.println("6 - SALIR ");
@@ -33,13 +37,14 @@ public class RodrigoGit87 {
             opcion = sc.nextInt();
             sc.nextLine(); // Limpiar el buffer después de leer un número
 
+            // -------- MENU ---------------
             switch (opcion) {
                 case 1: // lógica de añadir -> nombre_producto, cantidad_prods_vendida, precio
                     System.out.println("Nombre del producto: ");
                     String name = sc.nextLine();
                     System.out.println("Cantidad de productos vendida: ");
                     int amount = sc.nextInt();
-                    System.out.println("Precio de venta: ");
+                    System.out.println("Precio de venta(unitario): ");
                     double value = sc.nextDouble();
                     sc.nextLine(); // depurar buffer
 
@@ -47,7 +52,7 @@ public class RodrigoGit87 {
                                                                            // "añadir" nuevos
                         // productos sin borrar los anteriores. La clase FileWriter tiene un constructor
                         // especial para esto:
-                        // new FileWriter(instanciaDeFile, true); <- en false, sobreescribiría.
+                        // new FileWriter(instanciaDeFile, true); <- en false, elimina y sobreescribe.
                         writer.write(name + ", " + amount + ", " + value + "\n");
                         System.out.println(" datos añadidos");
 
@@ -67,6 +72,90 @@ public class RodrigoGit87 {
                     } catch (IOException e) {
                         System.out.println("No se pudo leer el archivo o aún no existe.");
                     }
+                    break;
+
+                // Para los cases 3 y 4, necesitamos una forma de pasar el archivo a una
+                // lista. Usaremos ArrayList<String>
+                case 3:
+                    System.out.print("Nombre del producto a actualizar: ");
+                    String productoAEditar = sc.nextLine();
+                    ArrayList<String> listaEdicion = new ArrayList<>();
+                    boolean is_Editado = false;
+
+                    try (Scanner lectorEdicion = new Scanner(user)) {
+                        while (lectorEdicion.hasNextLine()) {
+                            String linea = lectorEdicion.nextLine();
+                            String[] datos = linea.split(", ");
+
+                            if (datos[0].equalsIgnoreCase(productoAEditar)) {
+                                System.out.print("Nueva cantidad: ");
+                                int nCant = sc.nextInt();
+                                System.out.print("Nuevo precio: ");
+                                double nPrec = sc.nextDouble();
+                                sc.nextLine(); // Limpiar buffer
+
+                                // Añadimos la línea con los datos nuevos
+                                listaEdicion.add(datos[0] + ", " + nCant + ", " + nPrec);
+                                is_Editado = true;
+                            } else
+                                listaEdicion.add(linea); // Mantenemos la línea original
+                        }
+                    } catch (IOException e) {
+                        System.err.println(" error : " + e.getMessage());
+                    }
+
+                    // Guardar cambios (Sobrescritura)
+                    if (is_Editado) {
+                        try (FileWriter fw = new FileWriter(user, false)) { // Al ponerlo en false (o no poner nada), el
+                                                                            // archivo se vacía por completo antes de
+                                                                            // escribir las líneas de nuestra lista.
+                            for (String l : listaEdicion) // for each elemento en el arrayList, escribir elemento +
+                                                          // salto de linea
+                                fw.write(l + "\n");
+                            System.out.println(" Producto actualizado.");
+                        } catch (IOException e) {
+                            System.err.println("error al actualizar: " + e.getMessage());
+                        }
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Nombre del producto a eliminar: ");
+                    String productoAEliminar = sc.nextLine();
+                    ArrayList<String> todasLasLineas = new ArrayList<>();
+                    boolean is_productoEncontrado = false;
+                    // Leer user y añadir a un arrayList lo que no queremos borrar
+                    try (Scanner scan = new Scanner(user)) {
+                        while (scan.hasNextLine()) {
+                            String linea = scan.nextLine();
+                            if (!linea.split(", ")[0].equalsIgnoreCase(productoAEliminar)) { // ! <-- por que solo
+                                                                                             // queremos añadir al array
+                                                                                             // lo que no coincide con
+                                                                                             // productoAEliminar
+                                todasLasLineas.add(linea);
+                            } else
+                                is_productoEncontrado = true;
+
+                        }
+                    } catch (IOException e) {
+                        System.err.println(" Error al leer: " + e.getMessage());
+                    }
+                    // Ya tenemos 'salvaguardado' en el arrayList lo que no queremos borrar (lo que
+                    // sí, está en String productoAEliminar )
+                    // Ahora, sobreescribir el user con el arrayList
+
+                    if (is_productoEncontrado) {
+                        try (FileWriter fw = new FileWriter(user, false)) { // false para eliminar datos y escribir
+                                                                            // desde 0
+                            for (String l : todasLasLineas) {
+                                fw.write(l + "\n");
+                            }
+                            System.out.println(" Producto eliminado.");
+                        } catch (IOException e) {
+                            System.out.println(" Error al guardar cambios.");
+                        }
+                    } else
+                        System.out.println(" Producto no encontrado.");
                     break;
 
                 case 5:
@@ -99,10 +188,9 @@ public class RodrigoGit87 {
                     }
                     break;
 
-                    // TODO cases 3,4,6
                 case 6:
                     // Lógica para borrar archivo y salir
-                    System.out.println("Saliendo...");
+                    System.out.println("Archivo, " + user + " eliminado. Hasta la vista, baby !");
                     break;
                 default:
                     System.out.println("Opción no válida.");
