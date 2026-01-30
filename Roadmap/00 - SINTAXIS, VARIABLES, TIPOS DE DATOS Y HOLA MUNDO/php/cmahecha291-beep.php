ab247e9be9181ae05e9b26ba70bd9f573b21be74
Tomás García <cmahecha291@gmail.com>
2026-01-30 07:45:58 -0500

    Primera vez aca, aprendiendo y intentando aportar a esta comunidad.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/cmahecha291-beep.php
@@ -0,0 +1,14 @@
+<?php
+    //WEB OFICIAL PHP
+        // https://www.php.net/manual/es/index.php
+    //TIPOS DE COMENTARIOS EN PHP :
+        // Comentario de una sola linea " // " es utilizando dos barras o slash.
+        # Comentario de una sola linea " # " este se genera utilisando un numeral.
+        /* 
+            Comentario multilinea " / * * / " se genera utilizando una barra al inicio con un asterisco y se cierra 
+            al final con un asterisco y una barra, y sirve para hacer que todo el texto o codigo que este dentro de 
+            dichos simbolos no se tomara en cuenta a la hora de ejecutar el codigo.
+        */
+
+ 
+?>
