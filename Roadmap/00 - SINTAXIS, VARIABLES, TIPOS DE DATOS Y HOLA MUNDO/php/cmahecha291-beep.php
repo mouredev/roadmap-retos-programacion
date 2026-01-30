e2ec476cada3aef533845bdf76b80c63ae06f938
Tomás García <cmahecha291@gmail.com>
2026-01-30 08:19:00 -0500

    #37 PHP

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/cmahecha291-beep.php
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/cmahecha291-beep.php
@@ -1,6 +1,6 @@
 <?php
     //WEB OFICIAL PHP
-        // https://www.php.net/manual/es/index.php
+        // https://www.php.net
     //TIPOS DE COMENTARIOS EN PHP :
         // Comentario de una sola linea " // " es utilizando dos barras o slash.
         # Comentario de una sola linea " # " este se genera utilisando un numeral.
@@ -9,6 +9,15 @@
             al final con un asterisco y una barra, y sirve para hacer que todo el texto o codigo que este dentro de 
             dichos simbolos no se tomara en cuenta a la hora de ejecutar el codigo.
         */
+    //DECLARACION DE VARIABLES
+        /*
+            Para declara variables en el leguaje de programacion PHP se utiliza el simbolo "$" segudo del nombre de
+            la variable, luego en el signo " = " se le asigna un valor a dicha variable y por ultimo se finaliza 
+            con un punto y coma " ; ", ejemplo : $myfloat = 19,9; oh $name = value; , como php es un lenguje de tipado 
+            dinamico no es necesario asignar directamente el tipo de valor a ingresar, si es una cadena de texto seria 
+            " $mystring = "Christian"; " con comillas dobles para identificar que es una cadena de texto.
+        */
 
+    
  
 ?>
