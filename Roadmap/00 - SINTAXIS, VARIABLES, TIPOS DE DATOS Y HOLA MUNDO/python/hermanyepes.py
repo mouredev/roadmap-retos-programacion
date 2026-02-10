282fa1ed2b51c206e841eab54dca4dc093b1bacd
hermanyepes <hermanyepes@gmail.com>
2026-02-10 00:26:31 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/hermanyepes.py
@@ -0,0 +1,51 @@
+"""
+EJERCICIO:
+* Crea un comentario en el código y coloca la URL del sitio web oficial del
+  lenguaje de programación que has seleccionado.
+"""
+
+# La URL del sitio oficial de Python es: https://python.org/
+
+"""
+* Representa las diferentes sintaxis que existen de crear comentarios
+  en el lenguaje (en una línea, varias...).
+"""
+
+# Este es un comentario de una sola línea.
+
+"""
+Este es un comentario
+con saltos de línea.
+
+"""
+
+'''
+Este también es un comentario
+con saltos de línea.
+'''
+
+# Crea una variable (y una constante si el lenguaje lo soporta).
+
+my_variable = 10 # Asignamos el valor 10 a la variable.
+
+MY_CONSTANT = 3.14 # Las constantes se escriben en mayúsculas.
+
+"""
+* Crea variables representando todos los tipos de datos primitivos
+  del lenguaje (cadenas de texto, enteros, booleanos...).
+"""
+# Variable de tipo cadena de texto (string).
+my_string = "Hola, mundo!" 
+my_string_2 = '¡Hola, mundo 2!'
+# Variable de tipo entero (integer).
+my_int = 42 
+# Variable de tipo booleano (boolean).
+my_boolean = True
+# Variable de tipo flotante (float).
+my_float = 3.14 
+# Variable de tipo None (NoneType).
+my_null = None
+
+
+# * Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, Python🐍!")
