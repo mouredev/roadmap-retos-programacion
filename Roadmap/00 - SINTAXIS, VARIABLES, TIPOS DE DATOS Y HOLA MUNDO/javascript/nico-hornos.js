b1be2ab245f1a9897e614697e42cbcedc65a3809
Nicolás Hornos <104862764+nico-hornos@users.noreply.github.com>
2026-03-22 12:10:22 -0300

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/nico-hornos.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Sintaxis de comentario #1 (una línea)
+
+/* 
+Sintaxis
+de
+comentario
+#2 (varias líneas)
+*/
+
+let primerVariable = "Dato de mi primer variable.";
+const primerConstante = "Dato de mi primer constante. Su valor no puede cambiar.";
+
+let valorEntero = 22;
+let valorDecimal = 10.2;
+let valorBooleano = true;
+let valorCadenaDeTexto = "Hola mundo.";
+let valorVacio = "";
+let valorNulo = null;
+
+console.log("¡Hola, JavaScript!");
