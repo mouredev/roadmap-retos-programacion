f02cd8e880672d5f2b9cf49ee537fd9a8dcf336d
Kurogami <miguel.jose.diaz2007@gmail.com>
2026-04-13 19:06:35 -0400

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Kurogami2703.py
@@ -0,0 +1,27 @@
+#COmentari en una linea
+
+"""
+Comentario en varias lineas
+"""
+
+#URL del sitio web oficial de Python
+#https://www.python.org/
+
+#Crear una variable
+variable = "Esta es mi variable"
+
+#Crear una constante
+CONSTANTE = "Esta es mi constante"
+
+#Crear variables representando todos los tipos de datos primitivos
+variable_cadena = "cadena"
+variable_entero = 1
+variable_flotante = 1.0
+variable_booleano = True
+variable_lista = [1, 2, 3]
+variable_tupla = (1, 2, 3)
+variable_diccionario = {"clave": "valor"}
+variable_conjunto = {1, 2, 3}
+
+#Imprimir por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print("¡Hola, Python!")
