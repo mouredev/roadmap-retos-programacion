01f16b1d3b2afd764cd6a603e5b0b169b7248ed1
Maior <marianojeronimobautista@gmail.com>
2026-04-24 21:41:27 -0600

    #00 - Java

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Meir.java
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/Meir.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public class meir {
+    public static void main(String[] args) {
+       //https://www.java.com/en/
+        // one line
+        /* multi line
+        * comment
+        * */
 
-public class Meir {
+        var x = 10;
+        // "constant"
+        final int y = 20;
+
+        short z = 30;
+        int a = 40;
+        long b = 50;
+        float c = 60.0f;
+        double d = 70.0;
+        boolean e = true;
+        char f = 'a';
+
+        System.out.println("Hello, java");
+    }
 }
