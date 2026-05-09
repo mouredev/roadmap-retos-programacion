a53d2ad4f3938039052a7990ff336a956a309346
OptiAICol <optiai.com.co@gmail.com>
2026-05-08 22:06:00 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/OptiAICol.py
@@ -0,0 +1,33 @@
+# https://python.org
+
+# Comentario en una linea
+
+"""
+Esto tambien es 
+un comentario en varias lineas
+"""
+#Variables
+
+nombre_variable = "La variable numero uno"
+nombre_usuario = "OptiAICol"
+lenguaje_programacion = "Python"
+
+#Constante
+PAIS = "Colombia"
+
+# Variables representando todos los tipos de datos primitivos
+
+texto = "Hola Mundo"
+numero_entero = 42
+numero_decimal = 3.14
+booleano = True
+
+
+print("Hola, mi lenguaje de programacion es " + lenguaje_programacion + "!")
+
+print("Hola, mi nombre de usuario es " + nombre_usuario)
+
+print(type(texto))
+print(type(numero_entero))
+print(type(numero_decimal))
+print(type(booleano))
