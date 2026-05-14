2050b944480b5c9ed6319277c87036cd56285c73
mordev <david.pena@cabify.com>
2026-05-14 10:54:28 -0500

    feat: add basic arithmetic and comparison operator examples in python/mordev.py

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/mordev.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/mordev.py
@@ -8,10 +8,10 @@ Operadores
 print(f"suma: 10 + 9 = {10 + 9}") # suma 
 print(f"resta: 10 + 9 = {10 - 9}") # resta
 print(f"multiplicacion: 10 * 9 = {10 * 9}") # multiplicacion
-print(f"division: 10 / 9 = {10 / 9}\n") # division
+print(f"division: 10 / 9 = {10 / 9}") # division
 print(f"modulo: 10 % 9 = {10 % 9}") #es el valor que sobra al hacer una division
 print(f"exponente: 10 ** 9 = {10 ** 9}") # exponente elevar un numero a una potencia
-print(f"division entera: 10 // 9 = {10 // 9}\n") # division entera
+print(f"division entera: 10 // 9 = {10 // 9}") # division entera
 
 
 #operadores de comparacion
