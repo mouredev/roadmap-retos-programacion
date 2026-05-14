2177b4bff911249f4ea7b8fb422d5371cc667bf0
mordev <david.pena@cabify.com>
2026-05-14 10:52:01 -0500

    rapido

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/mordev.py
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/mordev.py
@@ -1,10 +1,18 @@
+
+
+
 """
 Operadores
 """
 
-print(f"suma: 10 + 9 = {10 + 9}")
-print(f"resta: 10 + 9 = {10 - 9}")
-print(f"multiplicacion: 10 * 9 = {10 * 9}")
-print(f"division: 10 / 9 = {10 / 9}")
-print(f"exponente: 10 ** 9 = {10 ** 9}")
-print(f"division entera: 10 // 9 = {10 // 9}")
+print(f"suma: 10 + 9 = {10 + 9}") # suma 
+print(f"resta: 10 + 9 = {10 - 9}") # resta
+print(f"multiplicacion: 10 * 9 = {10 * 9}") # multiplicacion
+print(f"division: 10 / 9 = {10 / 9}\n") # division
+print(f"modulo: 10 % 9 = {10 % 9}") #es el valor que sobra al hacer una division
+print(f"exponente: 10 ** 9 = {10 ** 9}") # exponente elevar un numero a una potencia
+print(f"division entera: 10 // 9 = {10 // 9}\n") # division entera
+
+
+#operadores de comparacion
+print(f"")
