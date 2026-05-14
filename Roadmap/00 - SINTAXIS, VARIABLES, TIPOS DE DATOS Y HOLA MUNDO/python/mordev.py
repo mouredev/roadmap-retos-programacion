e6d5505f58b194ddcbd5eb52edc5ff6a1c840758
mordev <david.pena@cabify.com>
2026-05-14 09:34:30 -0500

    feat: add python syntax, variables, and operator exercises to roadmap

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mordev.py
@@ -0,0 +1,32 @@
+#https//python.org
+
+#comentario en una linea
+
+
+"""
+esto tambien es un comentario en varias lineas
+"""
+
+'''
+esto tambien es un comentario en varias lineas
+'''
+
+
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi variable"
+
+MY_CONSTANT = "mi constante"
+
+my_int = 1
+my_float = 1.8
+my_bool = True
+my_bool = False
+my_string = "cadena de texto"
+my_other_string = "mi otra cadena de texto"
+
+print (f"var: {my_variable}\n")# un dato que va cambiando su valor
+print (f"int{my_int}\n")# dato entero
+print (f"bool:{my_bool}\n")# dato booleano true / false
+print (f"string:{my_string}\n")# dato de texto 
+
+print("HOLA,[PYTHON]")
---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/mordev.py
@@ -0,0 +1,10 @@
+"""
+Operadores
+"""
+
+print(f"suma: 10 + 9 = {10 + 9}")
+print(f"resta: 10 + 9 = {10 - 9}")
+print(f"multiplicacion: 10 * 9 = {10 * 9}")
+print(f"division: 10 / 9 = {10 / 9}")
+print(f"exponente: 10 ** 9 = {10 ** 9}")
+print(f"division entera: 10 // 9 = {10 // 9}")
