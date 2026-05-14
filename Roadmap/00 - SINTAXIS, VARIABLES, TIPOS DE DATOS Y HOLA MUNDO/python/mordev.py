ae3459636316198a36ca7752449ee8ca744a830d
mordev <david.pena@cabify.com>
2026-05-14 09:59:22 -0500

    primer reto de programacion variables y no existen constantes nativas en py pero con el nombre podemos declarar que es una constante py todas con variables

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mordev.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mordev.py
@@ -1,6 +1,31 @@
 #https//python.org
 
 #comentario en una linea
+"""
+/*
+ * ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+ * - Recuerda que todas las instrucciones de participación están en el
+ *   repositorio de GitHub.
+ *
+ * Lo primero... ¿Ya has elegido un lenguaje?
+ * - No todos son iguales, pero sus fundamentos suelen ser comunes.
+ * - Este primer reto te servirá para familiarizarte con la forma de participar
+ *   enviando tus propias soluciones.
+ *
+ * EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+ *
+ * ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+ * debemos comenzar por el principio.
+ */
+"""
 
 
 """
@@ -25,6 +50,7 @@ my_string = "cadena de texto"
 my_other_string = "mi otra cadena de texto"
 
 print (f"var: {my_variable}\n")# un dato que va cambiando su valor
+print (f"float:{my_float}\n")# dato decimal
 print (f"int{my_int}\n")# dato entero
 print (f"bool:{my_bool}\n")# dato booleano true / false
 print (f"string:{my_string}\n")# dato de texto 
