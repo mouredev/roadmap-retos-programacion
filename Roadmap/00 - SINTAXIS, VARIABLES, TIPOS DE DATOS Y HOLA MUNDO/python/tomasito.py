53a7a9938bb5f27a97adecfdacb87a86c668aea9
Tomasito <angel.luna@uttn.mx>
2026-05-26 14:16:23 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/tomasito.py
@@ -0,0 +1,36 @@
+# official python website :https://www.python.org/
+#This is a comment in a line of code
+'''
+This is a comment 
+in multiple lines 
+in python
+'''
+#...........
+# Variables and Constants
+#...........
+var = 10
+'''In python doesn't have a specific 
+sintaxix for declaring a constant
+but it has allow a option to declare a constant using uppercase letters'''
+CONST = 100
+
+#...........
+# DATA TYPES
+#...........
+# NUMBER
+number = 1
+#STRING
+text = "string"
+#FLOAT
+floaat = 1.50
+# CHARACTER
+char= '@'
+#BOLEAN
+boole = True
+# NONE
+none = None
+
+#...............
+# Message
+#...............
+print("Hello Python")
