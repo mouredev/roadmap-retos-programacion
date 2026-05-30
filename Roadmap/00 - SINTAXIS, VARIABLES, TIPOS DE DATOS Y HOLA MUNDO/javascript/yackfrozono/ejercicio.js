1a5051adf29cbef396014459b04ddc5b499501ef
yackfrozono <yackfrozono@gmail.com>
2026-05-30 19:53:26 -0400

    Create ejercicio.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/yackfrozono/ejercicio.js
@@ -0,0 +1,18 @@
+//pagina oficial de javascript: https://developer.mozilla.org/es/docs/Web/JavaScript
+/*pagina oficial de javascript: https://developer.mozilla.org/es/docs/Web/JavaScript*/
+
+let numero = 10;
+const cedula = 32370653;
+
+let numberInt = 10; //guarda un valor tipo int
+let numberFloat = 10.00; //guarda un valor tipo float
+let numberString = "anna"; //guarda un valor tipo string
+let variableBoolean = true; // guarda un valor Booleano entre VV o FF
+let variableNull = null; //variable sin algun valor
+let userName = `hola ${numberString} bienvenido`;
+let variableUndefined = undefined; //variable indefinida, se usa cuando no sabes que valor poner adentro de la variable
+let bigInt = 102010291919n; //variable usada para numeros enteros grandes
+let variableImmutable = Symbol("lol"); //variable para crear una variable unica e inmutable
+
+let language = "javascript";
+console.log(`¡hola, ${language}!`);
