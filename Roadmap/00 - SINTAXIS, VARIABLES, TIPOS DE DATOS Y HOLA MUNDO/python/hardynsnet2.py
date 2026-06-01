03cbd55d457a0fcd2a6a354da88b0eaaefff83f6
Harley <harleydenn2002@proton.me>
2026-06-01 00:01:33 -0500

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/hardynsnet2.py
@@ -0,0 +1,15 @@
+# https://www.python.org/
+
+# Sintaxis para crear comentarios
+# # <- Numeral (en una línea)
+# """ """ <- comillas triples (en varias líneas)
+
+nombre = "hardynsnet"
+
+# Tipos de datos
+entero = 5
+decimal = 10.9
+booleano = True
+texto = "Python"
+
+print("¡Hola, Python!")
