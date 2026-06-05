41805e0e1f8456b4e57639f32634772c3f1964e3
Emanuel <emanuelaristizabal7@gmail.com>
2026-06-05 18:15:42 -0500

    00-javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/aristizabal13.js
@@ -0,0 +1,45 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+
+// Comentario en una linea
+
+/*
+Comentario en
+varias
+lineas
+*/
+
+let myVariable = "Mi variable"
+
+const PI = 3.1416
+
+// Tipos de datos primitivos
+
+//String
+let str = "Hola Mundo" 
+let str2 = 'Hola Mundo' 
+
+//Number
+let entero = 17 
+let decimal = 1.71  
+
+//Boolean
+let isStudent = true  
+let isTeacher = false 
+
+//Null
+let emptyValue = null 
+
+//Undefined
+let undefinedValue 
+
+//BigInt
+let valueX = BigInt(913293012931903092390103193)
+let valueY = 913293012931903092390103193n
+
+//Symbol
+let sym1 = Symbol('sym1')
+
+
+console.log("Hola, Javascript!")
+console.log(sym1)
