1a99d063abd25ea549836d3ed4a401c5a61636b9
Balonike7 <eduard_balo@hotmail.com>
2026-07-24 13:55:51 -0600

    Solución al reto 00 en Python por Balonike7

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Balonike7.py
@@ -0,0 +1,28 @@
+# https://www.python.org/ 
+
+#comenntario de una sola linea 
+
+"""
+esto tambien es 
+un comentario 
+en varias lineas
+"""
+
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi variable"
+
+MY_CONSTANT = "mi constante" 
+
+my_int = 7
+my_float = 7.5
+my_bool = True
+my_bool = False
+my_string = "mi cadena de texto"
+my_other_string = 'mi otra cadena de texto'
+
+print("Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
