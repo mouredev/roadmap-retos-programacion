4a79e0f0b203da39b4867e863e2e732ccee72bc8
SOGitHubOS <95578677+SOGitHubOS@users.noreply.github.com>
2026-07-25 12:00:04 +0200

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/SOGitHubOS.py
@@ -0,0 +1,21 @@
+# https://www.python.org/
+# Esta es una forma de crear comentarios.
+'''
+Y esta es otra forma de crear comentarios en
+Python
+'''
+
+"""
+También podemos crear comentarios con 
+comillas dobles.
+"""
+
+a: str = ""  # Esto es una variable.
+B: str = "No es una constante"  # En Python si escribimos la variable en maúsculas damos a entender que es una constante.
+
+a = ""  # variable string o de cadena de caracteres.
+b = True  # variable booleana.
+c = False  # variable booleana.
+d = 2  # variable entera "int".
+e = 2.0  # variable de punto flotante "float".
+print("¡Hola, Python!")
