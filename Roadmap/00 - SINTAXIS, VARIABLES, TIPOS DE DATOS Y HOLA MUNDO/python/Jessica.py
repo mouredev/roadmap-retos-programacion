ac414c7f6cf4695f2842841f6da87c02bcbc34a1
Natalie Olvera <115767277+NatalieOlvera@users.noreply.github.com>
2026-07-25 18:21:18 -0600

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jessica.py
@@ -0,0 +1,21 @@
+#https://www.python.org
+#Comentario de linea
+'''
+Comentario multilinea
+Realmente no es un comentario 
+Pero toma la funcionalidad como tal
+'''
+"""
+Ahora lo mismo pero con
+comillas dobles
+"""
+
+#Creacion de variables
+variable_int_edad= 5
+variable_float_temperatura= 37.5
+variable_str_texto= "Python"
+variable_bool_cielo_es_azul= True
+constante = 7
+
+#Imprimir 
+print(f"Hola mi nombre es Jess, y estoy estudiando {variable_str_texto}")
