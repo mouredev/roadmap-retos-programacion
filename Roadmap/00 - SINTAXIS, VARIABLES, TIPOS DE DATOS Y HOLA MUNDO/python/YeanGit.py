3881d6a16f408bbb1ca37b056de965e9d31d1dbe
YeanGit <YeanFierro@outlook.cl>
2026-07-28 14:52:57 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/YeanGit.py
@@ -0,0 +1,35 @@
+# https://python.org/
+
+# Comentario en una línea
+
+"""
+Esto es 
+un comentario
+en varias líneas
+"""
+
+'''
+Esto también es 
+un comentario 
+en varias líneas
+'''
+# Declaración de variables y constantes
+my_variable: str = "Ejemplo de variable"
+my_variable: str = "Nuevo valor de mi variable"
+
+MY_CONSTANT: float = 3.1416 # por convención las constantes se escriben en mayúsculas
+
+# Tipos de datos primitivos
+my_int: int = 7
+my_float: float = 7.1
+my_bool: bool = True
+my_bool: bool = False
+my_string: str = "Ejemplo de string"
+my_other_string: str = 'Ejemplo de string con comillas simples'
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
