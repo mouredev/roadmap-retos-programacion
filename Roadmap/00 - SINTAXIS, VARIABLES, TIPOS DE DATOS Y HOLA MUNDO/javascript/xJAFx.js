7ec1c5206775c5e5cb511a1173db130c6e7b6d87
xJAFx <83126853+xJAFx@users.noreply.github.com>
2026-07-28 19:52:00 -0400

    #00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/xJAFx.js
@@ -0,0 +1,22 @@
+// https://developer.mozilla.org/es/docs/Web/JavaScript
+
+// Comentario de una línea
+
+/* Comentario
+para
+varias
+líneas */
+
+let posibleAlterar = "Esta es una variable posible de alterar tras ser declarada. Puede ser declarada sin asignarle un valor";
+
+const imposibleAlterar = "Esta es una constante, variable imposible de alterar tras ser declarada. Debe ser declarada con su valor respectivo";
+
+let interruptor = true;
+let nulo = null;
+let sinDefinir;
+let numero = 20;
+let numeroGrande = 73250731234720572n;
+let textos = "Variable para textos";
+let simbolo = Symbol();
+
+console.log("!Hola, JavaScript!");
