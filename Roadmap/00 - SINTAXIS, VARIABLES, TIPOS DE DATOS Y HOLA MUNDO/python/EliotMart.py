1e5c5c34321c722946619e123efb1d9aa13a0a6a
EliotMart <109325428+EliotMart@users.noreply.github.com>
2026-07-28 20:37:12 -0500

    #00-Python
    Add comments in single and multi-line formats to EliotMart.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EliotMart.py
@@ -0,0 +1,18 @@
+# https://www.python.org/ 
+
+# comentario en una linea
+
+"""
+Esto tambien es 
+un cmoentario 
+en varias lineas
+"""
+
+'''
+Esto tambien es 
+un cmoentario 
+en varias lineas
+'''
+
+
+my_variable
