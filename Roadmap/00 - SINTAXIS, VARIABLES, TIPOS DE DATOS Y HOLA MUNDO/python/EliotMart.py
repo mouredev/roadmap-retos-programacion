0fc6bf5997ea79dd16ff7a20d1f1d71c83e10d81
EliotMart <109325428+EliotMart@users.noreply.github.com>
2026-07-28 21:03:11 -0500

    Update EliotMart.py with variable and data type examples
    Added variable assignments and examples of data types in Python.

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EliotMart.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/EliotMart.py
@@ -15,4 +15,21 @@ en varias lineas
 '''
 
 
-my_variable
+my_variable = "Mi Variable"  
+my_variable = "Nuevo valor de mi variable"
+
+# en python no existe las constantes, todas las variables pueden variar
+MY_CONSTANT = "Mi constante" # como convencion se crea el nombre de la variable y el valor de la variable iguales o similares
+
+# crear tipos de datos fundamentale o primitivos, son 4 tipos
+
+my_int = 1  
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_strint = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+# para imprimir en consola se coloca print y entre comillas lo que se desea mostrar
+print("¡Hola, Python!")
+
