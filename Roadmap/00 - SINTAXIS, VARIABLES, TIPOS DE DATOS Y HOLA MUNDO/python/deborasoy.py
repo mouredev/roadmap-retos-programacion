2b04c79bbcb573276988d1fe5c8916c4b58ec398
Débora Peña <139586500+deborasoy@users.noreply.github.com>
2026-08-02 16:06:05 -0300

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/deborasoy.py
@@ -0,0 +1,35 @@
+#Python - sitio oficial : https://www.python.org/
+
+#comentario de una linea 
+
+"""
+ Representa las diferentes sintaxis que existen de crear comentarios
+ en el lenguaje (en una línea, varias...) 
+"""
+'''
+representa las diferentes sintaxis que existen de crear comentarios en el 
+lenguaje (en una linea, varias...)
+'''
+#Crea una variable (y una constante si el lenguaje lo soporta).
+edad_Persona = "25" #variable
+
+'''
+En python no existen las constantes, todos los datos que yo creo son mutables. Existen las variables, 
+pero si puedo por convencion dar a entender que no quiero que me cambien el dato de una
+variable y "tranformar" por medio de la interpretacion a constante
+'''
+NOMBRE_PERSONA = 'Débora' #esto se convierte por interpretacion (el nombre en mayuscula) en constante
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje
+""""
+datos primitivos:
+ integers, floats, booleans, y strings
+"""
+año_Bau = 2012 #tipo de dato: int (numero entero)
+años_en_meses = 0.8 #tipo de dato: float (numero decimal)
+tengo_mascota = True #tipo de dato: bool (True or False)
+estacion_favorita = 'primavera' #tipo de dato: str (cadena de texto)
+
+#Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print("Hola, python! I'm here")
