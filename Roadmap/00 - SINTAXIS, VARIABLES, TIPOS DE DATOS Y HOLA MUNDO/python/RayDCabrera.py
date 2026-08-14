f2fc5bb351f74f76d0b5292aa4b4e45999ba53a4
rcabrera <rdcabrera01@gmail.com>
2026-08-14 13:24:37 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/RayDCabrera.py
@@ -0,0 +1,24 @@
+#https://www.python.org
+
+'''
+Comentario en mas de una línea
+triple comillas simples o dobles
+'''
+
+# Comentario para una sola línea
+
+'''
+En python no existen las variables constantes
+pero para avisar que es una variable que no se debe cambiar el valor, se utiliza mayusculas para el nombre
+'''
+
+variable = 10
+PI = 3.1416
+
+cadena = 'Hola'
+numero = 1
+decimal = 1.2
+booleano = True
+
+
+print("Hola!, Python!")
