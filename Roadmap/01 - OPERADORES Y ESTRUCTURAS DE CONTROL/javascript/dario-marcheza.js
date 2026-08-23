4bc511c91181a3acc92d7b6bb40f6485f83f896b
Dario-Marcheza <dariomarcheza1410@gmail.com>
2026-08-23 00:22:26 -0500

    #01 - JavaScript (borrador)

---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/dario-marcheza.js
@@ -0,0 +1,27 @@
+//Asignación
+const a = 10;
+const b = 20;
+const c = 30;
+let d = 40;
+let e = 50;
+let f = "60";
+//Aritmeticos
+console.log(`Suma: ${a} + ${b} = ${a + b}`);
+console.log(`Resta: ${a} - ${b} = ${a - b}`);
+console.log(`Multiplicación: ${a} * ${b} = ${a * b}`);
+console.log(`División: ${a} / ${b} = ${a / b}`);
+console.log(`Resto: ${a} % ${b} = ${a % b}`);
+console.log(`Incremento: ${++d}, ${e++}, e se establece despues: e = ${e}`);
+console.log(`Incremento: ${--d}, ${e--}, e se establece despues: e = ${e}`);
+console.log(`Negación unaria: Devuelve la negación de su operando: ${-e}`);
+console.log(`Unario más: Convierte el operando a un número si es que aún no lo es: ${+f} o ${+e}`);
+console.log(`Operador de exponenciación: calcula "base^exponent": ${2 ** 2}`);
+
+
+
+
+
+
+
+
+
