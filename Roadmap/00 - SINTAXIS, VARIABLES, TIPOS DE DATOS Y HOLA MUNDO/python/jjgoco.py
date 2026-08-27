1a74a6fad80bb255302c7c389eeb55811640be29
jjgoco <jjgomezcorredor@gmail.com>
2026-08-27 14:37:06 +0200

    #[00] - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jjgoco.py
@@ -0,0 +1,50 @@
+# https://www.python.org/
+
+"""
+La triple comilla sirve en Pyton
+para dejar comentarios con vairas 
+líneas, aunque técnicamente no es
+un comentario, ya que no se ignora
+justo al inicio de uan función,
+clase o módulo, fuera cmo este
+ejemplo sí lo hace.
+"""
+
+variable = 1
+
+CONSTANTE = 1   # En Python no hay variables, se suelen representar como mayúsculas e indicaciones de no modificar por convención.
+
+# Variables numéricas:
+variable_int = 1    # Números enteros
+variable_float = 1.23   # Números con dos decimales
+variable_complex = 1 + 1j   # Números complejos con parte imaginaria (j)
+
+# Variables booleanas:
+boolt = True    # Valor verdadero
+boolf = False   # Valor falso
+
+# Variables de texto
+str = "Hola Python"    #Strings o cadenas de texto.
+
+# Secuencias
+tuple = (1, "tupla str")  # Colección ordenada e inmutable de elementos, no se pueden modificar los elementos tras crearse. Puede combinar distintos tipos de datos.
+frozenset = [1, 2, 3]  # Colección desordenada e inmutable de elementos, como la tupla, pero los datos pueden no estar ordenados.
+
+# Nulo o vacío
+nonetype = None     # Para valores vacíos o nulos
+
+# Usaré la función print para comprobar que funciona al ejecutar, pero no es necesario según el ejercicio.
+print(variable)
+print(CONSTANTE)
+print(variable_int)
+print(variable_float)
+print(variable_complex)
+print(boolf)
+print(boolt)
+print(str)
+print(tuple)
+print(frozenset)
+print(nonetype)
+
+print("Hola, Python")
+
