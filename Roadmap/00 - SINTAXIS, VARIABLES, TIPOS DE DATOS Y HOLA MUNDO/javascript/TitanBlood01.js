ddb27130924b73da84d3a510085da0e95e268d85
jose <jernestocastro01@gmail.com>
2026-08-27 22:20:32 -0400

    #00 - javascript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/TitanBlood01.js
@@ -0,0 +1,27 @@
+//https://developer.mozilla.org/es/docs/Web/JavaScript
+
+/* 
+This is other way to make a commentary with javascript, applying the
+astherics and a slash
+*/
+
+//Variable and Constant's declaration
+var primeraVariable = 'una variable'
+let variable = 'otra variable';
+const aConstante = '';
+
+//Data types in js
+
+let booleano = true  //Bool type - Can be false too
+let nullVariable = null  //null type
+let undefinedVariable = undefined  //undefined type
+const numberConst = 10 // Number Type
+const bigIntConst = 100000009923829929329383847382423823423422n
+const stringConst = 'Esto es un string' //A string type
+let symbol = Symbol("ThisSymbol") // A symbol type
+let objectType = {
+    par1: 'Primer propiedad',
+    par2: 2,
+} // An objetc type
+
+console.log('Hola JavaScript!!')
