cfc0d5e3af9a6015331f3c179509d2f9d02825ca
DukeFPV <jcarloslorenzo@uoc.edu>
2026-09-05 01:09:12 +0200

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/DukeFPV.py
@@ -0,0 +1,32 @@
+# #  https://python.org pagina oficial de Python (# extra para la extension better comments)
+
+"""
+Este es un archivo de ejemplo para aprender Python.
+de linea multiple.
+"""
+
+'''
+Este es un comentario de tres comillas simples.
+Funciona igual que el comentario de tres comillas dobles.
+'''
+
+# Este es un comentario de una sola linea.
+
+my_variable = "Hola, mundo!"  # Esta es una variable string
+my_variable = "Nuevo valor de variable"  # Esta es un nuevo valor de la variable de tipo string
+
+# En Python no existen constantes, pero se puede simular por covención usando variables en mayúsculas.
+MY_CONSTANT = "Este es un valor constante"
+
+## Tipos de datos en Python
+my_int = 10  # Entero
+my_float = 3.14  # Flotante
+my_boolean = True  # Booleano
+my_string = "Hola, mundo!"  # Cadena de texto. # # Se puede usar comillas simples o dobles para definir cadenas de texto. 
+
+print("Hola, mundo!")  # Imprime un mensaje en la consola
+
+print(type(my_int)) # Imprime el tipo de dato de la variable my_int
+print(type(my_float)) # Imprime el tipo de dato de la variable my_float
+print(type(my_boolean)) # Imprime el tipo de dato de la variable my_boolean
+print(type(my_string)) # Imprime el tipo de dato de la variable my_string
