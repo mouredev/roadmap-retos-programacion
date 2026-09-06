d883937aeeab425904aaabedccf2eadc5abc634e
cpc009233-bit <cpc009233@gmail.com>
2026-09-06 18:43:31 -0400

    Add files via upload

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/cpc009233-bit.py
@@ -0,0 +1,38 @@
+# https://www.python.org
+
+#comentario en una linea
+
+"""
+esto es un
+comentario en
+varias lineas
+me gusta
+la manzana
+con apio
+"""
+
+
+'''
+esto es un
+comentario en
+varias lineas
+me gusta
+la manzana
+con piña
+'''
+
+my_variable = "Hola a todos"
+my_variable = "Nuevo constante es una mentira"
+
+MY_CONSTANT = 'pepino con manzana' # igual que una variable pero sin cambiar 
+
+edad : float = 50
+años = 50
+my_int = 12
+my_float=1,24
+my_bool=False
+my_bool=True
+my_string="manzana con apio"
+
+print("hola,python")
+print(edad)
