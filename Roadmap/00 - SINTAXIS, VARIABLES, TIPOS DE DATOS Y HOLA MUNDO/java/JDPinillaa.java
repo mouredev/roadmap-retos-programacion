2d8709ca1a86ffd43406cdc9eb0ac1eddc99100e
Juan Diego Pinilla <juand.pinillaa@autonoma.edu.co>
2026-09-10 17:38:57 -0500

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/JDPinillaa.java
@@ -0,0 +1,33 @@
+
+public class JDPinillaa {
+    //Esto es un comentario
+    //https://www.java.com/es/
+
+    /*
+    Este es un comentario de varias lineas
+     */
+
+    /**
+     * Y este es un modo de comentario a usar para la documentacion oficial de java
+     */
+
+    String variable = "Esto es una cadena";
+    byte bit = 1; //esta es una variable byte
+    int num = 1000; //esta es una variable entera
+    double dob = 5.2; //esta es una variable double
+    float fl = 13.44F; //esta es una variable float
+    long lng = 10999999L; //esta es una variable long
+    char car = 'a'; //esta es una variable caracter.
+    boolean bool = false; //esta es una variable booleana.
+
+    final String constante = "Esto es una constante";
+
+
+    public static void main(){
+        System.out.println("Hola java");
+    }
+
+
+
+
+}
