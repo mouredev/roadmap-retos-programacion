91769f85f82118bec4d9cf0021784fabe5b0db0d
AndYasirDev <andreskny06@gmail.com>
2026-09-17 13:08:35 -0600

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/yassx-dev.py
@@ -0,0 +1,20 @@
+# https://www.python.org/ 
+
+# Comentario de una Línea
+
+"""
+Este es un comentario
+de varias
+líneas
+
+"""
+
+variable = "Mouredev el mejor"
+CONSTANTE = "Variable Constante"
+
+int = 6
+float = 6.6
+booleano = True
+cadena = "Yassx Dev"
+
+print("¡Hola, Python!")
