b25458a286c225c103c441a3b05810a7d6ca68af
JorgeMokay <134728495+JorgeMokay@users.noreply.github.com>
2026-09-17 13:46:28 -0700

    Update JorgeMokay.js

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JorgeMokay.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/JorgeMokay.js
@@ -1,1 +1,11 @@
+// WEB OFICIAL JAVASCRIPT
+// https://developer.mozilla.org/es/docs/Web/JavaScript
 
+/*
+formas de encerrar un comentario en mas lienas
+es de esta forma
+*/ 
+
+let variavleLet = "variable1";
+const variableconstante = "constante1"
+console.log(variableconstante)
