6528e2f263297822631f4a0fe97e182acb4dca83
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2026-09-21 11:46:30 +0200

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Alexis-j.py
@@ -0,0 +1,42 @@
+#00-PYTHON - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+# WEB OFICIAL
+# Sitio web oficial de Python: https://www.python.org/
+
+
+# Comentario en una linea
+
+
+"""
+aqui un comentario en varias lineas,
+que se me ocurrio :)
+"""
+
+'''
+esto es otro comentario
+'''
+
+my_variable = "Mi variable de cadena de texto"
+my_variable = "Nuevo valor de mi variable"
+
+
+MY_CONSTANT = "mi constante solo representativa Python no tiene constante" #por conveniencia.
+
+my_integer = 9 # numeros enteros
+my_float = 9,9 # numeros con decimales
+my_boolean = True # puede ser verdadero o falso
+my_boolean = False
+
+my_string: = "cadena de texto" # textos
+my_string = 'my otra cadna de cadena de texto' # textos
+
+print("Hola, Python")
+
+print(type(my_integer))
+print(type(my_float))
+print(type(my_boolean))
+print(type(my_string))
+
+
+
+
+30 horas. 1845 brutto mas ventas.
