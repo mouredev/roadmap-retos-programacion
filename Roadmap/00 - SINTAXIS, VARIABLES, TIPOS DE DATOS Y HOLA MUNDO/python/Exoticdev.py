30cada12db6c1d067c7a430971b4f916afbb92fa
Exoticdevelopment <exoticdev777@gmail.com>
2026-09-24 00:21:53 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Exoticdev.py
@@ -0,0 +1,39 @@
+
+
+# https://www.python.org/
+
+#Comentario en una linea
+""" 
+Tambien es un comentario en varias lineas
+"""
+
+'''
+Tmb es un comentario
+'''
+
+my_variable = "Mi nombre es Miguel Angel"
+
+ 
+
+EDAD= 18
+#Una constante es una variable que x convencio no debe cambiar de valor
+
+
+#DATOS PRIMITIVOS
+my_int = 10
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_str= "Miguel"
+
+#MOSTRAR TEXTO X CONSOLA
+
+print("Hola soy EXOTICDEV")
+
+
+#SABER TIPO DE DATO QUE ESTOY UTILIZANDO
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
